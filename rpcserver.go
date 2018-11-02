e9272016e24affde56d9dbabaca709f9320b7d39
Johan T. Halseth <johanth@gmail.com>
2018-11-02 11:33:01 +0100

    rpcserver: let sendPayment sendLoop listen for shutdown
    Intead of checking for shutdown in the receive loop, we let the sendLoop
    handle it, as it can return the error directly. This works since the
    returning sendLoop will trigger a close of the `reqQuit` channel, which
    will ensure the receive loop exits.

--- rpcserver.go
+++ rpcserver.go
@@ -3806,9 +3806,7 @@ func (r *rpcServer) sendPayment(stream *paymentStream) error {
 			select {
 			case <-reqQuit:
 				return
-			case <-r.quit:
-				errChan <- nil
-				return
+
 			default:
 				// Receive the next pending payment within the
 				// stream sent by the client. If we read the
@@ -3865,6 +3863,9 @@ func (r *rpcServer) sendPayment(stream *paymentStream) error {
 		select {
 		case err := <-errChan:
 			return err
+
+		case <-r.quit:
+			return errors.New("rpc server shutting down")
 
 		case payIntent := <-payChan:
 			// We launch a new goroutine to execute the current
