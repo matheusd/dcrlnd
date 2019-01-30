0165fb8a2849fb74b2de6312aee33e63c7217a61
Olaoluwa Osuntokun <laolu32@gmail.com>
2019-01-29 17:56:28 -0800

    Revert "funding+lnwallet: ensure max_htlc_value_in_flight smaller than capacity"
    This reverts commit 4aa52d267f000f84caf912c62fc14a5b8e7cacb5.
    
    It turns out that the other implementations set values for this field
    which aren't based on the actual capacity of the channel. As a result,
    we'll no reject most of their channel offerings, since they may offer a
    value of a max `uint64` or something else hard coded that's above the
    size of the channel. As a result, we're reverting this check for now to
    maintain proper compatibility.

--- fundingmanager.go
+++ fundingmanager.go
@@ -1081,7 +1081,7 @@ func (f *fundingManager) handleFundingOpen(fmsg *fundingOpenMsg) {
 		MaxAcceptedHtlcs: msg.MaxAcceptedHTLCs,
 		CsvDelay:         msg.CsvDelay,
 	}
-	err = reservation.CommitConstraints(channelConstraints, amt)
+	err = reservation.CommitConstraints(channelConstraints)
 	if err != nil {
 		fndgLog.Errorf("Unacceptable channel constraints: %v", err)
 		f.failFundingFlow(fmsg.peer, fmsg.msg.PendingChannelID, err)
@@ -1245,9 +1245,7 @@ func (f *fundingManager) handleFundingAccept(fmsg *fundingAcceptMsg) {
 		MaxAcceptedHtlcs: msg.MaxAcceptedHTLCs,
 		CsvDelay:         msg.CsvDelay,
 	}
-	err = resCtx.reservation.CommitConstraints(
-		channelConstraints, resCtx.chanAmt,
-	)
+	err = resCtx.reservation.CommitConstraints(channelConstraints)
 	if err != nil {
 		fndgLog.Warnf("Unacceptable channel constraints: %v", err)
 		f.failFundingFlow(fmsg.peer, fmsg.msg.PendingChannelID, err)
