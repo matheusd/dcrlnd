384778525afa6c9091ac901cabf5cb1bcf9b67ca
Conner Fromknecht <conner@lightning.engineering>
2019-02-11 13:02:25 -0800

    lnrpc+rpcserver: rename ScriptPubkey to PkScript on Utxo msg

--- cmd/dcrlncli/types.go
+++ cmd/dcrlncli/types.go
@@ -20,7 +20,7 @@ type Utxo struct {
 	Type          lnrpc.AddressType `json:"address_type"`
 	Address       string            `json:"address"`
 	AmountAtoms   int64             `json:"amount_atoms"`
-	ScriptPubkey  string            `json:"script_pubkey"`
+	PkScript      string            `json:"pk_script"`
 	OutPoint      OutPoint          `json:"outpoint"`
 	Confirmations int64             `json:"confirmations"`
 }
@@ -33,7 +33,7 @@ func NewUtxoFromProto(utxo *lnrpc.Utxo) *Utxo {
 		Type:          utxo.Type,
 		Address:       utxo.Address,
 		AmountAtoms:   utxo.AmountAtoms,
-		ScriptPubkey:  utxo.ScriptPubkey,
+		PkScript:      utxo.PkScript,
 		OutPoint:      NewOutPointFromProto(utxo.Outpoint),
 		Confirmations: utxo.Confirmations,
 	}
