58785b2849d4432b15a5d1bb14d8b6bae00e1ffb
Wilmer Paulino <wilmer.paulino@gmail.com>
2019-02-20 15:06:57 -0800

    multi: remove ErrDoubleSpend check for PublishTransaction
    In this commit, we address a lingering issue within some subsystems that
    are responsible for broadcasting transactions. Previously,
    ErrDoubleSpend indicated that a transaction was already included in the
    mempool/chain. This error was then modified to actually be returned for
    conflicting transactions, but its callers were not modified accordingly.
    This would lead to conflicting transactions to be interpreted as valid,
    when they shouldn't be.

--- contractcourt/htlc_success_resolver.go
+++ contractcourt/htlc_success_resolver.go
@@ -148,7 +148,7 @@ func (h *htlcSuccessResolver) Resolve() (ContractResolver, error) {
 		// constructed, we'll broadcast the sweep transaction to the
 		// network.
 		err := h.PublishTx(h.sweepTx)
-		if err != nil && err != lnwallet.ErrDoubleSpend {
+		if err != nil {
 			log.Infof("%T(%x): unable to publish tx: %v",
 				h, h.payHash[:], err)
 			return nil, err
@@ -200,7 +200,7 @@ func (h *htlcSuccessResolver) Resolve() (ContractResolver, error) {
 	//
 	// TODO(roasbeef): after changing sighashes send to tx bundler
 	err := h.PublishTx(h.htlcResolution.SignedSuccessTx)
-	if err != nil && err != lnwallet.ErrDoubleSpend {
+	if err != nil {
 		return nil, err
 	}
 
