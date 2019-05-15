95a90df1237391a3cfabc8b5dcddaec43b3db3b9
Matheus Degiovani <opensource@matheusd.com>
2019-05-15 16:14:55 -0300

    lnwallet: Move keyring off the underyling wallet
    This moves the dcrwallet-backed WalletKeyRing from directly using
    pubkeys derived within the wallet to keeping track of used indices
    directly in dcrlnd's channel db and deriving the keys itself based on
    the wallet's root coin type extended private key.
    
    The main goal for this move is to prevent the wallet from tracking a
    (potentially) large number of accounts and addresses that were never
    meant for onchain use and that the current implementation of the wallet
    can't work with anyway due to their use being only on ln related scripts
    (which the wallet doesn't understand).
    
    In the upstream lnd this was solved by using a separate purpose branch.
    If the wallet eventually implements some way of using different purpose
    branches, than this can be easily adjusted in the future.
    
    Another improvement of this commit is dropping the need for a separate
    public WalletKeyRing by using it as a composed struct to the DcrWallet
    struct. This makes it so that the main package doesn't need to
    separately initialize the keyring (which doesn't make sense anyway given
    any KeyRing implementation is likely to be intemately tied to a
    corresponding WalletController implementation).

--- lnwallet/dcrwallet/keychain.go
+++ lnwallet/dcrwallet/keychain.go
@@ -1,273 +1,119 @@
 package dcrwallet
 
 import (
-	"crypto/sha256"
-	"fmt"
-
+	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/keychain"
 
-	"github.com/decred/dcrd/dcrec/secp256k1"
-	"github.com/decred/dcrd/dcrutil"
 	"github.com/decred/dcrd/hdkeychain"
-	"github.com/decred/dcrwallet/errors"
 	"github.com/decred/dcrwallet/wallet/v2"
 	"github.com/decred/dcrwallet/wallet/v2/udb"
 )
 
-// WalletKeyRing is an implementation of both the KeyRing and SecretKeyRing
-// interfaces backed by dcrwallet's internal root udb. Internally, we'll be
-// using a ScopedKeyManager to do all of our derivations, using the key scope
-// and scope addr scehma defined above. Re-using the existing key scope
-// construction means that all key derivation will be protected under the root
-// seed of the wallet, making each derived key fully deterministic.
-type WalletKeyRing struct {
+// walletKeyRing is an implementation of both the KeyRing and SecretKeyRing
+// interfaces backed by dcrwallet's internal root keys.
+//
+// While the wallet's root keys are used, the actual final key derivation does
+// _not_ take place in the wallet; instead it is done here. This is done so
+// that the wallet does not attempt to keep track of a large amount of keys
+// (addresses) that are meant for off-chain processes and are unlikely to ever
+// be found on-chain.
+//
+// Even though the final keys are derived here, they are currently derived
+// following a BIP0043 style path starting at the first account (account number
+// 0) and for the external branch only, which maintains compatibility with a
+// previous version of this implementation that was done entirely on the
+// wallet. Note that changing this derivation procedure means invalidating
+// existing dcrlnd wallets.
+type walletKeyRing struct {
+	*keychain.HDKeyRing
+
 	// wallet is a pointer to the active instance of the dcrwallet core.
 	// This is required as we'll need to manually open database
 	// transactions in order to derive addresses and lookup relevant keys
 	wallet *wallet.Wallet
+
+	// db is a pointer to a channeldb.DB instance that stores the indices
+	// of used keys of the keyring. This is used to track the next
+	// available key to prevent key reuse when establishing channels.
+	db *channeldb.DB
 }
 
-// NewWalletKeyRing creates a new implementation of the
-// keychain.SecretKeyRing interface backed by dcrwallet.
+// Compile time type assertions to ensure walletKeyRing fulfills the desired
+// interfaces.
+var _ keychain.KeyRing = (*walletKeyRing)(nil)
+var _ keychain.SecretKeyRing = (*walletKeyRing)(nil)
+
+// newWalletKeyRing creates a new implementation of the keychain.SecretKeyRing
+// interface backed by dcrwallet.
 //
-// NOTE: The passed udb.Manager MUST be unlocked in order for the keychain
-// to function.
-func NewWalletKeyRing(w *wallet.Wallet) keychain.SecretKeyRing {
-	return &WalletKeyRing{
+// NOTE: The passed wallet MUST be unlocked in order for the keychain to
+// function.
+func newWalletKeyRing(w *wallet.Wallet, db *channeldb.DB) (*walletKeyRing, error) {
+
+	// This assumes that there are no discontinuities within the KeyFamily
+	// constants.
+	lastKeyFam := uint32(keychain.KeyFamilyNodeKey)
+	masterPubs := make(map[keychain.KeyFamily]*hdkeychain.ExtendedKey,
+		lastKeyFam+1)
+
+	ctKey, err := w.CoinTypePrivKey()
+	if err != nil {
+		return nil, err
+	}
+
+	// Derive the master pubs for each key family. They are mapped to the
+	// external branch for each corresponding wallet account.
+	for i := uint32(0); i <= lastKeyFam; i++ {
+		// Errors here cause fatal failures due to the wallet not
+		// attempting to generate the next account.
+		acctKey, err := ctKey.Child(hdkeychain.HardenedKeyStart + i)
+		if err != nil {
+			return nil, err
+		}
+
+		branchKey, err := acctKey.Child(udb.ExternalBranch)
+		if err != nil {
+			return nil, err
+		}
+
+		branchPub, err := branchKey.Neuter()
+		if err != nil {
+			return nil, err
+		}
+
+		masterPubs[keychain.KeyFamily(i)] = branchPub
+	}
+
+	wkr := &walletKeyRing{
 		wallet: w,
+		db:     db,
 	}
+	wkr.HDKeyRing = keychain.NewHDKeyRing(masterPubs, wkr.fetchMasterPriv,
+		wkr.nextIndex)
+	return wkr, nil
 }
 
-// createAccountsUpTo creates all accounts representing key families up to (and
-// including) the provided argument.
-// TODO(decred) extremely inefficient for large keyFam. Ideally dcrwallet
-// should support using arbitrary account numbers.
-func (b *WalletKeyRing) createAccountsUpTo(keyFam keychain.KeyFamily) error {
-
-	// If this is the multi-sig key family, then we can return early as
-	// this is the default account that's created.
-	if keyFam == keychain.KeyFamilyMultiSig {
-		return nil
-	}
-
-	// Otherwise, we'll check if the account already exists, if so, we can
-	// once again bail early.
-	_, err := b.wallet.AccountName(uint32(keyFam))
-	if err == nil {
-		return nil
-	}
-
-	dcrwLog.Infof("Creating wallet accounts up to %d", keyFam)
-
-	// Figure out all uncreated accounts between 0..keyFam
-	accounts, err := b.wallet.Accounts()
-	if err != nil {
-		return nil
-	}
-	maxExistAccount := uint32(0)
-	for _, acct := range accounts.Accounts {
-		if acct.AccountNumber < uint32(keyFam) && acct.AccountNumber > maxExistAccount {
-			maxExistAccount = acct.AccountNumber
-		}
-	}
-
-	for i := maxExistAccount + 1; i <= uint32(keyFam); i++ {
-		dcrwLog.Debugf("Creating account %d", i)
-		_, err = b.wallet.NextAccount(fmt.Sprintf("%d", i))
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
+func (wkr *walletKeyRing) nextIndex(keyFam keychain.KeyFamily) (uint32, error) {
+	return wkr.db.NextKeyFamilyIndex(uint32(keyFam))
 }
 
-// keyDescriptorForAddress returns the key descriptor for the given wallet
-// address. It assumes the address exists and is a P2PKH address, otherwise this
-// will error.
-func (b *WalletKeyRing) keyDescriptorForAddress(addr dcrutil.Address) (keychain.KeyDescriptor, error) {
-	var emptyKeyDesc keychain.KeyDescriptor
-	addrInfo, err := b.wallet.AddressInfo(addr)
-	if err != nil {
-		return emptyKeyDesc, err
-	}
+func (wkr *walletKeyRing) fetchMasterPriv(keyFam keychain.KeyFamily) (*hdkeychain.ExtendedKey,
+	error) {
 
-	pubAddrInfo, is := addrInfo.(udb.ManagedPubKeyAddress)
-	if !is {
-		return emptyKeyDesc, fmt.Errorf("generated address is not a ManagedPubKeyAddress")
-	}
-
-	pubKey, is := pubAddrInfo.PubKey().(*secp256k1.PublicKey)
-	if !is {
-		return emptyKeyDesc, fmt.Errorf("generated address is not a secp256k1 address")
-	}
-
-	return keychain.KeyDescriptor{
-		PubKey: pubKey,
-		KeyLocator: keychain.KeyLocator{
-			Family: keychain.KeyFamily(pubAddrInfo.Account()),
-			Index:  pubAddrInfo.Index(),
-		},
-	}, nil
-}
-
-// DeriveNextKey attempts to derive the *next* key within the key family
-// (account in BIP43) specified. This method should return the next external
-// child within this branch.
-//
-// NOTE: This is part of the keychain.KeyRing interface.
-func (b *WalletKeyRing) DeriveNextKey(keyFam keychain.KeyFamily) (keychain.KeyDescriptor, error) {
-	var (
-		addr         dcrutil.Address
-		err          error
-		emptyKeyDesc keychain.KeyDescriptor
-	)
-
-	err = b.createAccountsUpTo(keyFam)
-	if err != nil {
-		return emptyKeyDesc, err
-	}
-
-	// TODO(decred) Confirm use of gapPolicyIgnore
-	addr, err = b.wallet.NewExternalAddress(uint32(keyFam), wallet.WithGapPolicyIgnore())
-	if err != nil && errors.Is(errors.NotExist, err) {
-		// Account corresponding to this family does not exist. Create it.
-		err = b.createAccountsUpTo(keyFam)
-		if err != nil {
-			return emptyKeyDesc, err
-		}
-
-		// And re-derive the next address for it.
-		addr, err = b.wallet.NewExternalAddress(uint32(keyFam), wallet.WithGapPolicyIgnore())
-		if err != nil {
-			return emptyKeyDesc, err
-		}
-	}
-
-	return b.keyDescriptorForAddress(addr)
-}
-
-// DeriveKey attempts to derive an arbitrary key specified by the passed
-// KeyLocator. This may be used in several recovery scenarios, or when manually
-// rotating something like our current default node key.
-//
-// NOTE: This is part of the keychain.KeyRing interface.
-func (b *WalletKeyRing) DeriveKey(keyLoc keychain.KeyLocator) (keychain.KeyDescriptor, error) {
-	var emptyKeyDesc keychain.KeyDescriptor
-
-	err := b.createAccountsUpTo(keyLoc.Family)
-	if err != nil {
-		return emptyKeyDesc, err
-	}
-
-	famMasterPub, err := b.wallet.MasterPubKey(uint32(keyLoc.Family))
-	if err != nil {
-		return emptyKeyDesc, err
-	}
-	branchMasterPub, err := famMasterPub.Child(udb.ExternalBranch)
-	if err != nil {
-		return emptyKeyDesc, err
-	}
-	key, err := branchMasterPub.Child(keyLoc.Index)
-	if err != nil {
-		return emptyKeyDesc, err
-	}
-	pubKey, err := key.ECPubKey()
-	if err != nil {
-		return emptyKeyDesc, err
-	}
-
-	return keychain.KeyDescriptor{
-		KeyLocator: keyLoc,
-		PubKey:     pubKey,
-	}, nil
-}
-
-// DerivePrivKey attempts to derive the private key that corresponds to the
-// passed key descriptor.
-//
-// NOTE: This is part of the keychain.SecretKeyRing interface.
-func (b *WalletKeyRing) DerivePrivKey(keyDesc keychain.KeyDescriptor) (*secp256k1.PrivateKey, error) {
-
-	err := b.createAccountsUpTo(keyDesc.Family)
+	ctKey, err := wkr.wallet.CoinTypePrivKey()
 	if err != nil {
 		return nil, err
 	}
 
-	// We'll grab the master pub key for the provided account (family) then
-	// manually derive the addresses here.
-	famMasterPriv, err := b.wallet.MasterPrivKey(uint32(keyDesc.Family))
-	if err != nil {
-		return nil, err
-	}
-	famBranchPriv, err := famMasterPriv.Child(udb.ExternalBranch)
+	acctKey, err := ctKey.Child(hdkeychain.HardenedKeyStart + uint32(keyFam))
 	if err != nil {
 		return nil, err
 	}
 
-	// If the public key isn't set or they have a non-zero index,
-	// then we know that the caller instead knows the derivation
-	// path for a key.
-	if keyDesc.PubKey == nil || keyDesc.Index > 0 {
-		privKey, err := famBranchPriv.Child(keyDesc.Index)
-		if err != nil {
-			return nil, err
-		}
-		return privKey.ECPrivKey()
-	}
-
-	// If the public key isn't nil, then this indicates that we
-	// need to scan for the private key, assuming that we know the
-	// valid key family.
-	for i := 0; i < keychain.MaxKeyRangeScan; i++ {
-		// Derive the next key in the range and fetch its
-		// managed address.
-		privKey, err := famBranchPriv.Child(uint32(i))
-		if err == hdkeychain.ErrInvalidChild {
-			continue
-		}
-
-		if err != nil {
-			return nil, err
-		}
-
-		pubKey, err := privKey.ECPubKey()
-		if err != nil {
-			// simply skip invalid keys here
-			continue
-		}
-
-		if keyDesc.PubKey.IsEqual(pubKey) {
-			return privKey.ECPrivKey()
-		}
-	}
-
-	return nil, keychain.ErrCannotDerivePrivKey
-}
-
-// ScalarMult performs a scalar multiplication (ECDH-like operation) between
-// the target key descriptor and remote public key. The output returned will be
-// the sha256 of the resulting shared point serialized in compressed format. If
-// k is our private key, and P is the public key, we perform the following
-// operation:
-//
-//  sx := k*P s := sha256(sx.SerializeCompressed())
-//
-// NOTE: This is part of the keychain.SecretKeyRing interface.
-func (b *WalletKeyRing) ScalarMult(keyDesc keychain.KeyDescriptor,
-	pub *secp256k1.PublicKey) ([]byte, error) {
-
-	privKey, err := b.DerivePrivKey(keyDesc)
+	branchKey, err := acctKey.Child(udb.ExternalBranch)
 	if err != nil {
 		return nil, err
 	}
 
-	s := &secp256k1.PublicKey{}
-	x, y := secp256k1.S256().ScalarMult(pub.X, pub.Y, privKey.D.Bytes())
-	s.X = x
-	s.Y = y
-
-	h := sha256.Sum256(s.SerializeCompressed())
-
-	return h[:], nil
+	return branchKey, nil
 }
