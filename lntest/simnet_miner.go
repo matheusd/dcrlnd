12fdb27e13517d051847d26c9979eb850c06b40f
Matheus Degiovani <opensource@matheusd.com>
2019-05-17 12:16:33 -0300

    lntest: Use faster simnet block generator
    This switches the lntest Harness structure to use an adjusted simnet
    miner so that block generation for integration tests are done without
    triggering difficulty increases.
    
    The main purpose for this change is to maintain the lowest possible
    mining difficulty on simnet, so that the full integration test suite can
    be run without having to perform expensive cpu mining at the full
    network difficulty. This is specially important in CI environments that
    might have reduced cpu power available or might suffer throttling and it
    makes the test suite complete faster.
    
    It accomplishes this by using a newly introduced feature on dcrd's
    rpctest.VotingWallet to specify a custom mining function written to
    adjust the timestamp of blocks on simnet so that they are exactly (at
    least) one second apart which causes the median time of the blockchain
    to be equal to its target block time and thus do not trigger difficulty
    increases.

---
+++ lntest/simnet_miner.go
@@ -0,0 +1,141 @@
+package lntest
+
+import (
+	"encoding/hex"
+	"errors"
+	"math"
+	"runtime"
+	"time"
+
+	"github.com/decred/dcrd/blockchain"
+	"github.com/decred/dcrd/chaincfg/chainhash"
+	"github.com/decred/dcrd/rpcclient/v2"
+	"github.com/decred/dcrd/wire"
+)
+
+// solveBlock attempts to find a nonce which makes the passed block header hash
+// to a value less than the target difficulty.  When a successful solution is
+// found, true is returned and the nonce field of the passed header is updated
+// with the solution.  False is returned if no solution exists.
+func solveBlock(header *wire.BlockHeader) bool {
+	// sbResult is used by the solver goroutines to send results.
+	type sbResult struct {
+		found bool
+		nonce uint32
+	}
+
+	// solver accepts a block header and a nonce range to test. It is
+	// intended to be run as a goroutine.
+	targetDifficulty := blockchain.CompactToBig(header.Bits)
+	quit := make(chan bool)
+	results := make(chan sbResult)
+	solver := func(hdr wire.BlockHeader, startNonce, stopNonce uint32) {
+		// We need to modify the nonce field of the header, so make sure
+		// we work with a copy of the original header.
+		for i := startNonce; i >= startNonce && i <= stopNonce; i++ {
+			select {
+			case <-quit:
+				results <- sbResult{false, 0}
+				return
+			default:
+				hdr.Nonce = i
+				hash := hdr.BlockHash()
+				if blockchain.HashToBig(&hash).Cmp(
+					targetDifficulty) <= 0 {
+
+					results <- sbResult{true, i}
+					return
+				}
+			}
+		}
+		results <- sbResult{false, 0}
+	}
+
+	startNonce := uint32(1)
+	stopNonce := uint32(math.MaxUint32)
+	numCores := uint32(runtime.NumCPU())
+	noncesPerCore := (stopNonce - startNonce) / numCores
+	for i := uint32(0); i < numCores; i++ {
+		rangeStart := startNonce + (noncesPerCore * i)
+		rangeStop := startNonce + (noncesPerCore * (i + 1)) - 1
+		if i == numCores-1 {
+			rangeStop = stopNonce
+		}
+		go solver(*header, rangeStart, rangeStop)
+	}
+	var foundResult bool
+	for i := uint32(0); i < numCores; i++ {
+		result := <-results
+		if !foundResult && result.found {
+			close(quit)
+			header.Nonce = result.nonce
+			foundResult = true
+		}
+	}
+
+	return foundResult
+}
+
+// AdjustedSimnetMiner is an alternative miner function that instead of relying
+// on the backing node to mine a block, fetches the work required for the next
+// block and mines the block itself while adjusting the timestamp so that (on
+// simnet) no difficulty increase is trigered. After finding a block, it
+// automatically publishes it to the underlying node.
+//
+// This is only applicable for tests that run on simnet or other networks that
+// have a target block per count of 1 second.
+func AdjustedSimnetMiner(client *rpcclient.Client, nb uint32) ([]*chainhash.Hash, error) {
+
+	hashes := make([]*chainhash.Hash, nb)
+
+	for i := uint32(0); i < nb; i++ {
+		work, err := client.GetWork()
+		if err != nil {
+			return nil, err
+		}
+
+		workBytes, err := hex.DecodeString(work.Data)
+		if err != nil {
+			return nil, err
+		}
+
+		var header wire.BlockHeader
+		err = header.FromBytes(workBytes)
+		if err != nil {
+			return nil, err
+		}
+
+		prevBlock, err := client.GetBlock(&header.PrevBlock)
+		if err != nil {
+			return nil, err
+		}
+
+		header.Timestamp = prevBlock.Header.Timestamp.Add(time.Second)
+		header.Bits = prevBlock.Header.Bits
+		solved := solveBlock(&header)
+		if !solved {
+			return nil, errors.New("unable to solve block")
+		}
+
+		var extraBytes [12]byte
+		workBytes, err = header.Bytes()
+		if err != nil {
+			return nil, err
+		}
+		workBytes = append(workBytes, extraBytes[:]...)
+		workData := hex.EncodeToString(workBytes)
+		accepted, err := client.GetWorkSubmit(workData)
+		if err != nil {
+			return nil, err
+		}
+
+		if !accepted {
+			return nil, errors.New("solved block was not accepted")
+		}
+
+		bh := header.BlockHash()
+		hashes[i] = &bh
+	}
+
+	return hashes, nil
+}
