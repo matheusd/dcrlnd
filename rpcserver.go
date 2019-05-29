5d86ef5218143a5b336b765fca8002d80ddeedeb
Conner Fromknecht <conner@lightning.engineering>
2019-05-29 16:31:32 -0700

    rpcserver: thread payreq to PaymentCreationInfo

--- rpcserver.go
+++ rpcserver.go
@@ -2881,6 +2881,7 @@ type rpcPaymentIntent struct {
 	routeHints           [][]zpay32.HopHint
 	outgoingChannelID    *uint64
 	ignoreMaxOutboundAmt bool
+	payReq               []byte
 
 	route *route.Route
 }
@@ -2975,6 +2976,7 @@ func extractPaymentIntent(rpcPayReq *rpcPaymentRequest) (rpcPaymentIntent, error
 		copy(payIntent.dest[:], destKey)
 		payIntent.cltvDelta = uint16(payReq.MinFinalCLTVExpiry())
 		payIntent.routeHints = payReq.RouteHints
+		payIntent.payReq = []byte(rpcPayReq.PaymentRequest)
 
 		return payIntent, nil
 	}
@@ -3187,6 +3189,7 @@ func (r *rpcServer) dispatchPaymentIntent(
 			PaymentHash:       payIntent.rHash,
 			RouteHints:        payIntent.routeHints,
 			OutgoingChannelID: payIntent.outgoingChannelID,
+			PaymentRequest:    payIntent.payReq,
 		}
 
 		// If the final CLTV value was specified, then we'll use that
@@ -4365,6 +4368,7 @@ func (r *rpcServer) ListPayments(ctx context.Context,
 			Path:            path,
 			Fee:             int64(route.TotalFees().ToAtoms()),
 			PaymentPreimage: hex.EncodeToString(preimage[:]),
+			PaymentRequest:  string(payment.Info.PaymentRequest),
 		}
 	}
 
