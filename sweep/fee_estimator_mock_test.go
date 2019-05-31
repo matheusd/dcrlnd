mandelmonkey/lnd

sweep: add unit tests for TestDetermineFeePerKw and CraftSweepAllTx
Along the way we also extend the mockFeeEstimator to be able to return a
fee rate for a specified confirmation target.

sweep/fee_estimator_mock_test.go
@@ -1,8 +1,9 @@
 package sweep
 
 import (
-	"github.com/lightningnetwork/lnd/lnwallet"
 	"sync"
+
+	"github.com/lightningnetwork/lnd/lnwallet"
 )
 
 // mockFeeEstimator implements a mock fee estimator. It closely resembles
@@ -13,6 +14,8 @@ type mockFeeEstimator struct {
 
 	relayFee lnwallet.SatPerKWeight
 
+	blocksToFee map[uint32]lnwallet.SatPerKWeight
+
 	lock sync.Mutex
 }
 
@@ -20,8 +23,9 @@ func newMockFeeEstimator(feePerKW,
 	relayFee lnwallet.SatPerKWeight) *mockFeeEstimator {
 
 	return &mockFeeEstimator{
-		feePerKW: feePerKW,
-		relayFee: relayFee,
+		feePerKW:    feePerKW,
+		relayFee:    relayFee,
+		blocksToFee: make(map[uint32]lnwallet.SatPerKWeight),
 	}
 }
 
@@ -40,6 +44,10 @@ func (e *mockFeeEstimator) EstimateFeePerKW(numBlocks uint32) (
 
 	e.lock.Lock()
 	defer e.lock.Unlock()
+
+	if fee, ok := e.blocksToFee[numBlocks]; ok {
+		return fee, nil
+	}
 
 	return e.feePerKW, nil
 }

sweep/walletsweep.go
@@ -10,6 +10,13 @@ import (
 	"github.com/lightningnetwork/lnd/lnwallet"
 )
 
+const (
+	// defaultNumBlocksEstimate is the number of blocks that we fall back
+	// to issuing an estimate for if a fee pre fence doesn't specify an
+	// explicit conf target or fee rate.
+	defaultNumBlocksEstimate = 6
+)
+
 // FeePreference allows callers to express their time value for inclusion of a
 // transaction into a block via either a confirmation target, or a fee rate.
 type FeePreference struct {
@@ -30,6 +37,12 @@ func DetermineFeePerKw(feeEstimator lnwallet.FeeEstimator,
 	feePref FeePreference) (lnwallet.SatPerKWeight, error) {
 
 	switch {
+	// If both values are set, then we'll return an error as we require a
+	// strict directive.
+	case feePref.FeeRate != 0 && feePref.ConfTarget != 0:
+		return 0, fmt.Errorf("only FeeRate or ConfTarget should " +
+			"be set for FeePreferences")
+
 	// If the target number of confirmations is set, then we'll use that to
 	// consult our fee estimator for an adequate fee.
 	case feePref.ConfTarget != 0:
@@ -61,7 +74,9 @@ func DetermineFeePerKw(feeEstimator lnwallet.FeeEstimator,
 	// Otherwise, we'll attempt a relaxed confirmation target for the
 	// transaction
 	default:
-		feePerKw, err := feeEstimator.EstimateFeePerKW(6)
+		feePerKw, err := feeEstimator.EstimateFeePerKW(
+			defaultNumBlocksEstimate,
+		)
 		if err != nil {
 			return 0, fmt.Errorf("unable to query fee estimator: "+
 				"%v", err)

matheusd/dcrlnd

sweep/fee_estimator_mock_test.go
@@ -14,6 +14,8 @@ type mockFeeEstimator struct {
 
 	relayFee lnwallet.AtomPerKByte
 
+	blocksToFee map[uint32]lnwallet.AtomPerKByte
+
 	lock sync.Mutex
 }
 
@@ -21,8 +23,9 @@ func newMockFeeEstimator(feePerKB,
 	relayFee lnwallet.AtomPerKByte) *mockFeeEstimator {
 
 	return &mockFeeEstimator{
-		feePerKB: feePerKB,
-		relayFee: relayFee,
+		feePerKB:    feePerKB,
+		relayFee:    relayFee,
+		blocksToFee: make(map[uint32]lnwallet.AtomPerKByte),
 	}
 }
 
@@ -41,6 +44,10 @@ func (e *mockFeeEstimator) EstimateFeePerKB(numBlocks uint32) (
 
 	e.lock.Lock()
 	defer e.lock.Unlock()
+
+	if fee, ok := e.blocksToFee[numBlocks]; ok {
+		return fee, nil
+	}
 
 	return e.feePerKB, nil
 }

sweep/walletsweep.go
@@ -11,6 +11,13 @@ import (
 	"github.com/decred/dcrlnd/lnwallet"
 )
 
+const (
+	// defaultNumBlocksEstimate is the number of blocks that we fall back
+	// to issuing an estimate for if a fee pre fence doesn't specify an
+	// explicit conf target or fee rate.
+	defaultNumBlocksEstimate = 6
+)
+
 // FeePreference allows callers to express their time value for inclusion of a
 // transaction into a block via either a confirmation target, or a fee rate.
 type FeePreference struct {
@@ -31,6 +38,12 @@ func DetermineFeePerKB(feeEstimator lnwallet.FeeEstimator,
 	feePref FeePreference) (lnwallet.AtomPerKByte, error) {
 
 	switch {
+	// If both values are set, then we'll return an error as we require a
+	// strict directive.
+	case feePref.FeeRate != 0 && feePref.ConfTarget != 0:
+		return 0, fmt.Errorf("only FeeRate or ConfTarget should " +
+			"be set for FeePreferences")
+
 	// If the target number of confirmations is set, then we'll use that to
 	// consult our fee estimator for an adequate fee.
 	case feePref.ConfTarget != 0:
@@ -62,7 +75,9 @@ func DetermineFeePerKB(feeEstimator lnwallet.FeeEstimator,
 	// Otherwise, we'll attempt a relaxed confirmation target for the
 	// transaction
 	default:
-		feePerKB, err := feeEstimator.EstimateFeePerKB(6)
+		feePerKB, err := feeEstimator.EstimateFeePerKB(
+			defaultNumBlocksEstimate,
+		)
 		if err != nil {
 			return 0, fmt.Errorf("unable to query fee estimator: "+
 				"%v", err)