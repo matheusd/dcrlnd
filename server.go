lightningnetwork/lnd

server: set max htlc in ChannelUpdate createChannelUpdate
This method is called to convert an EdgePolicy to a ChannelUpdate. We
make sure to carry over the max_htlc value.

Co-authored-by: Johan T. Halseth <johanth@gmail.com>

server.go
@@ -3104,6 +3104,7 @@ func createChannelUpdate(info *channeldb.ChannelEdgeInfo,
 		ChannelFlags:    policy.ChannelFlags,
 		TimeLockDelta:   policy.TimeLockDelta,
 		HtlcMinimumMsat: policy.MinHTLC,
+		HtlcMaximumMsat: policy.MaxHTLC,
 		BaseFee:         uint32(policy.FeeBaseMSat),
 		FeeRate:         uint32(policy.FeeProportionalMillionths),
 		ExtraOpaqueData: policy.ExtraOpaqueData,

matheusd/dcrlnd

server.go
@@ -3141,6 +3141,7 @@ func createChannelUpdate(info *channeldb.ChannelEdgeInfo,
 		ChannelFlags:      policy.ChannelFlags,
 		TimeLockDelta:     policy.TimeLockDelta,
 		HtlcMinimumMAtoms: policy.MinHTLC,
+		HtlcMaximumMAtoms: policy.MaxHTLC,
 		BaseFee:           uint32(policy.FeeBaseMAtoms),
 		FeeRate:           uint32(policy.FeeProportionalMillionths),
 		ExtraOpaqueData:   policy.ExtraOpaqueData,