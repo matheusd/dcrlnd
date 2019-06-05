halseth/lnd

discovery/chan_series+utils: include max htlc when syncing with peers
In this commit, we ensure that max HTLC is included when we're
synchronizing ChannelUpdates with remote peers.

discovery/chan_series.go
@@ -331,6 +331,7 @@ func (c *ChanSeries) FetchChanUpdates(chain chainhash.Hash,
 			ChannelFlags:    e1.ChannelFlags,
 			TimeLockDelta:   e1.TimeLockDelta,
 			HtlcMinimumMsat: e1.MinHTLC,
+			HtlcMaximumMsat: e1.MaxHTLC,
 			BaseFee:         uint32(e1.FeeBaseMSat),
 			FeeRate:         uint32(e1.FeeProportionalMillionths),
 			ExtraOpaqueData: e1.ExtraOpaqueData,
@@ -351,6 +352,7 @@ func (c *ChanSeries) FetchChanUpdates(chain chainhash.Hash,
 			ChannelFlags:    e2.ChannelFlags,
 			TimeLockDelta:   e2.TimeLockDelta,
 			HtlcMinimumMsat: e2.MinHTLC,
+			HtlcMaximumMsat: e2.MaxHTLC,
 			BaseFee:         uint32(e2.FeeBaseMSat),
 			FeeRate:         uint32(e2.FeeProportionalMillionths),
 			ExtraOpaqueData: e2.ExtraOpaqueData,

discovery/utils.go
@@ -76,6 +76,7 @@ func CreateChanAnnouncement(chanProof *channeldb.ChannelAuthProof,
 			ChannelFlags:    e1.ChannelFlags,
 			TimeLockDelta:   e1.TimeLockDelta,
 			HtlcMinimumMsat: e1.MinHTLC,
+			HtlcMaximumMsat: e1.MaxHTLC,
 			BaseFee:         uint32(e1.FeeBaseMSat),
 			FeeRate:         uint32(e1.FeeProportionalMillionths),
 			ExtraOpaqueData: e1.ExtraOpaqueData,
@@ -94,6 +95,7 @@ func CreateChanAnnouncement(chanProof *channeldb.ChannelAuthProof,
 			ChannelFlags:    e2.ChannelFlags,
 			TimeLockDelta:   e2.TimeLockDelta,
 			HtlcMinimumMsat: e2.MinHTLC,
+			HtlcMaximumMsat: e2.MaxHTLC,
 			BaseFee:         uint32(e2.FeeBaseMSat),
 			FeeRate:         uint32(e2.FeeProportionalMillionths),
 			ExtraOpaqueData: e2.ExtraOpaqueData,

matheusd/dcrlnd

discovery/chan_series.go
@@ -331,6 +331,7 @@ func (c *ChanSeries) FetchChanUpdates(chain chainhash.Hash,
 			ChannelFlags:      e1.ChannelFlags,
 			TimeLockDelta:     e1.TimeLockDelta,
 			HtlcMinimumMAtoms: e1.MinHTLC,
+			HtlcMaximumMAtoms: e1.MaxHTLC,
 			BaseFee:           uint32(e1.FeeBaseMAtoms),
 			FeeRate:           uint32(e1.FeeProportionalMillionths),
 			ExtraOpaqueData:   e1.ExtraOpaqueData,
@@ -351,6 +352,7 @@ func (c *ChanSeries) FetchChanUpdates(chain chainhash.Hash,
 			ChannelFlags:      e2.ChannelFlags,
 			TimeLockDelta:     e2.TimeLockDelta,
 			HtlcMinimumMAtoms: e2.MinHTLC,
+			HtlcMaximumMAtoms: e2.MaxHTLC,
 			BaseFee:           uint32(e2.FeeBaseMAtoms),
 			FeeRate:           uint32(e2.FeeProportionalMillionths),
 			ExtraOpaqueData:   e2.ExtraOpaqueData,

discovery/utils.go
@@ -77,6 +77,7 @@ func CreateChanAnnouncement(chanProof *channeldb.ChannelAuthProof,
 			ChannelFlags:      e1.ChannelFlags,
 			TimeLockDelta:     e1.TimeLockDelta,
 			HtlcMinimumMAtoms: e1.MinHTLC,
+			HtlcMaximumMAtoms: e1.MaxHTLC,
 			BaseFee:           uint32(e1.FeeBaseMAtoms),
 			FeeRate:           uint32(e1.FeeProportionalMillionths),
 			ExtraOpaqueData:   e1.ExtraOpaqueData,
@@ -95,6 +96,7 @@ func CreateChanAnnouncement(chanProof *channeldb.ChannelAuthProof,
 			ChannelFlags:      e2.ChannelFlags,
 			TimeLockDelta:     e2.TimeLockDelta,
 			HtlcMinimumMAtoms: e2.MinHTLC,
+			HtlcMaximumMAtoms: e2.MaxHTLC,
 			BaseFee:           uint32(e2.FeeBaseMAtoms),
 			FeeRate:           uint32(e2.FeeProportionalMillionths),
 			ExtraOpaqueData:   e2.ExtraOpaqueData,