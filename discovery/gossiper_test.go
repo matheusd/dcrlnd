halseth/lnd

discovery/gossiper_test: set ChannelUpdate max htlc
In this commit, we alter the gossiper test's helper method
that creates channel updates to include the max htlc field
in the ChannelUpdates it creates.

Co-authored-by: Johan T. Halseth <johanth@gmail.com>

discovery/gossiper_test.go
@@ -453,15 +453,20 @@ func createUpdateAnnouncement(blockHeight uint32,
 
 	var err error
 
+	htlcMinMsat := lnwire.MilliSatoshi(prand.Int63())
 	a := &lnwire.ChannelUpdate{
 		ShortChannelID: lnwire.ShortChannelID{
 			BlockHeight: blockHeight,
 		},
 		Timestamp:       timestamp,
-		MessageFlags:    0,
+		MessageFlags:    lnwire.ChanUpdateOptionMaxHtlc,
 		ChannelFlags:    flags,
 		TimeLockDelta:   uint16(prand.Int63()),
-		HtlcMinimumMsat: lnwire.MilliSatoshi(prand.Int63()),
+		HtlcMinimumMsat: htlcMinMsat,
+
+		// Since the max HTLC must be greater than the min HTLC to pass channel
+		// update validation, set it to double the min htlc.
+		HtlcMaximumMsat: 2 * htlcMinMsat,
 		FeeRate:         uint32(prand.Int31()),
 		BaseFee:         uint32(prand.Int31()),
 	}

matheusd/dcrlnd

discovery/gossiper_test.go
@@ -449,15 +449,20 @@ func createUpdateAnnouncement(blockHeight uint32,
 
 	var err error
 
+	htlcMinMsat := lnwire.MilliAtom(prand.Int63())
 	a := &lnwire.ChannelUpdate{
 		ShortChannelID: lnwire.ShortChannelID{
 			BlockHeight: blockHeight,
 		},
 		Timestamp:         timestamp,
 		TimeLockDelta:     uint16(prand.Int63()),
-		MessageFlags:      0,
+		MessageFlags:      lnwire.ChanUpdateOptionMaxHtlc,
 		ChannelFlags:      flags,
 		HtlcMinimumMAtoms: lnwire.MilliAtom(prand.Int63()),
+
+		// Since the max HTLC must be greater than the min HTLC to pass
+		// channel update validation, set it to double the min htlc.
+		HtlcMaximumMAtoms: 2 * htlcMinMsat,
 		FeeRate:           uint32(prand.Int31()),
 		BaseFee:           uint32(prand.Int31()),
 	}