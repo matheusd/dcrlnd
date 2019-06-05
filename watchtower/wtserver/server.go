cfromknecht/lnd

watchtower/wtserver/server: return valid reward script

watchtower/wtserver/server.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/connmgr"
+	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/watchtower/wtdb"
@@ -357,21 +358,29 @@ func (s *Server) handleCreateSession(peer Peer, id *wtdb.SessionID,
 		)
 	}
 
-	rewardAddrBytes := rewardAddress.ScriptAddress()
+	// Construct the pkscript the client should pay to when signing justice
+	// transactions for this session.
+	rewardScript, err := txscript.PayToAddrScript(rewardAddress)
+	if err != nil {
+		log.Errorf("unable to generate reward script for %s", id)
+		return s.replyCreateSession(
+			peer, id, wtwire.CodeTemporaryFailure, nil,
+		)
+	}
 
 	// TODO(conner): create invoice for upfront payment
 
 	// Assemble the session info using the agreed upon parameters, reward
 	// address, and session id.
 	info := wtdb.SessionInfo{
-		ID:            *id,
-		RewardAddress: rewardAddrBytes,
+		ID: *id,
 		Policy: wtpolicy.Policy{
 			BlobVersion:  req.BlobVersion,
 			MaxUpdates:   req.MaxUpdates,
 			RewardRate:   req.RewardRate,
 			SweepFeeRate: req.SweepFeeRate,
 		},
+		RewardAddress: rewardScript,
 	}
 
 	// Insert the session info into the watchtower's database. If
@@ -387,7 +396,7 @@ func (s *Server) handleCreateSession(peer Peer, id *wtdb.SessionID,
 	log.Infof("Accepted session for %s", id)
 
 	return s.replyCreateSession(
-		peer, id, wtwire.CodeOK, rewardAddrBytes,
+		peer, id, wtwire.CodeOK, rewardScript,
 	)
 }
 

watchtower/wtserver/server_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/watchtower/wtdb"
@@ -17,9 +18,13 @@ import (
 	"github.com/lightningnetwork/lnd/watchtower/wtwire"
 )
 
-// addr is the server's reward address given to watchtower clients.
-var addr, _ = btcutil.DecodeAddress(
-	"mrX9vMRYLfVy1BnZbc5gZjuyaqH3ZW2ZHz", &chaincfg.TestNet3Params,
+var (
+	// addr is the server's reward address given to watchtower clients.
+	addr, _ = btcutil.DecodeAddress(
+		"mrX9vMRYLfVy1BnZbc5gZjuyaqH3ZW2ZHz", &chaincfg.TestNet3Params,
+	)
+
+	addrScript, _ = txscript.PayToAddrScript(addr)
 )
 
 // randPubKey generates a new secp keypair, and returns the public key.
@@ -162,11 +167,11 @@ var createSessionTests = []createSessionTestCase{
 		},
 		expReply: &wtwire.CreateSessionReply{
 			Code: wtwire.CodeOK,
-			Data: []byte(addr.ScriptAddress()),
+			Data: []byte(addrScript),
 		},
 		expDupReply: &wtwire.CreateSessionReply{
 			Code: wtwire.CreateSessionCodeAlreadyExists,
-			Data: []byte(addr.ScriptAddress()),
+			Data: []byte(addrScript),
 		},
 	},
 	// TODO(conner): add policy rejection tests

matheusd/dcrlnd

watchtower/wtserver/server.go
@@ -12,6 +12,7 @@ import (
 	"github.com/decred/dcrd/connmgr"
 	"github.com/decred/dcrd/dcrec/secp256k1"
 	"github.com/decred/dcrd/dcrutil"
+	"github.com/decred/dcrd/txscript"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/watchtower/blob"
 	"github.com/decred/dcrlnd/watchtower/wtdb"
@@ -364,7 +365,15 @@ func (s *Server) handleCreateSession(peer Peer, id *wtdb.SessionID,
 		)
 	}
 
-	rewardAddrBytes := rewardAddress.ScriptAddress()
+	// Construct the pkscript the client should pay to when signing justice
+	// transactions for this session.
+	rewardScript, err := txscript.PayToAddrScript(rewardAddress)
+	if err != nil {
+		log.Errorf("unable to generate reward script for %s", id)
+		return s.replyCreateSession(
+			peer, id, wtwire.CodeTemporaryFailure, nil,
+		)
+	}
 
 	// Ensure that the requested blob type is supported by our tower.
 	if !blob.IsSupportedType(req.BlobType) {
@@ -380,14 +389,14 @@ func (s *Server) handleCreateSession(peer Peer, id *wtdb.SessionID,
 	// Assemble the session info using the agreed upon parameters, reward
 	// address, and session id.
 	info := wtdb.SessionInfo{
-		ID:            *id,
-		RewardAddress: rewardAddrBytes,
+		ID: *id,
 		Policy: wtpolicy.Policy{
 			BlobType:     req.BlobType,
 			MaxUpdates:   req.MaxUpdates,
 			RewardRate:   req.RewardRate,
 			SweepFeeRate: req.SweepFeeRate,
 		},
+		RewardAddress: rewardScript,
 	}
 
 	// Insert the session info into the watchtower's database. If
@@ -403,7 +412,7 @@ func (s *Server) handleCreateSession(peer Peer, id *wtdb.SessionID,
 	log.Infof("Accepted session for %s", id)
 
 	return s.replyCreateSession(
-		peer, id, wtwire.CodeOK, rewardAddrBytes,
+		peer, id, wtwire.CodeOK, rewardScript,
 	)
 }
 

watchtower/wtserver/server_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/decred/dcrd/dcrec/secp256k1"
 	"github.com/decred/dcrd/dcrutil"
+	"github.com/decred/dcrd/txscript"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/watchtower/blob"
 	"github.com/decred/dcrlnd/watchtower/wtdb"
@@ -17,8 +18,12 @@ import (
 	"github.com/decred/dcrlnd/watchtower/wtwire"
 )
 
-// addr is the server's reward address given to watchtower clients.
-var addr, _ = dcrutil.DecodeAddress("TsVDyY1k1N2jZ7xYuoA1PEbwSP2mQnXR9qb")
+var (
+	// addr is the server's reward address given to watchtower clients.
+	addr, _ = dcrutil.DecodeAddress("TsVDyY1k1N2jZ7xYuoA1PEbwSP2mQnXR9qb")
+
+	addrScript, _ = txscript.PayToAddrScript(addr)
+)
 
 // randPubKey generates a new secp keypair, and returns the public key.
 func randPubKey(t *testing.T) *secp256k1.PublicKey {
@@ -160,11 +165,11 @@ var createSessionTests = []createSessionTestCase{
 		},
 		expReply: &wtwire.CreateSessionReply{
 			Code: wtwire.CodeOK,
-			Data: []byte(addr.ScriptAddress()),
+			Data: addrScript,
 		},
 		expDupReply: &wtwire.CreateSessionReply{
 			Code: wtwire.CreateSessionCodeAlreadyExists,
-			Data: []byte(addr.ScriptAddress()),
+			Data: addrScript,
 		},
 	},
 	{