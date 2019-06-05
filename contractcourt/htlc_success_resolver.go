mandelmonkey/lnd

cnct: make original htlc amt available for contract resolvers
Previously, contract resolvers that needed to publish a second level tx,
did not have access to the original htlc amount.

This commit reconstructs this amount from data that is already persisted
in arbitrator log.

Co-authored-by: Joost Jager <joost.jager@gmail.com>

contractcourt/htlc_success_resolver.go
@@ -3,6 +3,7 @@ package contractcourt
 import (
 	"encoding/binary"
 	"fmt"
+	"github.com/lightningnetwork/lnd/lnwire"
 	"io"
 
 	"github.com/btcsuite/btcd/wire"
@@ -45,6 +46,10 @@ type htlcSuccessResolver struct {
 	//
 	// TODO(roasbeef): send off to utxobundler
 	sweepTx *wire.MsgTx
+
+	// htlcAmt is the original amount of the htlc, not taking into
+	// account any fees that may have to be paid if it goes on chain.
+	htlcAmt lnwire.MilliSatoshi
 
 	ResolverKit
 }

contractcourt/htlc_timeout_resolver.go
@@ -40,6 +40,10 @@ type htlcTimeoutResolver struct {
 	// additional commitment state machine.
 	htlcIndex uint64
 
+	// htlcAmt is the original amount of the htlc, not taking into
+	// account any fees that may have to be paid if it goes on chain.
+	htlcAmt lnwire.MilliSatoshi
+
 	ResolverKit
 }
 

matheusd/dcrlnd

contractcourt/htlc_success_resolver.go
@@ -8,6 +8,7 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/lnwallet"
+	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/sweep"
 )
 
@@ -45,6 +46,10 @@ type htlcSuccessResolver struct {
 	//
 	// TODO(roasbeef): send off to utxobundler
 	sweepTx *wire.MsgTx
+
+	// htlcAmt is the original amount of the htlc, not taking into
+	// account any fees that may have to be paid if it goes on chain.
+	htlcAmt lnwire.MilliAtom
 
 	ResolverKit
 }

contractcourt/htlc_timeout_resolver.go
@@ -40,6 +40,10 @@ type htlcTimeoutResolver struct {
 	// additional commitment state machine.
 	htlcIndex uint64
 
+	// htlcAmt is the original amount of the htlc, not taking into
+	// account any fees that may have to be paid if it goes on chain.
+	htlcAmt lnwire.MilliAtom
+
 	ResolverKit
 }
 