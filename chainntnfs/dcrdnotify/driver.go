joostjager/lnd

chainntnfs/btcdnotify: add chainParams parameter to btcdnotify.New
In this commit, we add the current chain parameters to the BtcdNotifier.
This will be used in a future commit in order to convert outputs scripts
into addresses. This is needed since the btcd backend uses these
addresses to detect whether the script encoded within it was spent by a
transaction in the chain.

chainntnfs/btcdnotify/driver.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/lightningnetwork/lnd/chainntnfs"
 )
@@ -11,30 +12,36 @@ import (
 // createNewNotifier creates a new instance of the ChainNotifier interface
 // implemented by BtcdNotifier.
 func createNewNotifier(args ...interface{}) (chainntnfs.ChainNotifier, error) {
-	if len(args) != 3 {
+	if len(args) != 4 {
 		return nil, fmt.Errorf("incorrect number of arguments to "+
-			".New(...), expected 2, instead passed %v", len(args))
+			".New(...), expected 4, instead passed %v", len(args))
 	}
 
 	config, ok := args[0].(*rpcclient.ConnConfig)
 	if !ok {
-		return nil, errors.New("first argument to btcdnotifier.New " +
+		return nil, errors.New("first argument to btcdnotify.New " +
 			"is incorrect, expected a *rpcclient.ConnConfig")
 	}
 
-	spendHintCache, ok := args[1].(chainntnfs.SpendHintCache)
+	chainParams, ok := args[1].(*chaincfg.Params)
 	if !ok {
-		return nil, errors.New("second argument to btcdnotifier.New " +
+		return nil, errors.New("second argument to btcdnotify.New " +
+			"is incorrect, expected a *chaincfg.Params")
+	}
+
+	spendHintCache, ok := args[2].(chainntnfs.SpendHintCache)
+	if !ok {
+		return nil, errors.New("third argument to btcdnotify.New " +
 			"is incorrect, expected a chainntnfs.SpendHintCache")
 	}
 
-	confirmHintCache, ok := args[2].(chainntnfs.ConfirmHintCache)
+	confirmHintCache, ok := args[3].(chainntnfs.ConfirmHintCache)
 	if !ok {
-		return nil, errors.New("third argument to btcdnotifier.New " +
+		return nil, errors.New("fourth argument to btcdnotify.New " +
 			"is incorrect, expected a chainntnfs.ConfirmHintCache")
 	}
 
-	return New(config, spendHintCache, confirmHintCache)
+	return New(config, chainParams, spendHintCache, confirmHintCache)
 }
 
 // init registers a driver for the BtcdNotifier concrete implementation of the

matheusd/dcrlnd

chainntnfs/dcrdnotify/driver.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/decred/dcrd/chaincfg"
 	"github.com/decred/dcrd/rpcclient/v2"
 	"github.com/decred/dcrlnd/chainntnfs"
 )
@@ -11,11 +12,9 @@ import (
 // createNewNotifier creates a new instance of the ChainNotifier interface
 // implemented by DcrdNotifier.
 func createNewNotifier(args ...interface{}) (chainntnfs.ChainNotifier, error) {
-	const numRequiredArgs = 3
-	if len(args) != numRequiredArgs {
+	if len(args) != 4 {
 		return nil, fmt.Errorf("incorrect number of arguments to "+
-			".New(...), expected %d, instead passed %d", numRequiredArgs,
-			len(args))
+			".New(...), expected 4, instead passed %v", len(args))
 	}
 
 	config, ok := args[0].(*rpcclient.ConnConfig)
@@ -24,19 +23,25 @@ func createNewNotifier(args ...interface{}) (chainntnfs.ChainNotifier, error) {
 			"is incorrect, expected a *rpcclient.ConnConfig")
 	}
 
-	spendHintCache, ok := args[1].(chainntnfs.SpendHintCache)
+	chainParams, ok := args[1].(*chaincfg.Params)
 	if !ok {
 		return nil, errors.New("second argument to dcrdnotifier.New " +
+			"is incorrect, expected a *chaincfg.Params")
+	}
+
+	spendHintCache, ok := args[2].(chainntnfs.SpendHintCache)
+	if !ok {
+		return nil, errors.New("third argument to dcrdnotifier.New " +
 			"is incorrect, expected a chainntnfs.SpendHintCache")
 	}
 
-	confirmHintCache, ok := args[2].(chainntnfs.ConfirmHintCache)
+	confirmHintCache, ok := args[3].(chainntnfs.ConfirmHintCache)
 	if !ok {
 		return nil, errors.New("third argument to dcrdnotifier.New " +
 			"is incorrect, expected a chainntnfs.ConfirmHintCache")
 	}
 
-	return New(config, spendHintCache, confirmHintCache)
+	return New(config, chainParams, spendHintCache, confirmHintCache)
 }
 
 // init registers a driver for the DcrdNotifier concrete implementation of the