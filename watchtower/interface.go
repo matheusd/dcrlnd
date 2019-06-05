lightningnetwork/lnd

watchtower/interface: adds composite DB iface

watchtower/interface.go
@@ -1,0 +1,14 @@
+package watchtower
+
+import (
+	"github.com/lightningnetwork/lnd/watchtower/lookout"
+	"github.com/lightningnetwork/lnd/watchtower/wtserver"
+)
+
+// DB abstracts the persistent functionality required to run the watchtower
+// daemon. It composes the database interfaces required by the lookout and
+// wtserver subsystems.
+type DB interface {
+	lookout.DB
+	wtserver.DB
+}

matheusd/dcrlnd

watchtower/interface.go
@@ -1,0 +1,14 @@
+package watchtower
+
+import (
+	"github.com/decred/dcrlnd/watchtower/lookout"
+	"github.com/decred/dcrlnd/watchtower/wtserver"
+)
+
+// DB abstracts the persistent functionality required to run the watchtower
+// daemon. It composes the database interfaces required by the lookout and
+// wtserver subsystems.
+type DB interface {
+	lookout.DB
+	wtserver.DB
+}