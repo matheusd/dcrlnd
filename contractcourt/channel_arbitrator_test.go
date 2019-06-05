lightningnetwork/lnd

contractcourt+server: settle invoice for on-chain HTLC sweep
In this commit, we extend the htlcSuccessResolver to settle the invoice,
if any, of the corresponding on-chain HTLC sweep. This ensures that the
invoice state is consistent as when claiming the HTLC "off-chain".

contractcourt/channel_arbitrator_test.go
@@ -175,6 +175,9 @@ func createTestChannelArbitrator(log ArbitratorLog) (*ChannelArbitrator,
 			*lnwallet.IncomingHtlcResolution, uint32) error {
 			return nil
 		},
+		SettleInvoice: func(chainhash.Hash, lnwire.MilliSatoshi) error {
+			return nil
+		},
 	}
 
 	// We'll use the resolvedChan to synchronize on call to

contractcourt/htlc_success_resolver.go
@@ -3,8 +3,10 @@ package contractcourt
 import (
 	"encoding/binary"
 	"fmt"
-	"github.com/lightningnetwork/lnd/lnwire"
 	"io"
+
+	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/lnwire"
 
 	"github.com/btcsuite/btcd/wire"
 	"github.com/davecgh/go-spew/spew"
@@ -174,6 +176,14 @@ func (h *htlcSuccessResolver) Resolve() (ContractResolver, error) {
 			return nil, fmt.Errorf("quitting")
 		}
 
+		// With the HTLC claimed, we can attempt to settle its
+		// corresponding invoice if we were the original destination.
+		err = h.SettleInvoice(h.payHash, h.htlcAmt)
+		if err != nil && err != channeldb.ErrInvoiceNotFound {
+			log.Errorf("Unable to settle invoice with payment "+
+				"hash %x: %v", h.payHash, err)
+		}
+
 		// Once the transaction has received a sufficient number of
 		// confirmations, we'll mark ourselves as fully resolved and exit.
 		h.resolved = true
@@ -237,6 +247,14 @@ func (h *htlcSuccessResolver) Resolve() (ContractResolver, error) {
 
 	case <-h.Quit:
 		return nil, fmt.Errorf("quitting")
+	}
+
+	// With the HTLC claimed, we can attempt to settle its corresponding
+	// invoice if we were the original destination.
+	err = h.SettleInvoice(h.payHash, h.htlcAmt)
+	if err != nil && err != channeldb.ErrInvoiceNotFound {
+		log.Errorf("Unable to settle invoice with payment "+
+			"hash %x: %v", h.payHash, err)
 	}
 
 	h.resolved = true

matheusd/dcrlnd

contractcourt/channel_arbitrator_test.go
@@ -175,6 +175,9 @@ func createTestChannelArbitrator(log ArbitratorLog) (*ChannelArbitrator,
 			*lnwallet.IncomingHtlcResolution, uint32) error {
 			return nil
 		},
+		SettleInvoice: func(chainhash.Hash, lnwire.MilliAtom) error {
+			return nil
+		},
 	}
 
 	// We'll use the resolvedChan to synchronize on call to

contractcourt/htlc_success_resolver.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/decred/dcrd/wire"
+	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/lnwallet"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/sweep"
@@ -174,6 +175,14 @@ func (h *htlcSuccessResolver) Resolve() (ContractResolver, error) {
 			return nil, fmt.Errorf("quitting")
 		}
 
+		// With the HTLC claimed, we can attempt to settle its
+		// corresponding invoice if we were the original destination.
+		err = h.SettleInvoice(h.payHash, h.htlcAmt)
+		if err != nil && err != channeldb.ErrInvoiceNotFound {
+			log.Errorf("Unable to settle invoice with payment "+
+				"hash %x: %v", h.payHash, err)
+		}
+
 		// Once the transaction has received a sufficient number of
 		// confirmations, we'll mark ourselves as fully resolved and exit.
 		h.resolved = true
@@ -237,6 +246,14 @@ func (h *htlcSuccessResolver) Resolve() (ContractResolver, error) {
 
 	case <-h.Quit:
 		return nil, fmt.Errorf("quitting")
+	}
+
+	// With the HTLC claimed, we can attempt to settle its corresponding
+	// invoice if we were the original destination.
+	err = h.SettleInvoice(h.payHash, h.htlcAmt)
+	if err != nil && err != channeldb.ErrInvoiceNotFound {
+		log.Errorf("Unable to settle invoice with payment "+
+			"hash %x: %v", h.payHash, err)
 	}
 
 	h.resolved = true