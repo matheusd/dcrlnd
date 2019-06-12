valentinewallace/lnd

htlcswitch+lnwallet+peer: default max htlc in fwding policy of new chans
In this commit, we set a default max HTLC in the forwarding
policies of newly open channels.

The ForwardingPolicy's MaxHTLC field (added in this commit)
will later be used to decide whether an HTLC satisfies our policy before
forwarding it.

To ensure the ForwardingPolicy's MaxHTLC default matches the max HTLC
advertised in the ChannelUpdate sent out for this channel,  we also add
a MaxPendingAmount() function to the lnwallet.Channel.

lnwallet/channel.go
@@ -6272,3 +6272,9 @@ func (lc *LightningChannel) RemoteCommitHeight() uint64 {
 func (lc *LightningChannel) FwdMinHtlc() lnwire.MilliSatoshi {
 	return lc.localChanCfg.MinHTLC
 }
+
+// MaxPendingAmount returns the maximum HTLC value  that can be pending at
+// any time over this channel.
+func (lc *LightningChannel) MaxPendingAmount() lnwire.MilliSatoshi {
+	return lc.localChanCfg.MaxPendingAmount
+}

matheusd/dcrlnd

lnwallet/channel.go
@@ -6345,3 +6345,9 @@ func (lc *LightningChannel) RemoteCommitHeight() uint64 {
 func (lc *LightningChannel) FwdMinHtlc() lnwire.MilliAtom {
 	return lc.localChanCfg.MinHTLC
 }
+
+// MaxPendingAmount returns the maximum HTLC value that can be pending at
+// any time over this channel.
+func (lc *LightningChannel) MaxPendingAmount() lnwire.MilliAtom {
+	return lc.localChanCfg.MaxPendingAmount
+}