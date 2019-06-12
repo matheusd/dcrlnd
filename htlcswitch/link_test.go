valentinewallace/lnd

htlcswitch/link_test+test_utils: add max htlc to forwarding policies

htlcswitch/link_test.go
@@ -1524,6 +1524,7 @@ func newSingleLinkTestHarness(chanAmt, chanReserve btcutil.Amount) (
 		}
 		globalPolicy = ForwardingPolicy{
 			MinHTLC:       lnwire.NewMSatFromSatoshis(5),
+			MaxHTLC:       lnwire.NewMSatFromSatoshis(chanAmt),
 			BaseFee:       lnwire.NewMSatFromSatoshis(1),
 			TimeLockDelta: 6,
 		}
@@ -5143,6 +5144,7 @@ func TestHtlcSatisfyPolicy(t *testing.T) {
 			FwrdingPolicy: ForwardingPolicy{
 				TimeLockDelta: 20,
 				MinHTLC:       500,
+				MaxHTLC:       1000,
 				BaseFee:       10,
 			},
 			FetchLastChannelUpdate: fetchLastChannelUpdate,

matheusd/dcrlnd

htlcswitch/link_test.go
@@ -1532,6 +1532,7 @@ func newSingleLinkTestHarness(chanAmt, chanReserve dcrutil.Amount) (
 		}
 		globalPolicy = ForwardingPolicy{
 			MinHTLC:       lnwire.NewMAtomsFromAtoms(5),
+			MaxHTLC:       lnwire.NewMAtomsFromAtoms(chanAmt),
 			BaseFee:       lnwire.NewMAtomsFromAtoms(1),
 			TimeLockDelta: 6,
 		}
@@ -5401,6 +5402,7 @@ func TestHtlcSatisfyPolicy(t *testing.T) {
 			FwrdingPolicy: ForwardingPolicy{
 				TimeLockDelta: 20,
 				MinHTLC:       500,
+				MaxHTLC:       1000,
 				BaseFee:       10,
 			},
 			FetchLastChannelUpdate: fetchLastChannelUpdate,