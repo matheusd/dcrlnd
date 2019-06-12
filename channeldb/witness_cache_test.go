lightningnetwork/lnd

channeldb+witness_beacon: use sha256 lookup+delete witness

channeldb/witness_cache_test.go
@@ -1,17 +1,15 @@
 package channeldb
 
 import (
-	"bytes"
 	"crypto/sha256"
-	"reflect"
 	"testing"
 
 	"github.com/lightningnetwork/lnd/lntypes"
 )
 
-// TestWitnessCacheRetrieval tests that we're able to add and lookup new
-// witnesses to the witness cache.
-func TestWitnessCacheRetrieval(t *testing.T) {
+// TestWitnessCacheSha256Retrieval tests that we're able to add and lookup new
+// sha256 preimages to the witness cache.
+func TestWitnessCacheSha256Retrieval(t *testing.T) {
 	t.Parallel()
 
 	cdb, cleanUp, err := makeTestDB()
@@ -22,44 +20,41 @@ func TestWitnessCacheRetrieval(t *testing.T) {
 
 	wCache := cdb.NewWitnessCache()
 
-	// We'll be attempting to add then lookup two simple hash witnesses
+	// We'll be attempting to add then lookup two simple sha256 preimages
 	// within this test.
-	witness1 := rev[:]
-	witness1Key := sha256.Sum256(witness1)
+	preimage1 := lntypes.Preimage(rev)
+	preimage2 := lntypes.Preimage(key)
 
-	witness2 := key[:]
-	witness2Key := sha256.Sum256(witness2)
+	preimages := []lntypes.Preimage{preimage1, preimage2}
+	hashes := []lntypes.Hash{preimage1.Hash(), preimage2.Hash()}
 
-	witnesses := [][]byte{witness1, witness2}
-	keys := [][]byte{witness1Key[:], witness2Key[:]}
-
-	// First, we'll attempt to add the witnesses to the database.
-	err = wCache.AddWitnesses(Sha256HashWitness, witnesses...)
+	// First, we'll attempt to add the preimages to the database.
+	err = wCache.AddSha256Witnesses(preimages...)
 	if err != nil {
 		t.Fatalf("unable to add witness: %v", err)
 	}
 
-	// With the witnesses stored, we'll now attempt to look them up.
-	for i, key := range keys {
-		witness := witnesses[i]
+	// With the preimages stored, we'll now attempt to look them up.
+	for i, hash := range hashes {
+		preimage := preimages[i]
 
-		// We should get back the *exact* same witness as we originally
+		// We should get back the *exact* same preimage as we originally
 		// stored.
-		dbWitness, err := wCache.LookupWitness(Sha256HashWitness, key)
+		dbPreimage, err := wCache.LookupSha256Witness(hash)
 		if err != nil {
 			t.Fatalf("unable to look up witness: %v", err)
 		}
 
-		if !reflect.DeepEqual(witness, dbWitness[:]) {
+		if preimage != dbPreimage {
 			t.Fatalf("witnesses don't match: expected %x, got %x",
-				witness[:], dbWitness[:])
+				preimage[:], dbPreimage[:])
 		}
 	}
 }
 
-// TestWitnessCacheDeletion tests that we're able to delete a single witness,
-// and also a class of witnesses from the cache.
-func TestWitnessCacheDeletion(t *testing.T) {
+// TestWitnessCacheSha256Deletion tests that we're able to delete a single
+// sha256 preimage, and also a class of witnesses from the cache.
+func TestWitnessCacheSha256Deletion(t *testing.T) {
 	t.Parallel()
 
 	cdb, cleanUp, err := makeTestDB()
@@ -70,39 +65,39 @@ func TestWitnessCacheDeletion(t *testing.T) {
 
 	wCache := cdb.NewWitnessCache()
 
-	// We'll start by adding two witnesses to the cache.
-	witness1 := rev[:]
-	witness1Key := sha256.Sum256(witness1)
+	// We'll start by adding two preimages to the cache.
+	preimage1 := lntypes.Preimage(key)
+	hash1 := preimage1.Hash()
 
-	if err := wCache.AddWitnesses(Sha256HashWitness, witness1); err != nil {
+	preimage2 := lntypes.Preimage(rev)
+	hash2 := preimage2.Hash()
+
+	if err := wCache.AddSha256Witnesses(preimage1); err != nil {
 		t.Fatalf("unable to add witness: %v", err)
 	}
 
-	witness2 := key[:]
-	witness2Key := sha256.Sum256(witness2)
-
-	if err := wCache.AddWitnesses(Sha256HashWitness, witness2); err != nil {
+	if err := wCache.AddSha256Witnesses(preimage2); err != nil {
 		t.Fatalf("unable to add witness: %v", err)
 	}
 
-	// We'll now delete the first witness. If we attempt to look it up, we
+	// We'll now delete the first preimage. If we attempt to look it up, we
 	// should get ErrNoWitnesses.
-	err = wCache.DeleteWitness(Sha256HashWitness, witness1Key[:])
+	err = wCache.DeleteSha256Witness(hash1)
 	if err != nil {
 		t.Fatalf("unable to delete witness: %v", err)
 	}
-	_, err = wCache.LookupWitness(Sha256HashWitness, witness1Key[:])
+	_, err = wCache.LookupSha256Witness(hash1)
 	if err != ErrNoWitnesses {
 		t.Fatalf("expected ErrNoWitnesses instead got: %v", err)
 	}
 
 	// Next, we'll attempt to delete the entire witness class itself. When
-	// we try to lookup the second witness, we should again get
+	// we try to lookup the second preimage, we should again get
 	// ErrNoWitnesses.
 	if err := wCache.DeleteWitnessClass(Sha256HashWitness); err != nil {
 		t.Fatalf("unable to delete witness class: %v", err)
 	}
-	_, err = wCache.LookupWitness(Sha256HashWitness, witness2Key[:])
+	_, err = wCache.LookupSha256Witness(hash2)
 	if err != ErrNoWitnesses {
 		t.Fatalf("expected ErrNoWitnesses instead got: %v", err)
 	}
@@ -123,7 +118,7 @@ func TestWitnessCacheUnknownWitness(t *testing.T) {
 
 	// We'll attempt to add a new, undefined witness type to the database.
 	// We should get an error.
-	err = wCache.AddWitnesses(234, key[:])
+	err = wCache.legacyAddWitnesses(234, key[:])
 	if err != ErrUnknownWitnessType {
 		t.Fatalf("expected ErrUnknownWitnessType, got %v", err)
 	}
@@ -143,41 +138,41 @@ func TestAddSha256Witnesses(t *testing.T) {
 	// We'll start by adding a witnesses to the cache using the generic
 	// AddWitnesses method.
 	witness1 := rev[:]
-	witness1Key := sha256.Sum256(witness1)
+	preimage1 := lntypes.Preimage(rev)
+	hash1 := preimage1.Hash()
 
 	witness2 := key[:]
-	witness2Key := sha256.Sum256(witness2)
+	preimage2 := lntypes.Preimage(key)
+	hash2 := preimage2.Hash()
 
 	var (
-		preimages = []lntypes.Preimage{rev, key}
 		witnesses = [][]byte{witness1, witness2}
-		keys      = [][]byte{witness1Key[:], witness2Key[:]}
+		preimages = []lntypes.Preimage{preimage1, preimage2}
+		hashes    = []lntypes.Hash{hash1, hash2}
 	)
 
-	err = wCache.AddWitnesses(Sha256HashWitness, witnesses...)
+	err = wCache.legacyAddWitnesses(Sha256HashWitness, witnesses...)
 	if err != nil {
 		t.Fatalf("unable to add witness: %v", err)
 	}
 
-	for i, key := range keys {
-		witness := witnesses[i]
+	for i, hash := range hashes {
+		preimage := preimages[i]
 
-		dbWitness, err := wCache.LookupWitness(
-			Sha256HashWitness, key,
-		)
+		dbPreimage, err := wCache.LookupSha256Witness(hash)
 		if err != nil {
 			t.Fatalf("unable to lookup witness: %v", err)
 		}
 
 		// Assert that the retrieved witness matches the original.
-		if bytes.Compare(dbWitness, witness) != 0 {
+		if dbPreimage != preimage {
 			t.Fatalf("retrieved witness mismatch, want: %x, "+
-				"got: %x", witness, dbWitness)
+				"got: %x", preimage, dbPreimage)
 		}
 
 		// We'll now delete the witness, as we'll be reinserting it
 		// using the specialized AddSha256Witnesses method.
-		err = wCache.DeleteWitness(Sha256HashWitness, key)
+		err = wCache.DeleteSha256Witness(hash)
 		if err != nil {
 			t.Fatalf("unable to delete witness: %v", err)
 		}
@@ -193,20 +188,51 @@ func TestAddSha256Witnesses(t *testing.T) {
 	// Finally, iterate over the keys and assert that the returned witnesses
 	// match the original witnesses. This asserts that the specialized
 	// insertion method behaves identically to the generalized interface.
-	for i, key := range keys {
-		witness := witnesses[i]
+	for i, hash := range hashes {
+		preimage := preimages[i]
 
-		dbWitness, err := wCache.LookupWitness(
-			Sha256HashWitness, key,
-		)
+		dbPreimage, err := wCache.LookupSha256Witness(hash)
 		if err != nil {
 			t.Fatalf("unable to lookup witness: %v", err)
 		}
 
 		// Assert that the retrieved witness matches the original.
-		if bytes.Compare(dbWitness, witness) != 0 {
+		if dbPreimage != preimage {
 			t.Fatalf("retrieved witness mismatch, want: %x, "+
-				"got: %x", witness, dbWitness)
+				"got: %x", preimage, dbPreimage)
 		}
 	}
+}
+
+// legacyAddWitnesses adds a batch of new witnesses of wType to the witness
+// cache. The type of the witness will be used to map each witness to the key
+// that will be used to look it up. All witnesses should be of the same
+// WitnessType.
+//
+// NOTE: Previously this method exposed a generic interface for adding
+// witnesses, which has since been deprecated in favor of a strongly typed
+// interface for each witness class. We keep this method around to assert the
+// correctness of specialized witness adding methods.
+func (w *WitnessCache) legacyAddWitnesses(wType WitnessType,
+	witnesses ...[]byte) error {
+
+	// Optimistically compute the witness keys before attempting to start
+	// the db transaction.
+	entries := make([]witnessEntry, 0, len(witnesses))
+	for _, witness := range witnesses {
+		// Map each witness to its key by applying the appropriate
+		// transformation for the given witness type.
+		switch wType {
+		case Sha256HashWitness:
+			key := sha256.Sum256(witness)
+			entries = append(entries, witnessEntry{
+				key:     key[:],
+				witness: witness,
+			})
+		default:
+			return ErrUnknownWitnessType
+		}
+	}
+
+	return w.addWitnessEntries(wType, entries)
 }

matheusd/dcrlnd

channeldb/witness_cache_test.go
@@ -9,9 +9,9 @@ import (
 	"github.com/decred/dcrlnd/lntypes"
 )
 
-// TestWitnessCacheRetrieval tests that we're able to add and lookup new
-// witnesses to the witness cache.
-func TestWitnessCacheRetrieval(t *testing.T) {
+// TestWitnessCacheSha256Retrieval tests that we're able to add and lookup new
+// sha256 preimages to the witness cache.
+func TestWitnessCacheSha256Retrieval(t *testing.T) {
 	t.Parallel()
 
 	cdb, cleanUp, err := makeTestDB()
@@ -22,44 +22,41 @@ func TestWitnessCacheRetrieval(t *testing.T) {
 
 	wCache := cdb.NewWitnessCache()
 
-	// We'll be attempting to add then lookup two simple hash witnesses
+	// We'll be attempting to add then lookup two simple sha256 preimages
 	// within this test.
-	witness1 := rev[:]
-	witness1Key := chainhash.HashB(witness1)
+	preimage1 := lntypes.Preimage(rev)
+	preimage2 := lntypes.Preimage(key)
 
-	witness2 := key[:]
-	witness2Key := chainhash.HashB(witness2)
+	preimages := []lntypes.Preimage{preimage1, preimage2}
+	hashes := []lntypes.Hash{preimage1.Hash(), preimage2.Hash()}
 
-	witnesses := [][]byte{witness1, witness2}
-	keys := [][]byte{witness1Key[:], witness2Key[:]}
-
-	// First, we'll attempt to add the witnesses to the database.
-	err = wCache.AddWitnesses(Sha256HashWitness, witnesses...)
+	// First, we'll attempt to add the preimages to the database.
+	err = wCache.AddSha256Witnesses(preimages...)
 	if err != nil {
 		t.Fatalf("unable to add witness: %v", err)
 	}
 
-	// With the witnesses stored, we'll now attempt to look them up.
-	for i, key := range keys {
-		witness := witnesses[i]
+	// With the preimages stored, we'll now attempt to look them up.
+	for i, hash := range hashes {
+		preimage := preimages[i]
 
-		// We should get back the *exact* same witness as we originally
+		// We should get back the *exact* same preimage as we originally
 		// stored.
-		dbWitness, err := wCache.LookupWitness(Sha256HashWitness, key)
+		dbPreimage, err := wCache.LookupSha256Witness(hash)
 		if err != nil {
 			t.Fatalf("unable to look up witness: %v", err)
 		}
 
-		if !reflect.DeepEqual(witness, dbWitness[:]) {
+		if preimage != dbPreimage {
 			t.Fatalf("witnesses don't match: expected %x, got %x",
-				witness[:], dbWitness[:])
+				preimage[:], dbPreimage[:])
 		}
 	}
 }
 
-// TestWitnessCacheDeletion tests that we're able to delete a single witness,
-// and also a class of witnesses from the cache.
-func TestWitnessCacheDeletion(t *testing.T) {
+// TestWitnessCacheSha256Deletion tests that we're able to delete a single
+// sha256 preimage, and also a class of witnesses from the cache.
+func TestWitnessCacheSha256Deletion(t *testing.T) {
 	t.Parallel()
 
 	cdb, cleanUp, err := makeTestDB()
@@ -70,39 +67,39 @@ func TestWitnessCacheDeletion(t *testing.T) {
 
 	wCache := cdb.NewWitnessCache()
 
-	// We'll start by adding two witnesses to the cache.
-	witness1 := rev[:]
-	witness1Key := chainhash.HashB(witness1)
+	// We'll start by adding two preimages to the cache.
+	preimage1 := lntypes.Preimage(key)
+	hash1 := preimage1.Hash()
 
-	if err := wCache.AddWitnesses(Sha256HashWitness, witness1); err != nil {
+	preimage2 := lntypes.Preimage(rev)
+	hash2 := preimage2.Hash()
+
+	if err := wCache.AddSha256Witnesses(preimage1); err != nil {
 		t.Fatalf("unable to add witness: %v", err)
 	}
 
-	witness2 := key[:]
-	witness2Key := chainhash.HashB(witness2)
-
-	if err := wCache.AddWitnesses(Sha256HashWitness, witness2); err != nil {
+	if err := wCache.AddSha256Witnesses(preimage2); err != nil {
 		t.Fatalf("unable to add witness: %v", err)
 	}
 
-	// We'll now delete the first witness. If we attempt to look it up, we
+	// We'll now delete the first preimage. If we attempt to look it up, we
 	// should get ErrNoWitnesses.
-	err = wCache.DeleteWitness(Sha256HashWitness, witness1Key[:])
+	err = wCache.DeleteSha256Witness(hash1)
 	if err != nil {
 		t.Fatalf("unable to delete witness: %v", err)
 	}
-	_, err = wCache.LookupWitness(Sha256HashWitness, witness1Key[:])
+	_, err = wCache.LookupSha256Witness(hash1)
 	if err != ErrNoWitnesses {
 		t.Fatalf("expected ErrNoWitnesses instead got: %v", err)
 	}
 
 	// Next, we'll attempt to delete the entire witness class itself. When
-	// we try to lookup the second witness, we should again get
+	// we try to lookup the second preimage, we should again get
 	// ErrNoWitnesses.
 	if err := wCache.DeleteWitnessClass(Sha256HashWitness); err != nil {
 		t.Fatalf("unable to delete witness class: %v", err)
 	}
-	_, err = wCache.LookupWitness(Sha256HashWitness, witness2Key[:])
+	_, err = wCache.LookupSha256Witness(hash2)
 	if err != ErrNoWitnesses {
 		t.Fatalf("expected ErrNoWitnesses instead got: %v", err)
 	}
@@ -123,7 +120,7 @@ func TestWitnessCacheUnknownWitness(t *testing.T) {
 
 	// We'll attempt to add a new, undefined witness type to the database.
 	// We should get an error.
-	err = wCache.AddWitnesses(234, key[:])
+	err = wCache.legacyAddWitnesses(234, key[:])
 	if err != ErrUnknownWitnessType {
 		t.Fatalf("expected ErrUnknownWitnessType, got %v", err)
 	}
@@ -143,41 +140,41 @@ func TestAddSha256Witnesses(t *testing.T) {
 	// We'll start by adding a witnesses to the cache using the generic
 	// AddWitnesses method.
 	witness1 := rev[:]
-	witness1Key := chainhash.HashB(witness1)
+	preimage1 := lntypes.Preimage(rev)
+	hash1 := preimage1.Hash()
 
 	witness2 := key[:]
-	witness2Key := chainhash.HashB(witness2)
+	preimage2 := lntypes.Preimage(key)
+	hash2 := preimage2.Hash()
 
 	var (
-		preimages = []lntypes.Preimage{rev, key}
 		witnesses = [][]byte{witness1, witness2}
-		keys      = [][]byte{witness1Key[:], witness2Key[:]}
+		preimages = []lntypes.Preimage{preimage1, preimage2}
+		hashes    = []lntypes.Hash{hash1, hash2}
 	)
 
-	err = wCache.AddWitnesses(Sha256HashWitness, witnesses...)
+	err = wCache.legacyAddWitnesses(Sha256HashWitness, witnesses...)
 	if err != nil {
 		t.Fatalf("unable to add witness: %v", err)
 	}
 
-	for i, key := range keys {
-		witness := witnesses[i]
+	for i, hash := range hashes {
+		preimage := preimages[i]
 
-		dbWitness, err := wCache.LookupWitness(
-			Sha256HashWitness, key,
-		)
+		dbPreimage, err := wCache.LookupSha256Witness(hash)
 		if err != nil {
 			t.Fatalf("unable to lookup witness: %v", err)
 		}
 
 		// Assert that the retrieved witness matches the original.
-		if bytes.Compare(dbWitness, witness) != 0 {
+		if dbPreimage != preimage {
 			t.Fatalf("retrieved witness mismatch, want: %x, "+
-				"got: %x", witness, dbWitness)
+				"got: %x", preimage, dbPreimage)
 		}
 
 		// We'll now delete the witness, as we'll be reinserting it
 		// using the specialized AddSha256Witnesses method.
-		err = wCache.DeleteWitness(Sha256HashWitness, key)
+		err = wCache.DeleteSha256Witness(hash)
 		if err != nil {
 			t.Fatalf("unable to delete witness: %v", err)
 		}
@@ -193,20 +190,51 @@ func TestAddSha256Witnesses(t *testing.T) {
 	// Finally, iterate over the keys and assert that the returned witnesses
 	// match the original witnesses. This asserts that the specialized
 	// insertion method behaves identically to the generalized interface.
-	for i, key := range keys {
-		witness := witnesses[i]
+	for i, hash := range hashes {
+		preimage := preimages[i]
 
-		dbWitness, err := wCache.LookupWitness(
-			Sha256HashWitness, key,
-		)
+		dbPreimage, err := wCache.LookupSha256Witness(hash)
 		if err != nil {
 			t.Fatalf("unable to lookup witness: %v", err)
 		}
 
 		// Assert that the retrieved witness matches the original.
-		if bytes.Compare(dbWitness, witness) != 0 {
+		if dbPreimage != preimage {
 			t.Fatalf("retrieved witness mismatch, want: %x, "+
-				"got: %x", witness, dbWitness)
+				"got: %x", preimage, dbPreimage)
 		}
 	}
+}
+
+// legacyAddWitnesses adds a batch of new witnesses of wType to the witness
+// cache. The type of the witness will be used to map each witness to the key
+// that will be used to look it up. All witnesses should be of the same
+// WitnessType.
+//
+// NOTE: Previously this method exposed a generic interface for adding
+// witnesses, which has since been deprecated in favor of a strongly typed
+// interface for each witness class. We keep this method around to assert the
+// correctness of specialized witness adding methods.
+func (w *WitnessCache) legacyAddWitnesses(wType WitnessType,
+	witnesses ...[]byte) error {
+
+	// Optimistically compute the witness keys before attempting to start
+	// the db transaction.
+	entries := make([]witnessEntry, 0, len(witnesses))
+	for _, witness := range witnesses {
+		// Map each witness to its key by applying the appropriate
+		// transformation for the given witness type.
+		switch wType {
+		case Sha256HashWitness:
+			key := chainhash.HashB(witness)
+			entries = append(entries, witnessEntry{
+				key:     key[:],
+				witness: witness,
+			})
+		default:
+			return ErrUnknownWitnessType
+		}
+	}
+
+	return w.addWitnessEntries(wType, entries)
 }