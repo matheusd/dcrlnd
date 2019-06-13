lightningnetwork/lnd

lnrpc: rename MaxHtlc to MaxHtlcMsat in lnrpc.RoutingPolicy
In this commit, we rename the new `MaxHtlc` field to `MaxHtlcMsat`. We
do this in order to adhere to the new practice to suffix each amount
related field with their proper unit.

rpcserver.go
@@ -3850,7 +3850,7 @@ func marshalDbEdge(edgeInfo *channeldb.ChannelEdgeInfo,
 		edge.Node1Policy = &lnrpc.RoutingPolicy{
 			TimeLockDelta:    uint32(c1.TimeLockDelta),
 			MinHtlc:          int64(c1.MinHTLC),
-			MaxHtlc:          uint64(c1.MaxHTLC),
+			MaxHtlcMsat:      uint64(c1.MaxHTLC),
 			FeeBaseMsat:      int64(c1.FeeBaseMSat),
 			FeeRateMilliMsat: int64(c1.FeeProportionalMillionths),
 			Disabled:         c1.ChannelFlags&lnwire.ChanUpdateDisabled != 0,
@@ -3861,7 +3861,7 @@ func marshalDbEdge(edgeInfo *channeldb.ChannelEdgeInfo,
 		edge.Node2Policy = &lnrpc.RoutingPolicy{
 			TimeLockDelta:    uint32(c2.TimeLockDelta),
 			MinHtlc:          int64(c2.MinHTLC),
-			MaxHtlc:          uint64(c2.MaxHTLC),
+			MaxHtlcMsat:      uint64(c2.MaxHTLC),
 			FeeBaseMsat:      int64(c2.FeeBaseMSat),
 			FeeRateMilliMsat: int64(c2.FeeProportionalMillionths),
 			Disabled:         c2.ChannelFlags&lnwire.ChanUpdateDisabled != 0,
@@ -4410,7 +4410,7 @@ func marshallTopologyChange(topChange *routing.TopologyChange) *lnrpc.GraphTopol
 			RoutingPolicy: &lnrpc.RoutingPolicy{
 				TimeLockDelta:    uint32(channelUpdate.TimeLockDelta),
 				MinHtlc:          int64(channelUpdate.MinHTLC),
-				MaxHtlc:          uint64(channelUpdate.MaxHTLC),
+				MaxHtlcMsat:      uint64(channelUpdate.MaxHTLC),
 				FeeBaseMsat:      int64(channelUpdate.BaseFee),
 				FeeRateMilliMsat: int64(channelUpdate.FeeRate),
 				Disabled:         channelUpdate.Disabled,

matheusd/dcrlnd

rpcserver.go
@@ -4075,6 +4075,7 @@ func marshalDbEdge(edgeInfo *channeldb.ChannelEdgeInfo,
 		edge.Node1Policy = &lnrpc.RoutingPolicy{
 			TimeLockDelta:      uint32(c1.TimeLockDelta),
 			MinHtlc:            int64(c1.MinHTLC),
+			MaxHtlcMAtoms:      uint64(c1.MaxHTLC),
 			FeeBaseMAtoms:      int64(c1.FeeBaseMAtoms),
 			FeeRateMilliMAtoms: int64(c1.FeeProportionalMillionths),
 			Disabled:           c1.ChannelFlags&lnwire.ChanUpdateDisabled != 0,
@@ -4085,6 +4086,7 @@ func marshalDbEdge(edgeInfo *channeldb.ChannelEdgeInfo,
 		edge.Node2Policy = &lnrpc.RoutingPolicy{
 			TimeLockDelta:      uint32(c2.TimeLockDelta),
 			MinHtlc:            int64(c2.MinHTLC),
+			MaxHtlcMAtoms:      uint64(c2.MaxHTLC),
 			FeeBaseMAtoms:      int64(c2.FeeBaseMAtoms),
 			FeeRateMilliMAtoms: int64(c2.FeeProportionalMillionths),
 			Disabled:           c2.ChannelFlags&lnwire.ChanUpdateDisabled != 0,
@@ -4633,7 +4635,7 @@ func marshallTopologyChange(topChange *routing.TopologyChange) *lnrpc.GraphTopol
 			RoutingPolicy: &lnrpc.RoutingPolicy{
 				TimeLockDelta:      uint32(channelUpdate.TimeLockDelta),
 				MinHtlc:            int64(channelUpdate.MinHTLC),
-				MaxHtlc:            uint64(channelUpdate.MaxHTLC),
+				MaxHtlcMAtoms:      uint64(channelUpdate.MaxHTLC),
 				FeeBaseMAtoms:      int64(channelUpdate.BaseFee),
 				FeeRateMilliMAtoms: int64(channelUpdate.FeeRate),
 				Disabled:           channelUpdate.Disabled,