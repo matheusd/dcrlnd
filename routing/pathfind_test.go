lightningnetwork/lnd

routing: update test edge policy fields to include max htlc + new flags
Since the MaxHTLC field was recently added to the ChannelEdgePolicy struct,
and the Flags field was broken into ChannelFlags and MessageFlags, the
test edge policies should be updated accordingly.

routing/pathfind_test.go
@@ -89,9 +89,11 @@ type testChan struct {
 	Node2        string `json:"node_2"`
 	ChannelID    uint64 `json:"channel_id"`
 	ChannelPoint string `json:"channel_point"`
-	Flags        uint16 `json:"flags"`
+	ChannelFlags uint8  `json:"channel_flags"`
+	MessageFlags uint8  `json:"message_flags"`
 	Expiry       uint16 `json:"expiry"`
 	MinHTLC      int64  `json:"min_htlc"`
+	MaxHTLC      int64  `json:"max_htlc"`
 	FeeBaseMsat  int64  `json:"fee_base_msat"`
 	FeeRate      int64  `json:"fee_rate"`
 	Capacity     int64  `json:"capacity"`
@@ -271,12 +273,13 @@ func parseTestGraph(path string) (*testGraphInstance, error) {
 
 		edgePolicy := &channeldb.ChannelEdgePolicy{
 			SigBytes:                  testSig.Serialize(),
-			MessageFlags:              lnwire.ChanUpdateMsgFlags(edge.Flags >> 8),
-			ChannelFlags:              lnwire.ChanUpdateChanFlags(edge.Flags),
+			MessageFlags:              lnwire.ChanUpdateMsgFlags(edge.MessageFlags),
+			ChannelFlags:              lnwire.ChanUpdateChanFlags(edge.ChannelFlags),
 			ChannelID:                 edge.ChannelID,
 			LastUpdate:                testTime,
 			TimeLockDelta:             edge.Expiry,
 			MinHTLC:                   lnwire.MilliSatoshi(edge.MinHTLC),
+			MaxHTLC:                   lnwire.MilliSatoshi(edge.MaxHTLC),
 			FeeBaseMSat:               lnwire.MilliSatoshi(edge.FeeBaseMsat),
 			FeeProportionalMillionths: lnwire.MilliSatoshi(edge.FeeRate),
 		}
@@ -295,6 +298,7 @@ func parseTestGraph(path string) (*testGraphInstance, error) {
 type testChannelPolicy struct {
 	Expiry      uint16
 	MinHTLC     lnwire.MilliSatoshi
+	MaxHTLC     lnwire.MilliSatoshi
 	FeeBaseMsat lnwire.MilliSatoshi
 	FeeRate     lnwire.MilliSatoshi
 }
@@ -304,12 +308,13 @@ type testChannelEnd struct {
 	testChannelPolicy
 }
 
-func defaultTestChannelEnd(alias string) *testChannelEnd {
+func defaultTestChannelEnd(alias string, capacity btcutil.Amount) *testChannelEnd {
 	return &testChannelEnd{
 		Alias: alias,
 		testChannelPolicy: testChannelPolicy{
 			Expiry:      144,
 			MinHTLC:     lnwire.MilliSatoshi(1000),
+			MaxHTLC:     lnwire.NewMSatFromSatoshis(capacity),
 			FeeBaseMsat: lnwire.MilliSatoshi(1000),
 			FeeRate:     lnwire.MilliSatoshi(1),
 		},
@@ -486,14 +491,19 @@ func createTestGraphFromChannels(testChannels []*testChannel) (*testGraphInstanc
 			return nil, err
 		}
 
+		var msgFlags lnwire.ChanUpdateMsgFlags
+		if testChannel.Node1.MaxHTLC != 0 {
+			msgFlags = 1
+		}
 		edgePolicy := &channeldb.ChannelEdgePolicy{
 			SigBytes:                  testSig.Serialize(),
-			MessageFlags:              0,
+			MessageFlags:              msgFlags,
 			ChannelFlags:              0,
 			ChannelID:                 channelID,
 			LastUpdate:                testTime,
 			TimeLockDelta:             testChannel.Node1.Expiry,
 			MinHTLC:                   testChannel.Node1.MinHTLC,
+			MaxHTLC:                   testChannel.Node1.MaxHTLC,
 			FeeBaseMSat:               testChannel.Node1.FeeBaseMsat,
 			FeeProportionalMillionths: testChannel.Node1.FeeRate,
 		}
@@ -501,14 +511,19 @@ func createTestGraphFromChannels(testChannels []*testChannel) (*testGraphInstanc
 			return nil, err
 		}
 
+		msgFlags = 0
+		if testChannel.Node2.MaxHTLC != 0 {
+			msgFlags = 1
+		}
 		edgePolicy = &channeldb.ChannelEdgePolicy{
 			SigBytes:                  testSig.Serialize(),
-			MessageFlags:              0,
+			MessageFlags:              msgFlags,
 			ChannelFlags:              lnwire.ChanUpdateDirection,
 			ChannelID:                 channelID,
 			LastUpdate:                testTime,
 			TimeLockDelta:             testChannel.Node2.Expiry,
 			MinHTLC:                   testChannel.Node2.MinHTLC,
+			MaxHTLC:                   testChannel.Node2.MaxHTLC,
 			FeeBaseMSat:               testChannel.Node2.FeeBaseMsat,
 			FeeProportionalMillionths: testChannel.Node2.FeeRate,
 		}
@@ -543,26 +558,31 @@ func TestFindLowestFeePath(t *testing.T) {
 			Expiry:  144,
 			FeeRate: 400,
 			MinHTLC: 1,
+			MaxHTLC: 100000000,
 		}),
 		symmetricTestChannel("first", "a", 100000, &testChannelPolicy{
 			Expiry:  144,
 			FeeRate: 400,
 			MinHTLC: 1,
+			MaxHTLC: 100000000,
 		}),
 		symmetricTestChannel("a", "target", 100000, &testChannelPolicy{
 			Expiry:  144,
 			FeeRate: 400,
 			MinHTLC: 1,
+			MaxHTLC: 100000000,
 		}),
 		symmetricTestChannel("first", "b", 100000, &testChannelPolicy{
 			Expiry:  144,
 			FeeRate: 100,
 			MinHTLC: 1,
+			MaxHTLC: 100000000,
 		}),
 		symmetricTestChannel("b", "target", 100000, &testChannelPolicy{
 			Expiry:  144,
 			FeeRate: 600,
 			MinHTLC: 1,
+			MaxHTLC: 100000000,
 		}),
 	}
 

routing/router_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcutil"
 	"github.com/davecgh/go-spew/spew"
 
 	sphinx "github.com/lightningnetwork/lightning-onion"
@@ -361,16 +362,19 @@ func TestChannelUpdateValidation(t *testing.T) {
 	t.Parallel()
 
 	// Setup a three node network.
+	chanCapSat := btcutil.Amount(100000)
 	testChannels := []*testChannel{
-		symmetricTestChannel("a", "b", 100000, &testChannelPolicy{
+		symmetricTestChannel("a", "b", chanCapSat, &testChannelPolicy{
 			Expiry:  144,
 			FeeRate: 400,
 			MinHTLC: 1,
+			MaxHTLC: lnwire.NewMSatFromSatoshis(chanCapSat),
 		}, 1),
-		symmetricTestChannel("b", "c", 100000, &testChannelPolicy{
+		symmetricTestChannel("b", "c", chanCapSat, &testChannelPolicy{
 			Expiry:  144,
 			FeeRate: 400,
 			MinHTLC: 1,
+			MaxHTLC: lnwire.NewMSatFromSatoshis(chanCapSat),
 		}, 2),
 	}
 
@@ -542,20 +546,21 @@ func TestSendPaymentErrorRepeatedFeeInsufficient(t *testing.T) {
 	// son goku. We'll obtain this as we'll need to to generate the
 	// FeeInsufficient error that we'll send back.
 	chanID := uint64(12345)
-	_, _, edgeUpateToFail, err := ctx.graph.FetchChannelEdgesByID(chanID)
+	_, _, edgeUpdateToFail, err := ctx.graph.FetchChannelEdgesByID(chanID)
 	if err != nil {
 		t.Fatalf("unable to fetch chan id: %v", err)
 	}
 
 	errChanUpdate := lnwire.ChannelUpdate{
 		ShortChannelID:  lnwire.NewShortChanIDFromInt(chanID),
-		Timestamp:       uint32(edgeUpateToFail.LastUpdate.Unix()),
-		MessageFlags:    edgeUpateToFail.MessageFlags,
-		ChannelFlags:    edgeUpateToFail.ChannelFlags,
-		TimeLockDelta:   edgeUpateToFail.TimeLockDelta,
-		HtlcMinimumMsat: edgeUpateToFail.MinHTLC,
-		BaseFee:         uint32(edgeUpateToFail.FeeBaseMSat),
-		FeeRate:         uint32(edgeUpateToFail.FeeProportionalMillionths),
+		Timestamp:       uint32(edgeUpdateToFail.LastUpdate.Unix()),
+		MessageFlags:    edgeUpdateToFail.MessageFlags,
+		ChannelFlags:    edgeUpdateToFail.ChannelFlags,
+		TimeLockDelta:   edgeUpdateToFail.TimeLockDelta,
+		HtlcMinimumMsat: edgeUpdateToFail.MinHTLC,
+		HtlcMaximumMsat: edgeUpdateToFail.MaxHTLC,
+		BaseFee:         uint32(edgeUpdateToFail.FeeBaseMSat),
+		FeeRate:         uint32(edgeUpdateToFail.FeeProportionalMillionths),
 	}
 
 	// The error will be returned by Son Goku.
@@ -649,20 +654,21 @@ func TestSendPaymentErrorNonFinalTimeLockErrors(t *testing.T) {
 	// block height is.
 	chanID := uint64(12345)
 	roasbeefSongoku := lnwire.NewShortChanIDFromInt(chanID)
-	_, _, edgeUpateToFail, err := ctx.graph.FetchChannelEdgesByID(chanID)
+	_, _, edgeUpdateToFail, err := ctx.graph.FetchChannelEdgesByID(chanID)
 	if err != nil {
 		t.Fatalf("unable to fetch chan id: %v", err)
 	}
 
 	errChanUpdate := lnwire.ChannelUpdate{
 		ShortChannelID:  lnwire.NewShortChanIDFromInt(chanID),
-		Timestamp:       uint32(edgeUpateToFail.LastUpdate.Unix()),
-		MessageFlags:    edgeUpateToFail.MessageFlags,
-		ChannelFlags:    edgeUpateToFail.ChannelFlags,
-		TimeLockDelta:   edgeUpateToFail.TimeLockDelta,
-		HtlcMinimumMsat: edgeUpateToFail.MinHTLC,
-		BaseFee:         uint32(edgeUpateToFail.FeeBaseMSat),
-		FeeRate:         uint32(edgeUpateToFail.FeeProportionalMillionths),
+		Timestamp:       uint32(edgeUpdateToFail.LastUpdate.Unix()),
+		MessageFlags:    edgeUpdateToFail.MessageFlags,
+		ChannelFlags:    edgeUpdateToFail.ChannelFlags,
+		TimeLockDelta:   edgeUpdateToFail.TimeLockDelta,
+		HtlcMinimumMsat: edgeUpdateToFail.MinHTLC,
+		HtlcMaximumMsat: edgeUpdateToFail.MaxHTLC,
+		BaseFee:         uint32(edgeUpdateToFail.FeeBaseMSat),
+		FeeRate:         uint32(edgeUpdateToFail.FeeProportionalMillionths),
 	}
 
 	// The error will be returned by Son Goku.

matheusd/dcrlnd

routing/pathfind_test.go
@@ -89,9 +89,11 @@ type testChan struct {
 	Node2         string `json:"node_2"`
 	ChannelID     uint64 `json:"channel_id"`
 	ChannelPoint  string `json:"channel_point"`
-	Flags         uint16 `json:"flags"`
+	ChannelFlags  uint8  `json:"channel_flags"`
+	MessageFlags  uint8  `json:"message_flags"`
 	Expiry        uint16 `json:"expiry"`
 	MinHTLC       int64  `json:"min_htlc"`
+	MaxHTLC       int64  `json:"max_htlc"`
 	FeeBaseMAtoms int64  `json:"fee_base_m_atoms"`
 	FeeRate       int64  `json:"fee_rate"`
 	Capacity      int64  `json:"capacity"`
@@ -271,12 +273,13 @@ func parseTestGraph(path string) (*testGraphInstance, error) {
 
 		edgePolicy := &channeldb.ChannelEdgePolicy{
 			SigBytes:                  testSig.Serialize(),
-			MessageFlags:              lnwire.ChanUpdateMsgFlags(edge.Flags >> 8),
-			ChannelFlags:              lnwire.ChanUpdateChanFlags(edge.Flags),
+			MessageFlags:              lnwire.ChanUpdateMsgFlags(edge.MessageFlags),
+			ChannelFlags:              lnwire.ChanUpdateChanFlags(edge.ChannelFlags),
 			ChannelID:                 edge.ChannelID,
 			LastUpdate:                testTime,
 			TimeLockDelta:             edge.Expiry,
 			MinHTLC:                   lnwire.MilliAtom(edge.MinHTLC),
+			MaxHTLC:                   lnwire.MilliAtom(edge.MaxHTLC),
 			FeeBaseMAtoms:             lnwire.MilliAtom(edge.FeeBaseMAtoms),
 			FeeProportionalMillionths: lnwire.MilliAtom(edge.FeeRate),
 		}
@@ -295,6 +298,7 @@ func parseTestGraph(path string) (*testGraphInstance, error) {
 type testChannelPolicy struct {
 	Expiry        uint16
 	MinHTLC       lnwire.MilliAtom
+	MaxHTLC       lnwire.MilliAtom
 	FeeBaseMAtoms lnwire.MilliAtom
 	FeeRate       lnwire.MilliAtom
 }
@@ -304,12 +308,13 @@ type testChannelEnd struct {
 	testChannelPolicy
 }
 
-func defaultTestChannelEnd(alias string) *testChannelEnd {
+func defaultTestChannelEnd(alias string, capacity dcrutil.Amount) *testChannelEnd {
 	return &testChannelEnd{
 		Alias: alias,
 		testChannelPolicy: testChannelPolicy{
 			Expiry:        144,
 			MinHTLC:       lnwire.MilliAtom(1000),
+			MaxHTLC:       lnwire.NewMAtomsFromAtoms(capacity),
 			FeeBaseMAtoms: lnwire.MilliAtom(1000),
 			FeeRate:       lnwire.MilliAtom(1),
 		},
@@ -484,14 +489,19 @@ func createTestGraphFromChannels(testChannels []*testChannel) (*testGraphInstanc
 			return nil, err
 		}
 
+		var msgFlags lnwire.ChanUpdateMsgFlags
+		if testChannel.Node1.MaxHTLC != 0 {
+			msgFlags = 1
+		}
 		edgePolicy := &channeldb.ChannelEdgePolicy{
 			SigBytes:                  testSig.Serialize(),
-			MessageFlags:              0,
+			MessageFlags:              msgFlags,
 			ChannelFlags:              0,
 			ChannelID:                 channelID,
 			LastUpdate:                testTime,
 			TimeLockDelta:             testChannel.Node1.Expiry,
 			MinHTLC:                   testChannel.Node1.MinHTLC,
+			MaxHTLC:                   testChannel.Node1.MaxHTLC,
 			FeeBaseMAtoms:             testChannel.Node1.FeeBaseMAtoms,
 			FeeProportionalMillionths: testChannel.Node1.FeeRate,
 		}
@@ -499,14 +509,19 @@ func createTestGraphFromChannels(testChannels []*testChannel) (*testGraphInstanc
 			return nil, err
 		}
 
+		msgFlags = 0
+		if testChannel.Node2.MaxHTLC != 0 {
+			msgFlags = 1
+		}
 		edgePolicy = &channeldb.ChannelEdgePolicy{
 			SigBytes:                  testSig.Serialize(),
-			MessageFlags:              0,
+			MessageFlags:              msgFlags,
 			ChannelFlags:              lnwire.ChanUpdateDirection,
 			ChannelID:                 channelID,
 			LastUpdate:                testTime,
 			TimeLockDelta:             testChannel.Node2.Expiry,
 			MinHTLC:                   testChannel.Node2.MinHTLC,
+			MaxHTLC:                   testChannel.Node2.MaxHTLC,
 			FeeBaseMAtoms:             testChannel.Node2.FeeBaseMAtoms,
 			FeeProportionalMillionths: testChannel.Node2.FeeRate,
 		}
@@ -539,26 +554,31 @@ func TestFindLowestFeePath(t *testing.T) {
 			Expiry:  144,
 			FeeRate: 400,
 			MinHTLC: 1,
+			MaxHTLC: 100000000,
 		}),
 		symmetricTestChannel("first", "a", 100000, &testChannelPolicy{
 			Expiry:  144,
 			FeeRate: 400,
 			MinHTLC: 1,
+			MaxHTLC: 100000000,
 		}),
 		symmetricTestChannel("a", "target", 100000, &testChannelPolicy{
 			Expiry:  144,
 			FeeRate: 400,
 			MinHTLC: 1,
+			MaxHTLC: 100000000,
 		}),
 		symmetricTestChannel("first", "b", 100000, &testChannelPolicy{
 			Expiry:  144,
 			FeeRate: 100,
 			MinHTLC: 1,
+			MaxHTLC: 100000000,
 		}),
 		symmetricTestChannel("b", "target", 100000, &testChannelPolicy{
 			Expiry:  144,
 			FeeRate: 600,
 			MinHTLC: 1,
+			MaxHTLC: 100000000,
 		}),
 	}
 

routing/router_test.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrec/secp256k1"
+	"github.com/decred/dcrd/dcrutil"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/htlcswitch"
@@ -362,16 +363,19 @@ func TestChannelUpdateValidation(t *testing.T) {
 	t.Parallel()
 
 	// Setup a three node network.
+	chanCapSat := dcrutil.Amount(100000)
 	testChannels := []*testChannel{
-		symmetricTestChannel("a", "b", 100000, &testChannelPolicy{
+		symmetricTestChannel("a", "b", chanCapSat, &testChannelPolicy{
 			Expiry:  144,
 			FeeRate: 400,
 			MinHTLC: 1,
+			MaxHTLC: lnwire.NewMAtomsFromAtoms(chanCapSat),
 		}, 1),
-		symmetricTestChannel("b", "c", 100000, &testChannelPolicy{
+		symmetricTestChannel("b", "c", chanCapSat, &testChannelPolicy{
 			Expiry:  144,
 			FeeRate: 400,
 			MinHTLC: 1,
+			MaxHTLC: lnwire.NewMAtomsFromAtoms(chanCapSat),
 		}, 2),
 	}
 
@@ -543,20 +547,21 @@ func TestSendPaymentErrorRepeatedFeeInsufficient(t *testing.T) {
 	// son goku. We'll obtain this as we'll need to to generate the
 	// FeeInsufficient error that we'll send back.
 	chanID := uint64(12345)
-	_, _, edgeUpateToFail, err := ctx.graph.FetchChannelEdgesByID(chanID)
+	_, _, edgeUpdateToFail, err := ctx.graph.FetchChannelEdgesByID(chanID)
 	if err != nil {
 		t.Fatalf("unable to fetch chan id: %v", err)
 	}
 
 	errChanUpdate := lnwire.ChannelUpdate{
 		ShortChannelID:    lnwire.NewShortChanIDFromInt(chanID),
-		Timestamp:         uint32(edgeUpateToFail.LastUpdate.Unix()),
-		MessageFlags:      edgeUpateToFail.MessageFlags,
-		ChannelFlags:      edgeUpateToFail.ChannelFlags,
-		TimeLockDelta:     edgeUpateToFail.TimeLockDelta,
-		HtlcMinimumMAtoms: edgeUpateToFail.MinHTLC,
-		BaseFee:           uint32(edgeUpateToFail.FeeBaseMAtoms),
-		FeeRate:           uint32(edgeUpateToFail.FeeProportionalMillionths),
+		Timestamp:         uint32(edgeUpdateToFail.LastUpdate.Unix()),
+		MessageFlags:      edgeUpdateToFail.MessageFlags,
+		ChannelFlags:      edgeUpdateToFail.ChannelFlags,
+		TimeLockDelta:     edgeUpdateToFail.TimeLockDelta,
+		HtlcMinimumMAtoms: edgeUpdateToFail.MinHTLC,
+		HtlcMaximumMAtoms: edgeUpdateToFail.MaxHTLC,
+		BaseFee:           uint32(edgeUpdateToFail.FeeBaseMAtoms),
+		FeeRate:           uint32(edgeUpdateToFail.FeeProportionalMillionths),
 	}
 
 	// The error will be returned by Son Goku.
@@ -650,20 +655,21 @@ func TestSendPaymentErrorNonFinalTimeLockErrors(t *testing.T) {
 	// block height is.
 	chanID := uint64(12345)
 	roasbeefSongoku := lnwire.NewShortChanIDFromInt(chanID)
-	_, _, edgeUpateToFail, err := ctx.graph.FetchChannelEdgesByID(chanID)
+	_, _, edgeUpdateToFail, err := ctx.graph.FetchChannelEdgesByID(chanID)
 	if err != nil {
 		t.Fatalf("unable to fetch chan id: %v", err)
 	}
 
 	errChanUpdate := lnwire.ChannelUpdate{
 		ShortChannelID:    lnwire.NewShortChanIDFromInt(chanID),
-		Timestamp:         uint32(edgeUpateToFail.LastUpdate.Unix()),
-		MessageFlags:      edgeUpateToFail.MessageFlags,
-		ChannelFlags:      edgeUpateToFail.ChannelFlags,
-		TimeLockDelta:     edgeUpateToFail.TimeLockDelta,
-		HtlcMinimumMAtoms: edgeUpateToFail.MinHTLC,
-		BaseFee:           uint32(edgeUpateToFail.FeeBaseMAtoms),
-		FeeRate:           uint32(edgeUpateToFail.FeeProportionalMillionths),
+		Timestamp:         uint32(edgeUpdateToFail.LastUpdate.Unix()),
+		MessageFlags:      edgeUpdateToFail.MessageFlags,
+		ChannelFlags:      edgeUpdateToFail.ChannelFlags,
+		TimeLockDelta:     edgeUpdateToFail.TimeLockDelta,
+		HtlcMinimumMAtoms: edgeUpdateToFail.MinHTLC,
+		HtlcMaximumMAtoms: edgeUpdateToFail.MaxHTLC,
+		BaseFee:           uint32(edgeUpdateToFail.FeeBaseMAtoms),
+		FeeRate:           uint32(edgeUpdateToFail.FeeProportionalMillionths),
 	}
 
 	// The error will be returned by Son Goku.