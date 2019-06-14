d5215defc3b4088420456da6b80f55596e673c82
Olaoluwa Osuntokun <laolu32@gmail.com>
2019-06-14 13:53:54 -0700

    build: add additional sub-servers to release.sh

--- release.sh
+++ release.sh
@@ -63,8 +63,8 @@ for i in $SYS; do
     cd $PACKAGE-$i-$TAG
 
     echo "Building:" $OS $ARCH $ARM
-    env GOOS=$OS GOARCH=$ARCH GOARM=$ARM go build -v -ldflags "$COMMITFLAGS" -tags="signrpc walletrpc chainrpc invoicesrpc" github.com/decred/cmd/dcrlnd
-    env GOOS=$OS GOARCH=$ARCH GOARM=$ARM go build -v -ldflags "$COMMITFLAGS" -tags="invoicesrpc" github.com/decred/dcrlnd/cmd/dcrlncli
+    env GOOS=$OS GOARCH=$ARCH GOARM=$ARM go build -v -ldflags "$COMMITFLAGS" -tags="autopilot signrpc walletrpc chainrpc invoicesrpc routerrpc" github.com/decred/dcrlnd/cmd/dcrlnd
+    env GOOS=$OS GOARCH=$ARCH GOARM=$ARM go build -v -ldflags "$COMMITFLAGS" -tags="autopilotrpc invoicesrpc walletrpc routerrpc" github.com/decred/dcrlnd/cmd/dcrlncli
     cd ..
 
     if [[ $OS = "windows" ]]; then
