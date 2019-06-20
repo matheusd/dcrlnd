77b2a025fd545ffefc3ce74903f999932fb5f8be
Conner Fromknecht <conner@lightning.engineering>
2019-06-20 16:54:45 -0700

    lnd+rpcserver: thread watchtower into subserver configs

--- lnd.go
+++ lnd.go
@@ -421,7 +421,7 @@ func Main() error {
 	rpcServer, err := newRPCServer(
 		server, macaroonService, cfg.SubRPCServers, serverOpts,
 		restDialOpts, restProxyDest, atplManager, server.invoices,
-		tlsCfg,
+		tower, tlsCfg,
 	)
 	if err != nil {
 		srvrLog.Errorf("unable to start RPC server: %v", err)
