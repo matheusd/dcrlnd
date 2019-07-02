lightningnetwork/lnd

cnct: add invoice registry interface
Create an interface type to be able to mock the registry in unit tests.

contractcourt/interfaces.go
@@ -1,0 +1,28 @@
+package contractcourt
+
+import (
+	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/invoices"
+	"github.com/lightningnetwork/lnd/lntypes"
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+// Registry is an interface which represents the invoice registry.
+type Registry interface {
+	// LookupInvoice attempts to look up an invoice according to its 32
+	// byte payment hash. This method should also reutrn the min final CLTV
+	// delta for this invoice. We'll use this to ensure that the HTLC
+	// extended to us gives us enough time to settle as we prescribe.
+	LookupInvoice(lntypes.Hash) (channeldb.Invoice, uint32, error)
+
+	// NotifyExitHopHtlc attempts to mark an invoice as settled. If the
+	// invoice is a debug invoice, then this method is a noop as debug
+	// invoices are never fully settled. The return value describes how the
+	// htlc should be resolved. If the htlc cannot be resolved immediately,
+	// the resolution is sent on the passed in hodlChan later.
+	NotifyExitHopHtlc(payHash lntypes.Hash, paidAmount lnwire.MilliSatoshi,
+		hodlChan chan<- interface{}) (*invoices.HodlEvent, error)
+
+	// HodlUnsubscribeAll unsubscribes from all hodl events.
+	HodlUnsubscribeAll(subscriber chan<- interface{})
+}

matheusd/dcrlnd

contractcourt/interfaces.go
@@ -1,0 +1,28 @@
+package contractcourt
+
+import (
+	"github.com/decred/dcrlnd/channeldb"
+	"github.com/decred/dcrlnd/invoices"
+	"github.com/decred/dcrlnd/lntypes"
+	"github.com/decred/dcrlnd/lnwire"
+)
+
+// Registry is an interface which represents the invoice registry.
+type Registry interface {
+	// LookupInvoice attempts to look up an invoice according to its 32
+	// byte payment hash. This method should also reutrn the min final CLTV
+	// delta for this invoice. We'll use this to ensure that the HTLC
+	// extended to us gives us enough time to settle as we prescribe.
+	LookupInvoice(lntypes.Hash) (channeldb.Invoice, uint32, error)
+
+	// NotifyExitHopHtlc attempts to mark an invoice as settled. If the
+	// invoice is a debug invoice, then this method is a noop as debug
+	// invoices are never fully settled. The return value describes how the
+	// htlc should be resolved. If the htlc cannot be resolved immediately,
+	// the resolution is sent on the passed in hodlChan later.
+	NotifyExitHopHtlc(payHash lntypes.Hash, paidAmount lnwire.MilliAtom,
+		hodlChan chan<- interface{}) (*invoices.HodlEvent, error)
+
+	// HodlUnsubscribeAll unsubscribes from all hodl events.
+	HodlUnsubscribeAll(subscriber chan<- interface{})
+}