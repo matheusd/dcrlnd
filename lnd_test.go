mandelmonkey/lnd

lntest: define BackendCfg and btcd impl
BackendCfg is an interface that can be backed by different Bitcoin node
implementations. We currently use the btcdHarness as our chain backend.

lnd_test.go
@@ -12970,9 +12970,14 @@ func TestLightningNetworkDaemon(t *testing.T) {
 		}
 	}()
 
+	chainBackend := lntest.BtcdBackendConfig{
+		RPCConfig:  btcdHarness.RPCConfig(),
+		P2PAddress: btcdHarness.P2PAddress(),
+	}
+
 	// First create the network harness to gain access to its
 	// 'OnTxAccepted' call back.
-	lndHarness, err = lntest.NewNetworkHarness(btcdHarness)
+	lndHarness, err = lntest.NewNetworkHarness(btcdHarness, chainBackend)
 	if err != nil {
 		ht.Fatalf("unable to create lightning network harness: %v", err)
 	}

matheusd/dcrlnd

lnd_test.go
@@ -14800,9 +14800,14 @@ func TestLightningNetworkDaemon(t *testing.T) {
 		}
 	}()
 
+	chainBackend := lntest.DcrdBackendConfig{
+		RPCConfig: dcrdHarness.RPCConfig(),
+		// P2PAddress: dcrdHarness.P2PAddress(),
+	}
+
 	// First create the network harness to gain access to its
 	// 'OnTxAccepted' call back.
-	lndHarness, err = lntest.NewNetworkHarness(dcrdHarness)
+	lndHarness, err = lntest.NewNetworkHarness(dcrdHarness, chainBackend)
 	if err != nil {
 		ht.Fatalf("unable to create lightning network harness: %v", err)
 	}