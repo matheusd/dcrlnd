lightningnetwork/lnd

channeldb/codec: add privateKey serialization

channeldb/codec.go
@@ -148,6 +148,12 @@ func WriteElement(w io.Writer, element interface{}) error {
 			return err
 		}
 
+	case *btcec.PrivateKey:
+		b := e.Serialize()
+		if _, err := w.Write(b); err != nil {
+			return err
+		}
+
 	case *btcec.PublicKey:
 		b := e.SerializeCompressed()
 		if _, err := w.Write(b); err != nil {
@@ -319,6 +325,15 @@ func ReadElement(r io.Reader, element interface{}) error {
 		}
 
 		*e = lnwire.MilliSatoshi(a)
+
+	case **btcec.PrivateKey:
+		var b [btcec.PrivKeyBytesLen]byte
+		if _, err := io.ReadFull(r, b[:]); err != nil {
+			return err
+		}
+
+		priv, _ := btcec.PrivKeyFromBytes(btcec.S256(), b[:])
+		*e = priv
 
 	case **btcec.PublicKey:
 		var b [btcec.PubKeyBytesLenCompressed]byte

matheusd/dcrlnd

channeldb/codec.go
@@ -155,6 +155,12 @@ func WriteElement(w io.Writer, element interface{}) error {
 			return err
 		}
 
+	case *secp256k1.PrivateKey:
+		b := e.Serialize()
+		if _, err := w.Write(b); err != nil {
+			return err
+		}
+
 	case *secp256k1.PublicKey:
 		b := e.SerializeCompressed()
 		if _, err := w.Write(b); err != nil {
@@ -326,6 +332,15 @@ func ReadElement(r io.Reader, element interface{}) error {
 		}
 
 		*e = lnwire.MilliAtom(a)
+
+	case **secp256k1.PrivateKey:
+		var b [secp256k1.PrivKeyBytesLen]byte
+		if _, err := io.ReadFull(r, b[:]); err != nil {
+			return err
+		}
+
+		priv, _ := secp256k1.PrivKeyFromBytes(b[:])
+		*e = priv
 
 	case **secp256k1.PublicKey:
 		var b [secp256k1.PubKeyBytesLenCompressed]byte