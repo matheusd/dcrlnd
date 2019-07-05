joostjager/lnd

routing+routerrpc: expose mission control parameters in lnd config
This commit exposes the three main parameters that influence mission
control and path finding to the user as command line or config file
flags. It allows for fine-tuning for optimal results.

config.go
@@ -27,6 +27,7 @@ import (
 	"github.com/lightningnetwork/lnd/discovery"
 	"github.com/lightningnetwork/lnd/htlcswitch/hodl"
 	"github.com/lightningnetwork/lnd/lncfg"
+	"github.com/lightningnetwork/lnd/lnrpc/routerrpc"
 	"github.com/lightningnetwork/lnd/lnrpc/signrpc"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/routing"
@@ -365,7 +366,8 @@ func loadConfig() (*config, error) {
 		MinBackoff:         defaultMinBackoff,
 		MaxBackoff:         defaultMaxBackoff,
 		SubRPCServers: &subRPCServerConfigs{
-			SignRPC: &signrpc.Config{},
+			SignRPC:   &signrpc.Config{},
+			RouterRPC: routerrpc.DefaultConfig(),
 		},
 		Autopilot: &autoPilotConfig{
 			MaxChannels:    5,

lnrpc/routerrpc/config_active.go
@@ -3,7 +3,12 @@
 package routerrpc
 
 import (
+	"time"
+
+	"github.com/lightningnetwork/lnd/lnwire"
+
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd/macaroons"
 	"github.com/lightningnetwork/lnd/routing"
 )
@@ -18,6 +23,19 @@ type Config struct {
 	// then we assume that the macaroon will be found under the network
 	// directory, named DefaultRouterMacFilename.
 	RouterMacPath string `long:"routermacaroonpath" description:"Path to the router macaroon"`
+
+	// MinProbability is the minimum required route probability to attempt
+	// the payment.
+	MinProbability float64 `long:"minprobability" description:"Minimum required route probability to attempt the payment"`
+
+	// PenaltyHalfLife defines after how much time a penalized node or
+	// channel is back at 50% probability.
+	PenaltyHalfLife time.Duration `long:"penaltyhalflife" description:"Defines the duration after which a penalized node or channel is back at 50% probability"`
+
+	// AttemptCost is the virtual cost in path finding weight units of
+	// executing a payment attempt that fails. It is used to trade off
+	// potentially better routes against their probability of succeeding.
+	AttemptCost int64 `long:"attemptcost" description:"The (virtual) cost in sats of a failed payment attempt"`
 
 	// NetworkDir is the main network directory wherein the router rpc
 	// server will find the macaroon named DefaultRouterMacFilename.
@@ -44,4 +62,27 @@ type Config struct {
 	// RouterBackend contains shared logic between this sub server and the
 	// main rpc server.
 	RouterBackend *RouterBackend
+}
+
+// DefaultConfig defines the config defaults.
+func DefaultConfig() *Config {
+	return &Config{
+		MinProbability:  routing.DefaultMinProbability,
+		PenaltyHalfLife: routing.DefaultPenaltyHalfLife,
+		AttemptCost: int64(
+			routing.DefaultPaymentAttemptPenalty.ToSatoshis(),
+		),
+	}
+}
+
+// GetMissionControlConfig returns the mission control config based on this sub
+// server config.
+func GetMissionControlConfig(cfg *Config) *routing.MissionControlConfig {
+	return &routing.MissionControlConfig{
+		MinProbability: cfg.MinProbability,
+		PaymentAttemptPenalty: lnwire.NewMSatFromSatoshis(
+			btcutil.Amount(cfg.AttemptCost),
+		),
+		PenaltyHalfLife: cfg.PenaltyHalfLife,
+	}
 }

routing/missioncontrol.go
@@ -14,10 +14,10 @@ import (
 )
 
 const (
-	// defaultPenaltyHalfLife is the default half-life duration. The
+	// DefaultPenaltyHalfLife is the default half-life duration. The
 	// half-life duration defines after how much time a penalized node or
 	// channel is back at 50% probability.
-	defaultPenaltyHalfLife = time.Hour
+	DefaultPenaltyHalfLife = time.Hour
 )
 
 // MissionControl contains state which summarizes the past attempts of HTLC
@@ -42,9 +42,7 @@ type MissionControl struct {
 	// external function to enable deterministic unit tests.
 	now func() time.Time
 
-	// penaltyHalfLife defines after how much time a penalized node or
-	// channel is back at 50% probability.
-	penaltyHalfLife time.Duration
+	cfg *MissionControlConfig
 
 	sync.Mutex
 
@@ -52,6 +50,24 @@ type MissionControl struct {
 	// add to another generation
 
 	// TODO(roasbeef): also add favorable metrics for nodes
+}
+
+// MissionControlConfig defines parameters that control mission control
+// behaviour.
+type MissionControlConfig struct {
+	// PenaltyHalfLife defines after how much time a penalized node or
+	// channel is back at 50% probability.
+	PenaltyHalfLife time.Duration
+
+	// PaymentAttemptPenalty is the virtual cost in path finding weight
+	// units of executing a payment attempt that fails. It is used to trade
+	// off potentially better routes against their probability of
+	// succeeding.
+	PaymentAttemptPenalty lnwire.MilliSatoshi
+
+	// MinProbability defines the minimum success probability of the
+	// returned route.
+	MinProbability float64
 }
 
 // nodeHistory contains a summary of payment attempt outcomes involving a
@@ -122,15 +138,22 @@ type MissionControlChannelSnapshot struct {
 //
 // TODO(roasbeef): persist memory
 func NewMissionControl(g *channeldb.ChannelGraph, selfNode *channeldb.LightningNode,
-	qb func(*channeldb.ChannelEdgeInfo) lnwire.MilliSatoshi) *MissionControl {
+	qb func(*channeldb.ChannelEdgeInfo) lnwire.MilliSatoshi,
+	cfg *MissionControlConfig) *MissionControl {
+
+	log.Debugf("Instantiating mission control with config: "+
+		"PenaltyHalfLife=%v, PaymentAttemptPenalty=%v, "+
+		"MinProbability=%v", cfg.PenaltyHalfLife,
+		int64(cfg.PaymentAttemptPenalty.ToSatoshis()),
+		cfg.MinProbability)
 
 	return &MissionControl{
-		history:         make(map[route.Vertex]*nodeHistory),
-		selfNode:        selfNode,
-		queryBandwidth:  qb,
-		graph:           g,
-		now:             time.Now,
-		penaltyHalfLife: defaultPenaltyHalfLife,
+		history:        make(map[route.Vertex]*nodeHistory),
+		selfNode:       selfNode,
+		queryBandwidth: qb,
+		graph:          g,
+		now:            time.Now,
+		cfg:            cfg,
 	}
 }
 
@@ -319,7 +342,7 @@ func (m *MissionControl) getEdgeProbabilityForNode(nodeHistory *nodeHistory,
 	// the probability down to zero when a failure occurs. From there it
 	// recovers asymptotically back to 1. The rate at which this happens is
 	// controlled by the penaltyHalfLife parameter.
-	exp := -timeSinceLastFailure.Hours() / m.penaltyHalfLife.Hours()
+	exp := -timeSinceLastFailure.Hours() / m.cfg.PenaltyHalfLife.Hours()
 	probability := 1 - math.Pow(2, exp)
 
 	return probability

routing/payment_session.go
@@ -138,8 +138,8 @@ func (p *paymentSession) RequestRoute(payment *LightningPayment,
 			FeeLimit:              payment.FeeLimit,
 			OutgoingChannelID:     payment.OutgoingChannelID,
 			CltvLimit:             cltvLimit,
-			PaymentAttemptPenalty: DefaultPaymentAttemptPenalty,
-			MinProbability:        DefaultMinProbability,
+			PaymentAttemptPenalty: p.mc.cfg.PaymentAttemptPenalty,
+			MinProbability:        p.mc.cfg.MinProbability,
 		},
 		p.mc.selfNode.PubKeyBytes, payment.Target,
 		payment.Amount,

server.go
@@ -17,6 +17,8 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/lightningnetwork/lnd/lnrpc/routerrpc"
+
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/connmgr"
@@ -632,8 +634,13 @@ func newServer(listenAddrs []net.Addr, chanDB *channeldb.DB, cc *chainControl,
 		return link.Bandwidth()
 	}
 
+	// Instantiate mission control with config from the sub server.
+	//
+	// TODO(joostjager): When we are further in the process of moving to sub
+	// servers, the mission control instance itself can be moved there too.
 	s.missionControl = routing.NewMissionControl(
 		chanGraph, selfNode, queryBandwidth,
+		routerrpc.GetMissionControlConfig(cfg.SubRPCServers.RouterRPC),
 	)
 
 	// The router will get access to the payment ID sequencer, such that it

matheusd/dcrlnd

config.go
@@ -25,6 +25,7 @@ import (
 	"github.com/decred/dcrlnd/discovery"
 	"github.com/decred/dcrlnd/htlcswitch/hodl"
 	"github.com/decred/dcrlnd/lncfg"
+	"github.com/decred/dcrlnd/lnrpc/routerrpc"
 	"github.com/decred/dcrlnd/lnrpc/signrpc"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/routing"
@@ -316,7 +317,8 @@ func loadConfig() (*config, error) {
 		MinBackoff:         defaultMinBackoff,
 		MaxBackoff:         defaultMaxBackoff,
 		SubRPCServers: &subRPCServerConfigs{
-			SignRPC: &signrpc.Config{},
+			SignRPC:   &signrpc.Config{},
+			RouterRPC: routerrpc.DefaultConfig(),
 		},
 		Autopilot: &autoPilotConfig{
 			MaxChannels:    5,

lnrpc/routerrpc/config_active.go
@@ -3,7 +3,12 @@
 package routerrpc
 
 import (
+	"time"
+
+	"github.com/decred/dcrlnd/lnwire"
+
 	"github.com/decred/dcrd/chaincfg"
+	"github.com/decred/dcrd/dcrutil"
 	"github.com/decred/dcrlnd/macaroons"
 	"github.com/decred/dcrlnd/routing"
 )
@@ -18,6 +23,23 @@ type Config struct {
 	// then we assume that the macaroon will be found under the network
 	// directory, named DefaultRouterMacFilename.
 	RouterMacPath string `long:"routermacaroonpath" description:"Path to the router macaroon"`
+
+	// MinProbability is the minimum required route success probability to
+	// attempt the payment.
+	MinRouteProbability float64 `long:"minrtprob" description:"Minimum required route success probability to attempt the payment"`
+
+	// AprioriHopProbability is the assumed success probability of a hop in
+	// a route when no other information is available.
+	AprioriHopProbability float64 `long:"apriorihopprob" description:"Assumed success probability of a hop in a route when no other information is available."`
+
+	// PenaltyHalfLife defines after how much time a penalized node or
+	// channel is back at 50% probability.
+	PenaltyHalfLife time.Duration `long:"penaltyhalflife" description:"Defines the duration after which a penalized node or channel is back at 50% probability"`
+
+	// AttemptCost is the virtual cost in path finding weight units of
+	// executing a payment attempt that fails. It is used to trade off
+	// potentially better routes against their probability of succeeding.
+	AttemptCost int64 `long:"attemptcost" description:"The (virtual) cost in sats of a failed payment attempt"`
 
 	// NetworkDir is the main network directory wherein the router rpc
 	// server will find the macaroon named DefaultRouterMacFilename.
@@ -44,4 +66,29 @@ type Config struct {
 	// RouterBackend contains shared logic between this sub server and the
 	// main rpc server.
 	RouterBackend *RouterBackend
+}
+
+// DefaultConfig defines the config defaults.
+func DefaultConfig() *Config {
+	return &Config{
+		AprioriHopProbability: routing.DefaultAprioriHopProbability,
+		MinRouteProbability:   routing.DefaultMinRouteProbability,
+		PenaltyHalfLife:       routing.DefaultPenaltyHalfLife,
+		AttemptCost: int64(
+			routing.DefaultPaymentAttemptPenalty.ToAtoms(),
+		),
+	}
+}
+
+// GetMissionControlConfig returns the mission control config based on this sub
+// server config.
+func GetMissionControlConfig(cfg *Config) *routing.MissionControlConfig {
+	return &routing.MissionControlConfig{
+		AprioriHopProbability: cfg.AprioriHopProbability,
+		MinRouteProbability:   cfg.MinRouteProbability,
+		PaymentAttemptPenalty: lnwire.NewMAtomsFromAtoms(
+			dcrutil.Amount(cfg.AttemptCost),
+		),
+		PenaltyHalfLife: cfg.PenaltyHalfLife,
+	}
 }

routing/missioncontrol.go
@@ -15,14 +15,10 @@ import (
 )
 
 const (
-	// defaultPenaltyHalfLife is the default half-life duration. The
+	// DefaultPenaltyHalfLife is the default half-life duration. The
 	// half-life duration defines after how much time a penalized node or
 	// channel is back at 50% probability.
-	defaultPenaltyHalfLife = time.Hour
-
-	// aprioriHopProbability is the assumed success probability of a hop in
-	// a route when no other information is available.
-	aprioriHopProbability = 1
+	DefaultPenaltyHalfLife = time.Hour
 )
 
 // MissionControl contains state which summarizes the past attempts of HTLC
@@ -47,9 +43,7 @@ type MissionControl struct {
 	// external function to enable deterministic unit tests.
 	now func() time.Time
 
-	// penaltyHalfLife defines after how much time a penalized node or
-	// channel is back at 50% probability.
-	penaltyHalfLife time.Duration
+	cfg *MissionControlConfig
 
 	sync.Mutex
 
@@ -62,6 +56,28 @@ type MissionControl struct {
 // A compile time assertion to ensure MissionControl meets the
 // PaymentSessionSource interface.
 var _ PaymentSessionSource = (*MissionControl)(nil)
+
+// MissionControlConfig defines parameters that control mission control
+// behaviour.
+type MissionControlConfig struct {
+	// PenaltyHalfLife defines after how much time a penalized node or
+	// channel is back at 50% probability.
+	PenaltyHalfLife time.Duration
+
+	// PaymentAttemptPenalty is the virtual cost in path finding weight
+	// units of executing a payment attempt that fails. It is used to trade
+	// off potentially better routes against their probability of
+	// succeeding.
+	PaymentAttemptPenalty lnwire.MilliAtom
+
+	// MinProbability defines the minimum success probability of the
+	// returned route.
+	MinRouteProbability float64
+
+	// AprioriHopProbability is the assumed success probability of a hop in
+	// a route when no other information is available.
+	AprioriHopProbability float64
+}
 
 // nodeHistory contains a summary of payment attempt outcomes involving a
 // particular node.
@@ -131,15 +147,23 @@ type MissionControlChannelSnapshot struct {
 //
 // TODO(roasbeef): persist memory
 func NewMissionControl(g *channeldb.ChannelGraph, selfNode *channeldb.LightningNode,
-	qb func(*channeldb.ChannelEdgeInfo) lnwire.MilliAtom) *MissionControl {
+	qb func(*channeldb.ChannelEdgeInfo) lnwire.MilliAtom,
+	cfg *MissionControlConfig) *MissionControl {
+
+	log.Debugf("Instantiating mission control with config: "+
+		"PenaltyHalfLife=%v, PaymentAttemptPenalty=%v, "+
+		"MinRouteProbability=%v, AprioriHopProbability=%v",
+		cfg.PenaltyHalfLife,
+		int64(cfg.PaymentAttemptPenalty.ToAtoms()),
+		cfg.MinRouteProbability, cfg.AprioriHopProbability)
 
 	return &MissionControl{
-		history:         make(map[route.Vertex]*nodeHistory),
-		selfNode:        selfNode,
-		queryBandwidth:  qb,
-		graph:           g,
-		now:             time.Now,
-		penaltyHalfLife: defaultPenaltyHalfLife,
+		history:        make(map[route.Vertex]*nodeHistory),
+		selfNode:       selfNode,
+		queryBandwidth: qb,
+		graph:          g,
+		now:            time.Now,
+		cfg:            cfg,
 	}
 }
 
@@ -303,7 +327,7 @@ func (m *MissionControl) getEdgeProbability(fromNode route.Vertex,
 	// adjust this probability.
 	nodeHistory, ok := m.history[fromNode]
 	if !ok {
-		return aprioriHopProbability
+		return m.cfg.AprioriHopProbability
 	}
 
 	return m.getEdgeProbabilityForNode(nodeHistory, edge.ChannelID, amt)
@@ -338,7 +362,7 @@ func (m *MissionControl) getEdgeProbabilityForNode(nodeHistory *nodeHistory,
 	}
 
 	if lastFailure == nil {
-		return aprioriHopProbability
+		return m.cfg.AprioriHopProbability
 	}
 
 	timeSinceLastFailure := m.now().Sub(*lastFailure)
@@ -347,8 +371,8 @@ func (m *MissionControl) getEdgeProbabilityForNode(nodeHistory *nodeHistory,
 	// the probability down to zero when a failure occurs. From there it
 	// recovers asymptotically back to the a priori probability. The rate at
 	// which this happens is controlled by the penaltyHalfLife parameter.
-	exp := -timeSinceLastFailure.Hours() / m.penaltyHalfLife.Hours()
-	probability := aprioriHopProbability * (1 - math.Pow(2, exp))
+	exp := -timeSinceLastFailure.Hours() / m.cfg.PenaltyHalfLife.Hours()
+	probability := m.cfg.AprioriHopProbability * (1 - math.Pow(2, exp))
 
 	return probability
 }

routing/payment_session.go
@@ -180,8 +180,8 @@ func (p *paymentSession) RequestRoute(payment *LightningPayment,
 			FeeLimit:              payment.FeeLimit,
 			OutgoingChannelID:     payment.OutgoingChannelID,
 			CltvLimit:             cltvLimit,
-			PaymentAttemptPenalty: DefaultPaymentAttemptPenalty,
-			MinProbability:        DefaultMinProbability,
+			PaymentAttemptPenalty: p.mc.cfg.PaymentAttemptPenalty,
+			MinProbability:        p.mc.cfg.MinRouteProbability,
 		},
 		p.mc.selfNode.PubKeyBytes, payment.Target,
 		payment.Amount,

server.go
@@ -35,6 +35,7 @@ import (
 	"github.com/decred/dcrlnd/lncfg"
 	"github.com/decred/dcrlnd/lnpeer"
 	"github.com/decred/dcrlnd/lnrpc"
+	"github.com/decred/dcrlnd/lnrpc/routerrpc"
 	"github.com/decred/dcrlnd/lnwallet"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/nat"
@@ -638,8 +639,13 @@ func newServer(listenAddrs []net.Addr, chanDB *channeldb.DB, cc *chainControl,
 		return link.Bandwidth()
 	}
 
+	// Instantiate mission control with config from the sub server.
+	//
+	// TODO(joostjager): When we are further in the process of moving to sub
+	// servers, the mission control instance itself can be moved there too.
 	s.missionControl = routing.NewMissionControl(
 		chanGraph, selfNode, queryBandwidth,
+		routerrpc.GetMissionControlConfig(cfg.SubRPCServers.RouterRPC),
 	)
 
 	s.chanRouter, err = routing.New(routing.Config{