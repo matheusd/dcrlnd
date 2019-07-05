lightningnetwork/lnd

lncfg: add Watchtower config

lncfg/watchtower.go
@@ -1,0 +1,13 @@
+package lncfg
+
+import "github.com/lightningnetwork/lnd/watchtower"
+
+// Watchtower holds the daemon specific configuration parameters for running a
+// watchtower that shares resources with the daemon.
+type Watchtower struct {
+	Active bool `long:"active" description:"If the watchtower should be active or not"`
+
+	TowerDir string `long:"towerdir" description:"Directory of the watchtower.db"`
+
+	watchtower.Conf
+}

matheusd/dcrlnd

lncfg/watchtower.go
@@ -1,0 +1,13 @@
+package lncfg
+
+import "github.com/decred/dcrlnd/watchtower"
+
+// Watchtower holds the daemon specific configuration parameters for running a
+// watchtower that shares resources with the daemon.
+type Watchtower struct {
+	Active bool `long:"active" description:"If the watchtower should be active or not"`
+
+	TowerDir string `long:"towerdir" description:"Directory of the watchtower.db"`
+
+	watchtower.Conf
+}