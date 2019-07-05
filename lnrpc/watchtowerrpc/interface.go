lightningnetwork/lnd

lnrpc/watchtowerrpc: add WatchtowerBackend configs

lnrpc/watchtowerrpc/interface.go
@@ -1,0 +1,23 @@
+package watchtowerrpc
+
+import (
+	"net"
+
+	"github.com/btcsuite/btcd/btcec"
+)
+
+// WatchtowerBackend abstracts access to the watchtower information that is
+// served via RPC connections.
+type WatchtowerBackend interface {
+	// PubKey returns the public key for the watchtower used to
+	// authentication and encrypt traffic with clients.
+	PubKey() *btcec.PublicKey
+
+	// ListeningAddrs returns the listening addresses where the watchtower
+	// server can accept client connections.
+	ListeningAddrs() []net.Addr
+
+	// ExternalIPs returns the addresses where the watchtower can be reached
+	// by clients externally.
+	ExternalIPs() []net.Addr
+}

matheusd/dcrlnd

lnrpc/watchtowerrpc/interface.go
@@ -1,0 +1,23 @@
+package watchtowerrpc
+
+import (
+	"net"
+
+	"github.com/decred/dcrd/dcrec/secp256k1"
+)
+
+// WatchtowerBackend abstracts access to the watchtower information that is
+// served via RPC connections.
+type WatchtowerBackend interface {
+	// PubKey returns the public key for the watchtower used to
+	// authentication and encrypt traffic with clients.
+	PubKey() *secp256k1.PublicKey
+
+	// ListeningAddrs returns the listening addresses where the watchtower
+	// server can accept client connections.
+	ListeningAddrs() []net.Addr
+
+	// ExternalIPs returns the addresses where the watchtower can be reached
+	// by clients externally.
+	ExternalIPs() []net.Addr
+}
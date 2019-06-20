5cc6af9ae135f1f51e1408c7fb516ca9112d0833
Conner Fromknecht <conner@lightning.engineering>
2019-06-20 16:53:25 -0700

    lnrpc/watchtowerrpc: add WatchtowerBackend configs

---
+++ lnrpc/watchtowerrpc/interface.go
@@ -0,0 +1,23 @@
+package watchtowerrpc
+
+import (
+	"net"
+
+	"github.com/decred/dcrd/dcrec/secp256k1"
+)
+
+// WatchtowerBackend abstracts access to the watchtower information that is
+// served via RPC connections.
+type WatchtowerBackend interface {
+	// PubKey returns the public key for the watchtower used to
+	// authentication and encrypt traffic with clients.
+	PubKey() *secp256k1.PublicKey
+
+	// ListeningAddrs returns the listening addresses where the watchtower
+	// server can accept client connections.
+	ListeningAddrs() []net.Addr
+
+	// ExternalIPs returns the addresses where the watchtower can be reached
+	// by clients externally.
+	ExternalIPs() []net.Addr
+}
