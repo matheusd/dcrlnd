68d7c021f004e0830345c6e0d11171c34c84bc47
Joost Jager <joost.jager@gmail.com>
2019-07-13 23:26:26 +0200

    routerrpc: embed routing config

--- server.go
+++ server.go
@@ -661,12 +661,14 @@ func newServer(listenAddrs []net.Addr, chanDB *channeldb.DB,
 
 	srvrLog.Debugf("Instantiating payment session source with config: "+
 		"PaymentAttemptPenalty=%v, MinRouteProbability=%v",
-		int64(routingConfig.PaymentAttemptPenalty.ToAtoms()),
+		int64(routingConfig.AttemptCost),
 		routingConfig.MinRouteProbability)
 
 	pathFindingConfig := routing.PathFindingConfig{
-		PaymentAttemptPenalty: routingConfig.PaymentAttemptPenalty,
-		MinProbability:        routingConfig.MinRouteProbability,
+		PaymentAttemptPenalty: lnwire.NewMAtomsFromAtoms(
+			routingConfig.AttemptCost,
+		),
+		MinProbability: routingConfig.MinRouteProbability,
 	}
 
 	paymentSessionSource := &routing.SessionSource{
