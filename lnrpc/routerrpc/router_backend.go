joostjager/lnd

lnrpc/routerrpc: create mission control interface

lnrpc/routerrpc/router_backend.go
@@ -40,7 +40,7 @@ type RouterBackend struct {
 		amt lnwire.MilliSatoshi, restrictions *routing.RestrictParams,
 		finalExpiry ...uint16) (*route.Route, error)
 
-	MissionControl *routing.MissionControl
+	MissionControl MissionControl
 
 	// ActiveNetParams are the network parameters of the primary network
 	// that the route is operating on. This is necessary so we can ensure
@@ -51,6 +51,22 @@ type RouterBackend struct {
 	// Tower is the ControlTower instance that is used to track pending
 	// payments.
 	Tower routing.ControlTower
+}
+
+// MissionControl defines the mission control dependencies of routerrpc.
+type MissionControl interface {
+	// GetEdgeProbability is expected to return the success probability of a payment
+	// from fromNode along edge.
+	GetEdgeProbability(fromNode route.Vertex,
+		edge routing.EdgeLocator, amt lnwire.MilliSatoshi) float64
+
+	// ResetHistory resets the history of MissionControl returning it to a state as
+	// if no payment attempts have been made.
+	ResetHistory()
+
+	// GetHistorySnapshot takes a snapshot from the current mission control state
+	// and actual probability estimates.
+	GetHistorySnapshot() *routing.MissionControlSnapshot
 }
 
 // QueryRoutes attempts to query the daemons' Channel Router for a possible

matheusd/dcrlnd

lnrpc/routerrpc/router_backend.go
@@ -43,7 +43,7 @@ type RouterBackend struct {
 		amt lnwire.MilliAtom, restrictions *routing.RestrictParams,
 		finalExpiry ...uint16) (*route.Route, error)
 
-	MissionControl *routing.MissionControl
+	MissionControl MissionControl
 
 	// ActiveNetParams are the network parameters of the primary network
 	// that the route is operating on. This is necessary so we can ensure
@@ -54,6 +54,22 @@ type RouterBackend struct {
 	// Tower is the ControlTower instance that is used to track pending
 	// payments.
 	Tower routing.ControlTower
+}
+
+// MissionControl defines the mission control dependencies of routerrpc.
+type MissionControl interface {
+	// GetEdgeProbability is expected to return the success probability of a payment
+	// from fromNode along edge.
+	GetEdgeProbability(fromNode route.Vertex,
+		edge routing.EdgeLocator, amt lnwire.MilliAtom) float64
+
+	// ResetHistory resets the history of MissionControl returning it to a state as
+	// if no payment attempts have been made.
+	ResetHistory()
+
+	// GetHistorySnapshot takes a snapshot from the current mission control state
+	// and actual probability estimates.
+	GetHistorySnapshot() *routing.MissionControlSnapshot
 }
 
 // QueryRoutes attempts to query the daemons' Channel Router for a possible