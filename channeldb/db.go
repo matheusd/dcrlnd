lightningnetwork/lnd

channeldb + wtdb: specify freelist bbolt options by default
This commit specifies two bbolt options when opening the underlying
channel and watchtower databases so that there is reduced heap
pressure in case the bbolt database has a lot of free pages in the
B+ tree.

channeldb/db.go
@@ -135,7 +135,14 @@ func Open(dbPath string, modifiers ...OptionModifier) (*DB, error) {
 		modifier(&opts)
 	}
 
-	bdb, err := bbolt.Open(path, dbFilePermission, nil)
+	// Specify bbolt freelist options to reduce heap pressure in case the
+	// freelist grows to be very large.
+	options := &bbolt.Options{
+		NoFreelistSync: true,
+		FreelistType:   bbolt.FreelistMapType,
+	}
+
+	bdb, err := bbolt.Open(path, dbFilePermission, options)
 	if err != nil {
 		return nil, err
 	}

watchtower/wtdb/db_common.go
@@ -63,7 +63,14 @@ func createDBIfNotExist(dbPath, name string) (*bbolt.DB, bool, error) {
 		}
 	}
 
-	bdb, err := bbolt.Open(path, dbFilePermission, nil)
+	// Specify bbolt freelist options to reduce heap pressure in case the
+	// freelist grows to be very large.
+	options := &bbolt.Options{
+		NoFreelistSync: true,
+		FreelistType:   bbolt.FreelistMapType,
+	}
+
+	bdb, err := bbolt.Open(path, dbFilePermission, options)
 	if err != nil {
 		return nil, false, err
 	}

matheusd/dcrlnd

channeldb/db.go
@@ -135,7 +135,14 @@ func Open(dbPath string, modifiers ...OptionModifier) (*DB, error) {
 		modifier(&opts)
 	}
 
-	bdb, err := bolt.Open(path, dbFilePermission, nil)
+	// Specify bbolt freelist options to reduce heap pressure in case the
+	// freelist grows to be very large.
+	options := &bolt.Options{
+		NoFreelistSync: true,
+		FreelistType:   bolt.FreelistMapType,
+	}
+
+	bdb, err := bolt.Open(path, dbFilePermission, options)
 	if err != nil {
 		return nil, err
 	}

watchtower/wtdb/db_common.go
@@ -63,7 +63,14 @@ func createDBIfNotExist(dbPath, name string) (*bolt.DB, bool, error) {
 		}
 	}
 
-	bdb, err := bolt.Open(path, dbFilePermission, nil)
+	// Specify bbolt freelist options to reduce heap pressure in case the
+	// freelist grows to be very large.
+	options := &bolt.Options{
+		NoFreelistSync: true,
+		FreelistType:   bolt.FreelistMapType,
+	}
+
+	bdb, err := bolt.Open(path, dbFilePermission, options)
 	if err != nil {
 		return nil, false, err
 	}