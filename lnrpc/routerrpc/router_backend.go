lightningnetwork/lnd

lnrpc+lnrpc/routerrpc: add a new TLV record field to the Hop proto
In this commit, we add a new field to the Hop proto to allow callers to
be able to specify TLV records for the SendToRoute call, and also to be
able to display TLV records that were used during regular path finding.

We also update SendPayment to support dest TLV records.

lnrpc/routerrpc/router_backend.go
@@ -14,6 +14,7 @@ import (
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/routing"
 	"github.com/lightningnetwork/lnd/routing/route"
+	"github.com/lightningnetwork/lnd/tlv"
 	"github.com/lightningnetwork/lnd/zpay32"
 	context "golang.org/x/net/context"
 )
@@ -40,6 +41,7 @@ type RouterBackend struct {
 	// routes.
 	FindRoute func(source, target route.Vertex,
 		amt lnwire.MilliSatoshi, restrictions *routing.RestrictParams,
+		destTlvRecords []tlv.Record,
 		finalExpiry ...uint16) (*route.Route, error)
 
 	MissionControl MissionControl
@@ -188,6 +190,15 @@ func (r *RouterBackend) QueryRoutes(ctx context.Context,
 				fromNode, toNode, amt,
 			)
 		},
+		DestPayloadTLV: len(in.DestTlv) != 0,
+	}
+
+	// If we have any TLV records destined for the final hop, then we'll
+	// attempt to decode them now into a form that the router can more
+	// easily manipulate.
+	destTlvRecords, err := tlv.MapToRecords(in.DestTlv)
+	if err != nil {
+		return nil, err
 	}
 
 	// Query the channel router for a possible path to the destination that
@@ -201,11 +212,12 @@ func (r *RouterBackend) QueryRoutes(ctx context.Context,
 	if in.FinalCltvDelta == 0 {
 		route, findErr = r.FindRoute(
 			sourcePubKey, targetPubKey, amtMSat, restrictions,
+			destTlvRecords,
 		)
 	} else {
 		route, findErr = r.FindRoute(
 			sourcePubKey, targetPubKey, amtMSat, restrictions,
-			uint16(in.FinalCltvDelta),
+			destTlvRecords, uint16(in.FinalCltvDelta),
 		)
 	}
 	if findErr != nil {
@@ -214,8 +226,10 @@ func (r *RouterBackend) QueryRoutes(ctx context.Context,
 
 	// For each valid route, we'll convert the result into the format
 	// required by the RPC system.
-
-	rpcRoute := r.MarshallRoute(route)
+	rpcRoute, err := r.MarshallRoute(route)
+	if err != nil {
+		return nil, err
+	}
 
 	routeResp := &lnrpc.QueryRoutesResponse{
 		Routes: []*lnrpc.Route{rpcRoute},
@@ -266,7 +280,7 @@ func calculateFeeLimit(feeLimit *lnrpc.FeeLimit,
 }
 
 // MarshallRoute marshalls an internal route to an rpc route struct.
-func (r *RouterBackend) MarshallRoute(route *route.Route) *lnrpc.Route {
+func (r *RouterBackend) MarshallRoute(route *route.Route) (*lnrpc.Route, error) {
 	resp := &lnrpc.Route{
 		TotalTimeLock: route.TotalTimeLock,
 		TotalFees:     int64(route.TotalFees().ToSatoshis()),
@@ -290,6 +304,11 @@ func (r *RouterBackend) MarshallRoute(route *route.Route) *lnrpc.Route {
 			chanCapacity = incomingAmt.ToSatoshis()
 		}
 
+		tlvMap, err := tlv.RecordsToMap(hop.TLVRecords)
+		if err != nil {
+			return nil, err
+		}
+
 		resp.Hops[i] = &lnrpc.Hop{
 			ChanId:           hop.ChannelID,
 			ChanCapacity:     int64(chanCapacity),
@@ -301,11 +320,13 @@ func (r *RouterBackend) MarshallRoute(route *route.Route) *lnrpc.Route {
 			PubKey: hex.EncodeToString(
 				hop.PubKeyBytes[:],
 			),
+			TlvRecords: tlvMap,
+			TlvPayload: !hop.LegacyPayload,
 		}
 		incomingAmt = hop.AmtToForward
 	}
 
-	return resp
+	return resp, nil
 }
 
 // UnmarshallHopByChannelLookup unmarshalls an rpc hop for which the pub key is
@@ -331,11 +352,18 @@ func (r *RouterBackend) UnmarshallHopByChannelLookup(hop *lnrpc.Hop,
 		return nil, fmt.Errorf("channel edge does not match expected node")
 	}
 
+	tlvRecords, err := tlv.MapToRecords(hop.TlvRecords)
+	if err != nil {
+		return nil, err
+	}
+
 	return &route.Hop{
 		OutgoingTimeLock: hop.Expiry,
 		AmtToForward:     lnwire.MilliSatoshi(hop.AmtToForwardMsat),
 		PubKeyBytes:      pubKeyBytes,
 		ChannelID:        hop.ChanId,
+		TLVRecords:       tlvRecords,
+		LegacyPayload:    !hop.TlvPayload,
 	}, nil
 }
 
@@ -351,11 +379,21 @@ func UnmarshallKnownPubkeyHop(hop *lnrpc.Hop) (*route.Hop, error) {
 	var pubKeyBytes [33]byte
 	copy(pubKeyBytes[:], pubKey)
 
+	var tlvRecords []tlv.Record
+	if hop.TlvRecords != nil {
+		tlvRecords, err = tlv.MapToRecords(hop.TlvRecords)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return &route.Hop{
 		OutgoingTimeLock: hop.Expiry,
 		AmtToForward:     lnwire.MilliSatoshi(hop.AmtToForwardMsat),
 		PubKeyBytes:      pubKeyBytes,
 		ChannelID:        hop.ChanId,
+		TLVRecords:       tlvRecords,
+		LegacyPayload:    !hop.TlvPayload,
 	}, nil
 }
 
@@ -433,6 +471,16 @@ func (r *RouterBackend) extractIntentFromSendRequest(
 	// Set payment attempt timeout.
 	if rpcPayReq.TimeoutSeconds == 0 {
 		return nil, errors.New("timeout_seconds must be specified")
+	}
+
+	if len(rpcPayReq.DestTlv) != 0 {
+		var err error
+		payIntent.FinalDestRecords, err = tlv.MapToRecords(
+			rpcPayReq.DestTlv,
+		)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	payIntent.PayAttemptTimeout = time.Second *

lnrpc/routerrpc/router_backend_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/routing"
 	"github.com/lightningnetwork/lnd/routing/route"
+	"github.com/lightningnetwork/lnd/tlv"
 
 	"github.com/lightningnetwork/lnd/lnrpc"
 )
@@ -77,6 +78,7 @@ func testQueryRoutes(t *testing.T, useMissionControl bool) {
 
 	findRoute := func(source, target route.Vertex,
 		amt lnwire.MilliSatoshi, restrictions *routing.RestrictParams,
+		_ []tlv.Record,
 		finalExpiry ...uint16) (*route.Route, error) {
 
 		if int64(amt) != request.Amt*1000 {

rpcserver.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/lightningnetwork/lnd/lnrpc/routerrpc"
 	"github.com/lightningnetwork/lnd/routing/route"
+	"github.com/lightningnetwork/lnd/tlv"
 	"github.com/lightningnetwork/lnd/watchtower"
 
 	"github.com/btcsuite/btcd/blockchain"
@@ -2926,6 +2927,8 @@ type rpcPaymentIntent struct {
 	outgoingChannelID *uint64
 	payReq            []byte
 
+	destTLV []tlv.Record
+
 	route *route.Route
 }
 
@@ -2966,6 +2969,16 @@ func extractPaymentIntent(rpcPayReq *rpcPaymentRequest) (rpcPaymentIntent, error
 	// Take cltv limit from request if set.
 	if rpcPayReq.CltvLimit != 0 {
 		payIntent.cltvLimit = &rpcPayReq.CltvLimit
+	}
+
+	if len(rpcPayReq.DestTlv) != 0 {
+		var err error
+		payIntent.destTLV, err = tlv.MapToRecords(
+			rpcPayReq.DestTlv,
+		)
+		if err != nil {
+			return payIntent, err
+		}
 	}
 
 	// If the payment request field isn't blank, then the details of the
@@ -3123,6 +3136,7 @@ func (r *rpcServer) dispatchPaymentIntent(
 			OutgoingChannelID: payIntent.outgoingChannelID,
 			PaymentRequest:    payIntent.payReq,
 			PayAttemptTimeout: routing.DefaultPayAttemptTimeout,
+			FinalDestRecords:  payIntent.destTLV,
 		}
 
 		preImage, route, routerErr = r.server.chanRouter.SendPayment(
@@ -3293,10 +3307,16 @@ func (r *rpcServer) sendPayment(stream *paymentStream) error {
 					return
 				}
 
-				marshalledRouted := r.routerBackend.
-					MarshallRoute(resp.Route)
+				backend := r.routerBackend
+				marshalledRouted, err := backend.MarshallRoute(
+					resp.Route,
+				)
+				if err != nil {
+					errChan <- err
+					return
+				}
 
-				err := stream.send(&lnrpc.SendResponse{
+				err = stream.send(&lnrpc.SendResponse{
 					PaymentHash:     payIntent.rHash[:],
 					PaymentPreimage: resp.Preimage[:],
 					PaymentRoute:    marshalledRouted,
@@ -3375,10 +3395,15 @@ func (r *rpcServer) sendPaymentSync(ctx context.Context,
 		}, nil
 	}
 
+	rpcRoute, err := r.routerBackend.MarshallRoute(resp.Route)
+	if err != nil {
+		return nil, err
+	}
+
 	return &lnrpc.SendResponse{
 		PaymentHash:     payIntent.rHash[:],
 		PaymentPreimage: resp.Preimage[:],
-		PaymentRoute:    r.routerBackend.MarshallRoute(resp.Route),
+		PaymentRoute:    rpcRoute,
 	}, nil
 }
 

matheusd/dcrlnd

lnrpc/routerrpc/router_backend.go
@@ -14,6 +14,7 @@ import (
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/routing"
 	"github.com/decred/dcrlnd/routing/route"
+	"github.com/decred/dcrlnd/tlv"
 	"github.com/decred/dcrlnd/zpay32"
 
 	context "golang.org/x/net/context"
@@ -41,6 +42,7 @@ type RouterBackend struct {
 	// routes.
 	FindRoute func(source, target route.Vertex,
 		amt lnwire.MilliAtom, restrictions *routing.RestrictParams,
+		destTlvRecords []tlv.Record,
 		finalExpiry ...uint16) (*route.Route, error)
 
 	MissionControl MissionControl
@@ -189,6 +191,15 @@ func (r *RouterBackend) QueryRoutes(ctx context.Context,
 				fromNode, toNode, amt,
 			)
 		},
+		DestPayloadTLV: len(in.DestTlv) != 0,
+	}
+
+	// If we have any TLV records destined for the final hop, then we'll
+	// attempt to decode them now into a form that the router can more
+	// easily manipulate.
+	destTlvRecords, err := tlv.MapToRecords(in.DestTlv)
+	if err != nil {
+		return nil, err
 	}
 
 	// Query the channel router for a possible path to the destination that
@@ -202,11 +213,12 @@ func (r *RouterBackend) QueryRoutes(ctx context.Context,
 	if in.FinalCltvDelta == 0 {
 		route, findErr = r.FindRoute(
 			sourcePubKey, targetPubKey, amtMSat, restrictions,
+			destTlvRecords,
 		)
 	} else {
 		route, findErr = r.FindRoute(
 			sourcePubKey, targetPubKey, amtMSat, restrictions,
-			uint16(in.FinalCltvDelta),
+			destTlvRecords, uint16(in.FinalCltvDelta),
 		)
 	}
 	if findErr != nil {
@@ -215,8 +227,10 @@ func (r *RouterBackend) QueryRoutes(ctx context.Context,
 
 	// For each valid route, we'll convert the result into the format
 	// required by the RPC system.
-
-	rpcRoute := r.MarshallRoute(route)
+	rpcRoute, err := r.MarshallRoute(route)
+	if err != nil {
+		return nil, err
+	}
 
 	routeResp := &lnrpc.QueryRoutesResponse{
 		Routes: []*lnrpc.Route{rpcRoute},
@@ -267,7 +281,7 @@ func calculateFeeLimit(feeLimit *lnrpc.FeeLimit,
 }
 
 // MarshallRoute marshalls an internal route to an rpc route struct.
-func (r *RouterBackend) MarshallRoute(route *route.Route) *lnrpc.Route {
+func (r *RouterBackend) MarshallRoute(route *route.Route) (*lnrpc.Route, error) {
 	resp := &lnrpc.Route{
 		TotalTimeLock:   route.TotalTimeLock,
 		TotalFees:       int64(route.TotalFees().ToAtoms()),
@@ -291,6 +305,11 @@ func (r *RouterBackend) MarshallRoute(route *route.Route) *lnrpc.Route {
 			chanCapacity = incomingAmt.ToAtoms()
 		}
 
+		tlvMap, err := tlv.RecordsToMap(hop.TLVRecords)
+		if err != nil {
+			return nil, err
+		}
+
 		resp.Hops[i] = &lnrpc.Hop{
 			ChanId:             hop.ChannelID,
 			ChanCapacity:       int64(chanCapacity),
@@ -302,11 +321,13 @@ func (r *RouterBackend) MarshallRoute(route *route.Route) *lnrpc.Route {
 			PubKey: hex.EncodeToString(
 				hop.PubKeyBytes[:],
 			),
+			TlvRecords: tlvMap,
+			TlvPayload: !hop.LegacyPayload,
 		}
 		incomingAmt = hop.AmtToForward
 	}
 
-	return resp
+	return resp, nil
 }
 
 // UnmarshallHopByChannelLookup unmarshalls an rpc hop for which the pub key is
@@ -332,11 +353,18 @@ func (r *RouterBackend) UnmarshallHopByChannelLookup(hop *lnrpc.Hop,
 		return nil, fmt.Errorf("channel edge does not match expected node")
 	}
 
+	tlvRecords, err := tlv.MapToRecords(hop.TlvRecords)
+	if err != nil {
+		return nil, err
+	}
+
 	return &route.Hop{
 		OutgoingTimeLock: hop.Expiry,
 		AmtToForward:     lnwire.MilliAtom(hop.AmtToForwardMAtoms),
 		PubKeyBytes:      pubKeyBytes,
 		ChannelID:        hop.ChanId,
+		TLVRecords:       tlvRecords,
+		LegacyPayload:    !hop.TlvPayload,
 	}, nil
 }
 
@@ -352,11 +380,21 @@ func UnmarshallKnownPubkeyHop(hop *lnrpc.Hop) (*route.Hop, error) {
 	var pubKeyBytes [33]byte
 	copy(pubKeyBytes[:], pubKey)
 
+	var tlvRecords []tlv.Record
+	if hop.TlvRecords != nil {
+		tlvRecords, err = tlv.MapToRecords(hop.TlvRecords)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return &route.Hop{
 		OutgoingTimeLock: hop.Expiry,
 		AmtToForward:     lnwire.MilliAtom(hop.AmtToForwardMAtoms),
 		PubKeyBytes:      pubKeyBytes,
 		ChannelID:        hop.ChanId,
+		TLVRecords:       tlvRecords,
+		LegacyPayload:    !hop.TlvPayload,
 	}, nil
 }
 
@@ -434,6 +472,16 @@ func (r *RouterBackend) extractIntentFromSendRequest(
 	// Set payment attempt timeout.
 	if rpcPayReq.TimeoutSeconds == 0 {
 		return nil, errors.New("timeout_seconds must be specified")
+	}
+
+	if len(rpcPayReq.DestTlv) != 0 {
+		var err error
+		payIntent.FinalDestRecords, err = tlv.MapToRecords(
+			rpcPayReq.DestTlv,
+		)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	payIntent.PayAttemptTimeout = time.Second *

lnrpc/routerrpc/router_backend_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/routing"
 	"github.com/decred/dcrlnd/routing/route"
+	"github.com/decred/dcrlnd/tlv"
 
 	"github.com/decred/dcrlnd/lnrpc"
 )
@@ -77,6 +78,7 @@ func testQueryRoutes(t *testing.T, useMissionControl bool) {
 
 	findRoute := func(source, target route.Vertex,
 		amt lnwire.MilliAtom, restrictions *routing.RestrictParams,
+		_ []tlv.Record,
 		finalExpiry ...uint16) (*route.Route, error) {
 
 		if int64(amt) != request.Amt*1000 {

rpcserver.go
@@ -43,6 +43,7 @@ import (
 	"github.com/decred/dcrlnd/routing/route"
 	"github.com/decred/dcrlnd/signal"
 	"github.com/decred/dcrlnd/sweep"
+	"github.com/decred/dcrlnd/tlv"
 	"github.com/decred/dcrlnd/watchtower"
 	"github.com/decred/dcrlnd/zpay32"
 	"github.com/decred/dcrwallet/wallet/v2/txauthor"
@@ -2927,6 +2928,8 @@ type rpcPaymentIntent struct {
 	ignoreMaxOutboundAmt bool
 	payReq               []byte
 
+	destTLV []tlv.Record
+
 	route *route.Route
 }
 
@@ -2969,6 +2972,16 @@ func extractPaymentIntent(rpcPayReq *rpcPaymentRequest) (rpcPaymentIntent, error
 	// Take cltv limit from request if set.
 	if rpcPayReq.CltvLimit != 0 {
 		payIntent.cltvLimit = &rpcPayReq.CltvLimit
+	}
+
+	if len(rpcPayReq.DestTlv) != 0 {
+		var err error
+		payIntent.destTLV, err = tlv.MapToRecords(
+			rpcPayReq.DestTlv,
+		)
+		if err != nil {
+			return payIntent, err
+		}
 	}
 
 	// If the payment request field isn't blank, then the details of the
@@ -3234,6 +3247,7 @@ func (r *rpcServer) dispatchPaymentIntent(
 			OutgoingChannelID: payIntent.outgoingChannelID,
 			PaymentRequest:    payIntent.payReq,
 			PayAttemptTimeout: routing.DefaultPayAttemptTimeout,
+			FinalDestRecords:  payIntent.destTLV,
 		}
 
 		preImage, route, routerErr = r.server.chanRouter.SendPayment(
@@ -3404,10 +3418,16 @@ func (r *rpcServer) sendPayment(stream *paymentStream) error {
 					return
 				}
 
-				marshalledRouted := r.routerBackend.
-					MarshallRoute(resp.Route)
+				backend := r.routerBackend
+				marshalledRouted, err := backend.MarshallRoute(
+					resp.Route,
+				)
+				if err != nil {
+					errChan <- err
+					return
+				}
 
-				err := stream.send(&lnrpc.SendResponse{
+				err = stream.send(&lnrpc.SendResponse{
 					PaymentHash:     payIntent.rHash[:],
 					PaymentPreimage: resp.Preimage[:],
 					PaymentRoute:    marshalledRouted,
@@ -3486,10 +3506,15 @@ func (r *rpcServer) sendPaymentSync(ctx context.Context,
 		}, nil
 	}
 
+	rpcRoute, err := r.routerBackend.MarshallRoute(resp.Route)
+	if err != nil {
+		return nil, err
+	}
+
 	return &lnrpc.SendResponse{
 		PaymentHash:     payIntent.rHash[:],
 		PaymentPreimage: resp.Preimage[:],
-		PaymentRoute:    r.routerBackend.MarshallRoute(resp.Route),
+		PaymentRoute:    rpcRoute,
 	}, nil
 }
 