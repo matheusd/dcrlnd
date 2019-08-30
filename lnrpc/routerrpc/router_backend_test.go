lightningnetwork/lnd

routing: convert to node pair based
Previously mission control tracked failures on a per node, per channel basis.
This commit changes this to tracking on the level of directed node pairs. The goal
of moving to this coarser-grained level is to reduce the number of required
payment attempts without compromising payment reliability.

lnrpc/routerrpc/router_backend_test.go
@@ -50,11 +50,6 @@ func testQueryRoutes(t *testing.T, useMissionControl bool) {
 		t.Fatal(err)
 	}
 
-	ignoredEdge := routing.EdgeLocator{
-		ChannelID: 555,
-		Direction: 1,
-	}
-
 	request := &lnrpc.QueryRoutesRequest{
 		PubKey:         destKey,
 		Amt:            100000,
@@ -92,14 +87,14 @@ func testQueryRoutes(t *testing.T, useMissionControl bool) {
 			t.Fatal("unexpected fee limit")
 		}
 
-		if restrictions.ProbabilitySource(route.Vertex{},
-			ignoredEdge, 0,
+		if restrictions.ProbabilitySource(route.Vertex{2},
+			route.Vertex{1}, 0,
 		) != 0 {
 			t.Fatal("expecting 0% probability for ignored edge")
 		}
 
 		if restrictions.ProbabilitySource(ignoreNodeVertex,
-			routing.EdgeLocator{}, 0,
+			route.Vertex{6}, 0,
 		) != 0 {
 			t.Fatal("expecting 0% probability for ignored node")
 		}
@@ -108,8 +103,8 @@ func testQueryRoutes(t *testing.T, useMissionControl bool) {
 		if useMissionControl {
 			expectedProb = testMissionControlProb
 		}
-		if restrictions.ProbabilitySource(route.Vertex{},
-			routing.EdgeLocator{}, 0,
+		if restrictions.ProbabilitySource(route.Vertex{4},
+			route.Vertex{5}, 0,
 		) != expectedProb {
 			t.Fatal("expecting 100% probability")
 		}
@@ -128,6 +123,14 @@ func testQueryRoutes(t *testing.T, useMissionControl bool) {
 			return 1, nil
 		},
 		MissionControl: &mockMissionControl{},
+		FetchChannelEndpoints: func(chanID uint64) (route.Vertex,
+			route.Vertex, error) {
+
+			if chanID != 555 {
+				t.Fatal()
+			}
+			return route.Vertex{1}, route.Vertex{2}, nil
+		},
 	}
 
 	resp, err := backend.QueryRoutes(context.Background(), request)
@@ -142,8 +145,9 @@ func testQueryRoutes(t *testing.T, useMissionControl bool) {
 type mockMissionControl struct {
 }
 
-func (m *mockMissionControl) GetEdgeProbability(fromNode route.Vertex,
-	edge routing.EdgeLocator, amt lnwire.MilliSatoshi) float64 {
+func (m *mockMissionControl) GetEdgeProbability(fromNode, toNode route.Vertex,
+	amt lnwire.MilliSatoshi) float64 {
+
 	return testMissionControlProb
 }
 

lnrpc/routerrpc/router_server.go
@@ -451,40 +451,43 @@ func (s *Server) QueryMissionControl(ctx context.Context,
 
 	snapshot := s.cfg.RouterBackend.MissionControl.GetHistorySnapshot()
 
-	rpcNodes := make([]*NodeHistory, len(snapshot.Nodes))
-	for i, n := range snapshot.Nodes {
+	rpcNodes := make([]*NodeHistory, 0, len(snapshot.Nodes))
+	for _, n := range snapshot.Nodes {
 		// Copy node struct to prevent loop variable binding bugs.
 		node := n
 
-		channels := make([]*ChannelHistory, len(node.Channels))
-		for j, channel := range node.Channels {
-			channels[j] = &ChannelHistory{
-				ChannelId:    channel.ChannelID,
-				LastFailTime: channel.LastFail.Unix(),
-				MinPenalizeAmtSat: int64(
-					channel.MinPenalizeAmt.ToSatoshis(),
-				),
-				SuccessProb: float32(channel.SuccessProb),
-			}
-		}
-
-		var lastFail int64
-		if node.LastFail != nil {
-			lastFail = node.LastFail.Unix()
-		}
-
-		rpcNodes[i] = &NodeHistory{
+		rpcNode := NodeHistory{
 			Pubkey:       node.Node[:],
-			LastFailTime: lastFail,
-			OtherChanSuccessProb: float32(
-				node.OtherChanSuccessProb,
+			LastFailTime: node.LastFail.Unix(),
+			OtherSuccessProb: float32(
+				node.OtherSuccessProb,
 			),
-			Channels: channels,
 		}
+
+		rpcNodes = append(rpcNodes, &rpcNode)
+	}
+
+	rpcPairs := make([]*PairHistory, 0, len(snapshot.Pairs))
+	for _, p := range snapshot.Pairs {
+		// Prevent binding to loop variable
+		pair := p
+
+		rpcPair := PairHistory{
+			NodeFrom:     pair.Pair.From[:],
+			NodeTo:       pair.Pair.To[:],
+			LastFailTime: pair.LastFail.Unix(),
+			MinPenalizeAmtSat: int64(
+				pair.MinPenalizeAmt.ToSatoshis(),
+			),
+			SuccessProb: float32(pair.SuccessProb),
+		}
+
+		rpcPairs = append(rpcPairs, &rpcPair)
 	}
 
 	response := QueryMissionControlResponse{
 		Nodes: rpcNodes,
+		Pairs: rpcPairs,
 	}
 
 	return &response, nil

routing/missioncontrol_test.go
@@ -12,10 +12,6 @@ import (
 )
 
 var (
-	mcTestEdge = EdgeLocator{
-		ChannelID: 2,
-	}
-
 	mcTestRoute = &route.Route{
 		SourcePubKey: route.Vertex{10},
 		Hops: []*route.Hop{
@@ -94,14 +90,13 @@ func (ctx *mcTestContext) cleanup() {
 }
 
 // Assert that mission control returns a probability for an edge.
-func (ctx *mcTestContext) expectP(amt lnwire.MilliSatoshi,
-	expected float64) {
+func (ctx *mcTestContext) expectP(amt lnwire.MilliSatoshi, expected float64) {
 
 	ctx.t.Helper()
 
-	p := ctx.mc.GetEdgeProbability(mcTestNode1, mcTestEdge, amt)
+	p := ctx.mc.GetEdgeProbability(mcTestNode1, mcTestNode2, amt)
 	if p != expected {
-		ctx.t.Fatalf("unexpected probability %v", p)
+		ctx.t.Fatalf("expected probability %v but got %v", expected, p)
 	}
 }
 
@@ -175,7 +170,7 @@ func TestMissionControl(t *testing.T) {
 		t.Fatal("unexpected number of nodes")
 	}
 
-	if len(history.Nodes[0].Channels) != 1 {
+	if len(history.Pairs) != 1 {
 		t.Fatal("unexpected number of channels")
 	}
 }
@@ -184,7 +179,6 @@ func TestMissionControl(t *testing.T) {
 // penalizing the channel yet.
 func TestMissionControlChannelUpdate(t *testing.T) {
 	ctx := createMcTestContext(t)
-	defer ctx.cleanup()
 
 	// Report a policy related failure. Because it is the first, we don't
 	// expect a penalty.

routing/mock_test.go
@@ -113,7 +113,7 @@ func (m *mockMissionControl) ReportEdgePolicyFailure(failedEdge edge) {}
 
 func (m *mockMissionControl) ReportVertexFailure(v route.Vertex) {}
 
-func (m *mockMissionControl) GetEdgeProbability(fromNode route.Vertex, edge EdgeLocator,
+func (m *mockMissionControl) GetEdgeProbability(fromNode, toNode route.Vertex,
 	amt lnwire.MilliSatoshi) float64 {
 
 	return 0
@@ -137,12 +137,6 @@ func (m *mockPaymentSession) RequestRoute(payment *LightningPayment,
 
 	return r, nil
 }
-
-func (m *mockPaymentSession) ReportVertexFailure(v route.Vertex) {}
-
-func (m *mockPaymentSession) ReportEdgeFailure(failedEdge edge, minPenalizeAmt lnwire.MilliSatoshi) {}
-
-func (m *mockPaymentSession) ReportEdgePolicyFailure(failedEdge edge) {}
 
 type mockPayer struct {
 	sendResult       chan error

matheusd/dcrlnd

lnrpc/routerrpc/router_backend_test.go
@@ -23,6 +23,10 @@ const (
 
 var (
 	sourceKey = route.Vertex{1, 2, 3}
+
+	node1 = route.Vertex{10}
+
+	node2 = route.Vertex{11}
 )
 
 // TestQueryRoutes asserts that query routes rpc parameters are properly parsed
@@ -50,11 +54,6 @@ func testQueryRoutes(t *testing.T, useMissionControl bool) {
 		t.Fatal(err)
 	}
 
-	ignoredEdge := routing.EdgeLocator{
-		ChannelID: 555,
-		Direction: 1,
-	}
-
 	request := &lnrpc.QueryRoutesRequest{
 		PubKey:         destKey,
 		Amt:            100000,
@@ -68,6 +67,10 @@ func testQueryRoutes(t *testing.T, useMissionControl bool) {
 		IgnoredEdges: []*lnrpc.EdgeLocator{{
 			ChannelId:        555,
 			DirectionReverse: true,
+		}},
+		IgnoredPairs: []*lnrpc.NodePair{{
+			From: node1[:],
+			To:   node2[:],
 		}},
 		UseMissionControl: useMissionControl,
 	}
@@ -92,24 +95,28 @@ func testQueryRoutes(t *testing.T, useMissionControl bool) {
 			t.Fatal("unexpected fee limit")
 		}
 
-		if restrictions.ProbabilitySource(route.Vertex{},
-			ignoredEdge, 0,
+		if restrictions.ProbabilitySource(route.Vertex{2},
+			route.Vertex{1}, 0,
 		) != 0 {
 			t.Fatal("expecting 0% probability for ignored edge")
 		}
 
 		if restrictions.ProbabilitySource(ignoreNodeVertex,
-			routing.EdgeLocator{}, 0,
+			route.Vertex{6}, 0,
 		) != 0 {
 			t.Fatal("expecting 0% probability for ignored node")
+		}
+
+		if restrictions.ProbabilitySource(node1, node2, 0) != 0 {
+			t.Fatal("expecting 0% probability for ignored pair")
 		}
 
 		expectedProb := 1.0
 		if useMissionControl {
 			expectedProb = testMissionControlProb
 		}
-		if restrictions.ProbabilitySource(route.Vertex{},
-			routing.EdgeLocator{}, 0,
+		if restrictions.ProbabilitySource(route.Vertex{4},
+			route.Vertex{5}, 0,
 		) != expectedProb {
 			t.Fatal("expecting 100% probability")
 		}
@@ -128,6 +135,16 @@ func testQueryRoutes(t *testing.T, useMissionControl bool) {
 			return 1, nil
 		},
 		MissionControl: &mockMissionControl{},
+		FetchChannelEndpoints: func(chanID uint64) (route.Vertex,
+			route.Vertex, error) {
+
+			if chanID != 555 {
+				t.Fatal("expected endpoints to be fetched for "+
+					"channel 555, but got %v instead",
+					chanID)
+			}
+			return route.Vertex{1}, route.Vertex{2}, nil
+		},
 	}
 
 	resp, err := backend.QueryRoutes(context.Background(), request)
@@ -142,8 +159,9 @@ func testQueryRoutes(t *testing.T, useMissionControl bool) {
 type mockMissionControl struct {
 }
 
-func (m *mockMissionControl) GetEdgeProbability(fromNode route.Vertex,
-	edge routing.EdgeLocator, amt lnwire.MilliAtom) float64 {
+func (m *mockMissionControl) GetProbability(fromNode, toNode route.Vertex,
+	amt lnwire.MilliAtom) float64 {
+
 	return testMissionControlProb
 }
 

lnrpc/routerrpc/router_server.go
@@ -451,40 +451,43 @@ func (s *Server) QueryMissionControl(ctx context.Context,
 
 	snapshot := s.cfg.RouterBackend.MissionControl.GetHistorySnapshot()
 
-	rpcNodes := make([]*NodeHistory, len(snapshot.Nodes))
-	for i, n := range snapshot.Nodes {
+	rpcNodes := make([]*NodeHistory, 0, len(snapshot.Nodes))
+	for _, n := range snapshot.Nodes {
 		// Copy node struct to prevent loop variable binding bugs.
 		node := n
 
-		channels := make([]*ChannelHistory, len(node.Channels))
-		for j, channel := range node.Channels {
-			channels[j] = &ChannelHistory{
-				ChannelId:    channel.ChannelID,
-				LastFailTime: channel.LastFail.Unix(),
-				MinPenalizeAmtAtoms: int64(
-					channel.MinPenalizeAmt.ToAtoms(),
-				),
-				SuccessProb: float32(channel.SuccessProb),
-			}
-		}
-
-		var lastFail int64
-		if node.LastFail != nil {
-			lastFail = node.LastFail.Unix()
-		}
-
-		rpcNodes[i] = &NodeHistory{
+		rpcNode := NodeHistory{
 			Pubkey:       node.Node[:],
-			LastFailTime: lastFail,
-			OtherChanSuccessProb: float32(
-				node.OtherChanSuccessProb,
+			LastFailTime: node.LastFail.Unix(),
+			OtherSuccessProb: float32(
+				node.OtherSuccessProb,
 			),
-			Channels: channels,
 		}
+
+		rpcNodes = append(rpcNodes, &rpcNode)
+	}
+
+	rpcPairs := make([]*PairHistory, 0, len(snapshot.Pairs))
+	for _, p := range snapshot.Pairs {
+		// Prevent binding to loop variable.
+		pair := p
+
+		rpcPair := PairHistory{
+			NodeFrom:     pair.Pair.From[:],
+			NodeTo:       pair.Pair.To[:],
+			LastFailTime: pair.LastFail.Unix(),
+			MinPenalizeAmtAtoms: int64(
+				pair.MinPenalizeAmt.ToAtoms(),
+			),
+			SuccessProb: float32(pair.SuccessProb),
+		}
+
+		rpcPairs = append(rpcPairs, &rpcPair)
 	}
 
 	response := QueryMissionControlResponse{
 		Nodes: rpcNodes,
+		Pairs: rpcPairs,
 	}
 
 	return &response, nil

routing/missioncontrol_test.go
@@ -12,10 +12,6 @@ import (
 )
 
 var (
-	mcTestEdge = EdgeLocator{
-		ChannelID: 2,
-	}
-
 	mcTestRoute = &route.Route{
 		SourcePubKey: route.Vertex{10},
 		Hops: []*route.Hop{
@@ -94,14 +90,13 @@ func (ctx *mcTestContext) cleanup() {
 }
 
 // Assert that mission control returns a probability for an edge.
-func (ctx *mcTestContext) expectP(amt lnwire.MilliAtom,
-	expected float64) {
+func (ctx *mcTestContext) expectP(amt lnwire.MilliAtom, expected float64) {
 
 	ctx.t.Helper()
 
-	p := ctx.mc.GetEdgeProbability(mcTestNode1, mcTestEdge, amt)
+	p := ctx.mc.GetProbability(mcTestNode1, mcTestNode2, amt)
 	if p != expected {
-		ctx.t.Fatalf("unexpected probability %v", p)
+		ctx.t.Fatalf("expected probability %v but got %v", expected, p)
 	}
 }
 
@@ -175,7 +170,7 @@ func TestMissionControl(t *testing.T) {
 		t.Fatal("unexpected number of nodes")
 	}
 
-	if len(history.Nodes[0].Channels) != 1 {
+	if len(history.Pairs) != 1 {
 		t.Fatal("unexpected number of channels")
 	}
 }
@@ -184,7 +179,6 @@ func TestMissionControl(t *testing.T) {
 // penalizing the channel yet.
 func TestMissionControlChannelUpdate(t *testing.T) {
 	ctx := createMcTestContext(t)
-	defer ctx.cleanup()
 
 	// Report a policy related failure. Because it is the first, we don't
 	// expect a penalty.

routing/mock_test.go
@@ -105,7 +105,7 @@ func (m *mockMissionControl) ReportPaymentFail(paymentID uint64,
 	return false, 0, nil
 }
 
-func (m *mockMissionControl) GetEdgeProbability(fromNode route.Vertex, edge EdgeLocator,
+func (m *mockMissionControl) GetProbability(fromNode, toNode route.Vertex,
 	amt lnwire.MilliAtom) float64 {
 
 	return 0
@@ -129,12 +129,6 @@ func (m *mockPaymentSession) RequestRoute(payment *LightningPayment,
 
 	return r, nil
 }
-
-func (m *mockPaymentSession) ReportVertexFailure(v route.Vertex) {}
-
-func (m *mockPaymentSession) ReportEdgeFailure(failedEdge edge, minPenalizeAmt lnwire.MilliAtom) {}
-
-func (m *mockPaymentSession) ReportEdgePolicyFailure(failedEdge edge) {}
 
 type mockPayer struct {
 	sendResult       chan error