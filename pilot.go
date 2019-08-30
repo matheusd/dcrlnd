lightningnetwork/lnd

autopilot: extract default fee estimate target conf into constant

pilot.go
@@ -84,7 +84,9 @@ func (c *chanController) OpenChannel(target *btcec.PublicKey,
 
 	// With the connection established, we'll now establish our connection
 	// to the target peer, waiting for the first update before we exit.
-	feePerKw, err := c.server.cc.feeEstimator.EstimateFeePerKW(3)
+	feePerKw, err := c.server.cc.feeEstimator.EstimateFeePerKW(
+		autopilot.DefaultConfTarget,
+	)
 	if err != nil {
 		return err
 	}

matheusd/dcrlnd

pilot.go
@@ -84,7 +84,9 @@ func (c *chanController) OpenChannel(target *secp256k1.PublicKey,
 
 	// With the connection established, we'll now establish our connection
 	// to the target peer, waiting for the first update before we exit.
-	feePerKB, err := c.server.cc.feeEstimator.EstimateFeePerKB(3)
+	feePerKB, err := c.server.cc.feeEstimator.EstimateFeePerKB(
+		autopilot.DefaultConfTarget,
+	)
 	if err != nil {
 		return err
 	}