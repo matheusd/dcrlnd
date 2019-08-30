lightningnetwork/lnd

watchtower/multi: send connection features + chain hash in Init

watchtower/wtserver/server.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/connmgr"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcutil"
@@ -51,6 +52,9 @@ type Config struct {
 	// NewAddress is used to generate reward addresses, where a cut of
 	// successfully sent funds can be received.
 	NewAddress func() (btcutil.Address, error)
+
+	// ChainHash identifies the network that the server is watching.
+	ChainHash chainhash.Hash
 }
 
 // Server houses the state required to handle watchtower peers. It's primary job
@@ -68,7 +72,7 @@ type Server struct {
 	clients   map[wtdb.SessionID]Peer
 
 	globalFeatures *lnwire.RawFeatureVector
-	localFeatures  *lnwire.RawFeatureVector
+	connFeatures   *lnwire.RawFeatureVector
 
 	wg   sync.WaitGroup
 	quit chan struct{}
@@ -78,7 +82,7 @@ type Server struct {
 // clients connecting to the listener addresses, and allows them to open
 // sessions and send state updates.
 func New(cfg *Config) (*Server, error) {
-	localFeatures := lnwire.NewRawFeatureVector(
+	connFeatures := lnwire.NewRawFeatureVector(
 		wtwire.WtSessionsOptional,
 	)
 
@@ -86,7 +90,7 @@ func New(cfg *Config) (*Server, error) {
 		cfg:            cfg,
 		clients:        make(map[wtdb.SessionID]Peer),
 		globalFeatures: lnwire.NewRawFeatureVector(),
-		localFeatures:  localFeatures,
+		connFeatures:   connFeatures,
 		quit:           make(chan struct{}),
 	}
 
@@ -206,7 +210,7 @@ func (s *Server) handleClient(peer Peer) {
 	}
 
 	localInit := wtwire.NewInitMessage(
-		s.localFeatures, s.globalFeatures,
+		s.connFeatures, s.cfg.ChainHash,
 	)
 
 	err = s.sendMessage(peer, localInit)
@@ -296,25 +300,19 @@ func (s *Server) handleClient(peer Peer) {
 // handleInit accepts the local and remote Init messages, and verifies that the
 // client is not requesting any required features that are unknown to the tower.
 func (s *Server) handleInit(localInit, remoteInit *wtwire.Init) error {
-	remoteLocalFeatures := lnwire.NewFeatureVector(
-		remoteInit.LocalFeatures, wtwire.LocalFeatures,
-	)
-	remoteGlobalFeatures := lnwire.NewFeatureVector(
-		remoteInit.GlobalFeatures, wtwire.GlobalFeatures,
-	)
-
-	unknownLocalFeatures := remoteLocalFeatures.UnknownRequiredFeatures()
-	if len(unknownLocalFeatures) > 0 {
-		err := fmt.Errorf("Peer set unknown local feature bits: %v",
-			unknownLocalFeatures)
-		return err
+	if localInit.ChainHash != remoteInit.ChainHash {
+		return fmt.Errorf("Peer chain hash unknown: %x",
+			remoteInit.ChainHash)
 	}
 
-	unknownGlobalFeatures := remoteGlobalFeatures.UnknownRequiredFeatures()
-	if len(unknownGlobalFeatures) > 0 {
-		err := fmt.Errorf("Peer set unknown global feature bits: %v",
-			unknownGlobalFeatures)
-		return err
+	remoteConnFeatures := lnwire.NewFeatureVector(
+		remoteInit.ConnFeatures, wtwire.LocalFeatures,
+	)
+
+	unknownLocalFeatures := remoteConnFeatures.UnknownRequiredFeatures()
+	if len(unknownLocalFeatures) > 0 {
+		return fmt.Errorf("Peer set unknown local feature bits: %v",
+			unknownLocalFeatures)
 	}
 
 	return nil

watchtower/wtwire/init.go
@@ -1,21 +1,57 @@
 package wtwire
 
-import "github.com/lightningnetwork/lnd/lnwire"
+import (
+	"io"
+
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/lightningnetwork/lnd/lnwire"
+)
 
 // Init is the first message sent over the watchtower wire protocol, and
-// specifies features and level of requiredness maintained by the sending node.
-// The watchtower Init message is identical to the LN Init message, except it
-// uses a different message type to ensure the two are not conflated.
+// specifies connection features bits and level of requiredness maintained by
+// the sending node. The Init message also sends the chain hash identifying the
+// network that the sender is on.
 type Init struct {
-	*lnwire.Init
+	// ConnFeatures are the feature bits being advertised for the duration
+	// of a single connection with a peer.
+	ConnFeatures *lnwire.RawFeatureVector
+
+	// ChainHash is the genesis hash of the chain that the advertiser claims
+	// to be on.
+	ChainHash chainhash.Hash
 }
 
-// NewInitMessage generates a new Init message from raw global and local feature
-// vectors.
-func NewInitMessage(gf, lf *lnwire.RawFeatureVector) *Init {
+// NewInitMessage generates a new Init message from a raw connection feature
+// vector and chain hash.
+func NewInitMessage(connFeatures *lnwire.RawFeatureVector,
+	chainHash chainhash.Hash) *Init {
+
 	return &Init{
-		Init: lnwire.NewInitMessage(gf, lf),
+		ConnFeatures: connFeatures,
+		ChainHash:    chainHash,
 	}
+}
+
+// Encode serializes the target Init into the passed io.Writer observing the
+// protocol version specified.
+//
+// This is part of the wtwire.Message interface.
+func (msg *Init) Encode(w io.Writer, pver uint32) error {
+	return WriteElements(w,
+		msg.ConnFeatures,
+		msg.ChainHash,
+	)
+}
+
+// Decode deserializes a serialized Init message stored in the passed io.Reader
+// observing the specified protocol version.
+//
+// This is part of the wtwire.Message interface.
+func (msg *Init) Decode(r io.Reader, pver uint32) error {
+	return ReadElements(r,
+		&msg.ConnFeatures,
+		&msg.ChainHash,
+	)
 }
 
 // MsgType returns the integer uniquely identifying this message type on the
@@ -24,6 +60,14 @@ func NewInitMessage(gf, lf *lnwire.RawFeatureVector) *Init {
 // This is part of the wtwire.Message interface.
 func (msg *Init) MsgType() MessageType {
 	return MsgInit
+}
+
+// MaxPayloadLength returns the maximum allowed payload size for an Init
+// complete message observing the specified protocol version.
+//
+// This is part of the wtwire.Message interface.
+func (msg *Init) MaxPayloadLength(uint32) uint32 {
+	return MaxMessagePayload
 }
 
 // A compile-time constraint to ensure Init implements the Message interface.

watchtower/wtwire/wtwire.go
@@ -6,8 +6,10 @@ import (
 	"io"
 
 	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/lightningnetwork/lnd/lnwallet"
+	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/watchtower/blob"
 )
 
@@ -83,6 +85,20 @@ func WriteElement(w io.Writer, element interface{}) error {
 		var b [2]byte
 		binary.BigEndian.PutUint16(b[:], uint16(e))
 		if _, err := w.Write(b[:]); err != nil {
+			return err
+		}
+
+	case chainhash.Hash:
+		if _, err := w.Write(e[:]); err != nil {
+			return err
+		}
+
+	case *lnwire.RawFeatureVector:
+		if e == nil {
+			return fmt.Errorf("cannot write nil feature vector")
+		}
+
+		if err := e.Encode(w); err != nil {
 			return err
 		}
 
@@ -191,6 +207,20 @@ func ReadElement(r io.Reader, element interface{}) error {
 			return err
 		}
 		*e = ErrorCode(binary.BigEndian.Uint16(b[:]))
+
+	case *chainhash.Hash:
+		if _, err := io.ReadFull(r, e[:]); err != nil {
+			return err
+		}
+
+	case **lnwire.RawFeatureVector:
+		f := lnwire.NewRawFeatureVector()
+		err := f.Decode(r)
+		if err != nil {
+			return err
+		}
+
+		*e = f
 
 	case **btcec.PublicKey:
 		var b [btcec.PubKeyBytesLenCompressed]byte

watchtower/wtwire/wtwire_test.go
@@ -8,6 +8,7 @@ import (
 	"testing/quick"
 	"time"
 
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/watchtower/wtwire"
@@ -21,6 +22,12 @@ func randRawFeatureVector(r *rand.Rand) *lnwire.RawFeatureVector {
 		}
 	}
 	return featureVec
+}
+
+func randChainHash(r *rand.Rand) chainhash.Hash {
+	var hash chainhash.Hash
+	r.Read(hash[:])
+	return hash
 }
 
 // TestWatchtowerWireProtocol uses the testing/quick package to create a series
@@ -73,7 +80,7 @@ func TestWatchtowerWireProtocol(t *testing.T) {
 		wtwire.MsgInit: func(v []reflect.Value, r *rand.Rand) {
 			req := wtwire.NewInitMessage(
 				randRawFeatureVector(r),
-				randRawFeatureVector(r),
+				randChainHash(r),
 			)
 
 			v[0] = reflect.ValueOf(*req)

matheusd/dcrlnd

watchtower/wtserver/server.go
@@ -9,6 +9,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/connmgr"
 	"github.com/decred/dcrd/dcrec/secp256k1"
 	"github.com/decred/dcrd/dcrutil"
@@ -51,6 +52,9 @@ type Config struct {
 	// NewAddress is used to generate reward addresses, where a cut of
 	// successfully sent funds can be received.
 	NewAddress func() (dcrutil.Address, error)
+
+	// ChainHash identifies the network that the server is watching.
+	ChainHash chainhash.Hash
 }
 
 // Server houses the state required to handle watchtower peers. It's primary job
@@ -68,7 +72,7 @@ type Server struct {
 	clients   map[wtdb.SessionID]Peer
 
 	globalFeatures *lnwire.RawFeatureVector
-	localFeatures  *lnwire.RawFeatureVector
+	connFeatures   *lnwire.RawFeatureVector
 
 	wg   sync.WaitGroup
 	quit chan struct{}
@@ -78,7 +82,7 @@ type Server struct {
 // clients connecting to the listener addresses, and allows them to open
 // sessions and send state updates.
 func New(cfg *Config) (*Server, error) {
-	localFeatures := lnwire.NewRawFeatureVector(
+	connFeatures := lnwire.NewRawFeatureVector(
 		wtwire.WtSessionsOptional,
 	)
 
@@ -86,7 +90,7 @@ func New(cfg *Config) (*Server, error) {
 		cfg:            cfg,
 		clients:        make(map[wtdb.SessionID]Peer),
 		globalFeatures: lnwire.NewRawFeatureVector(),
-		localFeatures:  localFeatures,
+		connFeatures:   connFeatures,
 		quit:           make(chan struct{}),
 	}
 
@@ -206,7 +210,7 @@ func (s *Server) handleClient(peer Peer) {
 	}
 
 	localInit := wtwire.NewInitMessage(
-		s.localFeatures, s.globalFeatures,
+		s.connFeatures, s.cfg.ChainHash,
 	)
 
 	err = s.sendMessage(peer, localInit)
@@ -296,25 +300,19 @@ func (s *Server) handleClient(peer Peer) {
 // handleInit accepts the local and remote Init messages, and verifies that the
 // client is not requesting any required features that are unknown to the tower.
 func (s *Server) handleInit(localInit, remoteInit *wtwire.Init) error {
-	remoteLocalFeatures := lnwire.NewFeatureVector(
-		remoteInit.LocalFeatures, wtwire.LocalFeatures,
-	)
-	remoteGlobalFeatures := lnwire.NewFeatureVector(
-		remoteInit.GlobalFeatures, wtwire.GlobalFeatures,
-	)
-
-	unknownLocalFeatures := remoteLocalFeatures.UnknownRequiredFeatures()
-	if len(unknownLocalFeatures) > 0 {
-		err := fmt.Errorf("Peer set unknown local feature bits: %v",
-			unknownLocalFeatures)
-		return err
+	if localInit.ChainHash != remoteInit.ChainHash {
+		return fmt.Errorf("Peer chain hash unknown: %x",
+			remoteInit.ChainHash)
 	}
 
-	unknownGlobalFeatures := remoteGlobalFeatures.UnknownRequiredFeatures()
-	if len(unknownGlobalFeatures) > 0 {
-		err := fmt.Errorf("Peer set unknown global feature bits: %v",
-			unknownGlobalFeatures)
-		return err
+	remoteConnFeatures := lnwire.NewFeatureVector(
+		remoteInit.ConnFeatures, wtwire.LocalFeatures,
+	)
+
+	unknownLocalFeatures := remoteConnFeatures.UnknownRequiredFeatures()
+	if len(unknownLocalFeatures) > 0 {
+		return fmt.Errorf("Peer set unknown local feature bits: %v",
+			unknownLocalFeatures)
 	}
 
 	return nil

watchtower/wtwire/init.go
@@ -1,21 +1,57 @@
 package wtwire
 
-import "github.com/decred/dcrlnd/lnwire"
+import (
+	"io"
+
+	"github.com/decred/dcrd/chaincfg/chainhash"
+	"github.com/decred/dcrlnd/lnwire"
+)
 
 // Init is the first message sent over the watchtower wire protocol, and
-// specifies features and level of requiredness maintained by the sending node.
-// The watchtower Init message is identical to the LN Init message, except it
-// uses a different message type to ensure the two are not conflated.
+// specifies connection features bits and level of requiredness maintained by
+// the sending node. The Init message also sends the chain hash identifying the
+// network that the sender is on.
 type Init struct {
-	*lnwire.Init
+	// ConnFeatures are the feature bits being advertised for the duration
+	// of a single connection with a peer.
+	ConnFeatures *lnwire.RawFeatureVector
+
+	// ChainHash is the genesis hash of the chain that the advertiser claims
+	// to be on.
+	ChainHash chainhash.Hash
 }
 
-// NewInitMessage generates a new Init message from raw global and local feature
-// vectors.
-func NewInitMessage(gf, lf *lnwire.RawFeatureVector) *Init {
+// NewInitMessage generates a new Init message from a raw connection feature
+// vector and chain hash.
+func NewInitMessage(connFeatures *lnwire.RawFeatureVector,
+	chainHash chainhash.Hash) *Init {
+
 	return &Init{
-		Init: lnwire.NewInitMessage(gf, lf),
+		ConnFeatures: connFeatures,
+		ChainHash:    chainHash,
 	}
+}
+
+// Encode serializes the target Init into the passed io.Writer observing the
+// protocol version specified.
+//
+// This is part of the wtwire.Message interface.
+func (msg *Init) Encode(w io.Writer, pver uint32) error {
+	return WriteElements(w,
+		msg.ConnFeatures,
+		msg.ChainHash,
+	)
+}
+
+// Decode deserializes a serialized Init message stored in the passed io.Reader
+// observing the specified protocol version.
+//
+// This is part of the wtwire.Message interface.
+func (msg *Init) Decode(r io.Reader, pver uint32) error {
+	return ReadElements(r,
+		&msg.ConnFeatures,
+		&msg.ChainHash,
+	)
 }
 
 // MsgType returns the integer uniquely identifying this message type on the
@@ -24,6 +60,14 @@ func NewInitMessage(gf, lf *lnwire.RawFeatureVector) *Init {
 // This is part of the wtwire.Message interface.
 func (msg *Init) MsgType() MessageType {
 	return MsgInit
+}
+
+// MaxPayloadLength returns the maximum allowed payload size for an Init
+// complete message observing the specified protocol version.
+//
+// This is part of the wtwire.Message interface.
+func (msg *Init) MaxPayloadLength(uint32) uint32 {
+	return MaxMessagePayload
 }
 
 // A compile-time constraint to ensure Init implements the Message interface.

watchtower/wtwire/wtwire.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrec/secp256k1"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/lnwallet"
+	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/watchtower/blob"
 )
 
@@ -83,6 +85,20 @@ func WriteElement(w io.Writer, element interface{}) error {
 		var b [2]byte
 		binary.BigEndian.PutUint16(b[:], uint16(e))
 		if _, err := w.Write(b[:]); err != nil {
+			return err
+		}
+
+	case chainhash.Hash:
+		if _, err := w.Write(e[:]); err != nil {
+			return err
+		}
+
+	case *lnwire.RawFeatureVector:
+		if e == nil {
+			return fmt.Errorf("cannot write nil feature vector")
+		}
+
+		if err := e.Encode(w); err != nil {
 			return err
 		}
 
@@ -192,8 +208,23 @@ func ReadElement(r io.Reader, element interface{}) error {
 		}
 		*e = ErrorCode(binary.BigEndian.Uint16(b[:]))
 
+	case *chainhash.Hash:
+		if _, err := io.ReadFull(r, e[:]); err != nil {
+			return err
+		}
+
+	case **lnwire.RawFeatureVector:
+		f := lnwire.NewRawFeatureVector()
+		err := f.Decode(r)
+		if err != nil {
+			return err
+		}
+
+		*e = f
+
 	case **secp256k1.PublicKey:
 		var b [secp256k1.PubKeyBytesLenCompressed]byte
+
 		if _, err := io.ReadFull(r, b[:]); err != nil {
 			return err
 		}

watchtower/wtwire/wtwire_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/watchtower/wtwire"
 )
@@ -21,6 +22,12 @@ func randRawFeatureVector(r *rand.Rand) *lnwire.RawFeatureVector {
 		}
 	}
 	return featureVec
+}
+
+func randChainHash(r *rand.Rand) chainhash.Hash {
+	var hash chainhash.Hash
+	r.Read(hash[:])
+	return hash
 }
 
 // TestWatchtowerWireProtocol uses the testing/quick package to create a series
@@ -73,7 +80,7 @@ func TestWatchtowerWireProtocol(t *testing.T) {
 		wtwire.MsgInit: func(v []reflect.Value, r *rand.Rand) {
 			req := wtwire.NewInitMessage(
 				randRawFeatureVector(r),
-				randRawFeatureVector(r),
+				randChainHash(r),
 			)
 
 			v[0] = reflect.ValueOf(*req)