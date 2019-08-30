joostjager/lnd

invoices: extract invoice decoding from registry
Previously it was difficult to use the invoice registry in unit tests,
because it used zpay32 to decode the invoice. For that to succeed, a
valid signature is required on the payment request.

This commit injects the decode dependency on a different level so that
it is easier to mock.

invoices/invoiceregistry_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/lntypes"
 	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/lightningnetwork/lnd/zpay32"
 )
 
 var (
@@ -28,6 +29,14 @@ var (
 	testPayReq = "lnbc500u1pwywxzwpp5nd2u9xzq02t0tuf2654as7vma42lwkcjptx4yzfq0umq4swpa7cqdqqcqzysmlpc9ewnydr8rr8dnltyxphdyf6mcqrsd6dml8zajtyhwe6a45d807kxtmzayuf0hh2d9tn478ecxkecdg7c5g85pntupug5kakm7xcpn63zqk"
 )
 
+func decodeExpiry(payReq string) (uint32, error) {
+	invoice, err := zpay32.Decode(payReq, &chaincfg.MainNetParams)
+	if err != nil {
+		return 0, err
+	}
+	return uint32(invoice.MinFinalCLTVExpiry()), nil
+}
+
 // TestSettleInvoice tests settling of an invoice and related notifications.
 func TestSettleInvoice(t *testing.T) {
 	cdb, cleanup, err := newDB()
@@ -37,7 +46,7 @@ func TestSettleInvoice(t *testing.T) {
 	defer cleanup()
 
 	// Instantiate and start the invoice registry.
-	registry := NewRegistry(cdb, &chaincfg.MainNetParams)
+	registry := NewRegistry(cdb, decodeExpiry)
 
 	err = registry.Start()
 	if err != nil {
@@ -167,7 +176,7 @@ func TestCancelInvoice(t *testing.T) {
 	defer cleanup()
 
 	// Instantiate and start the invoice registry.
-	registry := NewRegistry(cdb, &chaincfg.MainNetParams)
+	registry := NewRegistry(cdb, decodeExpiry)
 
 	err = registry.Start()
 	if err != nil {

server.go
@@ -46,6 +46,7 @@ import (
 	"github.com/lightningnetwork/lnd/sweep"
 	"github.com/lightningnetwork/lnd/ticker"
 	"github.com/lightningnetwork/lnd/tor"
+	"github.com/lightningnetwork/lnd/zpay32"
 )
 
 const (
@@ -284,6 +285,14 @@ func newServer(listenAddrs []net.Addr, chanDB *channeldb.DB, cc *chainControl,
 		readBufferPool, runtime.NumCPU(), pool.DefaultWorkerTimeout,
 	)
 
+	decodeFinalCltvExpiry := func(payReq string) (uint32, error) {
+		invoice, err := zpay32.Decode(payReq, activeNetParams.Params)
+		if err != nil {
+			return 0, err
+		}
+		return uint32(invoice.MinFinalCLTVExpiry()), nil
+	}
+
 	s := &server{
 		chanDB:    chanDB,
 		cc:        cc,
@@ -291,7 +300,7 @@ func newServer(listenAddrs []net.Addr, chanDB *channeldb.DB, cc *chainControl,
 		writePool: writePool,
 		readPool:  readPool,
 
-		invoices: invoices.NewRegistry(chanDB, activeNetParams.Params),
+		invoices: invoices.NewRegistry(chanDB, decodeFinalCltvExpiry),
 
 		channelNotifier: channelnotifier.New(chanDB),
 

matheusd/dcrlnd

invoices/invoiceregistry_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/lntypes"
 	"github.com/decred/dcrlnd/lnwire"
+	"github.com/decred/dcrlnd/zpay32"
 )
 
 var (
@@ -28,6 +29,14 @@ var (
 	testPayReq = "lndcr1pvjluezpp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdq5xysxxatsyp3k7enxv4jsmvp0ygkvzd3zh9wkfj59cuze0se5fzuh4f7rysdukv68n6fafa45sudrzg8d33paaw50zczd5mzmppqaalvzneu0yd3zfrvzhnfzpkgppyrza2"
 )
 
+func decodeExpiry(payReq string) (uint32, error) {
+	invoice, err := zpay32.Decode(payReq, &chaincfg.MainNetParams)
+	if err != nil {
+		return 0, err
+	}
+	return uint32(invoice.MinFinalCLTVExpiry()), nil
+}
+
 // TestSettleInvoice tests settling of an invoice and related notifications.
 func TestSettleInvoice(t *testing.T) {
 
@@ -38,7 +47,7 @@ func TestSettleInvoice(t *testing.T) {
 	defer cleanup()
 
 	// Instantiate and start the invoice registry.
-	registry := NewRegistry(cdb, &chaincfg.MainNetParams)
+	registry := NewRegistry(cdb, decodeExpiry)
 
 	err = registry.Start()
 	if err != nil {
@@ -171,7 +180,7 @@ func TestCancelInvoice(t *testing.T) {
 	defer cleanup()
 
 	// Instantiate and start the invoice registry.
-	registry := NewRegistry(cdb, &chaincfg.MainNetParams)
+	registry := NewRegistry(cdb, decodeExpiry)
 
 	err = registry.Start()
 	if err != nil {

server.go
@@ -43,6 +43,7 @@ import (
 	"github.com/decred/dcrlnd/sweep"
 	"github.com/decred/dcrlnd/ticker"
 	"github.com/decred/dcrlnd/tor"
+	"github.com/decred/dcrlnd/zpay32"
 	sphinx "github.com/decred/lightning-onion"
 	"github.com/go-errors/errors"
 	bolt "go.etcd.io/bbolt"
@@ -285,6 +286,14 @@ func newServer(listenAddrs []net.Addr, chanDB *channeldb.DB, cc *chainControl,
 		readBufferPool, runtime.NumCPU(), pool.DefaultWorkerTimeout,
 	)
 
+	decodeFinalCltvExpiry := func(payReq string) (uint32, error) {
+		invoice, err := zpay32.Decode(payReq, activeNetParams.Params)
+		if err != nil {
+			return 0, err
+		}
+		return uint32(invoice.MinFinalCLTVExpiry()), nil
+	}
+
 	s := &server{
 		chanDB:    chanDB,
 		cc:        cc,
@@ -292,7 +301,7 @@ func newServer(listenAddrs []net.Addr, chanDB *channeldb.DB, cc *chainControl,
 		writePool: writePool,
 		readPool:  readPool,
 
-		invoices: invoices.NewRegistry(chanDB, activeNetParams.Params),
+		invoices: invoices.NewRegistry(chanDB, decodeFinalCltvExpiry),
 
 		channelNotifier: channelnotifier.New(chanDB),
 