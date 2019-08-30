lightningnetwork/lnd

lnwallet: move coin selection before ChannelReservation
Now that coin selection is independent of creating the channel
reservation, we can move it first, in preparation for doing custom coin
selection.

lnwallet/wallet.go
@@ -453,6 +453,29 @@ func (l *LightningWallet) handleFundingReserveRequest(req *InitFundingReserveMsg
 	capacity := req.LocalFundingAmt + req.RemoteFundingAmt
 	localFundingAmt := req.LocalFundingAmt
 
+	var (
+		selected *coinSelection
+		err      error
+	)
+
+	// If we're on the receiving end of a single funder channel then we
+	// don't need to perform any coin selection, and the remote contributes
+	// all funds. Otherwise, attempt to obtain enough coins to meet the
+	// required funding amount.
+	if req.LocalFundingAmt != 0 {
+		// Coin selection is done on the basis of sat/kw, so we'll use
+		// the fee rate passed in to perform coin selection.
+		var err error
+		selected, err = l.selectCoinsAndChange(
+			req.FundingFeePerKw, req.LocalFundingAmt, req.MinConfs,
+		)
+		if err != nil {
+			req.err <- err
+			req.resp <- nil
+			return
+		}
+	}
+
 	id := atomic.AddUint64(&l.nextFundingID, 1)
 	reservation, err := NewChannelReservation(
 		capacity, localFundingAmt, req.CommitFeePerKw, l, id,
@@ -468,26 +491,13 @@ func (l *LightningWallet) handleFundingReserveRequest(req *InitFundingReserveMsg
 	reservation.Lock()
 	defer reservation.Unlock()
 
-	reservation.nodeAddr = req.NodeAddr
-	reservation.partialState.IdentityPub = req.NodeID
-
-	// If we're on the receiving end of a single funder channel then we
-	// don't need to perform any coin selection. Otherwise, attempt to
-	// obtain enough coins to meet the required funding amount.
-	if req.LocalFundingAmt != 0 {
-		// Coin selection is done on the basis of sat/kw, so we'll use
-		// the fee rate passed in to perform coin selection.
-		selected, err := l.selectCoinsAndChange(
-			req.FundingFeePerKw, req.LocalFundingAmt, req.MinConfs,
-		)
-		if err != nil {
-			req.err <- err
-			req.resp <- nil
-			return
-		}
+	if selected != nil {
 		reservation.ourContribution.Inputs = selected.coins
 		reservation.ourContribution.ChangeOutputs = selected.change
 	}
+
+	reservation.nodeAddr = req.NodeAddr
+	reservation.partialState.IdentityPub = req.NodeID
 
 	// Next, we'll grab a series of keys from the wallet which will be used
 	// for the duration of the channel. The keys include: our multi-sig

matheusd/dcrlnd

lnwallet/wallet.go
@@ -447,6 +447,29 @@ func (l *LightningWallet) handleFundingReserveRequest(req *InitFundingReserveMsg
 	capacity := req.LocalFundingAmt + req.RemoteFundingAmt
 	localFundingAmt := req.LocalFundingAmt
 
+	var (
+		selected *coinSelection
+		err      error
+	)
+
+	// If we're on the receiving end of a single funder channel then we
+	// don't need to perform any coin selection, and the remote contributes
+	// all funds. Otherwise, attempt to obtain enough coins to meet the
+	// required funding amount.
+	if req.LocalFundingAmt != 0 {
+		// Coin selection is done on the basis of sat/kw, so we'll use
+		// the fee rate passed in to perform coin selection.
+		var err error
+		selected, err = l.selectCoinsAndChange(
+			req.FundingFeePerKB, req.LocalFundingAmt, req.MinConfs,
+		)
+		if err != nil {
+			req.err <- err
+			req.resp <- nil
+			return
+		}
+	}
+
 	id := atomic.AddUint64(&l.nextFundingID, 1)
 	reservation, err := NewChannelReservation(
 		capacity, localFundingAmt, req.CommitFeePerKB, l, id,
@@ -462,26 +485,13 @@ func (l *LightningWallet) handleFundingReserveRequest(req *InitFundingReserveMsg
 	reservation.Lock()
 	defer reservation.Unlock()
 
-	reservation.nodeAddr = req.NodeAddr
-	reservation.partialState.IdentityPub = req.NodeID
-
-	// If we're on the receiving end of a single funder channel then we
-	// don't need to perform any coin selection. Otherwise, attempt to
-	// obtain enough coins to meet the required funding amount.
-	if req.LocalFundingAmt != 0 {
-		// Coin selection is done on the basis of atom/kB, so we'll use
-		// the fee rate passed in to perform coin selection.
-		selected, err := l.selectCoinsAndChange(
-			req.FundingFeePerKB, req.LocalFundingAmt, req.MinConfs,
-		)
-		if err != nil {
-			req.err <- err
-			req.resp <- nil
-			return
-		}
+	if selected != nil {
 		reservation.ourContribution.Inputs = selected.coins
 		reservation.ourContribution.ChangeOutputs = selected.change
 	}
+
+	reservation.nodeAddr = req.NodeAddr
+	reservation.partialState.IdentityPub = req.NodeID
 
 	// Next, we'll grab a series of keys from the wallet which will be used
 	// for the duration of the channel. The keys include: our multi-sig