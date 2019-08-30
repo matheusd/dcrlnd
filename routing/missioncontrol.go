joostjager/lnd

zpay32: move HopHint and DefaultFinalCLTVDelta

routing/missioncontrol.go
@@ -8,6 +8,7 @@ import (
 	"github.com/coreos/bbolt"
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/lightningnetwork/lnd/zpay32"
 )
 
 const (
@@ -152,7 +153,7 @@ func (m *missionControl) GraphPruneView() graphPruneView {
 // view from Mission Control. An optional set of routing hints can be provided
 // in order to populate additional edges to explore when finding a path to the
 // payment's destination.
-func (m *missionControl) NewPaymentSession(routeHints [][]HopHint,
+func (m *missionControl) NewPaymentSession(routeHints [][]zpay32.HopHint,
 	target *btcec.PublicKey) (*paymentSession, error) {
 
 	viewSnapshot := m.GraphPruneView()

routing/pathfind_test.go
@@ -22,6 +22,7 @@ import (
 	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/lightningnetwork/lnd/zpay32"
 )
 
 const (
@@ -1737,11 +1738,11 @@ func TestPathFindSpecExample(t *testing.T) {
 	// The CLTV expiry should be the current height plus 9 (the expiry for
 	// the B -> C channel.
 	if firstRoute.TotalTimeLock !=
-		startingHeight+DefaultFinalCLTVDelta {
+		startingHeight+zpay32.DefaultFinalCLTVDelta {
 
 		t.Fatalf("wrong total time lock: got %v, expecting %v",
 			firstRoute.TotalTimeLock,
-			startingHeight+DefaultFinalCLTVDelta)
+			startingHeight+zpay32.DefaultFinalCLTVDelta)
 	}
 
 	// Next, we'll set A as the source node so we can assert that we create
@@ -1830,11 +1831,11 @@ func TestPathFindSpecExample(t *testing.T) {
 	// The outgoing CLTV value itself should be the current height plus 30
 	// to meet Carol's requirements.
 	if routes[0].Hops[0].OutgoingTimeLock !=
-		startingHeight+DefaultFinalCLTVDelta {
+		startingHeight+zpay32.DefaultFinalCLTVDelta {
 
 		t.Fatalf("wrong total time lock: got %v, expecting %v",
 			routes[0].Hops[0].OutgoingTimeLock,
-			startingHeight+DefaultFinalCLTVDelta)
+			startingHeight+zpay32.DefaultFinalCLTVDelta)
 	}
 
 	// For B -> C, we assert that the final hop also has the proper
@@ -1845,11 +1846,11 @@ func TestPathFindSpecExample(t *testing.T) {
 			lastHop.AmtToForward, amt)
 	}
 	if lastHop.OutgoingTimeLock !=
-		startingHeight+DefaultFinalCLTVDelta {
+		startingHeight+zpay32.DefaultFinalCLTVDelta {
 
 		t.Fatalf("wrong total time lock: got %v, expecting %v",
 			lastHop.OutgoingTimeLock,
-			startingHeight+DefaultFinalCLTVDelta)
+			startingHeight+zpay32.DefaultFinalCLTVDelta)
 	}
 
 	// We'll also make similar assertions for the second route from A to C
@@ -1860,7 +1861,7 @@ func TestPathFindSpecExample(t *testing.T) {
 		t.Fatalf("wrong amount: got %v, expected %v",
 			secondRoute.TotalAmount, expectedAmt)
 	}
-	expectedTimeLock := startingHeight + daveFinalCLTV + DefaultFinalCLTVDelta
+	expectedTimeLock := startingHeight + daveFinalCLTV + zpay32.DefaultFinalCLTVDelta
 	if secondRoute.TotalTimeLock != uint32(expectedTimeLock) {
 		t.Fatalf("wrong total time lock: got %v, expecting %v",
 			secondRoute.TotalTimeLock, expectedTimeLock)
@@ -1870,7 +1871,7 @@ func TestPathFindSpecExample(t *testing.T) {
 		t.Fatalf("wrong forward amount: got %v, expected %v",
 			onionPayload.AmtToForward, amt)
 	}
-	expectedTimeLock = startingHeight + DefaultFinalCLTVDelta
+	expectedTimeLock = startingHeight + zpay32.DefaultFinalCLTVDelta
 	if onionPayload.OutgoingTimeLock != uint32(expectedTimeLock) {
 		t.Fatalf("wrong outgoing time lock: got %v, expecting %v",
 			onionPayload.OutgoingTimeLock,
@@ -1884,11 +1885,11 @@ func TestPathFindSpecExample(t *testing.T) {
 			lastHop.AmtToForward, amt)
 	}
 	if lastHop.OutgoingTimeLock !=
-		startingHeight+DefaultFinalCLTVDelta {
+		startingHeight+zpay32.DefaultFinalCLTVDelta {
 
 		t.Fatalf("wrong total time lock: got %v, expecting %v",
 			lastHop.OutgoingTimeLock,
-			startingHeight+DefaultFinalCLTVDelta)
+			startingHeight+zpay32.DefaultFinalCLTVDelta)
 	}
 }
 

zpay32/hophint.go
@@ -1,0 +1,31 @@
+package zpay32
+
+import "github.com/btcsuite/btcd/btcec"
+
+const (
+	// DefaultFinalCLTVDelta is the default value to be used as the final
+	// CLTV delta for a route if one is unspecified.
+	DefaultFinalCLTVDelta = 9
+)
+
+// HopHint is a routing hint that contains the minimum information of a channel
+// required for an intermediate hop in a route to forward the payment to the
+// next. This should be ideally used for private channels, since they are not
+// publicly advertised to the network for routing.
+type HopHint struct {
+	// NodeID is the public key of the node at the start of the channel.
+	NodeID *btcec.PublicKey
+
+	// ChannelID is the unique identifier of the channel.
+	ChannelID uint64
+
+	// FeeBaseMSat is the base fee of the channel in millisatoshis.
+	FeeBaseMSat uint32
+
+	// FeeProportionalMillionths is the fee rate, in millionths of a
+	// satoshi, for every satoshi sent through the channel.
+	FeeProportionalMillionths uint32
+
+	// CLTVExpiryDelta is the time-lock delta of the channel.
+	CLTVExpiryDelta uint16
+}

matheusd/dcrlnd

routing/missioncontrol.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/lnwire"
+	"github.com/decred/dcrlnd/zpay32"
 )
 
 const (
@@ -153,7 +154,7 @@ func (m *missionControl) GraphPruneView() graphPruneView {
 // view from Mission Control. An optional set of routing hints can be provided
 // in order to populate additional edges to explore when finding a path to the
 // payment's destination.
-func (m *missionControl) NewPaymentSession(routeHints [][]HopHint,
+func (m *missionControl) NewPaymentSession(routeHints [][]zpay32.HopHint,
 	target Vertex) (*paymentSession, error) {
 
 	viewSnapshot := m.GraphPruneView()

routing/pathfind_test.go
@@ -22,6 +22,7 @@ import (
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/lnwire"
+	"github.com/decred/dcrlnd/zpay32"
 )
 
 const (
@@ -1741,11 +1742,11 @@ func TestPathFindSpecExample(t *testing.T) {
 	// The CLTV expiry should be the current height plus 9 (the expiry for
 	// the B -> C channel.
 	if firstRoute.TotalTimeLock !=
-		startingHeight+DefaultFinalCLTVDelta {
+		startingHeight+zpay32.DefaultFinalCLTVDelta {
 
 		t.Fatalf("wrong total time lock: got %v, expecting %v",
 			firstRoute.TotalTimeLock,
-			startingHeight+DefaultFinalCLTVDelta)
+			startingHeight+zpay32.DefaultFinalCLTVDelta)
 	}
 
 	// Next, we'll set A as the source node so we can assert that we create
@@ -1840,11 +1841,11 @@ func TestPathFindSpecExample(t *testing.T) {
 	// The outgoing CLTV value itself should be the current height plus 30
 	// to meet Carol's requirements.
 	if routes[0].Hops[0].OutgoingTimeLock !=
-		startingHeight+DefaultFinalCLTVDelta {
+		startingHeight+zpay32.DefaultFinalCLTVDelta {
 
 		t.Fatalf("wrong total time lock: got %v, expecting %v",
 			routes[0].Hops[0].OutgoingTimeLock,
-			startingHeight+DefaultFinalCLTVDelta)
+			startingHeight+zpay32.DefaultFinalCLTVDelta)
 	}
 
 	// For B -> C, we assert that the final hop also has the proper
@@ -1855,11 +1856,11 @@ func TestPathFindSpecExample(t *testing.T) {
 			lastHop.AmtToForward, amt)
 	}
 	if lastHop.OutgoingTimeLock !=
-		startingHeight+DefaultFinalCLTVDelta {
+		startingHeight+zpay32.DefaultFinalCLTVDelta {
 
 		t.Fatalf("wrong total time lock: got %v, expecting %v",
 			lastHop.OutgoingTimeLock,
-			startingHeight+DefaultFinalCLTVDelta)
+			startingHeight+zpay32.DefaultFinalCLTVDelta)
 	}
 
 	// We'll also make similar assertions for the second route from A to C
@@ -1870,7 +1871,7 @@ func TestPathFindSpecExample(t *testing.T) {
 		t.Fatalf("wrong amount: got %v, expected %v",
 			secondRoute.TotalAmount, expectedAmt)
 	}
-	expectedTimeLock := startingHeight + daveFinalCLTV + DefaultFinalCLTVDelta
+	expectedTimeLock := startingHeight + daveFinalCLTV + zpay32.DefaultFinalCLTVDelta
 	if secondRoute.TotalTimeLock != uint32(expectedTimeLock) {
 		t.Fatalf("wrong total time lock: got %v, expecting %v",
 			secondRoute.TotalTimeLock, expectedTimeLock)
@@ -1880,7 +1881,7 @@ func TestPathFindSpecExample(t *testing.T) {
 		t.Fatalf("wrong forward amount: got %v, expected %v",
 			onionPayload.AmtToForward, amt)
 	}
-	expectedTimeLock = startingHeight + DefaultFinalCLTVDelta
+	expectedTimeLock = startingHeight + zpay32.DefaultFinalCLTVDelta
 	if onionPayload.OutgoingTimeLock != uint32(expectedTimeLock) {
 		t.Fatalf("wrong outgoing time lock: got %v, expecting %v",
 			onionPayload.OutgoingTimeLock,
@@ -1894,11 +1895,11 @@ func TestPathFindSpecExample(t *testing.T) {
 			lastHop.AmtToForward, amt)
 	}
 	if lastHop.OutgoingTimeLock !=
-		startingHeight+DefaultFinalCLTVDelta {
+		startingHeight+zpay32.DefaultFinalCLTVDelta {
 
 		t.Fatalf("wrong total time lock: got %v, expecting %v",
 			lastHop.OutgoingTimeLock,
-			startingHeight+DefaultFinalCLTVDelta)
+			startingHeight+zpay32.DefaultFinalCLTVDelta)
 	}
 }
 

zpay32/hophint.go
@@ -1,0 +1,31 @@
+package zpay32
+
+import "github.com/decred/dcrd/dcrec/secp256k1"
+
+const (
+	// DefaultFinalCLTVDelta is the default value to be used as the final
+	// CLTV delta for a route if one is unspecified.
+	DefaultFinalCLTVDelta = 9
+)
+
+// HopHint is a routing hint that contains the minimum information of a channel
+// required for an intermediate hop in a route to forward the payment to the
+// next. This should be ideally used for private channels, since they are not
+// publicly advertised to the network for routing.
+type HopHint struct {
+	// NodeID is the public key of the node at the start of the channel.
+	NodeID *secp256k1.PublicKey
+
+	// ChannelID is the unique identifier of the channel.
+	ChannelID uint64
+
+	// FeeBaseMAtoms is the base fee of the channel in millisatoshis.
+	FeeBaseMAtoms uint32
+
+	// FeeProportionalMillionths is the fee rate, in millionths of a
+	// satoshi, for every satoshi sent through the channel.
+	FeeProportionalMillionths uint32
+
+	// CLTVExpiryDelta is the time-lock delta of the channel.
+	CLTVExpiryDelta uint16
+}