halseth/lnd

autopilot/graph: set max htlc in test edge policies
Co-authored-by: Johan T. Halseth <johanth@gmail.com>

autopilot/graph.go
@@ -227,9 +227,10 @@ func (d *databaseChannelGraph) addRandChannel(node1, node2 *btcec.PublicKey,
 		LastUpdate:                time.Now(),
 		TimeLockDelta:             10,
 		MinHTLC:                   1,
+		MaxHTLC:                   lnwire.NewMSatFromSatoshis(capacity),
 		FeeBaseMSat:               10,
 		FeeProportionalMillionths: 10000,
-		MessageFlags:              0,
+		MessageFlags:              1,
 		ChannelFlags:              0,
 	}
 
@@ -242,9 +243,10 @@ func (d *databaseChannelGraph) addRandChannel(node1, node2 *btcec.PublicKey,
 		LastUpdate:                time.Now(),
 		TimeLockDelta:             10,
 		MinHTLC:                   1,
+		MaxHTLC:                   lnwire.NewMSatFromSatoshis(capacity),
 		FeeBaseMSat:               10,
 		FeeProportionalMillionths: 10000,
-		MessageFlags:              0,
+		MessageFlags:              1,
 		ChannelFlags:              1,
 	}
 	if err := d.db.UpdateEdgePolicy(edgePolicy); err != nil {

matheusd/dcrlnd

autopilot/graph.go
@@ -227,9 +227,10 @@ func (d *databaseChannelGraph) addRandChannel(node1, node2 *secp256k1.PublicKey,
 		LastUpdate:                time.Now(),
 		TimeLockDelta:             10,
 		MinHTLC:                   1,
+		MaxHTLC:                   lnwire.NewMAtomsFromAtoms(capacity),
 		FeeBaseMAtoms:             10,
 		FeeProportionalMillionths: 10000,
-		MessageFlags:              0,
+		MessageFlags:              1,
 		ChannelFlags:              0,
 	}
 
@@ -242,9 +243,10 @@ func (d *databaseChannelGraph) addRandChannel(node1, node2 *secp256k1.PublicKey,
 		LastUpdate:                time.Now(),
 		TimeLockDelta:             10,
 		MinHTLC:                   1,
+		MaxHTLC:                   lnwire.NewMAtomsFromAtoms(capacity),
 		FeeBaseMAtoms:             10,
 		FeeProportionalMillionths: 10000,
-		MessageFlags:              0,
+		MessageFlags:              1,
 		ChannelFlags:              1,
 	}
 	if err := d.db.UpdateEdgePolicy(edgePolicy); err != nil {