lightningnetwork/lnd

lntest/node: print amounts in same format

lntest/harness.go
@@ -1279,7 +1279,7 @@ func (n *NetworkHarness) sendCoins(ctx context.Context, amt btcutil.Amount,
 	// the target node's unconfirmed balance reflects the expected balance
 	// and exit.
 	if !confirmed {
-		expectedBalance := initialBalance.UnconfirmedBalance + int64(amt)
+		expectedBalance := btcutil.Amount(initialBalance.UnconfirmedBalance) + amt
 		return target.WaitForBalance(expectedBalance, false)
 	}
 
@@ -1290,7 +1290,7 @@ func (n *NetworkHarness) sendCoins(ctx context.Context, amt btcutil.Amount,
 		return err
 	}
 
-	expectedBalance := initialBalance.ConfirmedBalance + int64(amt)
+	expectedBalance := btcutil.Amount(initialBalance.ConfirmedBalance) + amt
 	return target.WaitForBalance(expectedBalance, true)
 }
 

lntest/node.go
@@ -915,7 +915,7 @@ func (hn *HarnessNode) WaitForBlockchainSync(ctx context.Context) error {
 
 // WaitForBalance waits until the node sees the expected confirmed/unconfirmed
 // balance within their wallet.
-func (hn *HarnessNode) WaitForBalance(expectedBalance int64, confirmed bool) error {
+func (hn *HarnessNode) WaitForBalance(expectedBalance btcutil.Amount, confirmed bool) error {
 	ctx := context.Background()
 	req := &lnrpc.WalletBalanceRequest{}
 
@@ -928,11 +928,11 @@ func (hn *HarnessNode) WaitForBalance(expectedBalance int64, confirmed bool) err
 
 		if confirmed {
 			lastBalance = btcutil.Amount(balance.ConfirmedBalance)
-			return balance.ConfirmedBalance == expectedBalance
+			return btcutil.Amount(balance.ConfirmedBalance) == expectedBalance
 		}
 
 		lastBalance = btcutil.Amount(balance.UnconfirmedBalance)
-		return balance.UnconfirmedBalance == expectedBalance
+		return btcutil.Amount(balance.UnconfirmedBalance) == expectedBalance
 	}
 
 	err := WaitPredicate(doesBalanceMatch, 30*time.Second)

matheusd/dcrlnd

lntest/harness.go
@@ -1333,7 +1333,7 @@ func (n *NetworkHarness) sendCoins(ctx context.Context, amt dcrutil.Amount,
 	// the target node's unconfirmed balance reflects the expected balance
 	// and exit.
 	if !confirmed {
-		expectedBalance := initialBalance.UnconfirmedBalance + int64(amt)
+		expectedBalance := dcrutil.Amount(initialBalance.UnconfirmedBalance) + amt
 		return target.WaitForBalance(expectedBalance, false)
 	}
 
@@ -1344,7 +1344,7 @@ func (n *NetworkHarness) sendCoins(ctx context.Context, amt dcrutil.Amount,
 		return err
 	}
 
-	expectedBalance := initialBalance.ConfirmedBalance + int64(amt)
+	expectedBalance := dcrutil.Amount(initialBalance.ConfirmedBalance) + amt
 	return target.WaitForBalance(expectedBalance, true)
 }
 

lntest/node.go
@@ -918,7 +918,7 @@ func (hn *HarnessNode) WaitForBlockchainSync(ctx context.Context) error {
 
 // WaitForBalance waits until the node sees the expected confirmed/unconfirmed
 // balance within their wallet.
-func (hn *HarnessNode) WaitForBalance(expectedBalance int64, confirmed bool) error {
+func (hn *HarnessNode) WaitForBalance(expectedBalance dcrutil.Amount, confirmed bool) error {
 	ctx := context.Background()
 	req := &lnrpc.WalletBalanceRequest{}
 
@@ -931,11 +931,11 @@ func (hn *HarnessNode) WaitForBalance(expectedBalance int64, confirmed bool) err
 
 		if confirmed {
 			lastBalance = dcrutil.Amount(balance.ConfirmedBalance)
-			return balance.ConfirmedBalance == expectedBalance
+			return dcrutil.Amount(balance.ConfirmedBalance) == expectedBalance
 		}
 
 		lastBalance = dcrutil.Amount(balance.UnconfirmedBalance)
-		return balance.UnconfirmedBalance == expectedBalance
+		return dcrutil.Amount(balance.UnconfirmedBalance) == expectedBalance
 	}
 
 	err := WaitPredicate(doesBalanceMatch, 30*time.Second)