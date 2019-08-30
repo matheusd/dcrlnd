halseth/lnd

discovery/gossiper: persist remote channel policy updates' max htlc

discovery/gossiper.go
@@ -2014,6 +2014,7 @@ func (d *AuthenticatedGossiper) processNetworkAnnouncement(
 			ChannelFlags:              msg.ChannelFlags,
 			TimeLockDelta:             msg.TimeLockDelta,
 			MinHTLC:                   msg.HtlcMinimumMsat,
+			MaxHTLC:                   msg.HtlcMaximumMsat,
 			FeeBaseMSat:               lnwire.MilliSatoshi(msg.BaseFee),
 			FeeProportionalMillionths: lnwire.MilliSatoshi(msg.FeeRate),
 			ExtraOpaqueData:           msg.ExtraOpaqueData,

matheusd/dcrlnd

discovery/gossiper.go
@@ -2010,6 +2010,7 @@ func (d *AuthenticatedGossiper) processNetworkAnnouncement(
 			ChannelFlags:              msg.ChannelFlags,
 			TimeLockDelta:             msg.TimeLockDelta,
 			MinHTLC:                   msg.HtlcMinimumMAtoms,
+			MaxHTLC:                   msg.HtlcMaximumMAtoms,
 			FeeBaseMAtoms:             lnwire.MilliAtom(msg.BaseFee),
 			FeeProportionalMillionths: lnwire.MilliAtom(msg.FeeRate),
 			ExtraOpaqueData:           msg.ExtraOpaqueData,