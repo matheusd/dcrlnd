lightningnetwork/lnd

watchtower/multi: embed TxPolicy in wtpolicy.Policy
This commit splits out the parameters that shape the justice transaction
into their own struct, which then embedded within the overarching
wtpolicy.Policy which may have additional parameters describing
operation of the session.

This is done as a preliminary step to support comparison of sessions
based on matching TxPolicy configurations. This prevents otherwise
identical Policies from being counted as different if operational
parameters like MaxUpdates differ, even if it has no material difference
to the justice transaction.

watchtower/wtpolicy/policy.go
@@ -49,27 +49,27 @@ var (
 // used by clients or servers.
 func DefaultPolicy() Policy {
 	return Policy{
-		BlobType:   blob.TypeDefault,
+		TxPolicy: TxPolicy{
+			BlobType:   blob.TypeDefault,
+			RewardRate: DefaultRewardRate,
+			SweepFeeRate: lnwallet.SatPerKWeight(
+				DefaultSweepFeeRate,
+			),
+		},
 		MaxUpdates: DefaultMaxUpdates,
-		RewardRate: DefaultRewardRate,
-		SweepFeeRate: lnwallet.SatPerKWeight(
-			DefaultSweepFeeRate,
-		),
 	}
 }
 
-// Policy defines the negotiated parameters for a session between a client and
-// server. The parameters specify the format of encrypted blobs sent to the
-// tower, the reward schedule for the tower, and the number of encrypted blobs a
-// client can send in one session.
-type Policy struct {
+// TxPolicy defines the negotiate parameters that determine the form of the
+// justice transaction for a given breached state. Thus, for any given revoked
+// state, an identical key will result in an identical justice transaction
+// (barring signatures). The parameters specify the format of encrypted blobs
+// sent to the tower, the reward schedule for the tower, and the number of
+// encrypted blobs a client can send in one session.
+type TxPolicy struct {
 	// BlobType specifies the blob format that must be used by all updates sent
 	// under the session key used to negotiate this session.
 	BlobType blob.Type
-
-	// MaxUpdates is the maximum number of updates the watchtower will honor
-	// for this session.
-	MaxUpdates uint16
 
 	// RewardBase is the fixed amount allocated to the tower when the
 	// policy's blob type specifies a reward for the tower. This is taken
@@ -86,6 +86,18 @@ type Policy struct {
 	// for this session must use this value during construction, and the
 	// signatures must implicitly commit to the resulting output values.
 	SweepFeeRate lnwallet.SatPerKWeight
+}
+
+// Policy defines the negotiated parameters for a session between a client and
+// server. In addition to the TxPolicy that governs the shape of the justice
+// transaction, the Policy also includes features which only affect the
+// operation of the session.
+type Policy struct {
+	TxPolicy
+
+	// MaxUpdates is the maximum number of updates the watchtower will honor
+	// for this session.
+	MaxUpdates uint16
 }
 
 // String returns a human-readable description of the current policy.

matheusd/dcrlnd

watchtower/wtpolicy/policy.go
@@ -49,27 +49,27 @@ var (
 // used by clients or servers.
 func DefaultPolicy() Policy {
 	return Policy{
-		BlobType:   blob.TypeDefault,
+		TxPolicy: TxPolicy{
+			BlobType:   blob.TypeDefault,
+			RewardRate: DefaultRewardRate,
+			SweepFeeRate: lnwallet.AtomPerKByte(
+				DefaultSweepFeeRate,
+			),
+		},
 		MaxUpdates: DefaultMaxUpdates,
-		RewardRate: DefaultRewardRate,
-		SweepFeeRate: lnwallet.AtomPerKByte(
-			DefaultSweepFeeRate,
-		),
 	}
 }
 
-// Policy defines the negotiated parameters for a session between a client and
-// server. The parameters specify the format of encrypted blobs sent to the
-// tower, the reward schedule for the tower, and the number of encrypted blobs a
-// client can send in one session.
-type Policy struct {
+// TxPolicy defines the negotiate parameters that determine the form of the
+// justice transaction for a given breached state. Thus, for any given revoked
+// state, an identical key will result in an identical justice transaction
+// (barring signatures). The parameters specify the format of encrypted blobs
+// sent to the tower, the reward schedule for the tower, and the number of
+// encrypted blobs a client can send in one session.
+type TxPolicy struct {
 	// BlobType specifies the blob format that must be used by all updates sent
 	// under the session key used to negotiate this session.
 	BlobType blob.Type
-
-	// MaxUpdates is the maximum number of updates the watchtower will honor
-	// for this session.
-	MaxUpdates uint16
 
 	// RewardBase is the fixed amount allocated to the tower when the
 	// policy's blob type specifies a reward for the tower. This is taken
@@ -86,6 +86,18 @@ type Policy struct {
 	// for this session must use this value during construction, and the
 	// signatures must implicitly commit to the resulting output values.
 	SweepFeeRate lnwallet.AtomPerKByte
+}
+
+// Policy defines the negotiated parameters for a session between a client and
+// server. In addition to the TxPolicy that governs the shape of the justice
+// transaction, the Policy also includes features which only affect the
+// operation of the session.
+type Policy struct {
+	TxPolicy
+
+	// MaxUpdates is the maximum number of updates the watchtower will honor
+	// for this session.
+	MaxUpdates uint16
 }
 
 // String returns a human-readable description of the current policy.