lightningnetwork/lnd

rpcserver+monitoring/config: enable Prometheus monitoring.
Start the Prometheus exporter in rpcserver.go if monitoring is enabled through the
build tag. Also allow users to specify what address they want the Prometheus
exporter to be listening on.

log.go
@@ -28,6 +28,7 @@ import (
 	"github.com/lightningnetwork/lnd/lnrpc/signrpc"
 	"github.com/lightningnetwork/lnd/lnrpc/walletrpc"
 	"github.com/lightningnetwork/lnd/lnwallet"
+	"github.com/lightningnetwork/lnd/monitoring"
 	"github.com/lightningnetwork/lnd/netann"
 	"github.com/lightningnetwork/lnd/routing"
 	"github.com/lightningnetwork/lnd/signal"
@@ -85,6 +86,7 @@ var (
 	irpcLog = build.NewSubLogger("IRPC", backendLog.Logger)
 	chnfLog = build.NewSubLogger("CHNF", backendLog.Logger)
 	chbuLog = build.NewSubLogger("CHBU", backendLog.Logger)
+	promLog = build.NewSubLogger("PROM", backendLog.Logger)
 )
 
 // Initialize package-global logger variables.
@@ -112,6 +114,7 @@ func init() {
 	invoicesrpc.UseLogger(irpcLog)
 	channelnotifier.UseLogger(chnfLog)
 	chanbackup.UseLogger(chbuLog)
+	monitoring.UseLogger(promLog)
 
 	addSubLogger(routerrpc.Subsystem, routerrpc.UseLogger)
 }
@@ -155,6 +158,7 @@ var subsystemLoggers = map[string]btclog.Logger{
 	"IRPC": irpcLog,
 	"CHNF": chnfLog,
 	"CHBU": chbuLog,
+	"PROM": promLog,
 }
 
 // initLogRotator initializes the logging rotator to write logs to logFile and

rpcserver.go
@@ -28,6 +28,7 @@ import (
 	"github.com/btcsuite/btcwallet/wallet/txauthor"
 	"github.com/coreos/bbolt"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/grpc-ecosystem/go-grpc-middleware"
 	proxy "github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/lightningnetwork/lnd/autopilot"
 	"github.com/lightningnetwork/lnd/build"
@@ -45,6 +46,7 @@ import (
 	"github.com/lightningnetwork/lnd/lnwallet"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/macaroons"
+	"github.com/lightningnetwork/lnd/monitoring"
 	"github.com/lightningnetwork/lnd/routing"
 	"github.com/lightningnetwork/lnd/signal"
 	"github.com/lightningnetwork/lnd/sweep"
@@ -531,19 +533,36 @@ func newRPCServer(s *server, macService *macaroons.Service,
 	}
 
 	// If macaroons aren't disabled (a non-nil service), then we'll set up
-	// our set of interceptors which will allow us handle the macaroon
-	// authentication in a single location .
+	// our set of interceptors which will allow us to handle the macaroon
+	// authentication in a single location.
+	macUnaryInterceptors := []grpc.UnaryServerInterceptor{}
+	macStrmInterceptors := []grpc.StreamServerInterceptor{}
 	if macService != nil {
-		unaryInterceptor := grpc.UnaryInterceptor(
-			macService.UnaryServerInterceptor(permissions),
-		)
-		streamInterceptor := grpc.StreamInterceptor(
-			macService.StreamServerInterceptor(permissions),
-		)
+		unaryInterceptor := macService.UnaryServerInterceptor(permissions)
+		macUnaryInterceptors = append(macUnaryInterceptors, unaryInterceptor)
 
-		serverOpts = append(serverOpts,
-			unaryInterceptor, streamInterceptor,
+		strmInterceptor := macService.StreamServerInterceptor(permissions)
+		macStrmInterceptors = append(macStrmInterceptors, strmInterceptor)
+	}
+
+	// Get interceptors for Prometheus to gather gRPC performance metrics.
+	// If monitoring is disabled, GetPromInterceptors() will return empty
+	// slices.
+	promUnaryInterceptors, promStrmInterceptors := monitoring.GetPromInterceptors()
+
+	// Concatenate the slices of unary and stream interceptors respectively.
+	unaryInterceptors := append(macUnaryInterceptors, promUnaryInterceptors...)
+	strmInterceptors := append(macStrmInterceptors, promStrmInterceptors...)
+
+	// If any interceptors have been set up, add them to the server options.
+	if len(unaryInterceptors) != 0 && len(strmInterceptors) != 0 {
+		chainedUnary := grpc_middleware.WithUnaryServerChain(
+			unaryInterceptors...,
 		)
+		chainedStream := grpc_middleware.WithStreamServerChain(
+			strmInterceptors...,
+		)
+		serverOpts = append(serverOpts, chainedUnary, chainedStream)
 	}
 
 	// Finally, with all the pre-set up complete,  we can create the main
@@ -614,6 +633,16 @@ func (r *rpcServer) Start() error {
 			rpcsLog.Infof("RPC server listening on %s", lis.Addr())
 			r.grpcServer.Serve(lis)
 		}()
+	}
+
+	// If Prometheus monitoring is enabled, start the Prometheus exporter.
+	if cfg.Prometheus.Enabled() {
+		err := monitoring.ExportPrometheusMetrics(
+			r.grpcServer, cfg.Prometheus,
+		)
+		if err != nil {
+			return err
+		}
 	}
 
 	// Finally, start the REST proxy for our gRPC server above. We'll ensure

matheusd/dcrlnd

log.go
@@ -26,6 +26,7 @@ import (
 	"github.com/decred/dcrlnd/lnrpc/walletrpc"
 	"github.com/decred/dcrlnd/lnwallet"
 	"github.com/decred/dcrlnd/lnwallet/dcrwallet"
+	"github.com/decred/dcrlnd/monitoring"
 	"github.com/decred/dcrlnd/netann"
 	"github.com/decred/dcrlnd/routing"
 	"github.com/decred/dcrlnd/signal"
@@ -87,6 +88,7 @@ var (
 	irpcLog = build.NewSubLogger("IRPC", backendLog.Logger)
 	chnfLog = build.NewSubLogger("CHNF", backendLog.Logger)
 	chbuLog = build.NewSubLogger("CHBU", backendLog.Logger)
+	promLog = build.NewSubLogger("PROM", backendLog.Logger)
 )
 
 // Initialize package-global logger variables.
@@ -115,6 +117,7 @@ func init() {
 	invoicesrpc.UseLogger(irpcLog)
 	channelnotifier.UseLogger(chnfLog)
 	chanbackup.UseLogger(chbuLog)
+	monitoring.UseLogger(promLog)
 
 	addSubLogger(routerrpc.Subsystem, routerrpc.UseLogger)
 }
@@ -159,6 +162,7 @@ var subsystemLoggers = map[string]slog.Logger{
 	"IRPC": irpcLog,
 	"CHNF": chnfLog,
 	"CHBU": chbuLog,
+	"PROM": promLog,
 }
 
 // initLogRotator initializes the logging rotator to write logs to logFile and

rpcserver.go
@@ -38,6 +38,7 @@ import (
 	"github.com/decred/dcrlnd/lnwallet"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/macaroons"
+	"github.com/decred/dcrlnd/monitoring"
 	"github.com/decred/dcrlnd/routing"
 	"github.com/decred/dcrlnd/routing/route"
 	"github.com/decred/dcrlnd/signal"
@@ -46,6 +47,7 @@ import (
 	"github.com/decred/dcrwallet/wallet/v2/txauthor"
 	"github.com/decred/dcrwallet/wallet/v2/udb"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware"
 	proxy "github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/tv42/zbase32"
 	bolt "go.etcd.io/bbolt"
@@ -526,19 +528,36 @@ func newRPCServer(s *server, macService *macaroons.Service,
 	}
 
 	// If macaroons aren't disabled (a non-nil service), then we'll set up
-	// our set of interceptors which will allow us handle the macaroon
-	// authentication in a single location .
+	// our set of interceptors which will allow us to handle the macaroon
+	// authentication in a single location.
+	macUnaryInterceptors := []grpc.UnaryServerInterceptor{}
+	macStrmInterceptors := []grpc.StreamServerInterceptor{}
 	if macService != nil {
-		unaryInterceptor := grpc.UnaryInterceptor(
-			macService.UnaryServerInterceptor(permissions),
-		)
-		streamInterceptor := grpc.StreamInterceptor(
-			macService.StreamServerInterceptor(permissions),
-		)
+		unaryInterceptor := macService.UnaryServerInterceptor(permissions)
+		macUnaryInterceptors = append(macUnaryInterceptors, unaryInterceptor)
 
-		serverOpts = append(serverOpts,
-			unaryInterceptor, streamInterceptor,
+		strmInterceptor := macService.StreamServerInterceptor(permissions)
+		macStrmInterceptors = append(macStrmInterceptors, strmInterceptor)
+	}
+
+	// Get interceptors for Prometheus to gather gRPC performance metrics.
+	// If monitoring is disabled, GetPromInterceptors() will return empty
+	// slices.
+	promUnaryInterceptors, promStrmInterceptors := monitoring.GetPromInterceptors()
+
+	// Concatenate the slices of unary and stream interceptors respectively.
+	unaryInterceptors := append(macUnaryInterceptors, promUnaryInterceptors...)
+	strmInterceptors := append(macStrmInterceptors, promStrmInterceptors...)
+
+	// If any interceptors have been set up, add them to the server options.
+	if len(unaryInterceptors) != 0 && len(strmInterceptors) != 0 {
+		chainedUnary := grpc_middleware.WithUnaryServerChain(
+			unaryInterceptors...,
 		)
+		chainedStream := grpc_middleware.WithStreamServerChain(
+			strmInterceptors...,
+		)
+		serverOpts = append(serverOpts, chainedUnary, chainedStream)
 	}
 
 	// Finally, with all the pre-set up complete,  we can create the main
@@ -609,6 +628,16 @@ func (r *rpcServer) Start() error {
 			rpcsLog.Infof("RPC server listening on %s", lis.Addr())
 			r.grpcServer.Serve(lis)
 		}()
+	}
+
+	// If Prometheus monitoring is enabled, start the Prometheus exporter.
+	if cfg.Prometheus.Enabled() {
+		err := monitoring.ExportPrometheusMetrics(
+			r.grpcServer, cfg.Prometheus,
+		)
+		if err != nil {
+			return err
+		}
 	}
 
 	// Finally, start the REST proxy for our gRPC server above. We'll ensure