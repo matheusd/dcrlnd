lightningnetwork/lnd

server+funding: remove unused remoteFundingAmt

server.go
@@ -2979,8 +2979,7 @@ type openChanReq struct {
 
 	chainHash chainhash.Hash
 
-	localFundingAmt  btcutil.Amount
-	remoteFundingAmt btcutil.Amount
+	localFundingAmt btcutil.Amount
 
 	pushAmt lnwire.MilliSatoshi
 

matheusd/dcrlnd

server.go
@@ -3011,8 +3011,7 @@ type openChanReq struct {
 
 	chainHash chainhash.Hash
 
-	localFundingAmt  dcrutil.Amount
-	remoteFundingAmt dcrutil.Amount
+	localFundingAmt dcrutil.Amount
 
 	pushAmt lnwire.MilliAtom
 