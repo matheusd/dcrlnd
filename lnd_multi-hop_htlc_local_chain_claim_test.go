lightningnetwork/lnd

cnct+htlcswitch+invoices: move invoice parameter check out of link
This commit is the final step in making the link unaware of invoices. It
now purely offers the htlc to the invoice registry and follows
instructions from the invoice registry about how and when to respond to
the htlc.

The change also fixes a bug where upon restart, hodl htlcs were
subjected to the invoice minimum cltv delta requirement again. If the
block height has increased in the mean while, the htlc would be canceled
back.

Furthermore the invoice registry interaction is aligned between link and
contract resolvers.

lnd_multi-hop_htlc_local_chain_claim_test.go
@@ -10,7 +10,9 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/lightningnetwork/lnd/lnrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/invoicesrpc"
 	"github.com/lightningnetwork/lnd/lntest"
+	"github.com/lightningnetwork/lnd/lntypes"
 )
 
 // testMultiHopHtlcLocalChainClaim tests that in a multi-hop HTLC scenario, if
@@ -24,21 +26,29 @@ func testMultiHopHtlcLocalChainClaim(net *lntest.NetworkHarness, t *harnessTest)
 	// Carol refusing to actually settle or directly cancel any HTLC's
 	// self.
 	aliceChanPoint, bobChanPoint, carol := createThreeHopNetwork(
-		t, net, true,
+		t, net, false,
 	)
 
 	// Clean up carol's node when the test finishes.
 	defer shutdownAndAssert(net, t, carol)
 
-	// With the network active, we'll now add a new invoice at Carol's end.
-	invoiceReq := &lnrpc.Invoice{
-		Value:      100000,
+	// With the network active, we'll now add a new hodl invoice at Carol's
+	// end. Make sure the cltv expiry delta is large enough, otherwise Bob
+	// won't send out the outgoing htlc.
+
+	const invoiceAmt = 100000
+	preimage := lntypes.Preimage{1, 2, 3}
+	payHash := preimage.Hash()
+	invoiceReq := &invoicesrpc.AddHoldInvoiceRequest{
+		Value:      invoiceAmt,
 		CltvExpiry: 40,
+		Hash:       payHash[:],
 	}
-	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
-	carolInvoice, err := carol.AddInvoice(ctxt, invoiceReq)
+	ctxt, cancel := context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+	carolInvoice, err := carol.AddHoldInvoice(ctxt, invoiceReq)
 	if err != nil {
-		t.Fatalf("unable to generate carol invoice: %v", err)
+		t.Fatalf("unable to add invoice: %v", err)
 	}
 
 	// Now that we've created the invoice, we'll send a single payment from
@@ -58,12 +68,12 @@ func testMultiHopHtlcLocalChainClaim(net *lntest.NetworkHarness, t *harnessTest)
 		t.Fatalf("unable to send payment: %v", err)
 	}
 
-	// We'll now wait until all 3 nodes have the HTLC as just sent fully
-	// locked in.
+	// At this point, all 3 nodes should now have an active channel with
+	// the created HTLC pending on all of them.
 	var predErr error
 	nodes := []*lntest.HarnessNode{net.Alice, net.Bob, carol}
 	err = lntest.WaitPredicate(func() bool {
-		predErr = assertActiveHtlcs(nodes, carolInvoice.RHash)
+		predErr = assertActiveHtlcs(nodes, payHash[:])
 		if predErr != nil {
 			return false
 		}
@@ -71,8 +81,13 @@ func testMultiHopHtlcLocalChainClaim(net *lntest.NetworkHarness, t *harnessTest)
 		return true
 	}, time.Second*15)
 	if err != nil {
-		t.Fatalf("htlc mismatch: %v", err)
+		t.Fatalf("htlc mismatch: %v", predErr)
 	}
+
+	// Wait for carol to mark invoice as accepted. There is a small gap to
+	// bridge between adding the htlc to the channel and executing the exit
+	// hop logic.
+	waitForInvoiceAccepted(t, carol, payHash)
 
 	// At this point, Bob decides that he wants to exit the channel
 	// immediately, so he force closes his commitment transaction.
@@ -85,6 +100,26 @@ func testMultiHopHtlcLocalChainClaim(net *lntest.NetworkHarness, t *harnessTest)
 	if err != nil {
 		t.Fatalf("unable to find alice's sweep tx in miner mempool: %v",
 			err)
+	}
+
+	// Suspend Bob to force Carol to go to chain.
+	restartBob, err := net.SuspendNode(net.Bob)
+	if err != nil {
+		t.Fatalf("unable to suspend bob: %v", err)
+	}
+
+	// Settle invoice. This will just mark the invoice as settled, as there
+	// is no link anymore to remove the htlc from the commitment tx. For
+	// this test, it is important to actually settle and not leave the
+	// invoice in the accepted state, because without a known preimage, the
+	// channel arbitrator won't go to chain.
+	ctx, cancel = context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+	_, err = carol.SettleInvoice(ctx, &invoicesrpc.SettleInvoiceMsg{
+		Preimage: preimage[:],
+	})
+	if err != nil {
+		t.Fatalf("settle invoice: %v", err)
 	}
 
 	// We'll now mine enough blocks so Carol decides that she needs to go
@@ -128,6 +163,11 @@ func testMultiHopHtlcLocalChainClaim(net *lntest.NetworkHarness, t *harnessTest)
 			len(block.Transactions))
 	}
 	assertTxInBlock(t, block, commitHash)
+
+	// Restart bob again.
+	if err := restartBob(); err != nil {
+		t.Fatalf("unable to restart bob: %v", err)
+	}
 
 	// After the force close transacion is mined, Carol should broadcast
 	// her second level HTLC transacion. Bob will broadcast a sweep tx to
@@ -350,5 +390,32 @@ func testMultiHopHtlcLocalChainClaim(net *lntest.NetworkHarness, t *harnessTest)
 	}, time.Second*15)
 	if err != nil {
 		t.Fatalf(predErr.Error())
+	}
+}
+
+// waitForInvoiceAccepted waits until the specified invoice moved to the
+// accepted state by the node.
+func waitForInvoiceAccepted(t *harnessTest, node *lntest.HarnessNode,
+	payHash lntypes.Hash) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+	invoiceUpdates, err := node.SubscribeSingleInvoice(ctx,
+		&invoicesrpc.SubscribeSingleInvoiceRequest{
+			RHash: payHash[:],
+		},
+	)
+	if err != nil {
+		t.Fatalf("subscribe single invoice: %v", err)
+	}
+
+	for {
+		update, err := invoiceUpdates.Recv()
+		if err != nil {
+			t.Fatalf("invoice update err: %v", err)
+		}
+		if update.State == lnrpc.Invoice_ACCEPTED {
+			break
+		}
 	}
 }

lnd_multi-hop_htlc_receiver_chain_claim_test.go
@@ -11,7 +11,9 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/lightningnetwork/lnd/lnrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/invoicesrpc"
 	"github.com/lightningnetwork/lnd/lntest"
+	"github.com/lightningnetwork/lnd/lntypes"
 )
 
 // testMultiHopReceiverChainClaim tests that in the multi-hop setting, if the
@@ -26,24 +28,29 @@ func testMultiHopReceiverChainClaim(net *lntest.NetworkHarness, t *harnessTest) 
 	// Carol refusing to actually settle or directly cancel any HTLC's
 	// self.
 	aliceChanPoint, bobChanPoint, carol := createThreeHopNetwork(
-		t, net, true,
+		t, net, false,
 	)
 
 	// Clean up carol's node when the test finishes.
 	defer shutdownAndAssert(net, t, carol)
 
-	// With the network active, we'll now add a new invoice at Carol's end.
-	// Make sure the cltv expiry delta is large enough, otherwise Bob won't
-	// send out the outgoing htlc.
+	// With the network active, we'll now add a new hodl invoice at Carol's
+	// end. Make sure the cltv expiry delta is large enough, otherwise Bob
+	// won't send out the outgoing htlc.
+
 	const invoiceAmt = 100000
-	invoiceReq := &lnrpc.Invoice{
+	preimage := lntypes.Preimage{1, 2, 4}
+	payHash := preimage.Hash()
+	invoiceReq := &invoicesrpc.AddHoldInvoiceRequest{
 		Value:      invoiceAmt,
 		CltvExpiry: 40,
+		Hash:       payHash[:],
 	}
-	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
-	carolInvoice, err := carol.AddInvoice(ctxt, invoiceReq)
+	ctxt, cancel := context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+	carolInvoice, err := carol.AddHoldInvoice(ctxt, invoiceReq)
 	if err != nil {
-		t.Fatalf("unable to generate carol invoice: %v", err)
+		t.Fatalf("unable to add invoice: %v", err)
 	}
 
 	// Now that we've created the invoice, we'll send a single payment from
@@ -68,7 +75,7 @@ func testMultiHopReceiverChainClaim(net *lntest.NetworkHarness, t *harnessTest) 
 	var predErr error
 	nodes := []*lntest.HarnessNode{net.Alice, net.Bob, carol}
 	err = lntest.WaitPredicate(func() bool {
-		predErr = assertActiveHtlcs(nodes, carolInvoice.RHash)
+		predErr = assertActiveHtlcs(nodes, payHash[:])
 		if predErr != nil {
 			return false
 		}
@@ -77,6 +84,30 @@ func testMultiHopReceiverChainClaim(net *lntest.NetworkHarness, t *harnessTest) 
 	}, time.Second*15)
 	if err != nil {
 		t.Fatalf("htlc mismatch: %v", predErr)
+	}
+
+	// Wait for carol to mark invoice as accepted. There is a small gap to
+	// bridge between adding the htlc to the channel and executing the exit
+	// hop logic.
+	waitForInvoiceAccepted(t, carol, payHash)
+
+	restartBob, err := net.SuspendNode(net.Bob)
+	if err != nil {
+		t.Fatalf("unable to suspend bob: %v", err)
+	}
+
+	// Settle invoice. This will just mark the invoice as settled, as there
+	// is no link anymore to remove the htlc from the commitment tx. For
+	// this test, it is important to actually settle and not leave the
+	// invoice in the accepted state, because without a known preimage, the
+	// channel arbitrator won't go to chain.
+	ctx, cancel = context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+	_, err = carol.SettleInvoice(ctx, &invoicesrpc.SettleInvoiceMsg{
+		Preimage: preimage[:],
+	})
+	if err != nil {
+		t.Fatalf("settle invoice: %v", err)
 	}
 
 	// Now we'll mine enough blocks to prompt carol to actually go to the
@@ -122,6 +153,11 @@ func testMultiHopReceiverChainClaim(net *lntest.NetworkHarness, t *harnessTest) 
 
 	// Confirm the commitment.
 	mineBlocks(t, net, 1, 1)
+
+	// Restart bob again.
+	if err := restartBob(); err != nil {
+		t.Fatalf("unable to restart bob: %v", err)
+	}
 
 	// After the force close transaction is mined, Carol should broadcast
 	// her second level HTLC transaction. Bob will broadcast a sweep tx to

lnd_multi-hop_htlc_remote_chain_claim_test.go
@@ -10,7 +10,9 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/lightningnetwork/lnd/lnrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/invoicesrpc"
 	"github.com/lightningnetwork/lnd/lntest"
+	"github.com/lightningnetwork/lnd/lntypes"
 )
 
 // testMultiHopHtlcRemoteChainClaim tests that in the multi-hop HTLC scenario,
@@ -24,22 +26,28 @@ func testMultiHopHtlcRemoteChainClaim(net *lntest.NetworkHarness, t *harnessTest
 	// Carol refusing to actually settle or directly cancel any HTLC's
 	// self.
 	aliceChanPoint, bobChanPoint, carol := createThreeHopNetwork(
-		t, net, true,
+		t, net, false,
 	)
 
 	// Clean up carol's node when the test finishes.
 	defer shutdownAndAssert(net, t, carol)
 
-	// With the network active, we'll now add a new invoice at Carol's end.
+	// With the network active, we'll now add a new hodl invoice at Carol's
+	// end. Make sure the cltv expiry delta is large enough, otherwise Bob
+	// won't send out the outgoing htlc.
 	const invoiceAmt = 100000
-	invoiceReq := &lnrpc.Invoice{
+	preimage := lntypes.Preimage{1, 2, 5}
+	payHash := preimage.Hash()
+	invoiceReq := &invoicesrpc.AddHoldInvoiceRequest{
 		Value:      invoiceAmt,
 		CltvExpiry: 40,
+		Hash:       payHash[:],
 	}
-	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
-	carolInvoice, err := carol.AddInvoice(ctxt, invoiceReq)
+	ctxt, cancel := context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+	carolInvoice, err := carol.AddHoldInvoice(ctxt, invoiceReq)
 	if err != nil {
-		t.Fatalf("unable to generate carol invoice: %v", err)
+		t.Fatalf("unable to add invoice: %v", err)
 	}
 
 	// Now that we've created the invoice, we'll send a single payment from
@@ -59,12 +67,12 @@ func testMultiHopHtlcRemoteChainClaim(net *lntest.NetworkHarness, t *harnessTest
 		t.Fatalf("unable to send payment: %v", err)
 	}
 
-	// We'll now wait until all 3 nodes have the HTLC as just sent fully
-	// locked in.
+	// At this point, all 3 nodes should now have an active channel with
+	// the created HTLC pending on all of them.
 	var predErr error
 	nodes := []*lntest.HarnessNode{net.Alice, net.Bob, carol}
 	err = lntest.WaitPredicate(func() bool {
-		predErr = assertActiveHtlcs(nodes, carolInvoice.RHash)
+		predErr = assertActiveHtlcs(nodes, payHash[:])
 		if predErr != nil {
 			return false
 		}
@@ -72,8 +80,13 @@ func testMultiHopHtlcRemoteChainClaim(net *lntest.NetworkHarness, t *harnessTest
 		return true
 	}, time.Second*15)
 	if err != nil {
-		t.Fatalf("htlc mismatch: %v", err)
+		t.Fatalf("htlc mismatch: %v", predErr)
 	}
+
+	// Wait for carol to mark invoice as accepted. There is a small gap to
+	// bridge between adding the htlc to the channel and executing the exit
+	// hop logic.
+	waitForInvoiceAccepted(t, carol, payHash)
 
 	// Next, Alice decides that she wants to exit the channel, so she'll
 	// immediately force close the channel by broadcast her commitment
@@ -100,6 +113,26 @@ func testMultiHopHtlcRemoteChainClaim(net *lntest.NetworkHarness, t *harnessTest
 	_, err = waitForTxInMempool(net.Miner.Node, minerMempoolTimeout)
 	if err != nil {
 		t.Fatalf("unable to find sweeping tx in mempool: %v", err)
+	}
+
+	// Suspend bob, so Carol is forced to go on chain.
+	restartBob, err := net.SuspendNode(net.Bob)
+	if err != nil {
+		t.Fatalf("unable to suspend bob: %v", err)
+	}
+
+	// Settle invoice. This will just mark the invoice as settled, as there
+	// is no link anymore to remove the htlc from the commitment tx. For
+	// this test, it is important to actually settle and not leave the
+	// invoice in the accepted state, because without a known preimage, the
+	// channel arbitrator won't go to chain.
+	ctx, cancel = context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+	_, err = carol.SettleInvoice(ctx, &invoicesrpc.SettleInvoiceMsg{
+		Preimage: preimage[:],
+	})
+	if err != nil {
+		t.Fatalf("settle invoice: %v", err)
 	}
 
 	// We'll now mine enough blocks so Carol decides that she needs to go
@@ -143,6 +176,11 @@ func testMultiHopHtlcRemoteChainClaim(net *lntest.NetworkHarness, t *harnessTest
 			len(block.Transactions))
 	}
 	assertTxInBlock(t, block, commitHash)
+
+	// Restart bob again.
+	if err := restartBob(); err != nil {
+		t.Fatalf("unable to restart bob: %v", err)
+	}
 
 	// After the force close transacion is mined, Carol should broadcast
 	// her second level HTLC transacion. Bob will broadcast a sweep tx to

matheusd/dcrlnd

lnd_multi-hop_htlc_local_chain_claim_test.go
@@ -10,7 +10,9 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/lnrpc"
+	"github.com/decred/dcrlnd/lnrpc/invoicesrpc"
 	"github.com/decred/dcrlnd/lntest"
+	"github.com/decred/dcrlnd/lntypes"
 )
 
 // testMultiHopHtlcLocalChainClaim tests that in a multi-hop HTLC scenario, if
@@ -24,24 +26,29 @@ func testMultiHopHtlcLocalChainClaim(net *lntest.NetworkHarness, t *harnessTest)
 	// Carol refusing to actually settle or directly cancel any HTLC's
 	// self.
 	aliceChanPoint, bobChanPoint, carol := createThreeHopNetwork(
-		t, net, true,
+		t, net, false,
 	)
 
 	// Clean up carol's node when the test finishes.
 	defer shutdownAndAssert(net, t, carol)
 
-	// With the network active, we'll now add a new invoice at Carol's end.
-	// Make sure the cltv expiry delta is large enough, otherwise Bob won't
-	// send out the outgoing htlc.
+	// With the network active, we'll now add a new hodl invoice at Carol's
+	// end. Make sure the cltv expiry delta is large enough, otherwise Bob
+	// won't send out the outgoing htlc.
+
 	const invoiceAmt = 100000
-	invoiceReq := &lnrpc.Invoice{
+	preimage := lntypes.Preimage{1, 2, 3}
+	payHash := preimage.Hash()
+	invoiceReq := &invoicesrpc.AddHoldInvoiceRequest{
 		Value:      invoiceAmt,
 		CltvExpiry: 40,
+		Hash:       payHash[:],
 	}
-	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
-	carolInvoice, err := carol.AddInvoice(ctxt, invoiceReq)
+	ctxt, cancel := context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+	carolInvoice, err := carol.AddHoldInvoice(ctxt, invoiceReq)
 	if err != nil {
-		t.Fatalf("unable to generate carol invoice: %v", err)
+		t.Fatalf("unable to add invoice: %v", err)
 	}
 
 	// Now that we've created the invoice, we'll send a single payment from
@@ -61,17 +68,22 @@ func testMultiHopHtlcLocalChainClaim(net *lntest.NetworkHarness, t *harnessTest)
 		t.Fatalf("unable to send payment: %v", err)
 	}
 
-	// We'll now wait until all 3 nodes have the HTLC as just sent fully
-	// locked in.
+	// At this point, all 3 nodes should now have an active channel with
+	// the created HTLC pending on all of them.
 	var predErr error
 	nodes := []*lntest.HarnessNode{net.Alice, net.Bob, carol}
 	err = lntest.WaitPredicate(func() bool {
-		predErr = assertActiveHtlcs(nodes, carolInvoice.RHash)
+		predErr = assertActiveHtlcs(nodes, payHash[:])
 		return predErr == nil
 	}, time.Second*15)
 	if err != nil {
-		t.Fatalf("htlc mismatch: %v", err)
+		t.Fatalf("htlc mismatch: %v", predErr)
 	}
+
+	// Wait for carol to mark invoice as accepted. There is a small gap to
+	// bridge between adding the htlc to the channel and executing the exit
+	// hop logic.
+	waitForInvoiceAccepted(t, carol, payHash)
 
 	// At this point, Bob decides that he wants to exit the channel
 	// immediately, so he force closes his commitment transaction.
@@ -84,6 +96,26 @@ func testMultiHopHtlcLocalChainClaim(net *lntest.NetworkHarness, t *harnessTest)
 	if err != nil {
 		t.Fatalf("unable to find alice's sweep tx in miner mempool: %v",
 			err)
+	}
+
+	// Suspend Bob to force Carol to go to chain.
+	restartBob, err := net.SuspendNode(net.Bob)
+	if err != nil {
+		t.Fatalf("unable to suspend bob: %v", err)
+	}
+
+	// Settle invoice. This will just mark the invoice as settled, as there
+	// is no link anymore to remove the htlc from the commitment tx. For
+	// this test, it is important to actually settle and not leave the
+	// invoice in the accepted state, because without a known preimage, the
+	// channel arbitrator won't go to chain.
+	ctx, cancel = context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+	_, err = carol.SettleInvoice(ctx, &invoicesrpc.SettleInvoiceMsg{
+		Preimage: preimage[:],
+	})
+	if err != nil {
+		t.Fatalf("settle invoice: %v", err)
 	}
 
 	// We'll now mine enough blocks so Carol decides that she needs to go
@@ -126,6 +158,11 @@ func testMultiHopHtlcLocalChainClaim(net *lntest.NetworkHarness, t *harnessTest)
 			len(block.Transactions))
 	}
 	assertTxInBlock(t, block, commitHash)
+
+	// Restart bob again.
+	if err := restartBob(); err != nil {
+		t.Fatalf("unable to restart bob: %v", err)
+	}
 
 	// After the force close transacion is mined, Carol should broadcast
 	// her second level HTLC transacion. Bob will broadcast a sweep tx to
@@ -348,5 +385,32 @@ func testMultiHopHtlcLocalChainClaim(net *lntest.NetworkHarness, t *harnessTest)
 	}, time.Second*15)
 	if err != nil {
 		t.Fatalf(predErr.Error())
+	}
+}
+
+// waitForInvoiceAccepted waits until the specified invoice moved to the
+// accepted state by the node.
+func waitForInvoiceAccepted(t *harnessTest, node *lntest.HarnessNode,
+	payHash lntypes.Hash) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+	invoiceUpdates, err := node.SubscribeSingleInvoice(ctx,
+		&invoicesrpc.SubscribeSingleInvoiceRequest{
+			RHash: payHash[:],
+		},
+	)
+	if err != nil {
+		t.Fatalf("subscribe single invoice: %v", err)
+	}
+
+	for {
+		update, err := invoiceUpdates.Recv()
+		if err != nil {
+			t.Fatalf("invoice update err: %v", err)
+		}
+		if update.State == lnrpc.Invoice_ACCEPTED {
+			break
+		}
 	}
 }

lnd_multi-hop_htlc_receiver_chain_claim_test.go
@@ -11,7 +11,9 @@ import (
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/lnrpc"
+	"github.com/decred/dcrlnd/lnrpc/invoicesrpc"
 	"github.com/decred/dcrlnd/lntest"
+	"github.com/decred/dcrlnd/lntypes"
 )
 
 // testMultiHopReceiverChainClaim tests that in the multi-hop setting, if the
@@ -26,24 +28,29 @@ func testMultiHopReceiverChainClaim(net *lntest.NetworkHarness, t *harnessTest) 
 	// Carol refusing to actually settle or directly cancel any HTLC's
 	// self.
 	aliceChanPoint, bobChanPoint, carol := createThreeHopNetwork(
-		t, net, true,
+		t, net, false,
 	)
 
 	// Clean up carol's node when the test finishes.
 	defer shutdownAndAssert(net, t, carol)
 
-	// With the network active, we'll now add a new invoice at Carol's end.
-	// Make sure the cltv expiry delta is large enough, otherwise Bob won't
-	// send out the outgoing htlc.
+	// With the network active, we'll now add a new hodl invoice at Carol's
+	// end. Make sure the cltv expiry delta is large enough, otherwise Bob
+	// won't send out the outgoing htlc.
+
 	const invoiceAmt = 100000
-	invoiceReq := &lnrpc.Invoice{
+	preimage := lntypes.Preimage{1, 2, 4}
+	payHash := preimage.Hash()
+	invoiceReq := &invoicesrpc.AddHoldInvoiceRequest{
 		Value:      invoiceAmt,
 		CltvExpiry: 40,
+		Hash:       payHash[:],
 	}
-	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
-	carolInvoice, err := carol.AddInvoice(ctxt, invoiceReq)
+	ctxt, cancel := context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+	carolInvoice, err := carol.AddHoldInvoice(ctxt, invoiceReq)
 	if err != nil {
-		t.Fatalf("unable to generate carol invoice: %v", err)
+		t.Fatalf("unable to add invoice: %v", err)
 	}
 
 	// Now that we've created the invoice, we'll send a single payment from
@@ -68,11 +75,35 @@ func testMultiHopReceiverChainClaim(net *lntest.NetworkHarness, t *harnessTest) 
 	var predErr error
 	nodes := []*lntest.HarnessNode{net.Alice, net.Bob, carol}
 	err = lntest.WaitPredicate(func() bool {
-		predErr = assertActiveHtlcs(nodes, carolInvoice.RHash)
+		predErr = assertActiveHtlcs(nodes, payHash[:])
 		return predErr == nil
 	}, time.Second*15)
 	if err != nil {
 		t.Fatalf("htlc mismatch: %v", predErr)
+	}
+
+	// Wait for carol to mark invoice as accepted. There is a small gap to
+	// bridge between adding the htlc to the channel and executing the exit
+	// hop logic.
+	waitForInvoiceAccepted(t, carol, payHash)
+
+	restartBob, err := net.SuspendNode(net.Bob)
+	if err != nil {
+		t.Fatalf("unable to suspend bob: %v", err)
+	}
+
+	// Settle invoice. This will just mark the invoice as settled, as there
+	// is no link anymore to remove the htlc from the commitment tx. For
+	// this test, it is important to actually settle and not leave the
+	// invoice in the accepted state, because without a known preimage, the
+	// channel arbitrator won't go to chain.
+	ctx, cancel = context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+	_, err = carol.SettleInvoice(ctx, &invoicesrpc.SettleInvoiceMsg{
+		Preimage: preimage[:],
+	})
+	if err != nil {
+		t.Fatalf("settle invoice: %v", err)
 	}
 
 	// Now we'll mine enough blocks to prompt carol to actually go to the
@@ -116,6 +147,11 @@ func testMultiHopReceiverChainClaim(net *lntest.NetworkHarness, t *harnessTest) 
 
 	// Confirm the commitment.
 	mineBlocks(t, net, 1, 1)
+
+	// Restart bob again.
+	if err := restartBob(); err != nil {
+		t.Fatalf("unable to restart bob: %v", err)
+	}
 
 	// After the force close transaction is mined, Carol should broadcast
 	// her second level HTLC transaction. Bob will broadcast a sweep tx to

lnd_multi-hop_htlc_remote_chain_claim_test.go
@@ -10,7 +10,9 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/lnrpc"
+	"github.com/decred/dcrlnd/lnrpc/invoicesrpc"
 	"github.com/decred/dcrlnd/lntest"
+	"github.com/decred/dcrlnd/lntypes"
 )
 
 // testMultiHopHtlcRemoteChainClaim tests that in the multi-hop HTLC scenario,
@@ -24,22 +26,28 @@ func testMultiHopHtlcRemoteChainClaim(net *lntest.NetworkHarness, t *harnessTest
 	// Carol refusing to actually settle or directly cancel any HTLC's
 	// self.
 	aliceChanPoint, bobChanPoint, carol := createThreeHopNetwork(
-		t, net, true,
+		t, net, false,
 	)
 
 	// Clean up carol's node when the test finishes.
 	defer shutdownAndAssert(net, t, carol)
 
-	// With the network active, we'll now add a new invoice at Carol's end.
+	// With the network active, we'll now add a new hodl invoice at Carol's
+	// end. Make sure the cltv expiry delta is large enough, otherwise Bob
+	// won't send out the outgoing htlc.
 	const invoiceAmt = 100000
-	invoiceReq := &lnrpc.Invoice{
+	preimage := lntypes.Preimage{1, 2, 5}
+	payHash := preimage.Hash()
+	invoiceReq := &invoicesrpc.AddHoldInvoiceRequest{
 		Value:      invoiceAmt,
 		CltvExpiry: 40,
+		Hash:       payHash[:],
 	}
-	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
-	carolInvoice, err := carol.AddInvoice(ctxt, invoiceReq)
+	ctxt, cancel := context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+	carolInvoice, err := carol.AddHoldInvoice(ctxt, invoiceReq)
 	if err != nil {
-		t.Fatalf("unable to generate carol invoice: %v", err)
+		t.Fatalf("unable to add invoice: %v", err)
 	}
 
 	// Now that we've created the invoice, we'll send a single payment from
@@ -59,17 +67,22 @@ func testMultiHopHtlcRemoteChainClaim(net *lntest.NetworkHarness, t *harnessTest
 		t.Fatalf("unable to send payment: %v", err)
 	}
 
-	// We'll now wait until all 3 nodes have the HTLC as just sent fully
-	// locked in.
+	// At this point, all 3 nodes should now have an active channel with
+	// the created HTLC pending on all of them.
 	var predErr error
 	nodes := []*lntest.HarnessNode{net.Alice, net.Bob, carol}
 	err = lntest.WaitPredicate(func() bool {
-		predErr = assertActiveHtlcs(nodes, carolInvoice.RHash)
+		predErr = assertActiveHtlcs(nodes, payHash[:])
 		return predErr == nil
 	}, time.Second*15)
 	if err != nil {
-		t.Fatalf("htlc mismatch: %v", err)
+		t.Fatalf("htlc mismatch: %v", predErr)
 	}
+
+	// Wait for carol to mark invoice as accepted. There is a small gap to
+	// bridge between adding the htlc to the channel and executing the exit
+	// hop logic.
+	waitForInvoiceAccepted(t, carol, payHash)
 
 	// Next, Alice decides that she wants to exit the channel, so she'll
 	// immediately force close the channel by broadcast her commitment
@@ -96,6 +109,26 @@ func testMultiHopHtlcRemoteChainClaim(net *lntest.NetworkHarness, t *harnessTest
 	_, err = waitForTxInMempool(net.Miner.Node, minerMempoolTimeout)
 	if err != nil {
 		t.Fatalf("unable to find sweeping tx in mempool: %v", err)
+	}
+
+	// Suspend bob, so Carol is forced to go on chain.
+	restartBob, err := net.SuspendNode(net.Bob)
+	if err != nil {
+		t.Fatalf("unable to suspend bob: %v", err)
+	}
+
+	// Settle invoice. This will just mark the invoice as settled, as there
+	// is no link anymore to remove the htlc from the commitment tx. For
+	// this test, it is important to actually settle and not leave the
+	// invoice in the accepted state, because without a known preimage, the
+	// channel arbitrator won't go to chain.
+	ctx, cancel = context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+	_, err = carol.SettleInvoice(ctx, &invoicesrpc.SettleInvoiceMsg{
+		Preimage: preimage[:],
+	})
+	if err != nil {
+		t.Fatalf("settle invoice: %v", err)
 	}
 
 	// We'll now mine enough blocks so Carol decides that she needs to go
@@ -138,6 +171,11 @@ func testMultiHopHtlcRemoteChainClaim(net *lntest.NetworkHarness, t *harnessTest
 			len(block.Transactions))
 	}
 	assertTxInBlock(t, block, commitHash)
+
+	// Restart bob again.
+	if err := restartBob(); err != nil {
+		t.Fatalf("unable to restart bob: %v", err)
+	}
 
 	// After the force close transacion is mined, Carol should broadcast
 	// her second level HTLC transacion. Bob will broadcast a sweep tx to