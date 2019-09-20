7321cba6ac3ac5989a3911a17228d0916e5287c9
Oliver Gugger <gugger@gmail.com>
2019-09-20 16:01:31 +0200

    build: move log rotator code

--- log.go
+++ log.go
@@ -2,10 +2,6 @@ package dcrlnd
 
 import (
 	"context"
-	"fmt"
-	"io"
-	"os"
-	"path/filepath"
 
 	"github.com/decred/dcrd/connmgr"
 	"github.com/decred/dcrlnd/autopilot"
@@ -177,55 +173,6 @@ var subsystemLoggers = map[string]slog.Logger{
 	"PROM": promLog,
 	"WTCL": wtclLog,
 	"PRNF": prnfLog,
-}
-
-// initLogRotator initializes the logging rotator to write logs to logFile and
-// create roll files in the same directory.  It must be called before the
-// package-global log rotator variables are used.
-func initLogRotator(logFile string, maxLogFileSize, maxLogFiles int) {
-	logDir, _ := filepath.Split(logFile)
-	err := os.MkdirAll(logDir, 0700)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to create log directory: %v\n", err)
-		os.Exit(1)
-	}
-	r, err := rotator.New(logFile, int64(maxLogFileSize*1024), false, maxLogFiles)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to create file rotator: %v\n", err)
-		os.Exit(1)
-	}
-
-	pr, pw := io.Pipe()
-	go r.Run(pr)
-
-	logWriter.RotatorPipe = pw
-	logRotator = r
-}
-
-// setLogLevel sets the logging level for provided subsystem.  Invalid
-// subsystems are ignored.  Uninitialized subsystems are dynamically created as
-// needed.
-func setLogLevel(subsystemID string, logLevel string) {
-	// Ignore invalid subsystems.
-	logger, ok := subsystemLoggers[subsystemID]
-	if !ok {
-		return
-	}
-
-	// Defaults to info if the log level is invalid.
-	level, _ := slog.LevelFromString(logLevel)
-	logger.SetLevel(level)
-}
-
-// setLogLevels sets the log level for all subsystem loggers to the passed
-// level. It also dynamically creates the subsystem loggers as needed, so it
-// can be used to initialize the logging system.
-func setLogLevels(logLevel string) {
-	// Configure all sub-systems with the new logging level.  Dynamically
-	// create loggers as needed.
-	for subsystemID := range subsystemLoggers {
-		setLogLevel(subsystemID, logLevel)
-	}
 }
 
 // logClosure is used to provide a closure over expensive logging operations so
