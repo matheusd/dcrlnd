tanx/lnd

lnd+cmd/lnd/main: add ListenerCfg to Main
ListenerCfg allows passing custom listeners to the main method, to be
used for the wallet unlocker and rpc server. If these are set these will
be used instead of the regular RPC listeners.

cmd/lnd/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	// Call the "real" main in a nested manner so the defers will properly
 	// be executed in the case of a graceful shutdown.
-	if err := lnd.Main(); err != nil {
+	if err := lnd.Main(lnd.ListenerCfg{}); err != nil {
 		if e, ok := err.(*flags.Error); ok && e.Type == flags.ErrHelp {
 		} else {
 			fmt.Fprintln(os.Stderr, err)

matheusd/dcrlnd

cmd/dcrlnd/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	// Call the "real" main in a nested manner so the defers will properly
 	// be executed in the case of a graceful shutdown.
-	if err := dcrlnd.Main(); err != nil {
+	if err := dcrlnd.Main(dcrlnd.ListenerCfg{}); err != nil {
 		if e, ok := err.(*flags.Error); ok && e.Type == flags.ErrHelp {
 		} else {
 			fmt.Fprintln(os.Stderr, err)