joostjager/lnd

htlcswitch/hop: move NetworkHop to hop.Network

htlcswitch/iterator.go
@@ -9,38 +9,11 @@ import (
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/lightningnetwork/lightning-onion"
+	"github.com/lightningnetwork/lnd/htlcswitch/hop"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/record"
 	"github.com/lightningnetwork/lnd/tlv"
 )
-
-// NetworkHop indicates the blockchain network that is intended to be the next
-// hop for a forwarded HTLC. The existence of this field within the
-// ForwardingInfo struct enables the ability for HTLC to cross chain-boundaries
-// at will.
-type NetworkHop uint8
-
-const (
-	// BitcoinHop denotes that an HTLC is to be forwarded along the Bitcoin
-	// link with the specified short channel ID.
-	BitcoinHop NetworkHop = iota
-
-	// LitecoinHop denotes that an HTLC is to be forwarded along the
-	// Litecoin link with the specified short channel ID.
-	LitecoinHop
-)
-
-// String returns the string representation of the target NetworkHop.
-func (c NetworkHop) String() string {
-	switch c {
-	case BitcoinHop:
-		return "Bitcoin"
-	case LitecoinHop:
-		return "Litecoin"
-	default:
-		return "Kekcoin"
-	}
-}
 
 var (
 	// exitHop is a special "hop" which denotes that an incoming HTLC is
@@ -60,7 +33,7 @@ var (
 type ForwardingInfo struct {
 	// Network is the target blockchain network that the HTLC will travel
 	// over next.
-	Network NetworkHop
+	Network hop.Network
 
 	// NextHop is the channel ID of the next hop. The received HTLC should
 	// be forwarded to this particular channel in order to continue the
@@ -207,7 +180,7 @@ func (r *sphinxHopIterator) ForwardingInstructions() (ForwardingInfo, error) {
 	}
 
 	return ForwardingInfo{
-		Network:         BitcoinHop,
+		Network:         hop.BitcoinNetwork,
 		NextHop:         nextHop,
 		AmountToForward: lnwire.MilliSatoshi(amt),
 		OutgoingCTLV:    cltv,

htlcswitch/link_test.go
@@ -26,6 +26,7 @@ import (
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/contractcourt"
 	"github.com/lightningnetwork/lnd/htlcswitch/hodl"
+	"github.com/lightningnetwork/lnd/htlcswitch/hop"
 	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/lnpeer"
 	"github.com/lightningnetwork/lnd/lntypes"
@@ -4288,7 +4289,7 @@ func generateHtlcAndInvoice(t *testing.T,
 	htlcExpiry := testStartingHeight + testInvoiceCltvExpiry
 	hops := []ForwardingInfo{
 		{
-			Network:         BitcoinHop,
+			Network:         hop.BitcoinNetwork,
 			NextHop:         exitHop,
 			AmountToForward: htlcAmt,
 			OutgoingCTLV:    uint32(htlcExpiry),

htlcswitch/mock.go
@@ -23,6 +23,7 @@ import (
 	"github.com/lightningnetwork/lnd/chainntnfs"
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/contractcourt"
+	"github.com/lightningnetwork/lnd/htlcswitch/hop"
 	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/invoices"
 	"github.com/lightningnetwork/lnd/lnpeer"
@@ -485,7 +486,7 @@ func (f *ForwardingInfo) decode(r io.Reader) error {
 	if _, err := r.Read(net[:]); err != nil {
 		return err
 	}
-	f.Network = NetworkHop(net[0])
+	f.Network = hop.Network(net[0])
 
 	if err := binary.Read(r, binary.BigEndian, &f.NextHop); err != nil {
 		return err

htlcswitch/test_utils.go
@@ -25,6 +25,7 @@ import (
 	"github.com/go-errors/errors"
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/contractcourt"
+	"github.com/lightningnetwork/lnd/htlcswitch/hop"
 	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lnpeer"
@@ -680,7 +681,7 @@ func generateHops(payAmt lnwire.MilliSatoshi, startingHeight uint32,
 		}
 
 		hops[i] = ForwardingInfo{
-			Network:         BitcoinHop,
+			Network:         hop.BitcoinNetwork,
 			NextHop:         nextHop,
 			AmountToForward: amount,
 			OutgoingCTLV:    timeLock,

matheusd/dcrlnd

htlcswitch/iterator.go
@@ -7,33 +7,12 @@ import (
 	"io"
 
 	"github.com/decred/dcrd/dcrec/secp256k1"
+	"github.com/decred/dcrlnd/htlcswitch/hop"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/record"
 	"github.com/decred/dcrlnd/tlv"
 	sphinx "github.com/decred/lightning-onion/v2"
 )
-
-// NetworkHop indicates the blockchain network that is intended to be the next
-// hop for a forwarded HTLC. The existence of this field within the
-// ForwardingInfo struct enables the ability for HTLC to cross chain-boundaries
-// at will.
-type NetworkHop uint8
-
-const (
-	// DecredHop denotes that an HTLC is to be forwarded along the
-	// Decred link with the specified short channel ID.
-	DecredHop = iota
-)
-
-// String returns the string representation of the target NetworkHop.
-func (c NetworkHop) String() string {
-	switch c {
-	case DecredHop:
-		return "Decred"
-	default:
-		return "Kekcoin"
-	}
-}
 
 var (
 	// exitHop is a special "hop" which denotes that an incoming HTLC is
@@ -53,7 +32,7 @@ var (
 type ForwardingInfo struct {
 	// Network is the target blockchain network that the HTLC will travel
 	// over next.
-	Network NetworkHop
+	Network hop.Network
 
 	// NextHop is the channel ID of the next hop. The received HTLC should
 	// be forwarded to this particular channel in order to continue the
@@ -193,7 +172,7 @@ func (r *sphinxHopIterator) ForwardingInstructions() (ForwardingInfo, error) {
 	}
 
 	return ForwardingInfo{
-		Network:         DecredHop,
+		Network:         hop.DecredNetwork,
 		NextHop:         nextHop,
 		AmountToForward: lnwire.MilliAtom(amt),
 		OutgoingCTLV:    cltv,

htlcswitch/link_test.go
@@ -27,6 +27,7 @@ import (
 	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/contractcourt"
 	"github.com/decred/dcrlnd/htlcswitch/hodl"
+	"github.com/decred/dcrlnd/htlcswitch/hop"
 	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/lnpeer"
 	"github.com/decred/dcrlnd/lntypes"
@@ -4311,7 +4312,7 @@ func generateHtlcAndInvoice(t *testing.T,
 	htlcExpiry := testStartingHeight + testInvoiceCltvExpiry
 	hops := []ForwardingInfo{
 		{
-			Network:         DecredHop,
+			Network:         hop.DecredNetwork,
 			NextHop:         exitHop,
 			AmountToForward: htlcAmt,
 			OutgoingCTLV:    uint32(htlcExpiry),

htlcswitch/mock.go
@@ -21,6 +21,7 @@ import (
 	"github.com/decred/dcrlnd/chainntnfs"
 	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/contractcourt"
+	"github.com/decred/dcrlnd/htlcswitch/hop"
 	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/invoices"
 	"github.com/decred/dcrlnd/lnpeer"
@@ -485,7 +486,7 @@ func (f *ForwardingInfo) decode(r io.Reader) error {
 	if _, err := r.Read(net[:]); err != nil {
 		return err
 	}
-	f.Network = NetworkHop(net[0])
+	f.Network = hop.Network(net[0])
 
 	if err := binary.Read(r, binary.BigEndian, &f.NextHop); err != nil {
 		return err

htlcswitch/test_utils.go
@@ -22,6 +22,7 @@ import (
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/contractcourt"
+	"github.com/decred/dcrlnd/htlcswitch/hop"
 	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/keychain"
 	"github.com/decred/dcrlnd/lnpeer"
@@ -679,7 +680,7 @@ func generateHops(payAmt lnwire.MilliAtom, startingHeight uint32,
 		}
 
 		hops[i] = ForwardingInfo{
-			Network:         DecredHop,
+			Network:         hop.DecredNetwork,
 			NextHop:         nextHop,
 			AmountToForward: amount,
 			OutgoingCTLV:    timeLock,