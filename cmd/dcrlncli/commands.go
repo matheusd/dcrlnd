48f165098fcb630139f0dcdb5d126f49731bfd9b
Olaoluwa Osuntokun <laolu32@gmail.com>
2019-12-04 19:24:35 -0800

    cmd/lncli: exit if user opts to not recover SCB files

--- cmd/dcrlncli/commands.go
+++ cmd/dcrlncli/commands.go
@@ -1447,8 +1447,8 @@ func create(ctx *cli.Context) error {
 				restoreSCB = true
 				break warningLoop
 			case "n":
-				restoreSCB = false
-				break warningLoop
+				fmt.Println("Aborting SCB recovery")
+				return nil
 			}
 		}
 	}
