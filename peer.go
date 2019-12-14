a878e6c5733beaa0d41aa403b4133a39c380b3c1
Wilmer Paulino <wilmer.paulino@gmail.com>
2019-12-13 16:09:16 -0800

    peer: modify ReplyChannelRange log message
    The message now shows the block range the reply spans, which is a lot
    more useful.

--- peer.go
+++ peer.go
@@ -1337,8 +1337,10 @@ func messageSummary(msg lnwire.Message) string {
 			msg.Complete)
 
 	case *lnwire.ReplyChannelRange:
-		return fmt.Sprintf("complete=%v, encoding=%v, num_chans=%v",
-			msg.Complete, msg.EncodingType, len(msg.ShortChanIDs))
+		return fmt.Sprintf("start_height=%v, end_height=%v, "+
+			"num_chans=%v, encoding=%v", msg.FirstBlockHeight,
+			msg.LastBlockHeight(), len(msg.ShortChanIDs),
+			msg.EncodingType)
 
 	case *lnwire.QueryShortChanIDs:
 		return fmt.Sprintf("chain_hash=%v, encoding=%v, num_chans=%v",
@@ -1346,8 +1348,8 @@ func messageSummary(msg lnwire.Message) string {
 
 	case *lnwire.QueryChannelRange:
 		return fmt.Sprintf("chain_hash=%v, start_height=%v, "+
-			"num_blocks=%v", msg.ChainHash, msg.FirstBlockHeight,
-			msg.NumBlocks)
+			"end_height=%v", msg.ChainHash, msg.FirstBlockHeight,
+			msg.LastBlockHeight())
 
 	case *lnwire.GossipTimestampRange:
 		return fmt.Sprintf("chain_hash=%v, first_stamp=%v, "+
