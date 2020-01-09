b45c6db98f492352dddd0f081a8245af41dfb644
Matheus Degiovani <opensource@matheusd.com>
2020-01-09 15:40:59 -0300

    dcrwallet: Fix syncer not reconnecting
    This fixes an issue where the wallet would lose connection to the
    underlying dcrd node and never restart it.
    
    This bug was introduced when upgrading to the wallet v3 package.
    
    The RPCSyncer now correctly attempts to maintain the wallet synchronized
    until it is signalled to shutdown.

--- lnwallet/dcrwallet/rpcsync.go
+++ lnwallet/dcrwallet/rpcsync.go
@@ -3,6 +3,7 @@ package dcrwallet
 import (
 	"context"
 	"sync"
+	"time"
 
 	"github.com/decred/dcrd/chaincfg/v2"
 	"github.com/decred/dcrd/rpcclient/v5"
@@ -14,10 +15,15 @@ import (
 // RPCSyncer implements the required methods for synchronizing a DcrWallet
 // instance using a full node dcrd backend.
 type RPCSyncer struct {
-	cancel    func()
 	rpcConfig rpcclient.ConnConfig
 	net       *chaincfg.Params
 	wg        sync.WaitGroup
+
+	mtx sync.Mutex
+
+	// The following fields are protected by mtx.
+
+	cancel func()
 }
 
 // NewRPCSyncer initializes a new syncer backed by a full dcrd node. It
@@ -33,38 +39,56 @@ func NewRPCSyncer(rpcConfig rpcclient.ConnConfig, net *chaincfg.Params) (*RPCSyn
 // start the syncer backend and begin synchronizing the given wallet.
 func (s *RPCSyncer) start(w *DcrWallet) error {
 
-	dcrwLog.Debugf("Starting rpc syncer")
-
-	// This context will be canceled by `w` once its Stop() method is
-	// called.
-	var ctx context.Context
-	ctx, s.cancel = context.WithCancel(context.Background())
-
 	chainRpcOpts := chain.RPCOptions{
 		Address: s.rpcConfig.Host,
 		User:    s.rpcConfig.User,
 		Pass:    s.rpcConfig.Pass,
 		CA:      s.rpcConfig.Certificates,
 	}
-	syncer := chain.NewSyncer(w.wallet, &chainRpcOpts)
-	syncer.SetCallbacks(&chain.Callbacks{
-		Synced: w.onRPCSyncerSynced,
-	})
 
 	s.wg.Add(1)
 	go func() {
 		defer s.wg.Done()
-		err := syncer.Run(ctx)
-		dcrwLog.Debugf("RPCsyncer shutting down")
+		for {
+			// This context will be canceled by `w` once its Stop() method is
+			// called.
+			ctx, cancel := context.WithCancel(context.Background())
+			s.mtx.Lock()
+			s.cancel = cancel
+			s.mtx.Unlock()
 
-		// TODO: convert to errors.Is
-		if werr, is := err.(*errors.Error); is && werr.Err == context.Canceled {
-			// This was a graceful shutdown, so ignore the error.
-			dcrwLog.Debugf("RPCsyncer shutting down")
-			return
+			syncer := chain.NewSyncer(w.wallet, &chainRpcOpts)
+			syncer.SetCallbacks(&chain.Callbacks{
+				Synced: w.onRPCSyncerSynced,
+			})
+
+			dcrwLog.Debugf("Starting rpc syncer")
+			err := syncer.Run(ctx)
+			w.rpcSyncerFinished()
+
+			// TODO: convert to errors.Is
+			if werr, is := err.(*errors.Error); is && werr.Err == context.Canceled {
+				// This was a graceful shutdown, so ignore the error.
+				dcrwLog.Debugf("RPCsyncer shutting down")
+				return
+			}
+			dcrwLog.Errorf("RPCSyncer error: %v", err)
+
+			// Backoff for 5 seconds.
+			select {
+			case <-ctx.Done():
+				// Graceful shutdown.
+				dcrwLog.Debugf("RPCsyncer shutting down")
+				return
+			case <-time.After(5 * time.Second):
+			}
+
+			// Clear and call s.cancel() so we don't leak it.
+			s.mtx.Lock()
+			s.cancel = nil
+			s.mtx.Unlock()
+			cancel()
 		}
-
-		dcrwLog.Errorf("RPCSyncer error: %v", err)
 	}()
 
 	return nil
@@ -72,7 +96,12 @@ func (s *RPCSyncer) start(w *DcrWallet) error {
 
 func (s *RPCSyncer) stop() {
 	dcrwLog.Debugf("RPCSyncer requested shutdown")
-	s.cancel()
+	s.mtx.Lock()
+	if s.cancel != nil {
+		s.cancel()
+		s.cancel = nil
+	}
+	s.mtx.Unlock()
 }
 
 func (s *RPCSyncer) waitForShutdown() {
