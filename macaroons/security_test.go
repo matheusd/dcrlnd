bc16ebe1b5801358b6145973b50a3c332e884b43
Oliver Gugger <gugger@gmail.com>
2020-01-17 14:23:42 +0100

    macaroons: use fast scrypt options in itest and unit tests

---
+++ macaroons/security_test.go
@@ -0,0 +1,10 @@
+package macaroons
+
+func init() {
+	// Below are the reduced scrypt parameters that are used when creating
+	// the encryption key for the macaroon database with snacl.NewSecretKey.
+	// We use very low values for our itest/rpctest to speed things up.
+	scryptN = 2
+	scryptR = 1
+	scryptP = 1
+}
