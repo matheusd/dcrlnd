a59e5dec0c96e1f88c3bc26c9866513af9c463a9
Oliver Gugger <gugger@gmail.com>
2020-02-12 16:13:07 +0100

    lnrpc+rpcserver+lncli: rename RPC fields to previous JSON name

--- rpcserver.go
+++ rpcserver.go
@@ -939,7 +939,7 @@ func (r *rpcServer) ListUnspent(ctx context.Context,
 		}
 
 		utxoResp := lnrpc.Utxo{
-			Type:          addrType,
+			AddressType:   addrType,
 			AmountAtoms:   int64(utxo.Value),
 			PkScript:      hex.EncodeToString(utxo.PkScript),
 			Outpoint:      outpoint,
@@ -5351,7 +5351,7 @@ func (r *rpcServer) FeeReport(ctx context.Context,
 		// TODO(roasbeef): also add stats for revenue for each channel
 		feeReports = append(feeReports, &lnrpc.ChannelFeeReport{
 			ChanId:        chanInfo.ChannelID,
-			ChanPoint:     chanInfo.ChannelPoint.String(),
+			ChannelPoint:  chanInfo.ChannelPoint.String(),
 			BaseFeeMAtoms: int64(edgePolicy.FeeBaseMAtoms),
 			FeePerMil:     int64(feeRateFixedPoint),
 			FeeRate:       feeRate,
