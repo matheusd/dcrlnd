joostjager/lnd

invoices: accept mpp payments

lnrpc/invoicesrpc/utils.go
@@ -75,13 +75,14 @@ func CreateRPCInvoice(invoice *channeldb.Invoice,
 		}
 
 		rpcHtlc := lnrpc.InvoiceHTLC{
-			ChanId:       key.ChanID.ToUint64(),
-			HtlcIndex:    key.HtlcID,
-			AcceptHeight: int32(htlc.AcceptHeight),
-			AcceptTime:   htlc.AcceptTime.Unix(),
-			ExpiryHeight: int32(htlc.Expiry),
-			AmtMsat:      uint64(htlc.Amt),
-			State:        state,
+			ChanId:          key.ChanID.ToUint64(),
+			HtlcIndex:       key.HtlcID,
+			AcceptHeight:    int32(htlc.AcceptHeight),
+			AcceptTime:      htlc.AcceptTime.Unix(),
+			ExpiryHeight:    int32(htlc.Expiry),
+			AmtMsat:         uint64(htlc.Amt),
+			State:           state,
+			MppTotalAmtMsat: uint64(htlc.TotalAmt),
 		}
 
 		// Only report resolved times if htlc is resolved.

matheusd/dcrlnd

lnrpc/invoicesrpc/utils.go
@@ -75,14 +75,15 @@ func CreateRPCInvoice(invoice *channeldb.Invoice,
 		}
 
 		rpcHtlc := lnrpc.InvoiceHTLC{
-			ChanId:        key.ChanID.ToUint64(),
-			HtlcIndex:     key.HtlcID,
-			AcceptHeight:  int32(htlc.AcceptHeight),
-			AcceptTime:    htlc.AcceptTime.Unix(),
-			ExpiryHeight:  int32(htlc.Expiry),
-			AmtMAtoms:     uint64(htlc.Amt),
-			State:         state,
-			CustomRecords: htlc.CustomRecords,
+			ChanId:            key.ChanID.ToUint64(),
+			HtlcIndex:         key.HtlcID,
+			AcceptHeight:      int32(htlc.AcceptHeight),
+			AcceptTime:        htlc.AcceptTime.Unix(),
+			ExpiryHeight:      int32(htlc.Expiry),
+			AmtMAtoms:         uint64(htlc.Amt),
+			State:             state,
+			CustomRecords:     htlc.CustomRecords,
+			MppTotalAmtMAtoms: uint64(htlc.MppTotalAmt),
 		}
 
 		// Only report resolved times if htlc is resolved.