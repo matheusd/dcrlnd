lightningnetwork/lnd

channeldb: initialize migrations logger

channeldb/log.go
@@ -3,6 +3,7 @@ package channeldb
 import (
 	"github.com/btcsuite/btclog"
 	"github.com/lightningnetwork/lnd/build"
+	"github.com/lightningnetwork/lnd/channeldb/migration_01_to_11"
 )
 
 // log is a logger that is initialized with no output filters.  This
@@ -25,4 +26,5 @@ func DisableLog() {
 // using btclog.
 func UseLogger(logger btclog.Logger) {
 	log = logger
+	migration_01_to_11.UseLogger(logger)
 }

channeldb/migration_01_to_11/log.go
@@ -2,27 +2,13 @@ package migration_01_to_11
 
 import (
 	"github.com/btcsuite/btclog"
-	"github.com/lightningnetwork/lnd/build"
 )
 
-// log is a logger that is initialized with no output filters.  This
-// means the package will not perform any logging by default until the caller
-// requests it.
-var log btclog.Logger
-
-func init() {
-	UseLogger(build.NewSubLogger("CHDB", nil))
-}
-
-// DisableLog disables all library log output.  Logging output is disabled
-// by default until UseLogger is called.
-func DisableLog() {
-	UseLogger(btclog.Disabled)
-}
+// log is a logger that is initialized as disabled.  This means the package will
+// not perform any logging by default until a logger is set.
+var log = btclog.Disabled
 
 // UseLogger uses a specified Logger to output package logging info.
-// This should be used in preference to SetLogWriter if the caller is also
-// using btclog.
 func UseLogger(logger btclog.Logger) {
 	log = logger
 }

matheusd/dcrlnd

channeldb/log.go
@@ -2,6 +2,7 @@ package channeldb
 
 import (
 	"github.com/decred/dcrlnd/build"
+	"github.com/decred/dcrlnd/channeldb/migration_01_to_11"
 	"github.com/decred/slog"
 )
 
@@ -25,4 +26,5 @@ func DisableLog() {
 // using slog.
 func UseLogger(logger slog.Logger) {
 	log = logger
+	migration_01_to_11.UseLogger(logger)
 }

channeldb/migration_01_to_11/log.go
@@ -1,28 +1,14 @@
 package migration_01_to_11
 
 import (
-	"github.com/decred/dcrlnd/build"
 	"github.com/decred/slog"
 )
 
-// log is a logger that is initialized with no output filters.  This
-// means the package will not perform any logging by default until the caller
-// requests it.
-var log slog.Logger
-
-func init() {
-	UseLogger(build.NewSubLogger("CHDB", nil))
-}
-
-// DisableLog disables all library log output.  Logging output is disabled
-// by default until UseLogger is called.
-func DisableLog() {
-	UseLogger(slog.Disabled)
-}
+// log is a logger that is initialized as disabled.  This means the package will
+// not perform any logging by default until a logger is set.
+var log = slog.Disabled
 
 // UseLogger uses a specified Logger to output package logging info.
-// This should be used in preference to SetLogWriter if the caller is also
-// using slog.
 func UseLogger(logger slog.Logger) {
 	log = logger
 }