lightningnetwork/lnd

lnrpc+routerrpc+lncli: add amt and fee limit msat fields

lnrpc/routerrpc/router_backend.go
@@ -128,15 +128,17 @@ func (r *RouterBackend) QueryRoutes(ctx context.Context,
 	// Currently, within the bootstrap phase of the network, we limit the
 	// largest payment size allotted to (2^32) - 1 mSAT or 4.29 million
 	// satoshis.
-	amt := btcutil.Amount(in.Amt)
-	amtMSat := lnwire.NewMSatFromSatoshis(amt)
-	if amtMSat > r.MaxPaymentMSat {
+	amt, err := lnrpc.UnmarshallAmt(in.Amt, in.AmtMsat)
+	if err != nil {
+		return nil, err
+	}
+	if amt > r.MaxPaymentMSat {
 		return nil, fmt.Errorf("payment of %v is too large, max payment "+
 			"allowed is %v", amt, r.MaxPaymentMSat.ToSatoshis())
 	}
 
 	// Unmarshall restrictions from request.
-	feeLimit := lnrpc.CalculateFeeLimit(in.FeeLimit, amtMSat)
+	feeLimit := lnrpc.CalculateFeeLimit(in.FeeLimit, amt)
 
 	ignoredNodes := make(map[route.Vertex]struct{})
 	for _, ignorePubKey := range in.IgnoredNodes {
@@ -239,7 +241,7 @@ func (r *RouterBackend) QueryRoutes(ctx context.Context,
 	// can carry `in.Amt` satoshis _including_ the total fee required on
 	// the route.
 	route, err := r.FindRoute(
-		sourcePubKey, targetPubKey, amtMSat, restrictions,
+		sourcePubKey, targetPubKey, amt, restrictions,
 		destTlvRecords, finalCLTVDelta,
 	)
 	if err != nil {
@@ -505,9 +507,12 @@ func (r *RouterBackend) extractIntentFromSendRequest(
 	payIntent.CltvLimit = cltvLimit
 
 	// Take fee limit from request.
-	payIntent.FeeLimit = lnwire.NewMSatFromSatoshis(
-		btcutil.Amount(rpcPayReq.FeeLimitSat),
+	payIntent.FeeLimit, err = lnrpc.UnmarshallAmt(
+		rpcPayReq.FeeLimitSat, rpcPayReq.FeeLimitMsat,
 	)
+	if err != nil {
+		return nil, err
+	}
 
 	// Set payment attempt timeout.
 	if rpcPayReq.TimeoutSeconds == 0 {
@@ -534,6 +539,14 @@ func (r *RouterBackend) extractIntentFromSendRequest(
 		return nil, err
 	}
 	payIntent.RouteHints = routeHints
+
+	// Unmarshall either sat or msat amount from request.
+	reqAmt, err := lnrpc.UnmarshallAmt(
+		rpcPayReq.Amt, rpcPayReq.AmtMsat,
+	)
+	if err != nil {
+		return nil, err
+	}
 
 	// If the payment request field isn't blank, then the details of the
 	// invoice are encoded entirely within the encoded payReq.  So we'll
@@ -572,17 +585,15 @@ func (r *RouterBackend) extractIntentFromSendRequest(
 		// We override the amount to pay with the amount provided from
 		// the payment request.
 		if payReq.MilliSat == nil {
-			if rpcPayReq.Amt == 0 {
+			if reqAmt == 0 {
 				return nil, errors.New("amount must be " +
 					"specified when paying a zero amount " +
 					"invoice")
 			}
 
-			payIntent.Amount = lnwire.NewMSatFromSatoshis(
-				btcutil.Amount(rpcPayReq.Amt),
-			)
+			payIntent.Amount = reqAmt
 		} else {
-			if rpcPayReq.Amt != 0 {
+			if reqAmt != 0 {
 				return nil, errors.New("amount must not be " +
 					"specified when paying a non-zero " +
 					" amount invoice")
@@ -620,13 +631,11 @@ func (r *RouterBackend) extractIntentFromSendRequest(
 		}
 
 		// Amount.
-		if rpcPayReq.Amt == 0 {
+		if reqAmt == 0 {
 			return nil, errors.New("amount must be specified")
 		}
 
-		payIntent.Amount = lnwire.NewMSatFromSatoshis(
-			btcutil.Amount(rpcPayReq.Amt),
-		)
+		payIntent.Amount = reqAmt
 
 		// Payment hash.
 		copy(payIntent.PaymentHash[:], rpcPayReq.PaymentHash)

rpcserver.go
@@ -3079,15 +3079,19 @@ func extractPaymentIntent(rpcPayReq *rpcPaymentRequest) (rpcPaymentIntent, error
 		// We override the amount to pay with the amount provided from
 		// the payment request.
 		if payReq.MilliSat == nil {
-			if rpcPayReq.Amt == 0 {
+			amt, err := lnrpc.UnmarshallAmt(
+				rpcPayReq.Amt, rpcPayReq.AmtMsat,
+			)
+			if err != nil {
+				return payIntent, err
+			}
+			if amt == 0 {
 				return payIntent, errors.New("amount must be " +
 					"specified when paying a zero amount " +
 					"invoice")
 			}
 
-			payIntent.msat = lnwire.NewMSatFromSatoshis(
-				btcutil.Amount(rpcPayReq.Amt),
-			)
+			payIntent.msat = amt
 		} else {
 			payIntent.msat = *payReq.MilliSat
 		}
@@ -3128,9 +3132,12 @@ func extractPaymentIntent(rpcPayReq *rpcPaymentRequest) (rpcPaymentIntent, error
 	// Otherwise, If the payment request field was not specified
 	// (and a custom route wasn't specified), construct the payment
 	// from the other fields.
-	payIntent.msat = lnwire.NewMSatFromSatoshis(
-		btcutil.Amount(rpcPayReq.Amt),
+	payIntent.msat, err = lnrpc.UnmarshallAmt(
+		rpcPayReq.Amt, rpcPayReq.AmtMsat,
 	)
+	if err != nil {
+		return payIntent, err
+	}
 
 	// Calculate the fee limit that should be used for this payment.
 	payIntent.feeLimit = lnrpc.CalculateFeeLimit(

matheusd/dcrlnd

lnrpc/routerrpc/router_backend.go
@@ -128,15 +128,17 @@ func (r *RouterBackend) QueryRoutes(ctx context.Context,
 	// Currently, within the bootstrap phase of the network, we limit the
 	// largest payment size allotted to (2^32) - 1 mSAT or 4.29 million
 	// satoshis.
-	amt := dcrutil.Amount(in.Amt)
-	amtMSat := lnwire.NewMAtomsFromAtoms(amt)
-	if amtMSat > r.MaxPaymentMAtoms {
+	amt, err := lnrpc.UnmarshallAmt(in.Amt, in.AmtMAtoms)
+	if err != nil {
+		return nil, err
+	}
+	if amt > r.MaxPaymentMAtoms {
 		return nil, fmt.Errorf("payment of %v is too large, max payment "+
 			"allowed is %v", amt, r.MaxPaymentMAtoms.ToAtoms())
 	}
 
 	// Unmarshall restrictions from request.
-	feeLimit := lnrpc.CalculateFeeLimit(in.FeeLimit, amtMSat)
+	feeLimit := lnrpc.CalculateFeeLimit(in.FeeLimit, amt)
 
 	ignoredNodes := make(map[route.Vertex]struct{})
 	for _, ignorePubKey := range in.IgnoredNodes {
@@ -239,7 +241,7 @@ func (r *RouterBackend) QueryRoutes(ctx context.Context,
 	// can carry `in.Amt` satoshis _including_ the total fee required on
 	// the route.
 	route, err := r.FindRoute(
-		sourcePubKey, targetPubKey, amtMSat, restrictions,
+		sourcePubKey, targetPubKey, amt, restrictions,
 		destTlvRecords, finalCLTVDelta,
 	)
 	if err != nil {
@@ -505,9 +507,12 @@ func (r *RouterBackend) extractIntentFromSendRequest(
 	payIntent.CltvLimit = cltvLimit
 
 	// Take fee limit from request.
-	payIntent.FeeLimit = lnwire.NewMAtomsFromAtoms(
-		dcrutil.Amount(rpcPayReq.FeeLimitAtoms),
+	payIntent.FeeLimit, err = lnrpc.UnmarshallAmt(
+		rpcPayReq.FeeLimitAtoms, rpcPayReq.FeeLimitMAtoms,
 	)
+	if err != nil {
+		return nil, err
+	}
 
 	// Set payment attempt timeout.
 	if rpcPayReq.TimeoutSeconds == 0 {
@@ -534,6 +539,14 @@ func (r *RouterBackend) extractIntentFromSendRequest(
 		return nil, err
 	}
 	payIntent.RouteHints = routeHints
+
+	// Unmarshall either sat or msat amount from request.
+	reqAmt, err := lnrpc.UnmarshallAmt(
+		rpcPayReq.Amt, rpcPayReq.AmtMAtoms,
+	)
+	if err != nil {
+		return nil, err
+	}
 
 	// If the payment request field isn't blank, then the details of the
 	// invoice are encoded entirely within the encoded payReq.  So we'll
@@ -572,17 +585,15 @@ func (r *RouterBackend) extractIntentFromSendRequest(
 		// We override the amount to pay with the amount provided from
 		// the payment request.
 		if payReq.MilliAt == nil {
-			if rpcPayReq.Amt == 0 {
+			if reqAmt == 0 {
 				return nil, errors.New("amount must be " +
 					"specified when paying a zero amount " +
 					"invoice")
 			}
 
-			payIntent.Amount = lnwire.NewMAtomsFromAtoms(
-				dcrutil.Amount(rpcPayReq.Amt),
-			)
+			payIntent.Amount = reqAmt
 		} else {
-			if rpcPayReq.Amt != 0 {
+			if reqAmt != 0 {
 				return nil, errors.New("amount must not be " +
 					"specified when paying a non-zero " +
 					" amount invoice")
@@ -620,13 +631,11 @@ func (r *RouterBackend) extractIntentFromSendRequest(
 		}
 
 		// Amount.
-		if rpcPayReq.Amt == 0 {
+		if reqAmt == 0 {
 			return nil, errors.New("amount must be specified")
 		}
 
-		payIntent.Amount = lnwire.NewMAtomsFromAtoms(
-			dcrutil.Amount(rpcPayReq.Amt),
-		)
+		payIntent.Amount = reqAmt
 
 		// Payment hash.
 		copy(payIntent.PaymentHash[:], rpcPayReq.PaymentHash)

rpcserver.go
@@ -3081,15 +3081,19 @@ func extractPaymentIntent(rpcPayReq *rpcPaymentRequest) (rpcPaymentIntent, error
 		// We override the amount to pay with the amount provided from
 		// the payment request.
 		if payReq.MilliAt == nil {
-			if rpcPayReq.Amt == 0 {
+			amt, err := lnrpc.UnmarshallAmt(
+				rpcPayReq.Amt, rpcPayReq.AmtMAtoms,
+			)
+			if err != nil {
+				return payIntent, err
+			}
+			if amt == 0 {
 				return payIntent, errors.New("amount must be " +
 					"specified when paying a zero amount " +
 					"invoice")
 			}
 
-			payIntent.mat = lnwire.NewMAtomsFromAtoms(
-				dcrutil.Amount(rpcPayReq.Amt),
-			)
+			payIntent.mat = amt
 		} else {
 			payIntent.mat = *payReq.MilliAt
 		}
@@ -3130,9 +3134,12 @@ func extractPaymentIntent(rpcPayReq *rpcPaymentRequest) (rpcPaymentIntent, error
 	// Otherwise, If the payment request field was not specified
 	// (and a custom route wasn't specified), construct the payment
 	// from the other fields.
-	payIntent.mat = lnwire.NewMAtomsFromAtoms(
-		dcrutil.Amount(rpcPayReq.Amt),
+	payIntent.mat, err = lnrpc.UnmarshallAmt(
+		rpcPayReq.Amt, rpcPayReq.AmtMAtoms,
 	)
+	if err != nil {
+		return payIntent, err
+	}
 
 	// Calculate the fee limit that should be used for this payment.
 	payIntent.feeLimit = lnrpc.CalculateFeeLimit(