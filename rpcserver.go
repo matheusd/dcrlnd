lightningnetwork/lnd

lnd+lnrpc: add msat forwarding amount fields to ForwardingHistory

rpcserver.go
@@ -4723,18 +4723,20 @@ func (r *rpcServer) ForwardingHistory(ctx context.Context,
 		LastOffsetIndex:  timeSlice.LastIndexOffset,
 	}
 	for i, event := range timeSlice.ForwardingEvents {
-		amtInSat := event.AmtIn.ToSatoshis()
-		amtOutSat := event.AmtOut.ToSatoshis()
+		amtInMsat := event.AmtIn
+		amtOutMsat := event.AmtOut
 		feeMsat := event.AmtIn - event.AmtOut
 
 		resp.ForwardingEvents[i] = &lnrpc.ForwardingEvent{
-			Timestamp: uint64(event.Timestamp.Unix()),
-			ChanIdIn:  event.IncomingChanID.ToUint64(),
-			ChanIdOut: event.OutgoingChanID.ToUint64(),
-			AmtIn:     uint64(amtInSat),
-			AmtOut:    uint64(amtOutSat),
-			Fee:       uint64(feeMsat.ToSatoshis()),
-			FeeMsat:   uint64(feeMsat),
+			Timestamp:  uint64(event.Timestamp.Unix()),
+			ChanIdIn:   event.IncomingChanID.ToUint64(),
+			ChanIdOut:  event.OutgoingChanID.ToUint64(),
+			AmtIn:      uint64(amtInMsat.ToSatoshis()),
+			AmtOut:     uint64(amtOutMsat.ToSatoshis()),
+			Fee:        uint64(feeMsat.ToSatoshis()),
+			FeeMsat:    uint64(feeMsat),
+			AmtInMsat:  uint64(amtInMsat),
+			AmtOutMsat: uint64(amtOutMsat),
 		}
 	}
 

matheusd/dcrlnd

rpcserver.go
@@ -4928,18 +4928,20 @@ func (r *rpcServer) ForwardingHistory(ctx context.Context,
 		LastOffsetIndex:  timeSlice.LastIndexOffset,
 	}
 	for i, event := range timeSlice.ForwardingEvents {
-		amtInAt := event.AmtIn.ToAtoms()
-		amtOutAt := event.AmtOut.ToAtoms()
+		amtInMAtoms := event.AmtIn
+		amtOutMAtoms := event.AmtOut
 		feeMAtoms := event.AmtIn - event.AmtOut
 
 		resp.ForwardingEvents[i] = &lnrpc.ForwardingEvent{
-			Timestamp: uint64(event.Timestamp.Unix()),
-			ChanIdIn:  event.IncomingChanID.ToUint64(),
-			ChanIdOut: event.OutgoingChanID.ToUint64(),
-			AmtIn:     uint64(amtInAt),
-			AmtOut:    uint64(amtOutAt),
-			Fee:       uint64(feeMAtoms.ToAtoms()),
-			FeeMAtoms: uint64(feeMAtoms),
+			Timestamp:    uint64(event.Timestamp.Unix()),
+			ChanIdIn:     event.IncomingChanID.ToUint64(),
+			ChanIdOut:    event.OutgoingChanID.ToUint64(),
+			AmtIn:        uint64(amtInMAtoms.ToAtoms()),
+			AmtOut:       uint64(amtOutMAtoms.ToAtoms()),
+			Fee:          uint64(feeMAtoms.ToAtoms()),
+			FeeMAtoms:    uint64(feeMAtoms),
+			AmtInMAtoms:  uint64(amtInMAtoms),
+			AmtOutMAtoms: uint64(amtOutMAtoms),
 		}
 	}
 