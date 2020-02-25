lightningnetwork/lnd

tlv+record+routing: add payload size calculation

record/mpp.go
@@ -98,6 +98,11 @@ func (r *MPP) Record() tlv.Record {
 	)
 }
 
+// PayloadSize returns the size this record takes up in encoded form.
+func (r *MPP) PayloadSize() uint64 {
+	return 32 + tlv.SizeTUint64(uint64(r.totalMsat))
+}
+
 // String returns a human-readable representation of the mpp payload field.
 func (r *MPP) String() string {
 	return fmt.Sprintf("total=%v, addr=%x", r.totalMsat, r.paymentAddr)

tlv/varint.go
@@ -4,6 +4,8 @@ import (
 	"encoding/binary"
 	"errors"
 	"io"
+
+	"github.com/btcsuite/btcd/wire"
 )
 
 // ErrVarIntNotCanonical signals that the decoded varint was not minimally encoded.
@@ -106,4 +108,9 @@ func WriteVarInt(w io.Writer, val uint64, buf *[8]byte) error {
 
 	_, err := w.Write(buf[:length])
 	return err
+}
+
+// VarIntSize returns the required number of bytes to encode a var int.
+func VarIntSize(val uint64) uint64 {
+	return uint64(wire.VarIntSerializeSize(val))
 }

matheusd/dcrlnd

record/mpp.go
@@ -98,6 +98,11 @@ func (r *MPP) Record() tlv.Record {
 	)
 }
 
+// PayloadSize returns the size this record takes up in encoded form.
+func (r *MPP) PayloadSize() uint64 {
+	return 32 + tlv.SizeTUint64(uint64(r.totalMAtoms))
+}
+
 // String returns a human-readable representation of the mpp payload field.
 func (r *MPP) String() string {
 	return fmt.Sprintf("total=%v, addr=%x", r.totalMAtoms, r.paymentAddr)

tlv/varint.go
@@ -4,6 +4,8 @@ import (
 	"encoding/binary"
 	"errors"
 	"io"
+
+	"github.com/decred/dcrd/wire"
 )
 
 // ErrVarIntNotCanonical signals that the decoded varint was not minimally encoded.
@@ -106,4 +108,9 @@ func WriteVarInt(w io.Writer, val uint64, buf *[8]byte) error {
 
 	_, err := w.Write(buf[:length])
 	return err
+}
+
+// VarIntSize returns the required number of bytes to encode a var int.
+func VarIntSize(val uint64) uint64 {
+	return uint64(wire.VarIntSerializeSize(val))
 }