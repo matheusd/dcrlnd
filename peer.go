lightningnetwork/lnd

peer: clamp a link's max HTLC forwarding policy to current max HTLC pay size
In this commit, we start to clamp the max HTLC forwarding policy to the
current register max HTLC payment size. By doing this, we ensure that
any links that have a advertised max HTLC transit size above the max
payment size will reject any incoming or outgoing attempts for such
large payments.

peer.go
@@ -535,6 +535,9 @@ func (p *peer) loadActiveChannels(chans []*channeldb.OpenChannel) (
 				FeeRate:       selfPolicy.FeeProportionalMillionths,
 				TimeLockDelta: uint32(selfPolicy.TimeLockDelta),
 			}
+			if forwardingPolicy.MaxHTLC > MaxPaymentMSat {
+				forwardingPolicy.MaxHTLC = MaxPaymentMSat
+			}
 		} else {
 			peerLog.Warnf("Unable to find our forwarding policy "+
 				"for channel %v, using default values",
@@ -1865,6 +1868,9 @@ out:
 				BaseFee:       defaultPolicy.BaseFee,
 				FeeRate:       defaultPolicy.FeeRate,
 				TimeLockDelta: defaultPolicy.TimeLockDelta,
+			}
+			if forwardingPolicy.MaxHTLC > MaxPaymentMSat {
+				forwardingPolicy.MaxHTLC = MaxPaymentMSat
 			}
 
 			// Create the link and add it to the switch.

matheusd/dcrlnd

peer.go
@@ -536,6 +536,9 @@ func (p *peer) loadActiveChannels(chans []*channeldb.OpenChannel) (
 				FeeRate:       selfPolicy.FeeProportionalMillionths,
 				TimeLockDelta: uint32(selfPolicy.TimeLockDelta),
 			}
+			if forwardingPolicy.MaxHTLC > MaxPaymentMAtoms {
+				forwardingPolicy.MaxHTLC = MaxPaymentMAtoms
+			}
 		} else {
 			peerLog.Warnf("Unable to find our forwarding policy "+
 				"for channel %v, using default values",
@@ -1867,6 +1870,9 @@ out:
 				BaseFee:       defaultPolicy.BaseFee,
 				FeeRate:       defaultPolicy.FeeRate,
 				TimeLockDelta: defaultPolicy.TimeLockDelta,
+			}
+			if forwardingPolicy.MaxHTLC > MaxPaymentMAtoms {
+				forwardingPolicy.MaxHTLC = MaxPaymentMAtoms
 			}
 
 			// Create the link and add it to the switch.
e2f864476c54508068208faf15360f4535fa12b8
Olaoluwa Osuntokun <laolu32@gmail.com>
2019-12-04 15:01:38 -0800

    peer: clamp a link's max HTLC forwarding policy to current max HTLC pay size
    In this commit, we start to clamp the max HTLC forwarding policy to the
    current register max HTLC payment size. By doing this, we ensure that
    any links that have a advertised max HTLC transit size above the max
    payment size will reject any incoming or outgoing attempts for such
    large payments.

--- peer.go
+++ peer.go
@@ -536,6 +536,9 @@ func (p *peer) loadActiveChannels(chans []*channeldb.OpenChannel) (
 				FeeRate:       selfPolicy.FeeProportionalMillionths,
 				TimeLockDelta: uint32(selfPolicy.TimeLockDelta),
 			}
+			if forwardingPolicy.MaxHTLC > MaxPaymentMAtoms {
+				forwardingPolicy.MaxHTLC = MaxPaymentMAtoms
+			}
 		} else {
 			peerLog.Warnf("Unable to find our forwarding policy "+
 				"for channel %v, using default values",
@@ -1867,6 +1870,9 @@ out:
 				BaseFee:       defaultPolicy.BaseFee,
 				FeeRate:       defaultPolicy.FeeRate,
 				TimeLockDelta: defaultPolicy.TimeLockDelta,
+			}
+			if forwardingPolicy.MaxHTLC > MaxPaymentMAtoms {
+				forwardingPolicy.MaxHTLC = MaxPaymentMAtoms
 			}
 
 			// Create the link and add it to the switch.
