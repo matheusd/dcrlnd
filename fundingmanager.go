lightningnetwork/lnd

funding: ensure the chan policy max htlc size is below max pay size

fundingmanager.go
@@ -2359,6 +2359,9 @@ func (f *fundingManager) annAfterSixConfs(completeChan *channeldb.OpenChannel,
 		if fwdMaxHTLC > capacityMSat {
 			fwdMaxHTLC = capacityMSat
 		}
+		if fwdMaxHTLC > MaxPaymentMSat {
+			fwdMaxHTLC = MaxPaymentMSat
+		}
 
 		// Create and broadcast the proofs required to make this channel
 		// public and usable for other nodes for routing.

matheusd/dcrlnd

fundingmanager.go
@@ -2350,6 +2350,9 @@ func (f *fundingManager) annAfterSixConfs(completeChan *channeldb.OpenChannel,
 		if fwdMaxHTLC > capacityMAtoms {
 			fwdMaxHTLC = capacityMAtoms
 		}
+		if fwdMaxHTLC > MaxPaymentMAtoms {
+			fwdMaxHTLC = MaxPaymentMAtoms
+		}
 
 		// Create and broadcast the proofs required to make this channel
 		// public and usable for other nodes for routing.