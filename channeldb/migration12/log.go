lightningnetwork/lnd

channeldb/migration12: write out invoices using TLV encoding

channeldb/migration12/log.go
@@ -1,0 +1,14 @@
+package migration12
+
+import (
+	"github.com/btcsuite/btclog"
+)
+
+// log is a logger that is initialized as disabled.  This means the package will
+// not perform any logging by default until a logger is set.
+var log = btclog.Disabled
+
+// UseLogger uses a specified Logger to output package logging info.
+func UseLogger(logger btclog.Logger) {
+	log = logger
+}

channeldb/migration12/migration.go
@@ -1,0 +1,74 @@
+package migration12
+
+import (
+	"bytes"
+
+	"github.com/coreos/bbolt"
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+var emptyFeatures = lnwire.NewFeatureVector(nil, nil)
+
+// MigrateInvoiceTLV migrates all existing invoice bodies over to be serialized
+// in a single TLV stream. In the process, we drop the Receipt field and add
+// PaymentAddr and Features to the invoice Terms.
+func MigrateInvoiceTLV(tx *bbolt.Tx) error {
+	log.Infof("Migrating invoice bodies to TLV, " +
+		"adding payment addresses and feature vectors.")
+
+	invoiceB := tx.Bucket(invoiceBucket)
+	if invoiceB == nil {
+		return nil
+	}
+
+	type keyedInvoice struct {
+		key     []byte
+		invoice Invoice
+	}
+
+	// Read in all existing invoices using the old format.
+	var invoices []keyedInvoice
+	err := invoiceB.ForEach(func(k, v []byte) error {
+		if v == nil {
+			return nil
+		}
+
+		invoiceReader := bytes.NewReader(v)
+		invoice, err := LegacyDeserializeInvoice(invoiceReader)
+		if err != nil {
+			return err
+		}
+
+		// Insert an empty feature vector on all old payments.
+		invoice.Terms.Features = emptyFeatures
+
+		invoices = append(invoices, keyedInvoice{
+			key:     k,
+			invoice: invoice,
+		})
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	// Write out each one under its original key using TLV.
+	for _, ki := range invoices {
+		var b bytes.Buffer
+		err = SerializeInvoice(&b, &ki.invoice)
+		if err != nil {
+			return err
+		}
+
+		err = invoiceB.Put(ki.key, b.Bytes())
+		if err != nil {
+			return err
+		}
+	}
+
+	log.Infof("Migration to TLV invoice bodies, " +
+		"payment address, and features complete!")
+
+	return nil
+}

matheusd/dcrlnd

channeldb/migration12/log.go
@@ -1,0 +1,14 @@
+package migration12
+
+import (
+	"github.com/decred/slog"
+)
+
+// log is a logger that is initialized as disabled.  This means the package will
+// not perform any logging by default until a logger is set.
+var log = slog.Disabled
+
+// UseLogger uses a specified Logger to output package logging info.
+func UseLogger(logger slog.Logger) {
+	log = logger
+}

channeldb/migration12/migration.go
@@ -1,0 +1,74 @@
+package migration12
+
+import (
+	"bytes"
+
+	bolt "go.etcd.io/bbolt"
+	"github.com/decred/dcrlnd/lnwire"
+)
+
+var emptyFeatures = lnwire.NewFeatureVector(nil, nil)
+
+// MigrateInvoiceTLV migrates all existing invoice bodies over to be serialized
+// in a single TLV stream. In the process, we drop the Receipt field and add
+// PaymentAddr and Features to the invoice Terms.
+func MigrateInvoiceTLV(tx *bolt.Tx) error {
+	log.Infof("Migrating invoice bodies to TLV, " +
+		"adding payment addresses and feature vectors.")
+
+	invoiceB := tx.Bucket(invoiceBucket)
+	if invoiceB == nil {
+		return nil
+	}
+
+	type keyedInvoice struct {
+		key     []byte
+		invoice Invoice
+	}
+
+	// Read in all existing invoices using the old format.
+	var invoices []keyedInvoice
+	err := invoiceB.ForEach(func(k, v []byte) error {
+		if v == nil {
+			return nil
+		}
+
+		invoiceReader := bytes.NewReader(v)
+		invoice, err := LegacyDeserializeInvoice(invoiceReader)
+		if err != nil {
+			return err
+		}
+
+		// Insert an empty feature vector on all old payments.
+		invoice.Terms.Features = emptyFeatures
+
+		invoices = append(invoices, keyedInvoice{
+			key:     k,
+			invoice: invoice,
+		})
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	// Write out each one under its original key using TLV.
+	for _, ki := range invoices {
+		var b bytes.Buffer
+		err = SerializeInvoice(&b, &ki.invoice)
+		if err != nil {
+			return err
+		}
+
+		err = invoiceB.Put(ki.key, b.Bytes())
+		if err != nil {
+			return err
+		}
+	}
+
+	log.Infof("Migration to TLV invoice bodies, " +
+		"payment address, and features complete!")
+
+	return nil
+}