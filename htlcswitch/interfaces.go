lightningnetwork/lnd

htlcswitch: rename policy check functions

htlcswitch/interfaces.go
@@ -100,22 +100,21 @@ type ChannelLink interface {
 	// policy to govern if it an incoming HTLC should be forwarded or not.
 	UpdateForwardingPolicy(ForwardingPolicy)
 
-	// HtlcSatifiesPolicy should return a nil error if the passed HTLC
-	// details satisfy the current forwarding policy fo the target link.
-	// Otherwise, a valid protocol failure message should be returned in
-	// order to signal to the source of the HTLC, the policy consistency
-	// issue.
-	HtlcSatifiesPolicy(payHash [32]byte, incomingAmt lnwire.MilliSatoshi,
+	// CheckHtlcForward should return a nil error if the passed HTLC details
+	// satisfy the current forwarding policy fo the target link. Otherwise,
+	// a valid protocol failure message should be returned in order to
+	// signal to the source of the HTLC, the policy consistency issue.
+	CheckHtlcForward(payHash [32]byte, incomingAmt lnwire.MilliSatoshi,
 		amtToForward lnwire.MilliSatoshi,
 		incomingTimeout, outgoingTimeout uint32,
 		heightNow uint32) lnwire.FailureMessage
 
-	// HtlcSatifiesPolicyLocal should return a nil error if the passed HTLC
-	// details satisfy the current channel policy.  Otherwise, a valid
-	// protocol failure message should be returned in order to signal the
-	// violation. This call is intended to be used for locally initiated
-	// payments for which there is no corresponding incoming htlc.
-	HtlcSatifiesPolicyLocal(payHash [32]byte, amt lnwire.MilliSatoshi,
+	// CheckHtlcTransit should return a nil error if the passed HTLC details
+	// satisfy the current channel policy.  Otherwise, a valid protocol
+	// failure message should be returned in order to signal the violation.
+	// This call is intended to be used for locally initiated payments for
+	// which there is no corresponding incoming htlc.
+	CheckHtlcTransit(payHash [32]byte, amt lnwire.MilliSatoshi,
 		timeout uint32, heightNow uint32) lnwire.FailureMessage
 
 	// Bandwidth returns the amount of milli-satoshis which current link

htlcswitch/mock.go
@@ -696,12 +696,12 @@ func (f *mockChannelLink) HandleChannelUpdate(lnwire.Message) {
 
 func (f *mockChannelLink) UpdateForwardingPolicy(_ ForwardingPolicy) {
 }
-func (f *mockChannelLink) HtlcSatifiesPolicy([32]byte, lnwire.MilliSatoshi,
+func (f *mockChannelLink) CheckHtlcForward([32]byte, lnwire.MilliSatoshi,
 	lnwire.MilliSatoshi, uint32, uint32, uint32) lnwire.FailureMessage {
 	return nil
 }
 
-func (f *mockChannelLink) HtlcSatifiesPolicyLocal(payHash [32]byte,
+func (f *mockChannelLink) CheckHtlcTransit(payHash [32]byte,
 	amt lnwire.MilliSatoshi, timeout uint32,
 	heightNow uint32) lnwire.FailureMessage {
 

matheusd/dcrlnd

htlcswitch/interfaces.go
@@ -100,22 +100,22 @@ type ChannelLink interface {
 	// policy to govern if it an incoming HTLC should be forwarded or not.
 	UpdateForwardingPolicy(ForwardingPolicy)
 
-	// HtlcSatifiesPolicy should return a nil error if the passed HTLC
+	// CheckHtlcForward should return a nil error if the passed HTLC
 	// details satisfy the current forwarding policy fo the target link.
 	// Otherwise, a valid protocol failure message should be returned in
 	// order to signal to the source of the HTLC, the policy consistency
 	// issue.
-	HtlcSatifiesPolicy(payHash [32]byte, incomingAmt lnwire.MilliAtom,
+	CheckHtlcForward(payHash [32]byte, incomingAmt lnwire.MilliAtom,
 		amtToForward lnwire.MilliAtom,
 		incomingTimeout, outgoingTimeout uint32,
 		heightNow uint32) lnwire.FailureMessage
 
-	// HtlcSatifiesPolicyLocal should return a nil error if the passed HTLC
-	// details satisfy the current channel policy.  Otherwise, a valid
-	// protocol failure message should be returned in order to signal the
-	// violation. This call is intended to be used for locally initiated
-	// payments for which there is no corresponding incoming htlc.
-	HtlcSatifiesPolicyLocal(payHash [32]byte, amt lnwire.MilliAtom,
+	// CheckHtlcTransit should return a nil error if the passed HTLC details
+	// satisfy the current channel policy.  Otherwise, a valid protocol
+	// failure message should be returned in order to signal the violation.
+	// This call is intended to be used for locally initiated payments for
+	// which there is no corresponding incoming htlc.
+	CheckHtlcTransit(payHash [32]byte, amt lnwire.MilliAtom,
 		timeout uint32, heightNow uint32) lnwire.FailureMessage
 
 	// Bandwidth returns the amount of milli-atoms which current link might

htlcswitch/mock.go
@@ -700,12 +700,12 @@ func (f *mockChannelLink) HandleChannelUpdate(lnwire.Message) {
 
 func (f *mockChannelLink) UpdateForwardingPolicy(_ ForwardingPolicy) {
 }
-func (f *mockChannelLink) HtlcSatifiesPolicy([32]byte, lnwire.MilliAtom,
+func (f *mockChannelLink) CheckHtlcForward([32]byte, lnwire.MilliAtom,
 	lnwire.MilliAtom, uint32, uint32, uint32) lnwire.FailureMessage {
 	return nil
 }
 
-func (f *mockChannelLink) HtlcSatifiesPolicyLocal(payHash [32]byte,
+func (f *mockChannelLink) CheckHtlcTransit(payHash [32]byte,
 	amt lnwire.MilliAtom, timeout uint32,
 	heightNow uint32) lnwire.FailureMessage {
 