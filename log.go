lightningnetwork/lnd

localchans: log policy update error

log.go
@@ -31,6 +31,7 @@ import (
 	"github.com/lightningnetwork/lnd/netann"
 	"github.com/lightningnetwork/lnd/peernotifier"
 	"github.com/lightningnetwork/lnd/routing"
+	"github.com/lightningnetwork/lnd/routing/localchans"
 	"github.com/lightningnetwork/lnd/signal"
 	"github.com/lightningnetwork/lnd/sweep"
 	"github.com/lightningnetwork/lnd/watchtower"
@@ -99,6 +100,7 @@ func init() {
 	addSubLogger("CHFD", chanfunding.UseLogger)
 
 	addSubLogger(routing.Subsystem, routing.UseLogger)
+	addSubLogger(routing.Subsystem, localchans.UseLogger)
 	addSubLogger(routerrpc.Subsystem, routerrpc.UseLogger)
 	addSubLogger(wtclientrpc.Subsystem, wtclientrpc.UseLogger)
 	addSubLogger(chanfitness.Subsystem, chanfitness.UseLogger)

routing/localchans/log.go
@@ -1,0 +1,16 @@
+package localchans
+
+import (
+	"github.com/btcsuite/btclog"
+)
+
+// log is a logger that is initialized with no output filters.  This
+// means the package will not perform any logging by default until the caller
+// requests it.
+var log btclog.Logger
+
+// UseLogger uses a specified Logger to output package logging info. This
+// function is called from the parent package htlcswitch logger initialization.
+func UseLogger(logger btclog.Logger) {
+	log = logger
+}

matheusd/dcrlnd

log.go
@@ -31,6 +31,7 @@ import (
 	"github.com/decred/dcrlnd/netann"
 	"github.com/decred/dcrlnd/peernotifier"
 	"github.com/decred/dcrlnd/routing"
+	"github.com/decred/dcrlnd/routing/localchans"
 	"github.com/decred/dcrlnd/signal"
 	"github.com/decred/dcrlnd/sweep"
 	"github.com/decred/dcrlnd/watchtower"
@@ -100,6 +101,7 @@ func init() {
 	addSubLogger("CHFD", chanfunding.UseLogger)
 
 	addSubLogger(routing.Subsystem, routing.UseLogger)
+	addSubLogger(routing.Subsystem, localchans.UseLogger)
 	addSubLogger(routerrpc.Subsystem, routerrpc.UseLogger)
 	addSubLogger(wtclientrpc.Subsystem, wtclientrpc.UseLogger)
 	addSubLogger(chanfitness.Subsystem, chanfitness.UseLogger)

routing/localchans/log.go
@@ -1,0 +1,16 @@
+package localchans
+
+import (
+	"github.com/decred/slog"
+)
+
+// log is a logger that is initialized with no output filters.  This
+// means the package will not perform any logging by default until the caller
+// requests it.
+var log slog.Logger
+
+// UseLogger uses a specified Logger to output package logging info. This
+// function is called from the parent package htlcswitch logger initialization.
+func UseLogger(logger slog.Logger) {
+	log = logger
+}