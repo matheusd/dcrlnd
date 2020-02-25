lightningnetwork/lnd

invoices: adding InvoiceExpryWatcher to cancel expired invoices
This commit adds InvoiceExpryWatcher which is a separate class that
receives new invoices (and existing ones upon restart) from InvoiceRegistry
and actively watches their expiry. When an invoice is expired
InvoiceExpiryWatcher will call into InvoiceRegistry to cancel the
invoice and by that notify all subscribers about the state change.

channeldb/invoices.go
@@ -565,6 +565,83 @@ func (d *DB) LookupInvoice(paymentHash [32]byte) (Invoice, error) {
 	return invoice, nil
 }
 
+// InvoiceWithPaymentHash is used to store an invoice and its corresponding
+// payment hash. This struct is only used to store results of
+// ChannelDB.FetchAllInvoicesWithPaymentHash() call.
+type InvoiceWithPaymentHash struct {
+	// Invoice holds the invoice as selected from the invoices bucket.
+	Invoice Invoice
+
+	// PaymentHash is the payment hash for the Invoice.
+	PaymentHash lntypes.Hash
+}
+
+// FetchAllInvoicesWithPaymentHash returns all invoices and their payment hashes
+// currently stored within the database. If the pendingOnly param is true, then
+// only unsettled invoices and their payment hashes will be returned, skipping
+// all invoices that are fully settled or canceled. Note that the returned
+// array is not ordered by add index.
+func (d *DB) FetchAllInvoicesWithPaymentHash(pendingOnly bool) (
+	[]InvoiceWithPaymentHash, error) {
+
+	var result []InvoiceWithPaymentHash
+
+	err := d.View(func(tx *bbolt.Tx) error {
+		invoices := tx.Bucket(invoiceBucket)
+		if invoices == nil {
+			return ErrNoInvoicesCreated
+		}
+
+		invoiceIndex := invoices.Bucket(invoiceIndexBucket)
+		if invoiceIndex == nil {
+			// Mask the error if there's no invoice
+			// index as that simply means there are no
+			// invoices added yet to the DB. In this case
+			// we simply return an empty list.
+			return nil
+		}
+
+		return invoiceIndex.ForEach(func(k, v []byte) error {
+			// Skip the special numInvoicesKey as that does not
+			// point to a valid invoice.
+			if bytes.Equal(k, numInvoicesKey) {
+				return nil
+			}
+
+			if v == nil {
+				return nil
+			}
+
+			invoice, err := fetchInvoice(v, invoices)
+			if err != nil {
+				return err
+			}
+
+			if pendingOnly &&
+				(invoice.State == ContractSettled ||
+					invoice.State == ContractCanceled) {
+
+				return nil
+			}
+
+			invoiceWithPaymentHash := InvoiceWithPaymentHash{
+				Invoice: invoice,
+			}
+
+			copy(invoiceWithPaymentHash.PaymentHash[:], k)
+			result = append(result, invoiceWithPaymentHash)
+
+			return nil
+		})
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
 // FetchAllInvoices returns all invoices currently stored within the database.
 // If the pendingOnly param is true, then only unsettled invoices will be
 // returned, skipping all invoices that are fully settled.

htlcswitch/mock.go
@@ -22,6 +22,7 @@ import (
 	sphinx "github.com/lightningnetwork/lightning-onion"
 	"github.com/lightningnetwork/lnd/chainntnfs"
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/clock"
 	"github.com/lightningnetwork/lnd/contractcourt"
 	"github.com/lightningnetwork/lnd/htlcswitch/hop"
 	"github.com/lightningnetwork/lnd/input"
@@ -792,6 +793,7 @@ func newMockRegistry(minDelta uint32) *mockInvoiceRegistry {
 
 	registry := invoices.NewRegistry(
 		cdb,
+		invoices.NewInvoiceExpiryWatcher(clock.NewDefaultClock()),
 		&invoices.RegistryConfig{
 			FinalCltvRejectDelta: 5,
 		},

invoices/invoice_expiry_watcher_test.go
@@ -1,0 +1,125 @@
+package invoices
+
+import (
+	"testing"
+	"time"
+
+	"github.com/lightningnetwork/lnd/clock"
+	"github.com/lightningnetwork/lnd/lntypes"
+)
+
+// invoiceExpiryWatcherTest holds a test fixture and implements checks
+// for InvoiceExpiryWatcher tests.
+type invoiceExpiryWatcherTest struct {
+	t                *testing.T
+	watcher          *InvoiceExpiryWatcher
+	testData         invoiceExpiryTestData
+	canceledInvoices []lntypes.Hash
+}
+
+// newInvoiceExpiryWatcherTest creates a new InvoiceExpiryWatcher test fixture
+// and sets up the test environment.
+func newInvoiceExpiryWatcherTest(t *testing.T, now time.Time,
+	numExpiredInvoices, numPendingInvoices int) *invoiceExpiryWatcherTest {
+
+	test := &invoiceExpiryWatcherTest{
+		watcher: NewInvoiceExpiryWatcher(clock.NewTestClock(testTime)),
+		testData: generateInvoiceExpiryTestData(
+			t, now, 0, numExpiredInvoices, numPendingInvoices,
+		),
+	}
+
+	err := test.watcher.Start(func(paymentHash lntypes.Hash) error {
+		test.canceledInvoices = append(test.canceledInvoices, paymentHash)
+		return nil
+	})
+
+	if err != nil {
+		t.Fatalf("cannot start InvoiceExpiryWatcher: %v", err)
+	}
+
+	return test
+}
+
+func (t *invoiceExpiryWatcherTest) checkExpectations() {
+	// Check that invoices that got canceled during the test are the ones
+	// that expired.
+	if len(t.canceledInvoices) != len(t.testData.expiredInvoices) {
+		t.t.Fatalf("expected %v cancellations, got %v",
+			len(t.testData.expiredInvoices), len(t.canceledInvoices))
+	}
+
+	for i := range t.canceledInvoices {
+		if _, ok := t.testData.expiredInvoices[t.canceledInvoices[i]]; !ok {
+			t.t.Fatalf("wrong invoice canceled")
+		}
+	}
+}
+
+// Tests that InvoiceExpiryWatcher can be started and stopped.
+func TestInvoiceExpiryWatcherStartStop(t *testing.T) {
+	watcher := NewInvoiceExpiryWatcher(clock.NewTestClock(testTime))
+	cancel := func(lntypes.Hash) error {
+		t.Fatalf("unexpected call")
+		return nil
+	}
+
+	if err := watcher.Start(cancel); err != nil {
+		t.Fatalf("unexpected error upon start: %v", err)
+	}
+
+	if err := watcher.Start(cancel); err == nil {
+		t.Fatalf("expected error upon second start")
+	}
+
+	watcher.Stop()
+
+	if err := watcher.Start(cancel); err != nil {
+		t.Fatalf("unexpected error upon start: %v", err)
+	}
+}
+
+// Tests that no invoices will expire from an empty InvoiceExpiryWatcher.
+func TestInvoiceExpiryWithNoInvoices(t *testing.T) {
+	t.Parallel()
+	test := newInvoiceExpiryWatcherTest(t, testTime, 0, 0)
+
+	time.Sleep(testTimeout)
+	test.watcher.Stop()
+	test.checkExpectations()
+}
+
+// Tests that if all add invoices are expired, then all invoices
+// will be canceled.
+func TestInvoiceExpiryWithOnlyExpiredInvoices(t *testing.T) {
+	t.Parallel()
+
+	test := newInvoiceExpiryWatcherTest(t, testTime, 0, 5)
+
+	for paymentHash, invoice := range test.testData.pendingInvoices {
+		test.watcher.AddInvoice(paymentHash, invoice)
+	}
+
+	time.Sleep(testTimeout)
+	test.watcher.Stop()
+	test.checkExpectations()
+}
+
+// Tests that if some invoices are expired, then those invoices
+// will be canceled.
+func TestInvoiceExpiryWithPendingAndExpiredInvoices(t *testing.T) {
+	t.Parallel()
+	test := newInvoiceExpiryWatcherTest(t, testTime, 5, 5)
+
+	for paymentHash, invoice := range test.testData.expiredInvoices {
+		test.watcher.AddInvoice(paymentHash, invoice)
+	}
+
+	for paymentHash, invoice := range test.testData.pendingInvoices {
+		test.watcher.AddInvoice(paymentHash, invoice)
+	}
+
+	time.Sleep(testTimeout)
+	test.watcher.Stop()
+	test.checkExpectations()
+}

invoices/test_utils_test.go
@@ -1,6 +1,7 @@
 package invoices
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"io/ioutil"
@@ -51,7 +52,7 @@ var (
 	testPrivKeyBytes, _ = hex.DecodeString(
 		"e126f68f7eafcc8b74f54d269fe206be715000f94dac067d1c04a8ca3b2db734")
 
-	testPrivKey, testPubKey = btcec.PrivKeyFromBytes(
+	testPrivKey, _ = btcec.PrivKeyFromBytes(
 		btcec.S256(), testPrivKeyBytes)
 
 	testInvoiceDescription = "coffee"
@@ -75,6 +76,8 @@ var (
 	)
 
 	testPayload = &mockPayload{}
+
+	testInvoiceCreationDate = testTime
 )
 
 var (
@@ -83,16 +86,20 @@ var (
 		Terms: channeldb.ContractTerm{
 			PaymentPreimage: testInvoicePreimage,
 			Value:           testInvoiceAmt,
+			Expiry:          time.Hour,
 			Features:        testFeatures,
 		},
+		CreationDate: testInvoiceCreationDate,
 	}
 
 	testHodlInvoice = &channeldb.Invoice{
 		Terms: channeldb.ContractTerm{
 			PaymentPreimage: channeldb.UnknownPreimage,
 			Value:           testInvoiceAmt,
+			Expiry:          time.Hour,
 			Features:        testFeatures,
 		},
+		CreationDate: testInvoiceCreationDate,
 	}
 )
 
@@ -120,6 +127,7 @@ func newTestChannelDB() (*channeldb.DB, func(), error) {
 }
 
 type testContext struct {
+	cdb      *channeldb.DB
 	registry *InvoiceRegistry
 	clock    *clock.TestClock
 
@@ -136,13 +144,15 @@ func newTestContext(t *testing.T) *testContext {
 	}
 	cdb.Now = clock.Now
 
+	expiryWatcher := NewInvoiceExpiryWatcher(clock)
+
 	// Instantiate and start the invoice ctx.registry.
 	cfg := RegistryConfig{
 		FinalCltvRejectDelta: testFinalCltvRejectDelta,
 		HtlcHoldDuration:     30 * time.Second,
 		Clock:                clock,
 	}
-	registry := NewRegistry(cdb, &cfg)
+	registry := NewRegistry(cdb, expiryWatcher, &cfg)
 
 	err = registry.Start()
 	if err != nil {
@@ -151,6 +161,7 @@ func newTestContext(t *testing.T) *testContext {
 	}
 
 	ctx := testContext{
+		cdb:      cdb,
 		registry: registry,
 		clock:    clock,
 		t:        t,
@@ -172,7 +183,7 @@ func getCircuitKey(htlcID uint64) channeldb.CircuitKey {
 	}
 }
 
-func newTestInvoice(t *testing.T,
+func newTestInvoice(t *testing.T, preimage lntypes.Preimage,
 	timestamp time.Time, expiry time.Duration) *channeldb.Invoice {
 
 	if expiry == 0 {
@@ -181,7 +192,7 @@ func newTestInvoice(t *testing.T,
 
 	rawInvoice, err := zpay32.NewInvoice(
 		testNetParams,
-		testInvoicePaymentHash,
+		preimage.Hash(),
 		timestamp,
 		zpay32.Amount(testInvoiceAmount),
 		zpay32.Description(testInvoiceDescription),
@@ -199,7 +210,7 @@ func newTestInvoice(t *testing.T,
 
 	return &channeldb.Invoice{
 		Terms: channeldb.ContractTerm{
-			PaymentPreimage: testInvoicePreimage,
+			PaymentPreimage: preimage,
 			Value:           testInvoiceAmount,
 			Expiry:          expiry,
 			Features:        testFeatures,
@@ -228,4 +239,42 @@ func timeout() func() {
 	return func() {
 		close(done)
 	}
+}
+
+// invoiceExpiryTestData simply holds generated expired and pending invoices.
+type invoiceExpiryTestData struct {
+	expiredInvoices map[lntypes.Hash]*channeldb.Invoice
+	pendingInvoices map[lntypes.Hash]*channeldb.Invoice
+}
+
+// generateInvoiceExpiryTestData generates the specified number of fake expired
+// and pending invoices anchored to the passed now timestamp.
+func generateInvoiceExpiryTestData(
+	t *testing.T, now time.Time,
+	offset, numExpired, numPending int) invoiceExpiryTestData {
+
+	var testData invoiceExpiryTestData
+
+	testData.expiredInvoices = make(map[lntypes.Hash]*channeldb.Invoice)
+	testData.pendingInvoices = make(map[lntypes.Hash]*channeldb.Invoice)
+
+	expiredCreationDate := now.Add(-24 * time.Hour)
+
+	for i := 1; i <= numExpired; i++ {
+		var preimage lntypes.Preimage
+		binary.BigEndian.PutUint32(preimage[:4], uint32(offset+i))
+		expiry := time.Duration((i+offset)%24) * time.Hour
+		invoice := newTestInvoice(t, preimage, expiredCreationDate, expiry)
+		testData.expiredInvoices[preimage.Hash()] = invoice
+	}
+
+	for i := 1; i <= numPending; i++ {
+		var preimage lntypes.Preimage
+		binary.BigEndian.PutUint32(preimage[4:], uint32(offset+i))
+		expiry := time.Duration((i+offset)%24) * time.Hour
+		invoice := newTestInvoice(t, preimage, now, expiry)
+		testData.pendingInvoices[preimage.Hash()] = invoice
+	}
+
+	return testData
 }

matheusd/dcrlnd

channeldb/invoices.go
@@ -565,6 +565,83 @@ func (d *DB) LookupInvoice(paymentHash [32]byte) (Invoice, error) {
 	return invoice, nil
 }
 
+// InvoiceWithPaymentHash is used to store an invoice and its corresponding
+// payment hash. This struct is only used to store results of
+// ChannelDB.FetchAllInvoicesWithPaymentHash() call.
+type InvoiceWithPaymentHash struct {
+	// Invoice holds the invoice as selected from the invoices bucket.
+	Invoice Invoice
+
+	// PaymentHash is the payment hash for the Invoice.
+	PaymentHash lntypes.Hash
+}
+
+// FetchAllInvoicesWithPaymentHash returns all invoices and their payment hashes
+// currently stored within the database. If the pendingOnly param is true, then
+// only unsettled invoices and their payment hashes will be returned, skipping
+// all invoices that are fully settled or canceled. Note that the returned
+// array is not ordered by add index.
+func (d *DB) FetchAllInvoicesWithPaymentHash(pendingOnly bool) (
+	[]InvoiceWithPaymentHash, error) {
+
+	var result []InvoiceWithPaymentHash
+
+	err := d.View(func(tx *bolt.Tx) error {
+		invoices := tx.Bucket(invoiceBucket)
+		if invoices == nil {
+			return ErrNoInvoicesCreated
+		}
+
+		invoiceIndex := invoices.Bucket(invoiceIndexBucket)
+		if invoiceIndex == nil {
+			// Mask the error if there's no invoice
+			// index as that simply means there are no
+			// invoices added yet to the DB. In this case
+			// we simply return an empty list.
+			return nil
+		}
+
+		return invoiceIndex.ForEach(func(k, v []byte) error {
+			// Skip the special numInvoicesKey as that does not
+			// point to a valid invoice.
+			if bytes.Equal(k, numInvoicesKey) {
+				return nil
+			}
+
+			if v == nil {
+				return nil
+			}
+
+			invoice, err := fetchInvoice(v, invoices)
+			if err != nil {
+				return err
+			}
+
+			if pendingOnly &&
+				(invoice.State == ContractSettled ||
+					invoice.State == ContractCanceled) {
+
+				return nil
+			}
+
+			invoiceWithPaymentHash := InvoiceWithPaymentHash{
+				Invoice: invoice,
+			}
+
+			copy(invoiceWithPaymentHash.PaymentHash[:], k)
+			result = append(result, invoiceWithPaymentHash)
+
+			return nil
+		})
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
 // FetchAllInvoices returns all invoices currently stored within the database.
 // If the pendingOnly param is true, then only unsettled invoices will be
 // returned, skipping all invoices that are fully settled.

htlcswitch/mock.go
@@ -20,6 +20,7 @@ import (
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/chainntnfs"
 	"github.com/decred/dcrlnd/channeldb"
+	"github.com/decred/dcrlnd/clock"
 	"github.com/decred/dcrlnd/contractcourt"
 	"github.com/decred/dcrlnd/htlcswitch/hop"
 	"github.com/decred/dcrlnd/input"
@@ -796,6 +797,7 @@ func newMockRegistry(minDelta uint32) *mockInvoiceRegistry {
 
 	registry := invoices.NewRegistry(
 		cdb,
+		invoices.NewInvoiceExpiryWatcher(clock.NewDefaultClock()),
 		&invoices.RegistryConfig{
 			FinalCltvRejectDelta: 5,
 		},

invoices/invoice_expiry_watcher_test.go
@@ -1,0 +1,125 @@
+package invoices
+
+import (
+	"testing"
+	"time"
+
+	"github.com/decred/dcrlnd/clock"
+	"github.com/decred/dcrlnd/lntypes"
+)
+
+// invoiceExpiryWatcherTest holds a test fixture and implements checks
+// for InvoiceExpiryWatcher tests.
+type invoiceExpiryWatcherTest struct {
+	t                *testing.T
+	watcher          *InvoiceExpiryWatcher
+	testData         invoiceExpiryTestData
+	canceledInvoices []lntypes.Hash
+}
+
+// newInvoiceExpiryWatcherTest creates a new InvoiceExpiryWatcher test fixture
+// and sets up the test environment.
+func newInvoiceExpiryWatcherTest(t *testing.T, now time.Time,
+	numExpiredInvoices, numPendingInvoices int) *invoiceExpiryWatcherTest {
+
+	test := &invoiceExpiryWatcherTest{
+		watcher: NewInvoiceExpiryWatcher(clock.NewTestClock(testTime)),
+		testData: generateInvoiceExpiryTestData(
+			t, now, 0, numExpiredInvoices, numPendingInvoices,
+		),
+	}
+
+	err := test.watcher.Start(func(paymentHash lntypes.Hash) error {
+		test.canceledInvoices = append(test.canceledInvoices, paymentHash)
+		return nil
+	})
+
+	if err != nil {
+		t.Fatalf("cannot start InvoiceExpiryWatcher: %v", err)
+	}
+
+	return test
+}
+
+func (t *invoiceExpiryWatcherTest) checkExpectations() {
+	// Check that invoices that got canceled during the test are the ones
+	// that expired.
+	if len(t.canceledInvoices) != len(t.testData.expiredInvoices) {
+		t.t.Fatalf("expected %v cancellations, got %v",
+			len(t.testData.expiredInvoices), len(t.canceledInvoices))
+	}
+
+	for i := range t.canceledInvoices {
+		if _, ok := t.testData.expiredInvoices[t.canceledInvoices[i]]; !ok {
+			t.t.Fatalf("wrong invoice canceled")
+		}
+	}
+}
+
+// Tests that InvoiceExpiryWatcher can be started and stopped.
+func TestInvoiceExpiryWatcherStartStop(t *testing.T) {
+	watcher := NewInvoiceExpiryWatcher(clock.NewTestClock(testTime))
+	cancel := func(lntypes.Hash) error {
+		t.Fatalf("unexpected call")
+		return nil
+	}
+
+	if err := watcher.Start(cancel); err != nil {
+		t.Fatalf("unexpected error upon start: %v", err)
+	}
+
+	if err := watcher.Start(cancel); err == nil {
+		t.Fatalf("expected error upon second start")
+	}
+
+	watcher.Stop()
+
+	if err := watcher.Start(cancel); err != nil {
+		t.Fatalf("unexpected error upon start: %v", err)
+	}
+}
+
+// Tests that no invoices will expire from an empty InvoiceExpiryWatcher.
+func TestInvoiceExpiryWithNoInvoices(t *testing.T) {
+	t.Parallel()
+	test := newInvoiceExpiryWatcherTest(t, testTime, 0, 0)
+
+	time.Sleep(testTimeout)
+	test.watcher.Stop()
+	test.checkExpectations()
+}
+
+// Tests that if all add invoices are expired, then all invoices
+// will be canceled.
+func TestInvoiceExpiryWithOnlyExpiredInvoices(t *testing.T) {
+	t.Parallel()
+
+	test := newInvoiceExpiryWatcherTest(t, testTime, 0, 5)
+
+	for paymentHash, invoice := range test.testData.pendingInvoices {
+		test.watcher.AddInvoice(paymentHash, invoice)
+	}
+
+	time.Sleep(testTimeout)
+	test.watcher.Stop()
+	test.checkExpectations()
+}
+
+// Tests that if some invoices are expired, then those invoices
+// will be canceled.
+func TestInvoiceExpiryWithPendingAndExpiredInvoices(t *testing.T) {
+	t.Parallel()
+	test := newInvoiceExpiryWatcherTest(t, testTime, 5, 5)
+
+	for paymentHash, invoice := range test.testData.expiredInvoices {
+		test.watcher.AddInvoice(paymentHash, invoice)
+	}
+
+	for paymentHash, invoice := range test.testData.pendingInvoices {
+		test.watcher.AddInvoice(paymentHash, invoice)
+	}
+
+	time.Sleep(testTimeout)
+	test.watcher.Stop()
+	test.checkExpectations()
+}

invoices/test_utils_test.go
@@ -1,6 +1,7 @@
 package invoices
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"io/ioutil"
@@ -51,7 +52,7 @@ var (
 	testPrivKeyBytes, _ = hex.DecodeString(
 		"e126f68f7eafcc8b74f54d269fe206be715000f94dac067d1c04a8ca3b2db734")
 
-	testPrivKey, testPubKey = secp256k1.PrivKeyFromBytes(
+	testPrivKey, _ = secp256k1.PrivKeyFromBytes(
 		testPrivKeyBytes)
 
 	testInvoiceDescription = "coffee"
@@ -75,6 +76,8 @@ var (
 	)
 
 	testPayload = &mockPayload{}
+
+	testInvoiceCreationDate = testTime
 )
 
 var (
@@ -83,16 +86,20 @@ var (
 		Terms: channeldb.ContractTerm{
 			PaymentPreimage: testInvoicePreimage,
 			Value:           testInvoiceAmt,
+			Expiry:          time.Hour,
 			Features:        testFeatures,
 		},
+		CreationDate: testInvoiceCreationDate,
 	}
 
 	testHodlInvoice = &channeldb.Invoice{
 		Terms: channeldb.ContractTerm{
 			PaymentPreimage: channeldb.UnknownPreimage,
 			Value:           testInvoiceAmt,
+			Expiry:          time.Hour,
 			Features:        testFeatures,
 		},
+		CreationDate: testInvoiceCreationDate,
 	}
 )
 
@@ -120,6 +127,7 @@ func newTestChannelDB() (*channeldb.DB, func(), error) {
 }
 
 type testContext struct {
+	cdb      *channeldb.DB
 	registry *InvoiceRegistry
 	clock    *clock.TestClock
 
@@ -136,13 +144,15 @@ func newTestContext(t *testing.T) *testContext {
 	}
 	cdb.Now = clock.Now
 
+	expiryWatcher := NewInvoiceExpiryWatcher(clock)
+
 	// Instantiate and start the invoice ctx.registry.
 	cfg := RegistryConfig{
 		FinalCltvRejectDelta: testFinalCltvRejectDelta,
 		HtlcHoldDuration:     30 * time.Second,
 		Clock:                clock,
 	}
-	registry := NewRegistry(cdb, &cfg)
+	registry := NewRegistry(cdb, expiryWatcher, &cfg)
 
 	err = registry.Start()
 	if err != nil {
@@ -151,6 +161,7 @@ func newTestContext(t *testing.T) *testContext {
 	}
 
 	ctx := testContext{
+		cdb:      cdb,
 		registry: registry,
 		clock:    clock,
 		t:        t,
@@ -172,7 +183,7 @@ func getCircuitKey(htlcID uint64) channeldb.CircuitKey {
 	}
 }
 
-func newTestInvoice(t *testing.T,
+func newTestInvoice(t *testing.T, preimage lntypes.Preimage,
 	timestamp time.Time, expiry time.Duration) *channeldb.Invoice {
 
 	if expiry == 0 {
@@ -181,7 +192,7 @@ func newTestInvoice(t *testing.T,
 
 	rawInvoice, err := zpay32.NewInvoice(
 		testNetParams,
-		testInvoicePaymentHash,
+		preimage.Hash(),
 		timestamp,
 		zpay32.Amount(testInvoiceAmount),
 		zpay32.Description(testInvoiceDescription),
@@ -199,7 +210,7 @@ func newTestInvoice(t *testing.T,
 
 	return &channeldb.Invoice{
 		Terms: channeldb.ContractTerm{
-			PaymentPreimage: testInvoicePreimage,
+			PaymentPreimage: preimage,
 			Value:           testInvoiceAmount,
 			Expiry:          expiry,
 			Features:        testFeatures,
@@ -228,4 +239,42 @@ func timeout() func() {
 	return func() {
 		close(done)
 	}
+}
+
+// invoiceExpiryTestData simply holds generated expired and pending invoices.
+type invoiceExpiryTestData struct {
+	expiredInvoices map[lntypes.Hash]*channeldb.Invoice
+	pendingInvoices map[lntypes.Hash]*channeldb.Invoice
+}
+
+// generateInvoiceExpiryTestData generates the specified number of fake expired
+// and pending invoices anchored to the passed now timestamp.
+func generateInvoiceExpiryTestData(
+	t *testing.T, now time.Time,
+	offset, numExpired, numPending int) invoiceExpiryTestData {
+
+	var testData invoiceExpiryTestData
+
+	testData.expiredInvoices = make(map[lntypes.Hash]*channeldb.Invoice)
+	testData.pendingInvoices = make(map[lntypes.Hash]*channeldb.Invoice)
+
+	expiredCreationDate := now.Add(-24 * time.Hour)
+
+	for i := 1; i <= numExpired; i++ {
+		var preimage lntypes.Preimage
+		binary.BigEndian.PutUint32(preimage[:4], uint32(offset+i))
+		expiry := time.Duration((i+offset)%24) * time.Hour
+		invoice := newTestInvoice(t, preimage, expiredCreationDate, expiry)
+		testData.expiredInvoices[preimage.Hash()] = invoice
+	}
+
+	for i := 1; i <= numPending; i++ {
+		var preimage lntypes.Preimage
+		binary.BigEndian.PutUint32(preimage[4:], uint32(offset+i))
+		expiry := time.Duration((i+offset)%24) * time.Hour
+		invoice := newTestInvoice(t, preimage, now, expiry)
+		testData.pendingInvoices[preimage.Hash()] = invoice
+	}
+
+	return testData
 }