lightningnetwork/lnd

multi: distinguish between htlc in and out constraints

fundingmanager.go
@@ -285,6 +285,10 @@ type fundingConfig struct {
 	// initially announcing channels.
 	DefaultRoutingPolicy htlcswitch.ForwardingPolicy
 
+	// DefaultMinHtlcIn is the default minimum incoming htlc value that is
+	// set as a channel parameter.
+	DefaultMinHtlcIn lnwire.MilliSatoshi
+
 	// NumRequiredConfs is a function closure that helps the funding
 	// manager decide how many confirmations it should require for a
 	// channel extended to it. The function is able to take into account
@@ -1300,7 +1304,7 @@ func (f *fundingManager) handleFundingOpen(fmsg *fundingOpenMsg) {
 	chanReserve := f.cfg.RequiredRemoteChanReserve(amt, msg.DustLimit)
 	maxValue := f.cfg.RequiredRemoteMaxValue(amt)
 	maxHtlcs := f.cfg.RequiredRemoteMaxHTLCs(amt)
-	minHtlc := f.cfg.DefaultRoutingPolicy.MinHTLC
+	minHtlc := f.cfg.DefaultMinHtlcIn
 
 	// Once the reservation has been created successfully, we add it to
 	// this peer's map of pending reservations to track this particular
@@ -2806,7 +2810,7 @@ func (f *fundingManager) handleInitFundingMsg(msg *initFundingMsg) {
 	var (
 		peerKey        = msg.peer.IdentityKey()
 		localAmt       = msg.localFundingAmt
-		minHtlc        = msg.minHtlc
+		minHtlcIn      = msg.minHtlcIn
 		remoteCsvDelay = msg.remoteCsvDelay
 	)
 
@@ -2922,8 +2926,8 @@ func (f *fundingManager) handleInitFundingMsg(msg *initFundingMsg) {
 	}
 
 	// If no minimum HTLC value was specified, use the default one.
-	if minHtlc == 0 {
-		minHtlc = f.cfg.DefaultRoutingPolicy.MinHTLC
+	if minHtlcIn == 0 {
+		minHtlcIn = f.cfg.DefaultMinHtlcIn
 	}
 
 	// If a pending channel map for this peer isn't already created, then
@@ -2938,7 +2942,7 @@ func (f *fundingManager) handleInitFundingMsg(msg *initFundingMsg) {
 	resCtx := &reservationWithCtx{
 		chanAmt:        capacity,
 		remoteCsvDelay: remoteCsvDelay,
-		remoteMinHtlc:  minHtlc,
+		remoteMinHtlc:  minHtlcIn,
 		reservation:    reservation,
 		peer:           msg.peer,
 		updates:        msg.updates,
@@ -2972,7 +2976,7 @@ func (f *fundingManager) handleInitFundingMsg(msg *initFundingMsg) {
 		DustLimit:             ourContribution.DustLimit,
 		MaxValueInFlight:      maxValue,
 		ChannelReserve:        chanReserve,
-		HtlcMinimum:           minHtlc,
+		HtlcMinimum:           minHtlcIn,
 		FeePerKiloWeight:      uint32(commitFeePerKw),
 		CsvDelay:              remoteCsvDelay,
 		MaxAcceptedHTLCs:      maxHtlcs,

htlcswitch/link.go
@@ -71,10 +71,8 @@ const (
 // the error possibly carrying along a ChannelUpdate message that includes the
 // latest policy.
 type ForwardingPolicy struct {
-	// MinHTLC is the smallest HTLC that is to be forwarded. This is
-	// set when a channel is first opened, and will be static for the
-	// lifetime of the channel.
-	MinHTLC lnwire.MilliSatoshi
+	// MinHTLC is the smallest HTLC that is to be forwarded.
+	MinHTLCOut lnwire.MilliSatoshi
 
 	// MaxHTLC is the largest HTLC that is to be forwarded.
 	MaxHTLC lnwire.MilliSatoshi
@@ -2218,9 +2216,9 @@ func (l *channelLink) canSendHtlc(policy ForwardingPolicy,
 	// As our first sanity check, we'll ensure that the passed HTLC isn't
 	// too small for the next hop. If so, then we'll cancel the HTLC
 	// directly.
-	if amt < policy.MinHTLC {
+	if amt < policy.MinHTLCOut {
 		l.log.Errorf("outgoing htlc(%x) is too small: min_htlc=%v, "+
-			"htlc_value=%v", payHash[:], policy.MinHTLC,
+			"htlc_value=%v", payHash[:], policy.MinHTLCOut,
 			amt)
 
 		// As part of the returned error, we'll send our latest routing

htlcswitch/link_test.go
@@ -1694,7 +1694,7 @@ func newSingleLinkTestHarness(chanAmt, chanReserve btcutil.Amount) (
 			quit:     make(chan struct{}),
 		}
 		globalPolicy = ForwardingPolicy{
-			MinHTLC:       lnwire.NewMSatFromSatoshis(5),
+			MinHTLCOut:    lnwire.NewMSatFromSatoshis(5),
 			MaxHTLC:       lnwire.NewMSatFromSatoshis(chanAmt),
 			BaseFee:       lnwire.NewMSatFromSatoshis(1),
 			TimeLockDelta: 6,
@@ -4253,7 +4253,7 @@ func (h *persistentLinkHarness) restartLink(
 		}
 
 		globalPolicy = ForwardingPolicy{
-			MinHTLC:       lnwire.NewMSatFromSatoshis(5),
+			MinHTLCOut:    lnwire.NewMSatFromSatoshis(5),
 			BaseFee:       lnwire.NewMSatFromSatoshis(1),
 			TimeLockDelta: 6,
 		}
@@ -5512,7 +5512,7 @@ func TestCheckHtlcForward(t *testing.T) {
 		cfg: ChannelLinkConfig{
 			FwrdingPolicy: ForwardingPolicy{
 				TimeLockDelta: 20,
-				MinHTLC:       500,
+				MinHTLCOut:    500,
 				MaxHTLC:       1000,
 				BaseFee:       10,
 			},

pilot.go
@@ -92,7 +92,7 @@ func (c *chanController) OpenChannel(target *btcec.PublicKey,
 	}
 
 	// TODO(halseth): make configurable?
-	minHtlc := lnwire.NewMSatFromSatoshis(1)
+	minHtlcIn := lnwire.NewMSatFromSatoshis(1)
 
 	// Construct the open channel request and send it to the server to begin
 	// the funding workflow.
@@ -102,7 +102,7 @@ func (c *chanController) OpenChannel(target *btcec.PublicKey,
 		subtractFees:    true,
 		localFundingAmt: amt,
 		pushAmt:         0,
-		minHtlc:         minHtlc,
+		minHtlcIn:       minHtlcIn,
 		fundingFeePerKw: feePerKw,
 		private:         c.private,
 		remoteCsvDelay:  0,

matheusd/dcrlnd

fundingmanager.go
@@ -270,6 +270,10 @@ type fundingConfig struct {
 	// initially announcing channels.
 	DefaultRoutingPolicy htlcswitch.ForwardingPolicy
 
+	// DefaultMinHtlcIn is the default minimum incoming htlc value that is
+	// set as a channel parameter.
+	DefaultMinHtlcIn lnwire.MilliAtom
+
 	// NumRequiredConfs is a function closure that helps the funding
 	// manager decide how many confirmations it should require for a
 	// channel extended to it. The function is able to take into account
@@ -1291,7 +1295,7 @@ func (f *fundingManager) handleFundingOpen(fmsg *fundingOpenMsg) {
 	chanReserve := f.cfg.RequiredRemoteChanReserve(amt, msg.DustLimit)
 	maxValue := f.cfg.RequiredRemoteMaxValue(amt)
 	maxHtlcs := f.cfg.RequiredRemoteMaxHTLCs(amt)
-	minHtlc := f.cfg.DefaultRoutingPolicy.MinHTLC
+	minHtlc := f.cfg.DefaultMinHtlcIn
 
 	// Once the reservation has been created successfully, we add it to
 	// this peer's map of pending reservations to track this particular
@@ -2797,7 +2801,7 @@ func (f *fundingManager) handleInitFundingMsg(msg *initFundingMsg) {
 	var (
 		peerKey        = msg.peer.IdentityKey()
 		localAmt       = msg.localFundingAmt
-		minHtlc        = msg.minHtlc
+		minHtlcIn      = msg.minHtlcIn
 		remoteCsvDelay = msg.remoteCsvDelay
 	)
 
@@ -2918,8 +2922,8 @@ func (f *fundingManager) handleInitFundingMsg(msg *initFundingMsg) {
 	}
 
 	// If no minimum HTLC value was specified, use the default one.
-	if minHtlc == 0 {
-		minHtlc = f.cfg.DefaultRoutingPolicy.MinHTLC
+	if minHtlcIn == 0 {
+		minHtlcIn = f.cfg.DefaultMinHtlcIn
 	}
 
 	// If a pending channel map for this peer isn't already created, then
@@ -2934,7 +2938,7 @@ func (f *fundingManager) handleInitFundingMsg(msg *initFundingMsg) {
 	resCtx := &reservationWithCtx{
 		chanAmt:        capacity,
 		remoteCsvDelay: remoteCsvDelay,
-		remoteMinHtlc:  minHtlc,
+		remoteMinHtlc:  minHtlcIn,
 		reservation:    reservation,
 		peer:           msg.peer,
 		updates:        msg.updates,
@@ -2968,7 +2972,7 @@ func (f *fundingManager) handleInitFundingMsg(msg *initFundingMsg) {
 		DustLimit:             ourContribution.DustLimit,
 		MaxValueInFlight:      maxValue,
 		ChannelReserve:        chanReserve,
-		HtlcMinimum:           minHtlc,
+		HtlcMinimum:           minHtlcIn,
 		FeePerKiloByte:        uint32(commitFeePerKB),
 		CsvDelay:              remoteCsvDelay,
 		MaxAcceptedHTLCs:      maxHtlcs,

htlcswitch/link.go
@@ -72,10 +72,8 @@ const (
 // the error possibly carrying along a ChannelUpdate message that includes the
 // latest policy.
 type ForwardingPolicy struct {
-	// MinHTLC is the smallest HTLC that is to be forwarded. This is
-	// set when a channel is first opened, and will be static for the
-	// lifetime of the channel.
-	MinHTLC lnwire.MilliAtom
+	// MinHTLC is the smallest HTLC that is to be forwarded.
+	MinHTLCOut lnwire.MilliAtom
 
 	// MaxHTLC is the largest HTLC that is to be forwarded.
 	MaxHTLC lnwire.MilliAtom
@@ -2219,9 +2217,9 @@ func (l *channelLink) canSendHtlc(policy ForwardingPolicy,
 	// As our first sanity check, we'll ensure that the passed HTLC isn't
 	// too small for the next hop. If so, then we'll cancel the HTLC
 	// directly.
-	if amt < policy.MinHTLC {
+	if amt < policy.MinHTLCOut {
 		l.log.Errorf("outgoing htlc(%x) is too small: min_htlc=%v, "+
-			"htlc_value=%v", payHash[:], policy.MinHTLC,
+			"htlc_value=%v", payHash[:], policy.MinHTLCOut,
 			amt)
 
 		// As part of the returned error, we'll send our latest routing

htlcswitch/link_test.go
@@ -1698,7 +1698,7 @@ func newSingleLinkTestHarness(chanAmt, chanReserve dcrutil.Amount) (
 			quit:     make(chan struct{}),
 		}
 		globalPolicy = ForwardingPolicy{
-			MinHTLC:       lnwire.NewMAtomsFromAtoms(5),
+			MinHTLCOut:    lnwire.NewMAtomsFromAtoms(5),
 			MaxHTLC:       lnwire.NewMAtomsFromAtoms(chanAmt),
 			BaseFee:       lnwire.NewMAtomsFromAtoms(1),
 			TimeLockDelta: 6,
@@ -4257,7 +4257,7 @@ func (h *persistentLinkHarness) restartLink(
 		}
 
 		globalPolicy = ForwardingPolicy{
-			MinHTLC:       lnwire.NewMAtomsFromAtoms(5),
+			MinHTLCOut:    lnwire.NewMAtomsFromAtoms(5),
 			BaseFee:       lnwire.NewMAtomsFromAtoms(1),
 			TimeLockDelta: 6,
 		}
@@ -5516,7 +5516,7 @@ func TestCheckHtlcForward(t *testing.T) {
 		cfg: ChannelLinkConfig{
 			FwrdingPolicy: ForwardingPolicy{
 				TimeLockDelta: 20,
-				MinHTLC:       500,
+				MinHTLCOut:    500,
 				MaxHTLC:       1000,
 				BaseFee:       10,
 			},

pilot.go
@@ -92,7 +92,7 @@ func (c *chanController) OpenChannel(target *secp256k1.PublicKey,
 	}
 
 	// TODO(halseth): make configurable?
-	minHtlc := lnwire.NewMAtomsFromAtoms(1)
+	minHtlcIn := lnwire.NewMAtomsFromAtoms(1)
 
 	// Construct the open channel request and send it to the server to begin
 	// the funding workflow.
@@ -102,7 +102,7 @@ func (c *chanController) OpenChannel(target *secp256k1.PublicKey,
 		subtractFees:    true,
 		localFundingAmt: amt,
 		pushAmt:         0,
-		minHtlc:         minHtlc,
+		minHtlcIn:       minHtlcIn,
 		fundingFeePerKB: feePerKB,
 		private:         c.private,
 		remoteCsvDelay:  0,