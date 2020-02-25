lightningnetwork/lnd

chainfee: create new chainfee package extracting fees from lnwallet
In this commit, we create a new chainfee package, that houses all fee
related functionality used within the codebase. The creation of this new
package furthers our long-term goal of extracting functionality from the
bloated `lnwallet` package into new distinct packages. Additionally,
this new packages resolves a class of import cycle that could arise if a
new package that was imported by something in `lnwallet` wanted to use
the existing fee related functions in the prior `lnwallet` package.

chancloser.go
@@ -9,6 +9,7 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/lightningnetwork/lnd/htlcswitch"
 	"github.com/lightningnetwork/lnd/lnwallet"
+	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 	"github.com/lightningnetwork/lnd/lnwire"
 )
 
@@ -150,7 +151,7 @@ type channelCloser struct {
 // passed configuration, and delivery+fee preference. The final argument should
 // only be populated iff, we're the initiator of this closing request.
 func newChannelCloser(cfg chanCloseCfg, deliveryScript []byte,
-	idealFeePerKw lnwallet.SatPerKWeight, negotiationHeight uint32,
+	idealFeePerKw chainfee.SatPerKWeight, negotiationHeight uint32,
 	closeReq *htlcswitch.ChanClose) *channelCloser {
 
 	// Given the target fee-per-kw, we'll compute what our ideal _total_

lnwallet/wallet_test.go
@@ -5,12 +5,15 @@ import (
 
 	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd/input"
+	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 )
 
 // fundingFee is a helper method that returns the fee estimate used for a tx
 // with the given number of inputs and the optional change output. This matches
 // the estimate done by the wallet.
-func fundingFee(feeRate SatPerKWeight, numInput int, change bool) btcutil.Amount {
+func fundingFee(feeRate chainfee.SatPerKWeight, numInput int, // nolint:unparam
+	change bool) btcutil.Amount {
+
 	var weightEstimate input.TxWeightEstimator
 
 	// All inputs.
@@ -39,7 +42,7 @@ func fundingFee(feeRate SatPerKWeight, numInput int, change bool) btcutil.Amount
 func TestCoinSelect(t *testing.T) {
 	t.Parallel()
 
-	const feeRate = SatPerKWeight(100)
+	const feeRate = chainfee.SatPerKWeight(100)
 	const dust = btcutil.Amount(100)
 
 	type testCase struct {
@@ -185,7 +188,7 @@ func TestCoinSelect(t *testing.T) {
 func TestCoinSelectSubtractFees(t *testing.T) {
 	t.Parallel()
 
-	const feeRate = SatPerKWeight(100)
+	const feeRate = chainfee.SatPerKWeight(100)
 	const dustLimit = btcutil.Amount(1000)
 	const dust = btcutil.Amount(100)
 

rpcserver.go
@@ -44,6 +44,7 @@ import (
 	"github.com/lightningnetwork/lnd/lnrpc/routerrpc"
 	"github.com/lightningnetwork/lnd/lntypes"
 	"github.com/lightningnetwork/lnd/lnwallet"
+	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/macaroons"
 	"github.com/lightningnetwork/lnd/monitoring"
@@ -815,7 +816,7 @@ func addrPairsToOutputs(addrPairs map[string]int64) ([]*wire.TxOut, error) {
 // more addresses specified in the passed payment map. The payment map maps an
 // address to a specified output value to be sent to that address.
 func (r *rpcServer) sendCoinsOnChain(paymentMap map[string]int64,
-	feeRate lnwallet.SatPerKWeight) (*chainhash.Hash, error) {
+	feeRate chainfee.SatPerKWeight) (*chainhash.Hash, error) {
 
 	outputs, err := addrPairsToOutputs(paymentMap)
 	if err != nil {
@@ -1000,7 +1001,7 @@ func (r *rpcServer) SendCoins(ctx context.Context,
 
 	// Based on the passed fee related parameters, we'll determine an
 	// appropriate fee rate for this transaction.
-	satPerKw := lnwallet.SatPerKVByte(in.SatPerByte * 1000).FeePerKWeight()
+	satPerKw := chainfee.SatPerKVByte(in.SatPerByte * 1000).FeePerKWeight()
 	feePerKw, err := sweep.DetermineFeePerKw(
 		r.server.cc.feeEstimator, sweep.FeePreference{
 			ConfTarget: uint32(in.TargetConf),
@@ -1122,7 +1123,7 @@ func (r *rpcServer) SendMany(ctx context.Context,
 
 	// Based on the passed fee related parameters, we'll determine an
 	// appropriate fee rate for this transaction.
-	satPerKw := lnwallet.SatPerKVByte(in.SatPerByte * 1000).FeePerKWeight()
+	satPerKw := chainfee.SatPerKVByte(in.SatPerByte * 1000).FeePerKWeight()
 	feePerKw, err := sweep.DetermineFeePerKw(
 		r.server.cc.feeEstimator, sweep.FeePreference{
 			ConfTarget: uint32(in.TargetConf),
@@ -1504,7 +1505,7 @@ func (r *rpcServer) OpenChannel(in *lnrpc.OpenChannelRequest,
 
 	// Based on the passed fee related parameters, we'll determine an
 	// appropriate fee rate for the funding transaction.
-	satPerKw := lnwallet.SatPerKVByte(in.SatPerByte * 1000).FeePerKWeight()
+	satPerKw := chainfee.SatPerKVByte(in.SatPerByte * 1000).FeePerKWeight()
 	feeRate, err := sweep.DetermineFeePerKw(
 		r.server.cc.feeEstimator, sweep.FeePreference{
 			ConfTarget: uint32(in.TargetConf),
@@ -1649,7 +1650,7 @@ func (r *rpcServer) OpenChannelSync(ctx context.Context,
 
 	// Based on the passed fee related parameters, we'll determine an
 	// appropriate fee rate for the funding transaction.
-	satPerKw := lnwallet.SatPerKVByte(in.SatPerByte * 1000).FeePerKWeight()
+	satPerKw := chainfee.SatPerKVByte(in.SatPerByte * 1000).FeePerKWeight()
 	feeRate, err := sweep.DetermineFeePerKw(
 		r.server.cc.feeEstimator, sweep.FeePreference{
 			ConfTarget: uint32(in.TargetConf),
@@ -1847,7 +1848,7 @@ func (r *rpcServer) CloseChannel(in *lnrpc.CloseChannelRequest,
 		// Based on the passed fee related parameters, we'll determine
 		// an appropriate fee rate for the cooperative closure
 		// transaction.
-		satPerKw := lnwallet.SatPerKVByte(
+		satPerKw := chainfee.SatPerKVByte(
 			in.SatPerByte * 1000,
 		).FeePerKWeight()
 		feeRate, err := sweep.DetermineFeePerKw(

sweep/sweeper.go
@@ -16,13 +16,14 @@ import (
 	"github.com/lightningnetwork/lnd/chainntnfs"
 	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/lnwallet"
+	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 )
 
 const (
 	// DefaultMaxFeeRate is the default maximum fee rate allowed within the
 	// UtxoSweeper. The current value is equivalent to a fee rate of 10,000
 	// sat/vbyte.
-	DefaultMaxFeeRate = lnwallet.FeePerKwFloor * 1e4
+	DefaultMaxFeeRate = chainfee.FeePerKwFloor * 1e4
 
 	// DefaultFeeRateBucketSize is the default size of fee rate buckets
 	// we'll use when clustering inputs into buckets with similar fee rates
@@ -92,7 +93,7 @@ type pendingInput struct {
 
 	// lastFeeRate is the most recent fee rate used for this input within a
 	// transaction broadcast to the network.
-	lastFeeRate lnwallet.SatPerKWeight
+	lastFeeRate chainfee.SatPerKWeight
 }
 
 // pendingInputs is a type alias for a set of pending inputs.
@@ -101,7 +102,7 @@ type pendingInputs = map[wire.OutPoint]*pendingInput
 // inputCluster is a helper struct to gather a set of pending inputs that should
 // be swept with the specified fee rate.
 type inputCluster struct {
-	sweepFeeRate lnwallet.SatPerKWeight
+	sweepFeeRate chainfee.SatPerKWeight
 	inputs       pendingInputs
 }
 
@@ -126,7 +127,7 @@ type PendingInput struct {
 
 	// LastFeeRate is the most recent fee rate used for the input being
 	// swept within a transaction broadcast to the network.
-	LastFeeRate lnwallet.SatPerKWeight
+	LastFeeRate chainfee.SatPerKWeight
 
 	// BroadcastAttempts is the number of attempts we've made to sweept the
 	// input.
@@ -182,7 +183,7 @@ type UtxoSweeper struct {
 
 	currentOutputScript []byte
 
-	relayFeeRate lnwallet.SatPerKWeight
+	relayFeeRate chainfee.SatPerKWeight
 
 	quit chan struct{}
 	wg   sync.WaitGroup
@@ -197,7 +198,7 @@ type UtxoSweeperConfig struct {
 	// FeeEstimator is used when crafting sweep transactions to estimate
 	// the necessary fee relative to the expected size of the sweep
 	// transaction.
-	FeeEstimator lnwallet.FeeEstimator
+	FeeEstimator chainfee.Estimator
 
 	// PublishTransaction facilitates the process of broadcasting a signed
 	// transaction to the appropriate network.
@@ -235,7 +236,7 @@ type UtxoSweeperConfig struct {
 
 	// MaxFeeRate is the the maximum fee rate allowed within the
 	// UtxoSweeper.
-	MaxFeeRate lnwallet.SatPerKWeight
+	MaxFeeRate chainfee.SatPerKWeight
 
 	// FeeRateBucketSize is the default size of fee rate buckets we'll use
 	// when clustering inputs into buckets with similar fee rates within the
@@ -403,7 +404,7 @@ func (s *UtxoSweeper) SweepInput(input input.Input,
 // feeRateForPreference returns a fee rate for the given fee preference. It
 // ensures that the fee rate respects the bounds of the UtxoSweeper.
 func (s *UtxoSweeper) feeRateForPreference(
-	feePreference FeePreference) (lnwallet.SatPerKWeight, error) {
+	feePreference FeePreference) (chainfee.SatPerKWeight, error) {
 
 	// Ensure a type of fee preference is specified to prevent using a
 	// default below.
@@ -637,10 +638,10 @@ func (s *UtxoSweeper) collector(blockEpochs <-chan *chainntnfs.BlockEpoch) {
 // bucketForFeeReate determines the proper bucket for a fee rate. This is done
 // in order to batch inputs with similar fee rates together.
 func (s *UtxoSweeper) bucketForFeeRate(
-	feeRate lnwallet.SatPerKWeight) lnwallet.SatPerKWeight {
+	feeRate chainfee.SatPerKWeight) chainfee.SatPerKWeight {
 
-	minBucket := s.relayFeeRate + lnwallet.SatPerKWeight(s.cfg.FeeRateBucketSize)
-	return lnwallet.SatPerKWeight(
+	minBucket := s.relayFeeRate + chainfee.SatPerKWeight(s.cfg.FeeRateBucketSize)
+	return chainfee.SatPerKWeight(
 		math.Ceil(float64(feeRate) / float64(minBucket)),
 	)
 }
@@ -650,8 +651,8 @@ func (s *UtxoSweeper) bucketForFeeRate(
 // sweep fee rate, which is determined by calculating the average fee rate of
 // all inputs within that cluster.
 func (s *UtxoSweeper) clusterBySweepFeeRate() []inputCluster {
-	bucketInputs := make(map[lnwallet.SatPerKWeight]pendingInputs)
-	inputFeeRates := make(map[wire.OutPoint]lnwallet.SatPerKWeight)
+	bucketInputs := make(map[chainfee.SatPerKWeight]pendingInputs)
+	inputFeeRates := make(map[wire.OutPoint]chainfee.SatPerKWeight)
 
 	// First, we'll group together all inputs with similar fee rates. This
 	// is done by determining the fee rate bucket they should belong in.
@@ -678,11 +679,11 @@ func (s *UtxoSweeper) clusterBySweepFeeRate() []inputCluster {
 	// calculating the average fee rate of the inputs within each set.
 	inputClusters := make([]inputCluster, 0, len(bucketInputs))
 	for _, inputs := range bucketInputs {
-		var sweepFeeRate lnwallet.SatPerKWeight
+		var sweepFeeRate chainfee.SatPerKWeight
 		for op := range inputs {
 			sweepFeeRate += inputFeeRates[op]
 		}
-		sweepFeeRate /= lnwallet.SatPerKWeight(len(inputs))
+		sweepFeeRate /= chainfee.SatPerKWeight(len(inputs))
 		inputClusters = append(inputClusters, inputCluster{
 			sweepFeeRate: sweepFeeRate,
 			inputs:       inputs,
@@ -836,7 +837,7 @@ func (s *UtxoSweeper) getInputLists(cluster inputCluster,
 
 // sweep takes a set of preselected inputs, creates a sweep tx and publishes the
 // tx. The output address is only marked as used if the publish succeeds.
-func (s *UtxoSweeper) sweep(inputs inputSet, feeRate lnwallet.SatPerKWeight,
+func (s *UtxoSweeper) sweep(inputs inputSet, feeRate chainfee.SatPerKWeight,
 	currentHeight int32) error {
 
 	// Generate an output script if there isn't an unused script available.

sweep/walletsweep_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd/lnwallet"
+	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 )
 
 // TestDetermineFeePerKw tests that given a fee preference, the
@@ -17,8 +18,8 @@ import (
 func TestDetermineFeePerKw(t *testing.T) {
 	t.Parallel()
 
-	defaultFee := lnwallet.SatPerKWeight(999)
-	relayFee := lnwallet.SatPerKWeight(300)
+	defaultFee := chainfee.SatPerKWeight(999)
+	relayFee := chainfee.SatPerKWeight(300)
 
 	feeEstimator := newMockFeeEstimator(defaultFee, relayFee)
 
@@ -35,7 +36,7 @@ func TestDetermineFeePerKw(t *testing.T) {
 
 		// fee is the value the DetermineFeePerKw should return given
 		// the FeePreference above
-		fee lnwallet.SatPerKWeight
+		fee chainfee.SatPerKWeight
 
 		// fail determines if this test case should fail or not.
 		fail bool
@@ -43,9 +44,9 @@ func TestDetermineFeePerKw(t *testing.T) {
 		// A fee rate below the fee rate floor should output the floor.
 		{
 			feePref: FeePreference{
-				FeeRate: lnwallet.SatPerKWeight(99),
+				FeeRate: chainfee.SatPerKWeight(99),
 			},
-			fee: lnwallet.FeePerKwFloor,
+			fee: chainfee.FeePerKwFloor,
 		},
 
 		// A fee rate above the floor, should pass through and return

watchtower/wtwire/wtwire.go
@@ -8,7 +8,7 @@ import (
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
-	"github.com/lightningnetwork/lnd/lnwallet"
+	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/watchtower/blob"
 )
@@ -74,7 +74,7 @@ func WriteElement(w io.Writer, element interface{}) error {
 			return err
 		}
 
-	case lnwallet.SatPerKWeight:
+	case chainfee.SatPerKWeight:
 		var b [8]byte
 		binary.BigEndian.PutUint64(b[:], uint64(e))
 		if _, err := w.Write(b[:]); err != nil {
@@ -194,12 +194,12 @@ func ReadElement(r io.Reader, element interface{}) error {
 		}
 		*e = bytes
 
-	case *lnwallet.SatPerKWeight:
+	case *chainfee.SatPerKWeight:
 		var b [8]byte
 		if _, err := io.ReadFull(r, b[:]); err != nil {
 			return err
 		}
-		*e = lnwallet.SatPerKWeight(binary.BigEndian.Uint64(b[:]))
+		*e = chainfee.SatPerKWeight(binary.BigEndian.Uint64(b[:]))
 
 	case *ErrorCode:
 		var b [2]byte

matheusd/dcrlnd

chancloser.go
@@ -9,6 +9,7 @@ import (
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/htlcswitch"
 	"github.com/decred/dcrlnd/lnwallet"
+	"github.com/decred/dcrlnd/lnwallet/chainfee"
 	"github.com/decred/dcrlnd/lnwire"
 )
 
@@ -150,7 +151,7 @@ type channelCloser struct {
 // passed configuration, and delivery+fee preference. The final argument should
 // only be populated iff, we're the initiator of this closing request.
 func newChannelCloser(cfg chanCloseCfg, deliveryScript []byte,
-	idealFeePerKw lnwallet.AtomPerKByte, negotiationHeight uint32,
+	idealFeePerKw chainfee.AtomPerKByte, negotiationHeight uint32,
 	closeReq *htlcswitch.ChanClose) *channelCloser {
 
 	// Given the target fee-per-kw, we'll compute what our ideal _total_

lnwallet/wallet_test.go
@@ -5,12 +5,15 @@ import (
 
 	"github.com/decred/dcrd/dcrutil/v2"
 	"github.com/decred/dcrlnd/input"
+	"github.com/decred/dcrlnd/lnwallet/chainfee"
 )
 
 // fundingFee is a helper method that returns the fee estimate used for a tx
 // with the given number of inputs and the optional change output. This matches
 // the estimate done by the wallet.
-func fundingFee(feeRate AtomPerKByte, numInput int, change bool) dcrutil.Amount {
+func fundingFee(feeRate chainfee.AtomPerKByte, numInput int,
+	change bool) dcrutil.Amount {
+
 	var sizeEstimate input.TxSizeEstimator
 
 	// All inputs.
@@ -39,7 +42,7 @@ func fundingFee(feeRate AtomPerKByte, numInput int, change bool) dcrutil.Amount 
 func TestCoinSelect(t *testing.T) {
 	t.Parallel()
 
-	const feeRate = AtomPerKByte(100)
+	const feeRate = chainfee.AtomPerKByte(100)
 	const dust = dcrutil.Amount(100)
 
 	type testCase struct {
@@ -185,7 +188,7 @@ func TestCoinSelect(t *testing.T) {
 func TestCoinSelectSubtractFees(t *testing.T) {
 	t.Parallel()
 
-	const feeRate = AtomPerKByte(100)
+	const feeRate = chainfee.AtomPerKByte(100)
 	const dustLimit = dcrutil.Amount(1000)
 	const dust = dcrutil.Amount(100)
 

rpcserver.go
@@ -40,6 +40,7 @@ import (
 	"github.com/decred/dcrlnd/lnrpc/routerrpc"
 	"github.com/decred/dcrlnd/lntypes"
 	"github.com/decred/dcrlnd/lnwallet"
+	"github.com/decred/dcrlnd/lnwallet/chainfee"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/macaroons"
 	"github.com/decred/dcrlnd/monitoring"
@@ -814,7 +815,7 @@ func addrPairsToOutputs(addrPairs map[string]int64, netParams *chaincfg.Params) 
 // more addresses specified in the passed payment map. The payment map maps an
 // address to a specified output value to be sent to that address.
 func (r *rpcServer) sendCoinsOnChain(paymentMap map[string]int64,
-	feeRate lnwallet.AtomPerKByte) (*chainhash.Hash, error) {
+	feeRate chainfee.AtomPerKByte) (*chainhash.Hash, error) {
 
 	outputs, err := addrPairsToOutputs(paymentMap, activeNetParams.Params)
 	if err != nil {
@@ -997,7 +998,7 @@ func (r *rpcServer) SendCoins(ctx context.Context,
 
 	// Based on the passed fee related parameters, we'll determine an
 	// appropriate fee rate for this transaction.
-	atomsPerKB := lnwallet.AtomPerKByte(in.AtomsPerByte * 1000)
+	atomsPerKB := chainfee.AtomPerKByte(in.AtomsPerByte * 1000)
 	feePerKB, err := sweep.DetermineFeePerKB(
 		r.server.cc.feeEstimator, sweep.FeePreference{
 			ConfTarget: uint32(in.TargetConf),
@@ -1113,7 +1114,7 @@ func (r *rpcServer) SendMany(ctx context.Context,
 
 	// Based on the passed fee related parameters, we'll determine an
 	// appropriate fee rate for this transaction.
-	atomsPerKB := lnwallet.AtomPerKByte(in.AtomsPerByte * 1000)
+	atomsPerKB := chainfee.AtomPerKByte(in.AtomsPerByte * 1000)
 	feePerKB, err := sweep.DetermineFeePerKB(
 		r.server.cc.feeEstimator, sweep.FeePreference{
 			ConfTarget: uint32(in.TargetConf),
@@ -1482,7 +1483,7 @@ func (r *rpcServer) OpenChannel(in *lnrpc.OpenChannelRequest,
 
 	// Based on the passed fee related parameters, we'll determine an
 	// appropriate fee rate for the funding transaction.
-	atomsPerKB := lnwallet.AtomPerKByte(in.AtomsPerByte * 1000)
+	atomsPerKB := chainfee.AtomPerKByte(in.AtomsPerByte * 1000)
 	feeRate, err := sweep.DetermineFeePerKB(
 		r.server.cc.feeEstimator, sweep.FeePreference{
 			ConfTarget: uint32(in.TargetConf),
@@ -1627,7 +1628,7 @@ func (r *rpcServer) OpenChannelSync(ctx context.Context,
 
 	// Based on the passed fee related parameters, we'll determine an
 	// appropriate fee rate for the funding transaction.
-	atomsPerKB := lnwallet.AtomPerKByte(in.AtomsPerByte * 1000)
+	atomsPerKB := chainfee.AtomPerKByte(in.AtomsPerByte * 1000)
 	feeRate, err := sweep.DetermineFeePerKB(
 		r.server.cc.feeEstimator, sweep.FeePreference{
 			ConfTarget: uint32(in.TargetConf),
@@ -1825,7 +1826,7 @@ func (r *rpcServer) CloseChannel(in *lnrpc.CloseChannelRequest,
 		// Based on the passed fee related parameters, we'll determine
 		// an appropriate fee rate for the cooperative closure
 		// transaction.
-		atomsPerKB := lnwallet.AtomPerKByte(in.AtomsPerByte * 1000)
+		atomsPerKB := chainfee.AtomPerKByte(in.AtomsPerByte * 1000)
 		feeRate, err := sweep.DetermineFeePerKB(
 			r.server.cc.feeEstimator, sweep.FeePreference{
 				ConfTarget: uint32(in.TargetConf),

sweep/sweeper.go
@@ -17,13 +17,14 @@ import (
 	"github.com/decred/dcrlnd/chainntnfs"
 	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/lnwallet"
+	"github.com/decred/dcrlnd/lnwallet/chainfee"
 )
 
 const (
 	// DefaultMaxFeeRate is the default maximum fee rate allowed within the
 	// UtxoSweeper. The current value is equivalent to a fee rate of
 	// 0.00100000 DCR/KByte.
-	DefaultMaxFeeRate lnwallet.AtomPerKByte = 1e6
+	DefaultMaxFeeRate chainfee.AtomPerKByte = 1e6
 
 	// DefaultFeeRateBucketSize is the default size of fee rate buckets
 	// we'll use when clustering inputs into buckets with similar fee rates
@@ -93,7 +94,7 @@ type pendingInput struct {
 
 	// lastFeeRate is the most recent fee rate used for this input within a
 	// transaction broadcast to the network.
-	lastFeeRate lnwallet.AtomPerKByte
+	lastFeeRate chainfee.AtomPerKByte
 }
 
 // pendingInputs is a type alias for a set of pending inputs.
@@ -102,7 +103,7 @@ type pendingInputs = map[wire.OutPoint]*pendingInput
 // inputCluster is a helper struct to gather a set of pending inputs that should
 // be swept with the specified fee rate.
 type inputCluster struct {
-	sweepFeeRate lnwallet.AtomPerKByte
+	sweepFeeRate chainfee.AtomPerKByte
 	inputs       pendingInputs
 }
 
@@ -127,7 +128,7 @@ type PendingInput struct {
 
 	// LastFeeRate is the most recent fee rate used for the input being
 	// swept within a transaction broadcast to the network.
-	LastFeeRate lnwallet.AtomPerKByte
+	LastFeeRate chainfee.AtomPerKByte
 
 	// BroadcastAttempts is the number of attempts we've made to sweept the
 	// input.
@@ -183,7 +184,7 @@ type UtxoSweeper struct {
 
 	currentOutputScript []byte
 
-	relayFeeRate lnwallet.AtomPerKByte
+	relayFeeRate chainfee.AtomPerKByte
 
 	quit chan struct{}
 	wg   sync.WaitGroup
@@ -198,7 +199,7 @@ type UtxoSweeperConfig struct {
 	// FeeEstimator is used when crafting sweep transactions to estimate
 	// the necessary fee relative to the expected size of the sweep
 	// transaction.
-	FeeEstimator lnwallet.FeeEstimator
+	FeeEstimator chainfee.Estimator
 
 	// PublishTransaction facilitates the process of broadcasting a signed
 	// transaction to the appropriate network.
@@ -240,7 +241,7 @@ type UtxoSweeperConfig struct {
 
 	// MaxFeeRate is the the maximum fee rate allowed within the
 	// UtxoSweeper.
-	MaxFeeRate lnwallet.AtomPerKByte
+	MaxFeeRate chainfee.AtomPerKByte
 
 	// FeeRateBucketSize is the default size of fee rate buckets we'll use
 	// when clustering inputs into buckets with similar fee rates within the
@@ -408,7 +409,7 @@ func (s *UtxoSweeper) SweepInput(input input.Input,
 // feeRateForPreference returns a fee rate for the given fee preference. It
 // ensures that the fee rate respects the bounds of the UtxoSweeper.
 func (s *UtxoSweeper) feeRateForPreference(
-	feePreference FeePreference) (lnwallet.AtomPerKByte, error) {
+	feePreference FeePreference) (chainfee.AtomPerKByte, error) {
 
 	// Ensure a type of fee preference is specified to prevent using a
 	// default below.
@@ -642,10 +643,10 @@ func (s *UtxoSweeper) collector(blockEpochs <-chan *chainntnfs.BlockEpoch) {
 // bucketForFeeReate determines the proper bucket for a fee rate. This is done
 // in order to batch inputs with similar fee rates together.
 func (s *UtxoSweeper) bucketForFeeRate(
-	feeRate lnwallet.AtomPerKByte) lnwallet.AtomPerKByte {
+	feeRate chainfee.AtomPerKByte) chainfee.AtomPerKByte {
 
-	minBucket := s.relayFeeRate + lnwallet.AtomPerKByte(s.cfg.FeeRateBucketSize)
-	return lnwallet.AtomPerKByte(
+	minBucket := s.relayFeeRate + chainfee.AtomPerKByte(s.cfg.FeeRateBucketSize)
+	return chainfee.AtomPerKByte(
 		math.Ceil(float64(feeRate) / float64(minBucket)),
 	)
 }
@@ -655,8 +656,8 @@ func (s *UtxoSweeper) bucketForFeeRate(
 // sweep fee rate, which is determined by calculating the average fee rate of
 // all inputs within that cluster.
 func (s *UtxoSweeper) clusterBySweepFeeRate() []inputCluster {
-	bucketInputs := make(map[lnwallet.AtomPerKByte]pendingInputs)
-	inputFeeRates := make(map[wire.OutPoint]lnwallet.AtomPerKByte)
+	bucketInputs := make(map[chainfee.AtomPerKByte]pendingInputs)
+	inputFeeRates := make(map[wire.OutPoint]chainfee.AtomPerKByte)
 
 	// First, we'll group together all inputs with similar fee rates. This
 	// is done by determining the fee rate bucket they should belong in.
@@ -683,11 +684,11 @@ func (s *UtxoSweeper) clusterBySweepFeeRate() []inputCluster {
 	// calculating the average fee rate of the inputs within each set.
 	inputClusters := make([]inputCluster, 0, len(bucketInputs))
 	for _, inputs := range bucketInputs {
-		var sweepFeeRate lnwallet.AtomPerKByte
+		var sweepFeeRate chainfee.AtomPerKByte
 		for op := range inputs {
 			sweepFeeRate += inputFeeRates[op]
 		}
-		sweepFeeRate /= lnwallet.AtomPerKByte(len(inputs))
+		sweepFeeRate /= chainfee.AtomPerKByte(len(inputs))
 		inputClusters = append(inputClusters, inputCluster{
 			sweepFeeRate: sweepFeeRate,
 			inputs:       inputs,
@@ -841,7 +842,7 @@ func (s *UtxoSweeper) getInputLists(cluster inputCluster,
 
 // sweep takes a set of preselected inputs, creates a sweep tx and publishes the
 // tx. The output address is only marked as used if the publish succeeds.
-func (s *UtxoSweeper) sweep(inputs inputSet, feeRate lnwallet.AtomPerKByte,
+func (s *UtxoSweeper) sweep(inputs inputSet, feeRate chainfee.AtomPerKByte,
 	currentHeight int32) error {
 
 	// Generate an output script if there isn't an unused script available.

sweep/walletsweep_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/decred/dcrd/txscript/v2"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/lnwallet"
+	"github.com/decred/dcrlnd/lnwallet/chainfee"
 )
 
 // TestDetermineFeePerKB tests that given a fee preference, the
@@ -17,8 +18,8 @@ import (
 func TestDetermineFeePerKw(t *testing.T) {
 	t.Parallel()
 
-	defaultFee := lnwallet.AtomPerKByte(99999)
-	relayFee := lnwallet.AtomPerKByte(30000)
+	defaultFee := chainfee.AtomPerKByte(99999)
+	relayFee := chainfee.AtomPerKByte(30000)
 
 	feeEstimator := newMockFeeEstimator(defaultFee, relayFee)
 
@@ -35,7 +36,7 @@ func TestDetermineFeePerKw(t *testing.T) {
 
 		// fee is the value the DetermineFeePerKB should return given
 		// the FeePreference above
-		fee lnwallet.AtomPerKByte
+		fee chainfee.AtomPerKByte
 
 		// fail determines if this test case should fail or not.
 		fail bool
@@ -43,9 +44,9 @@ func TestDetermineFeePerKw(t *testing.T) {
 		// A fee rate below the fee rate floor should output the floor.
 		{
 			feePref: FeePreference{
-				FeeRate: lnwallet.AtomPerKByte(99),
+				FeeRate: chainfee.AtomPerKByte(99),
 			},
-			fee: lnwallet.FeePerKBFloor,
+			fee: chainfee.FeePerKBFloor,
 		},
 
 		// A fee rate above the floor, should pass through and return

watchtower/wtwire/wtwire.go
@@ -8,7 +8,7 @@ import (
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrec/secp256k1/v2"
 	"github.com/decred/dcrd/wire"
-	"github.com/decred/dcrlnd/lnwallet"
+	"github.com/decred/dcrlnd/lnwallet/chainfee"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/watchtower/blob"
 )
@@ -74,7 +74,7 @@ func WriteElement(w io.Writer, element interface{}) error {
 			return err
 		}
 
-	case lnwallet.AtomPerKByte:
+	case chainfee.AtomPerKByte:
 		var b [8]byte
 		binary.BigEndian.PutUint64(b[:], uint64(e))
 		if _, err := w.Write(b[:]); err != nil {
@@ -194,12 +194,12 @@ func ReadElement(r io.Reader, element interface{}) error {
 		}
 		*e = bytes
 
-	case *lnwallet.AtomPerKByte:
+	case *chainfee.AtomPerKByte:
 		var b [8]byte
 		if _, err := io.ReadFull(r, b[:]); err != nil {
 			return err
 		}
-		*e = lnwallet.AtomPerKByte(binary.BigEndian.Uint64(b[:]))
+		*e = chainfee.AtomPerKByte(binary.BigEndian.Uint64(b[:]))
 
 	case *ErrorCode:
 		var b [2]byte