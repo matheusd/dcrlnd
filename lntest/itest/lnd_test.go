lightningnetwork/lnd

lntest: specify lnd binary
Integration tests in external projects might not have the same folder
structure as lnd does. Therefore we want to allow the path to the
lnd itest binary to be configurable.

lntest/itest/lnd_test.go
@@ -56,6 +56,7 @@ const (
 	minerMempoolTimeout = lntest.MinerMempoolTimeout
 	channelOpenTimeout  = lntest.ChannelOpenTimeout
 	channelCloseTimeout = lntest.ChannelCloseTimeout
+	itestLndBinary      = "../../lnd-itest"
 )
 
 // harnessTest wraps a regular testing.T providing enhanced error detection
@@ -15237,7 +15238,9 @@ func TestLightningNetworkDaemon(t *testing.T) {
 
 	// Now we can set up our test harness (LND instance), with the chain
 	// backend we just created.
-	lndHarness, err = lntest.NewNetworkHarness(miner, chainBackend)
+	lndHarness, err = lntest.NewNetworkHarness(
+		miner, chainBackend, itestLndBinary,
+	)
 	if err != nil {
 		ht.Fatalf("unable to create lightning network harness: %v", err)
 	}

matheusd/dcrlnd

lntest/itest/lnd_test.go
@@ -66,6 +66,8 @@ const (
 	// of the lntest harness, assuming the harness initializes those nodes
 	// with 10 outputs of 1 DCR each.
 	defaultChanAmt = dcrutil.Amount(1<<24) - 1
+
+	itestLndBinary = "../../dcrlnd-itest"
 )
 
 // harnessTest wraps a regular testing.T providing enhanced error detection
@@ -16984,7 +16986,9 @@ func TestLightningNetworkDaemon(t *testing.T) {
 
 	// Now we can set up our test harness (LND instance), with the chain
 	// backend we just created.
-	lndHarness, err = lntest.NewNetworkHarness(miner, chainBackend)
+	lndHarness, err = lntest.NewNetworkHarness(
+		miner, chainBackend, itestLndBinary,
+	)
 	if err != nil {
 		ht.Fatalf("unable to create lightning network harness: %v", err)
 	}