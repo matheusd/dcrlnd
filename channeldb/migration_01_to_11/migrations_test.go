lightningnetwork/lnd

migration_01_to_11: remove version checking for migration tests

channeldb/migration_01_to_11/migrations_test.go
@@ -135,15 +135,6 @@ func TestPaymentStatusesMigration(t *testing.T) {
 	// Verify that the created payment status is "Completed" for our one
 	// fake payment.
 	afterMigrationFunc := func(d *DB) {
-		meta, err := d.FetchMeta(nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if meta.DbVersionNumber != 1 {
-			t.Fatal("migration 'paymentStatusesMigration' wasn't applied")
-		}
-
 		// Check that our completed payments were migrated.
 		paymentStatus, err := d.fetchPaymentStatus(paymentHash)
 		if err != nil {
@@ -404,15 +395,6 @@ func TestMigrateOptionalChannelCloseSummaryFields(t *testing.T) {
 
 		// After the migration it should be found in the new format.
 		afterMigrationFunc := func(d *DB) {
-			meta, err := d.FetchMeta(nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if meta.DbVersionNumber != 1 {
-				t.Fatal("migration wasn't applied")
-			}
-
 			// We generate the new serialized version, to check
 			// against what is found in the DB.
 			var b bytes.Buffer
@@ -521,16 +503,8 @@ func TestMigrateGossipMessageStoreKeys(t *testing.T) {
 	//   2. We can find the message under its new key.
 	//   3. The message matches the original.
 	afterMigration := func(db *DB) {
-		meta, err := db.FetchMeta(nil)
-		if err != nil {
-			t.Fatalf("unable to fetch db version: %v", err)
-		}
-		if meta.DbVersionNumber != 1 {
-			t.Fatalf("migration should have succeeded but didn't")
-		}
-
 		var rawMsg []byte
-		err = db.View(func(tx *bbolt.Tx) error {
+		err := db.View(func(tx *bbolt.Tx) error {
 			messageStore := tx.Bucket(messageStoreBucket)
 			if messageStore == nil {
 				return errors.New("message store bucket not " +
@@ -617,15 +591,6 @@ func TestOutgoingPaymentsMigration(t *testing.T) {
 
 	// Verify that all payments were migrated.
 	afterMigrationFunc := func(d *DB) {
-		meta, err := d.FetchMeta(nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if meta.DbVersionNumber != 1 {
-			t.Fatal("migration 'paymentStatusesMigration' wasn't applied")
-		}
-
 		sentPayments, err := d.fetchPaymentsMigration9()
 		if err != nil {
 			t.Fatalf("unable to fetch sent payments: %v", err)

matheusd/dcrlnd

channeldb/migration_01_to_11/migrations_test.go
@@ -135,15 +135,6 @@ func TestPaymentStatusesMigration(t *testing.T) {
 	// Verify that the created payment status is "Completed" for our one
 	// fake payment.
 	afterMigrationFunc := func(d *DB) {
-		meta, err := d.FetchMeta(nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if meta.DbVersionNumber != 1 {
-			t.Fatal("migration 'paymentStatusesMigration' wasn't applied")
-		}
-
 		// Check that our completed payments were migrated.
 		paymentStatus, err := d.fetchPaymentStatus(paymentHash)
 		if err != nil {
@@ -404,15 +395,6 @@ func TestMigrateOptionalChannelCloseSummaryFields(t *testing.T) {
 
 		// After the migration it should be found in the new format.
 		afterMigrationFunc := func(d *DB) {
-			meta, err := d.FetchMeta(nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if meta.DbVersionNumber != 1 {
-				t.Fatal("migration wasn't applied")
-			}
-
 			// We generate the new serialized version, to check
 			// against what is found in the DB.
 			var b bytes.Buffer
@@ -521,16 +503,8 @@ func TestMigrateGossipMessageStoreKeys(t *testing.T) {
 	//   2. We can find the message under its new key.
 	//   3. The message matches the original.
 	afterMigration := func(db *DB) {
-		meta, err := db.FetchMeta(nil)
-		if err != nil {
-			t.Fatalf("unable to fetch db version: %v", err)
-		}
-		if meta.DbVersionNumber != 1 {
-			t.Fatalf("migration should have succeeded but didn't")
-		}
-
 		var rawMsg []byte
-		err = db.View(func(tx *bolt.Tx) error {
+		err := db.View(func(tx *bolt.Tx) error {
 			messageStore := tx.Bucket(messageStoreBucket)
 			if messageStore == nil {
 				return errors.New("message store bucket not " +
@@ -617,15 +591,6 @@ func TestOutgoingPaymentsMigration(t *testing.T) {
 
 	// Verify that all payments were migrated.
 	afterMigrationFunc := func(d *DB) {
-		meta, err := d.FetchMeta(nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if meta.DbVersionNumber != 1 {
-			t.Fatal("migration 'paymentStatusesMigration' wasn't applied")
-		}
-
 		sentPayments, err := d.fetchPaymentsMigration9()
 		if err != nil {
 			t.Fatalf("unable to fetch sent payments: %v", err)