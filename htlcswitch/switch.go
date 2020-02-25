lightningnetwork/lnd

multi: Use user provided close address for cooperative closes
This commit is adapted from @Bluetegu's original
pull request #1462.

This commit reads an optional address to pay funds out to
from a user iniitiated close channel address. If the channel
already has a shutdown script set, the request will fail if
an address is provided. Otherwise, the cooperative close will
pay out to the address provided.

htlcswitch/switch.go
@@ -105,6 +105,9 @@ type ChanClose struct {
 	// process for the cooperative closure transaction kicks off.
 	TargetFeePerKw chainfee.SatPerKWeight
 
+	// DeliveryScript is an optional delivery script to pay funds out to.
+	DeliveryScript lnwire.DeliveryAddress
+
 	// Updates is used by request creator to receive the notifications about
 	// execution of the close channel request.
 	Updates chan interface{}
@@ -1365,12 +1368,13 @@ func (s *Switch) teardownCircuit(pkt *htlcPacket) error {
 }
 
 // CloseLink creates and sends the close channel command to the target link
-// directing the specified closure type. If the closure type if CloseRegular,
-// then the last parameter should be the ideal fee-per-kw that will be used as
-// a starting point for close negotiation.
-func (s *Switch) CloseLink(chanPoint *wire.OutPoint, closeType ChannelCloseType,
-	targetFeePerKw chainfee.SatPerKWeight) (chan interface{},
-	chan error) {
+// directing the specified closure type. If the closure type is CloseRegular,
+// targetFeePerKw parameter should be the ideal fee-per-kw that will be used as
+// a starting point for close negotiation. The deliveryScript parameter is an
+// optional parameter which sets a user specified script to close out to.
+func (s *Switch) CloseLink(chanPoint *wire.OutPoint,
+	closeType ChannelCloseType, targetFeePerKw chainfee.SatPerKWeight,
+	deliveryScript lnwire.DeliveryAddress) (chan interface{}, chan error) {
 
 	// TODO(roasbeef) abstract out the close updates.
 	updateChan := make(chan interface{}, 2)
@@ -1381,6 +1385,7 @@ func (s *Switch) CloseLink(chanPoint *wire.OutPoint, closeType ChannelCloseType,
 		ChanPoint:      chanPoint,
 		Updates:        updateChan,
 		TargetFeePerKw: targetFeePerKw,
+		DeliveryScript: deliveryScript,
 		Err:            errChan,
 	}
 

rpcserver.go
@@ -1881,8 +1881,28 @@ func (r *rpcServer) CloseChannel(in *lnrpc.CloseChannelRequest,
 		// cooperative channel closure. So we'll forward the request to
 		// the htlc switch which will handle the negotiation and
 		// broadcast details.
+
+		var deliveryScript lnwire.DeliveryAddress
+
+		// If a delivery address to close out to was specified, decode it.
+		if len(in.DeliveryAddress) > 0 {
+			// Decode the address provided.
+			addr, err := btcutil.DecodeAddress(
+				in.DeliveryAddress, activeNetParams.Params,
+			)
+			if err != nil {
+				return fmt.Errorf("invalid delivery address: %v", err)
+			}
+
+			// Create a script to pay out to the address provided.
+			deliveryScript, err = txscript.PayToAddrScript(addr)
+			if err != nil {
+				return err
+			}
+		}
+
 		updateChan, errChan = r.server.htlcSwitch.CloseLink(
-			chanPoint, htlcswitch.CloseRegular, feeRate,
+			chanPoint, htlcswitch.CloseRegular, feeRate, deliveryScript,
 		)
 	}
 out:

matheusd/dcrlnd

htlcswitch/switch.go
@@ -105,6 +105,9 @@ type ChanClose struct {
 	// process for the cooperative closure transaction kicks off.
 	TargetFeePerKB chainfee.AtomPerKByte
 
+	// DeliveryScript is an optional delivery script to pay funds out to.
+	DeliveryScript lnwire.DeliveryAddress
+
 	// Updates is used by request creator to receive the notifications about
 	// execution of the close channel request.
 	Updates chan interface{}
@@ -1365,12 +1368,13 @@ func (s *Switch) teardownCircuit(pkt *htlcPacket) error {
 }
 
 // CloseLink creates and sends the close channel command to the target link
-// directing the specified closure type. If the closure type if CloseRegular,
-// then the last parameter should be the ideal fee-per-kw that will be used as
-// a starting point for close negotiation.
-func (s *Switch) CloseLink(chanPoint *wire.OutPoint, closeType ChannelCloseType,
-	targetFeePerKB chainfee.AtomPerKByte) (chan interface{},
-	chan error) {
+// directing the specified closure type. If the closure type is CloseRegular,
+// targetFeePerKw parameter should be the ideal fee-per-kb that will be used as
+// a starting point for close negotiation. The deliveryScript parameter is an
+// optional parameter which sets a user specified script to close out to.
+func (s *Switch) CloseLink(chanPoint *wire.OutPoint,
+	closeType ChannelCloseType, targetFeePerKB chainfee.AtomPerKByte,
+	deliveryScript lnwire.DeliveryAddress) (chan interface{}, chan error) {
 
 	// TODO(roasbeef) abstract out the close updates.
 	updateChan := make(chan interface{}, 2)
@@ -1381,6 +1385,7 @@ func (s *Switch) CloseLink(chanPoint *wire.OutPoint, closeType ChannelCloseType,
 		ChanPoint:      chanPoint,
 		Updates:        updateChan,
 		TargetFeePerKB: targetFeePerKB,
+		DeliveryScript: deliveryScript,
 		Err:            errChan,
 	}
 

rpcserver.go
@@ -1857,8 +1857,28 @@ func (r *rpcServer) CloseChannel(in *lnrpc.CloseChannelRequest,
 		// cooperative channel closure. So we'll forward the request to
 		// the htlc switch which will handle the negotiation and
 		// broadcast details.
+
+		var deliveryScript lnwire.DeliveryAddress
+
+		// If a delivery address to close out to was specified, decode it.
+		if len(in.DeliveryAddress) > 0 {
+			// Decode the address provided.
+			addr, err := dcrutil.DecodeAddress(
+				in.DeliveryAddress, activeNetParams.Params,
+			)
+			if err != nil {
+				return fmt.Errorf("invalid delivery address: %v", err)
+			}
+
+			// Create a script to pay out to the address provided.
+			deliveryScript, err = txscript.PayToAddrScript(addr)
+			if err != nil {
+				return err
+			}
+		}
+
 		updateChan, errChan = r.server.htlcSwitch.CloseLink(
-			chanPoint, htlcswitch.CloseRegular, feeRate,
+			chanPoint, htlcswitch.CloseRegular, feeRate, deliveryScript,
 		)
 	}
 out: