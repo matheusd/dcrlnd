lightningnetwork/lnd

routerrpc: create pair data proto message
With a separate proto message, it becomes possible to also return the
pair data for a single pair. This prepares for the new mc probability
querying rpc.

cmd/lncli/cmd_query_mission_control.go
@@ -48,9 +48,9 @@ func queryMissionControl(ctx *cli.Context) error {
 			displayPairHistory{
 				NodeFrom:              hex.EncodeToString(n.NodeFrom),
 				NodeTo:                hex.EncodeToString(n.NodeTo),
-				LastAttemptSuccessful: n.LastAttemptSuccessful,
-				Timestamp:             n.Timestamp,
-				MinPenalizeAmtSat:     n.MinPenalizeAmtSat,
+				LastAttemptSuccessful: n.History.LastAttemptSuccessful,
+				Timestamp:             n.History.Timestamp,
+				MinPenalizeAmtSat:     n.History.MinPenalizeAmtSat,
 			},
 		)
 	}

lnrpc/routerrpc/router_server.go
@@ -472,13 +472,9 @@ func (s *Server) QueryMissionControl(ctx context.Context,
 		pair := p
 
 		rpcPair := PairHistory{
-			NodeFrom:  pair.Pair.From[:],
-			NodeTo:    pair.Pair.To[:],
-			Timestamp: pair.Timestamp.Unix(),
-			MinPenalizeAmtSat: int64(
-				pair.MinPenalizeAmt.ToSatoshis(),
-			),
-			LastAttemptSuccessful: pair.Success,
+			NodeFrom: pair.Pair.From[:],
+			NodeTo:   pair.Pair.To[:],
+			History:  toRPCPairData(&pair.TimedPairResult),
 		}
 
 		rpcPairs = append(rpcPairs, &rpcPair)
@@ -489,6 +485,22 @@ func (s *Server) QueryMissionControl(ctx context.Context,
 	}
 
 	return &response, nil
+}
+
+// toRPCPairData marshalls mission control pair data to the rpc struct.
+func toRPCPairData(data *routing.TimedPairResult) *PairData {
+	rpcData := PairData{
+		MinPenalizeAmtSat: int64(
+			data.MinPenalizeAmt.ToSatoshis(),
+		),
+		LastAttemptSuccessful: data.Success,
+	}
+
+	if !data.Timestamp.IsZero() {
+		rpcData.Timestamp = data.Timestamp.Unix()
+	}
+
+	return &rpcData
 }
 
 // TrackPayment returns a stream of payment state updates. The stream is

matheusd/dcrlnd

cmd/dcrlncli/cmd_query_mission_control.go
@@ -48,9 +48,9 @@ func queryMissionControl(ctx *cli.Context) error {
 			displayPairHistory{
 				NodeFrom:              hex.EncodeToString(n.NodeFrom),
 				NodeTo:                hex.EncodeToString(n.NodeTo),
-				LastAttemptSuccessful: n.LastAttemptSuccessful,
-				Timestamp:             n.Timestamp,
-				MinPenalizeAmtAtoms:   n.MinPenalizeAmtAtoms,
+				LastAttemptSuccessful: n.History.LastAttemptSuccessful,
+				Timestamp:             n.History.Timestamp,
+				MinPenalizeAmtAtoms:   n.History.MinPenalizeAmtAtoms,
 			},
 		)
 	}

lnrpc/routerrpc/router_server.go
@@ -472,13 +472,9 @@ func (s *Server) QueryMissionControl(ctx context.Context,
 		pair := p
 
 		rpcPair := PairHistory{
-			NodeFrom:  pair.Pair.From[:],
-			NodeTo:    pair.Pair.To[:],
-			Timestamp: pair.Timestamp.Unix(),
-			MinPenalizeAmtAtoms: int64(
-				pair.MinPenalizeAmt.ToAtoms(),
-			),
-			LastAttemptSuccessful: pair.Success,
+			NodeFrom: pair.Pair.From[:],
+			NodeTo:   pair.Pair.To[:],
+			History:  toRPCPairData(&pair.TimedPairResult),
 		}
 
 		rpcPairs = append(rpcPairs, &rpcPair)
@@ -489,6 +485,22 @@ func (s *Server) QueryMissionControl(ctx context.Context,
 	}
 
 	return &response, nil
+}
+
+// toRPCPairData marshalls mission control pair data to the rpc struct.
+func toRPCPairData(data *routing.TimedPairResult) *PairData {
+	rpcData := PairData{
+		MinPenalizeAmtAtoms: int64(
+			data.MinPenalizeAmt.ToAtoms(),
+		),
+		LastAttemptSuccessful: data.Success,
+	}
+
+	if !data.Timestamp.IsZero() {
+		rpcData.Timestamp = data.Timestamp.Unix()
+	}
+
+	return &rpcData
 }
 
 // TrackPayment returns a stream of payment state updates. The stream is