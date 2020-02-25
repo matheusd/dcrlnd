lightningnetwork/lnd

routing+lnrpc+lncli: allow setting min htlc policy

cmd/lncli/commands.go
@@ -3460,6 +3460,12 @@ var updateChannelPolicyCommand = cli.Command{
 				"forwarded HTLCs",
 		},
 		cli.Uint64Flag{
+			Name: "min_htlc_msat",
+			Usage: "if set, the min HTLC size that will be applied " +
+				"to all forwarded HTLCs. If unset, the min HTLC " +
+				"is left unchanged.",
+		},
+		cli.Uint64Flag{
 			Name: "max_htlc_msat",
 			Usage: "if set, the max HTLC size that will be applied " +
 				"to all forwarded HTLCs. If unset, the max HTLC " +
@@ -3579,6 +3585,11 @@ func updateChannelPolicy(ctx *cli.Context) error {
 		FeeRate:       feeRate,
 		TimeLockDelta: uint32(timeLockDelta),
 		MaxHtlcMsat:   ctx.Uint64("max_htlc_msat"),
+	}
+
+	if ctx.IsSet("min_htlc_msat") {
+		req.MinHtlcMsat = ctx.Uint64("min_htlc_msat")
+		req.MinHtlcMsatSpecified = true
 	}
 
 	if chanPoint != nil {

routing/router.go
@@ -224,6 +224,10 @@ type ChannelPolicy struct {
 	// MaxHTLC is the maximum HTLC size including fees we are allowed to
 	// forward over this channel.
 	MaxHTLC lnwire.MilliSatoshi
+
+	// MinHTLC is the minimum HTLC size including fees we are allowed to
+	// forward over this channel.
+	MinHTLC *lnwire.MilliSatoshi
 }
 
 // Config defines the configuration for the ChannelRouter. ALL elements within

rpcserver.go
@@ -4864,15 +4864,25 @@ func (r *rpcServer) UpdateChannelPolicy(ctx context.Context,
 		FeeRate: feeRateFixed,
 	}
 
+	maxHtlc := lnwire.MilliSatoshi(req.MaxHtlcMsat)
+	var minHtlc *lnwire.MilliSatoshi
+	if req.MinHtlcMsatSpecified {
+		min := lnwire.MilliSatoshi(req.MinHtlcMsat)
+		minHtlc = &min
+	}
+
 	chanPolicy := routing.ChannelPolicy{
 		FeeSchema:     feeSchema,
 		TimeLockDelta: req.TimeLockDelta,
-		MaxHTLC:       lnwire.MilliSatoshi(req.MaxHtlcMsat),
+		MaxHTLC:       maxHtlc,
+		MinHTLC:       minHtlc,
 	}
 
 	rpcsLog.Debugf("[updatechanpolicy] updating channel policy base_fee=%v, "+
-		"rate_float=%v, rate_fixed=%v, time_lock_delta: %v, targets=%v",
+		"rate_float=%v, rate_fixed=%v, time_lock_delta: %v, "+
+		"min_htlc=%v, max_htlc=%v, targets=%v",
 		req.BaseFeeMsat, req.FeeRate, feeRateFixed, req.TimeLockDelta,
+		minHtlc, maxHtlc,
 		spew.Sdump(targetChans))
 
 	// With the scope resolved, we'll now send this to the local channel

matheusd/dcrlnd

cmd/dcrlncli/commands.go
@@ -3461,6 +3461,12 @@ var updateChannelPolicyCommand = cli.Command{
 				"forwarded HTLCs",
 		},
 		cli.Uint64Flag{
+			Name: "min_htlc_m_atoms",
+			Usage: "if set, the min HTLC size that will be applied " +
+				"to all forwarded HTLCs. If unset, the min HTLC " +
+				"is left unchanged.",
+		},
+		cli.Uint64Flag{
 			Name: "max_htlc_m_atoms",
 			Usage: "if set, the max HTLC size that will be applied " +
 				"to all forwarded HTLCs. If unset, the max HTLC " +
@@ -3580,6 +3586,11 @@ func updateChannelPolicy(ctx *cli.Context) error {
 		FeeRate:       feeRate,
 		TimeLockDelta: uint32(timeLockDelta),
 		MaxHtlcMAtoms: ctx.Uint64("max_htlc_m_atoms"),
+	}
+
+	if ctx.IsSet("min_htlc_m_atoms") {
+		req.MinHtlcMAtoms = ctx.Uint64("min_htlc_m_atoms")
+		req.MinHtlcMAtomsSpecified = true
 	}
 
 	if chanPoint != nil {

routing/router.go
@@ -224,6 +224,10 @@ type ChannelPolicy struct {
 	// MaxHTLC is the maximum HTLC size including fees we are allowed to
 	// forward over this channel.
 	MaxHTLC lnwire.MilliAtom
+
+	// MinHTLC is the minimum HTLC size including fees we are allowed to
+	// forward over this channel.
+	MinHTLC *lnwire.MilliAtom
 }
 
 // Config defines the configuration for the ChannelRouter. ALL elements within

rpcserver.go
@@ -5070,15 +5070,25 @@ func (r *rpcServer) UpdateChannelPolicy(ctx context.Context,
 		FeeRate: feeRateFixed,
 	}
 
+	maxHtlc := lnwire.MilliAtom(req.MaxHtlcMAtoms)
+	var minHtlc *lnwire.MilliAtom
+	if req.MinHtlcMAtomsSpecified {
+		min := lnwire.MilliAtom(req.MinHtlcMAtoms)
+		minHtlc = &min
+	}
+
 	chanPolicy := routing.ChannelPolicy{
 		FeeSchema:     feeSchema,
 		TimeLockDelta: req.TimeLockDelta,
-		MaxHTLC:       lnwire.MilliAtom(req.MaxHtlcMAtoms),
+		MaxHTLC:       maxHtlc,
+		MinHTLC:       minHtlc,
 	}
 
 	rpcsLog.Debugf("[updatechanpolicy] updating channel policy base_fee=%v, "+
-		"rate_float=%v, rate_fixed=%v, time_lock_delta: %v, targets=%v",
+		"rate_float=%v, rate_fixed=%v, time_lock_delta: %v, "+
+		"min_htlc=%v, max_htlc=%v, targets=%v",
 		req.BaseFeeMAtoms, req.FeeRate, feeRateFixed, req.TimeLockDelta,
+		minHtlc, maxHtlc,
 		spew.Sdump(targetChans))
 
 	// With the scope resolved, we'll now send this to the local channel