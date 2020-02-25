lightningnetwork/lnd

invoicesrpc+lnrpc: add msat fields to invoices

lnrpc/invoicesrpc/addinvoice.go
@@ -71,8 +71,8 @@ type AddInvoiceData struct {
 	// htlc will be accepted and held until the preimage becomes known.
 	Hash *lntypes.Hash
 
-	// The value of this invoice in satoshis.
-	Value btcutil.Amount
+	// The value of this invoice in millisatoshis.
+	Value lnwire.MilliSatoshi
 
 	// Hash (SHA-256) of a description of the payment. Used if the
 	// description of payment (memo) is too long to naturally fit within the
@@ -169,7 +169,7 @@ func AddInvoice(ctx context.Context, cfg *AddInvoiceConfig,
 			"are not allowed, value is %v", invoice.Value)
 	}
 
-	amtMSat := lnwire.NewMSatFromSatoshis(invoice.Value)
+	amtMSat := invoice.Value
 
 	// The value of the invoice must also not exceed the current soft-limit
 	// on the largest payment within the network.

lnrpc/invoicesrpc/invoices_server.go
@@ -11,7 +11,6 @@ import (
 	"google.golang.org/grpc"
 	"gopkg.in/macaroon-bakery.v2/bakery"
 
-	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lntypes"
@@ -261,10 +260,15 @@ func (s *Server) AddHoldInvoice(ctx context.Context,
 		return nil, err
 	}
 
+	value, err := lnrpc.UnmarshallAmt(invoice.Value, invoice.ValueMsat)
+	if err != nil {
+		return nil, err
+	}
+
 	addInvoiceData := &AddInvoiceData{
 		Memo:            invoice.Memo,
 		Hash:            &hash,
-		Value:           btcutil.Amount(invoice.Value),
+		Value:           value,
 		DescriptionHash: invoice.DescriptionHash,
 		Expiry:          invoice.Expiry,
 		FallbackAddr:    invoice.FallbackAddr,

lnrpc/invoicesrpc/utils.go
@@ -97,6 +97,7 @@ func CreateRPCInvoice(invoice *channeldb.Invoice,
 		Receipt:         invoice.Receipt[:],
 		RHash:           decoded.PaymentHash[:],
 		Value:           int64(satAmt),
+		ValueMsat:       int64(invoice.Terms.Value),
 		CreationDate:    invoice.CreationDate.Unix(),
 		SettleDate:      settleDate,
 		Settled:         isSettled,

rpcserver.go
@@ -3509,10 +3509,15 @@ func (r *rpcServer) AddInvoice(ctx context.Context,
 		ChanDB:            r.server.chanDB,
 	}
 
+	value, err := lnrpc.UnmarshallAmt(invoice.Value, invoice.ValueMsat)
+	if err != nil {
+		return nil, err
+	}
+
 	addInvoiceData := &invoicesrpc.AddInvoiceData{
 		Memo:            invoice.Memo,
 		Receipt:         invoice.Receipt,
-		Value:           btcutil.Amount(invoice.Value),
+		Value:           value,
 		DescriptionHash: invoice.DescriptionHash,
 		Expiry:          invoice.Expiry,
 		FallbackAddr:    invoice.FallbackAddr,

matheusd/dcrlnd

lnrpc/invoicesrpc/addinvoice.go
@@ -71,8 +71,8 @@ type AddInvoiceData struct {
 	// htlc will be accepted and held until the preimage becomes known.
 	Hash *lntypes.Hash
 
-	// The value of this invoice in atoms.
-	Value dcrutil.Amount
+	// The value of this invoice in milliatoms.
+	Value lnwire.MilliAtom
 
 	// Hash (SHA-256) of a description of the payment. Used if the
 	// description of payment (memo) is too long to naturally fit within the
@@ -169,7 +169,7 @@ func AddInvoice(ctx context.Context, cfg *AddInvoiceConfig,
 			"are not allowed, value is %v", invoice.Value)
 	}
 
-	amtMAtoms := lnwire.NewMAtomsFromAtoms(invoice.Value)
+	amtMAtoms := invoice.Value
 
 	// The value of the invoice must also not exceed the current soft-limit
 	// on the largest payment within the network.

lnrpc/invoicesrpc/invoices_server.go
@@ -11,7 +11,6 @@ import (
 	"google.golang.org/grpc"
 	"gopkg.in/macaroon-bakery.v2/bakery"
 
-	"github.com/decred/dcrd/dcrutil/v2"
 	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/lnrpc"
 	"github.com/decred/dcrlnd/lntypes"
@@ -261,10 +260,15 @@ func (s *Server) AddHoldInvoice(ctx context.Context,
 		return nil, err
 	}
 
+	value, err := lnrpc.UnmarshallAmt(invoice.Value, invoice.ValueMAtoms)
+	if err != nil {
+		return nil, err
+	}
+
 	addInvoiceData := &AddInvoiceData{
 		Memo:            invoice.Memo,
 		Hash:            &hash,
-		Value:           dcrutil.Amount(invoice.Value),
+		Value:           value,
 		DescriptionHash: invoice.DescriptionHash,
 		Expiry:          invoice.Expiry,
 		FallbackAddr:    invoice.FallbackAddr,

lnrpc/invoicesrpc/utils.go
@@ -97,6 +97,7 @@ func CreateRPCInvoice(invoice *channeldb.Invoice,
 		Receipt:         invoice.Receipt[:],
 		RHash:           decoded.PaymentHash[:],
 		Value:           int64(atomsAmt),
+		ValueMAtoms:     int64(invoice.Terms.Value),
 		CreationDate:    invoice.CreationDate.Unix(),
 		SettleDate:      settleDate,
 		Settled:         isSettled,

rpcserver.go
@@ -3726,10 +3726,15 @@ func (r *rpcServer) AddInvoice(ctx context.Context,
 		ChanDB:            r.server.chanDB,
 	}
 
+	value, err := lnrpc.UnmarshallAmt(invoice.Value, invoice.ValueMAtoms)
+	if err != nil {
+		return nil, err
+	}
+
 	addInvoiceData := &invoicesrpc.AddInvoiceData{
 		Memo:            invoice.Memo,
 		Receipt:         invoice.Receipt,
-		Value:           dcrutil.Amount(invoice.Value),
+		Value:           value,
 		DescriptionHash: invoice.DescriptionHash,
 		Expiry:          invoice.Expiry,
 		FallbackAddr:    invoice.FallbackAddr,