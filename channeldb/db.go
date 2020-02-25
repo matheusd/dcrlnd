lightningnetwork/lnd

channeldb: complete migration 12 for TLV invoices

channeldb/db.go
@@ -13,6 +13,7 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/coreos/bbolt"
 	"github.com/go-errors/errors"
+	"github.com/lightningnetwork/lnd/channeldb/migration12"
 	"github.com/lightningnetwork/lnd/channeldb/migration_01_to_11"
 	"github.com/lightningnetwork/lnd/lnwire"
 )
@@ -115,6 +116,12 @@ var (
 			// Add invoice htlc and cltv delta fields.
 			number:    11,
 			migration: migration_01_to_11.MigrateInvoices,
+		},
+		{
+			// Migrate to TLV invoice bodies, add payment address
+			// and features, remove receipt.
+			number:    12,
+			migration: migration12.MigrateInvoiceTLV,
 		},
 	}
 

channeldb/log.go
@@ -3,6 +3,7 @@ package channeldb
 import (
 	"github.com/btcsuite/btclog"
 	"github.com/lightningnetwork/lnd/build"
+	"github.com/lightningnetwork/lnd/channeldb/migration12"
 	"github.com/lightningnetwork/lnd/channeldb/migration_01_to_11"
 )
 
@@ -27,4 +28,5 @@ func DisableLog() {
 func UseLogger(logger btclog.Logger) {
 	log = logger
 	migration_01_to_11.UseLogger(logger)
+	migration12.UseLogger(logger)
 }

invoices/invoiceregistry_test.go
@@ -28,6 +28,10 @@ var (
 	testFinalCltvRejectDelta = int32(4)
 
 	testCurrentHeight = int32(1)
+
+	testFeatures = lnwire.NewFeatureVector(
+		nil, lnwire.Features,
+	)
 )
 
 var (
@@ -35,6 +39,15 @@ var (
 		Terms: channeldb.ContractTerm{
 			PaymentPreimage: preimage,
 			Value:           lnwire.MilliSatoshi(100000),
+			Features:        testFeatures,
+		},
+	}
+
+	testHodlInvoice = &channeldb.Invoice{
+		Terms: channeldb.ContractTerm{
+			PaymentPreimage: channeldb.UnknownPreimage,
+			Value:           lnwire.MilliSatoshi(100000),
+			Features:        testFeatures,
 		},
 	}
 )
@@ -382,14 +395,7 @@ func TestSettleHoldInvoice(t *testing.T) {
 	}
 
 	// Add the invoice.
-	invoice := &channeldb.Invoice{
-		Terms: channeldb.ContractTerm{
-			PaymentPreimage: channeldb.UnknownPreimage,
-			Value:           lnwire.MilliSatoshi(100000),
-		},
-	}
-
-	_, err = registry.AddInvoice(invoice, hash)
+	_, err = registry.AddInvoice(testHodlInvoice, hash)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -543,14 +549,7 @@ func TestCancelHoldInvoice(t *testing.T) {
 	defer registry.Stop()
 
 	// Add the invoice.
-	invoice := &channeldb.Invoice{
-		Terms: channeldb.ContractTerm{
-			PaymentPreimage: channeldb.UnknownPreimage,
-			Value:           lnwire.MilliSatoshi(100000),
-		},
-	}
-
-	_, err = registry.AddInvoice(invoice, hash)
+	_, err = registry.AddInvoice(testHodlInvoice, hash)
 	if err != nil {
 		t.Fatal(err)
 	}

matheusd/dcrlnd

channeldb/db.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/decred/dcrd/dcrec/secp256k1/v2"
 	"github.com/decred/dcrd/wire"
+	"github.com/decred/dcrlnd/channeldb/migration12"
 	"github.com/decred/dcrlnd/channeldb/migration_01_to_11"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/go-errors/errors"
@@ -115,6 +116,12 @@ var (
 			// Add invoice htlc and cltv delta fields.
 			number:    11,
 			migration: migration_01_to_11.MigrateInvoices,
+		},
+		{
+			// Migrate to TLV invoice bodies, add payment address
+			// and features, remove receipt.
+			number:    12,
+			migration: migration12.MigrateInvoiceTLV,
 		},
 	}
 

channeldb/log.go
@@ -2,6 +2,7 @@ package channeldb
 
 import (
 	"github.com/decred/dcrlnd/build"
+	"github.com/decred/dcrlnd/channeldb/migration12"
 	"github.com/decred/dcrlnd/channeldb/migration_01_to_11"
 	"github.com/decred/slog"
 )
@@ -27,4 +28,5 @@ func DisableLog() {
 func UseLogger(logger slog.Logger) {
 	log = logger
 	migration_01_to_11.UseLogger(logger)
+	migration12.UseLogger(logger)
 }

invoices/invoiceregistry_test.go
@@ -28,6 +28,10 @@ var (
 	testFinalCltvRejectDelta = int32(4)
 
 	testCurrentHeight = int32(1)
+
+	testFeatures = lnwire.NewFeatureVector(
+		nil, lnwire.Features,
+	)
 )
 
 var (
@@ -35,6 +39,15 @@ var (
 		Terms: channeldb.ContractTerm{
 			PaymentPreimage: preimage,
 			Value:           lnwire.MilliAtom(100000),
+			Features:        testFeatures,
+		},
+	}
+
+	testHodlInvoice = &channeldb.Invoice{
+		Terms: channeldb.ContractTerm{
+			PaymentPreimage: channeldb.UnknownPreimage,
+			Value:           lnwire.MilliAtom(100000),
+			Features:        testFeatures,
 		},
 	}
 )
@@ -391,14 +404,7 @@ func TestSettleHoldInvoice(t *testing.T) {
 	time.Sleep(time.Millisecond * 5)
 
 	// Add the invoice.
-	invoice := &channeldb.Invoice{
-		Terms: channeldb.ContractTerm{
-			PaymentPreimage: channeldb.UnknownPreimage,
-			Value:           lnwire.MilliAtom(100000),
-		},
-	}
-
-	_, err = registry.AddInvoice(invoice, hash)
+	_, err = registry.AddInvoice(testHodlInvoice, hash)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -552,14 +558,7 @@ func TestCancelHoldInvoice(t *testing.T) {
 	defer registry.Stop()
 
 	// Add the invoice.
-	invoice := &channeldb.Invoice{
-		Terms: channeldb.ContractTerm{
-			PaymentPreimage: channeldb.UnknownPreimage,
-			Value:           lnwire.MilliAtom(100000),
-		},
-	}
-
-	_, err = registry.AddInvoice(invoice, hash)
+	_, err = registry.AddInvoice(testHodlInvoice, hash)
 	if err != nil {
 		t.Fatal(err)
 	}