lightningnetwork/lnd

multi: replace NotifyExitHopHtlc eob param with hop.Payload

invoices/interface.go
@@ -1,0 +1,11 @@
+package invoices
+
+import "github.com/lightningnetwork/lnd/record"
+
+// Payload abstracts access to any additional fields provided in the final hop's
+// TLV onion payload.
+type Payload interface {
+	// MultiPath returns the record corresponding the option_mpp parsed from
+	// the onion payload.
+	MultiPath() *record.MPP
+}

matheusd/dcrlnd

invoices/interface.go
@@ -1,0 +1,11 @@
+package invoices
+
+import "github.com/decred/dcrlnd/record"
+
+// Payload abstracts access to any additional fields provided in the final hop's
+// TLV onion payload.
+type Payload interface {
+	// MultiPath returns the record corresponding the option_mpp parsed from
+	// the onion payload.
+	MultiPath() *record.MPP
+}