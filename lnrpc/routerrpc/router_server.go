lightningnetwork/lnd

routing+routerrpc: also expose amt for success results in mc

lnrpc/routerrpc/router_server.go
@@ -494,9 +494,8 @@ func (s *Server) QueryMissionControl(ctx context.Context,
 // toRPCPairData marshalls mission control pair data to the rpc struct.
 func toRPCPairData(data *routing.TimedPairResult) *PairData {
 	rpcData := PairData{
-		MinPenalizeAmtSat: int64(
-			data.MinPenalizeAmt.ToSatoshis(),
-		),
+		AmtSat:                int64(data.Amt.ToSatoshis()),
+		AmtMsat:               int64(data.Amt),
 		LastAttemptSuccessful: data.Success,
 	}
 

routing/missioncontrol.go
@@ -128,11 +128,10 @@ type TimedPairResult struct {
 	// timestamp is the time when this result was obtained.
 	Timestamp time.Time
 
-	// minPenalizeAmt is the minimum amount for which a penalty should be
-	// applied based on this result. Only applies to fail results.
-	MinPenalizeAmt lnwire.MilliSatoshi
+	// Amt is the amount that was forwarded in the last payment attempt.
+	Amt lnwire.MilliSatoshi
 
-	// success indicates whether the payment attempt was successful through
+	// Success indicates whether the payment attempt was successful through
 	// this pair.
 	Success bool
 }
@@ -142,9 +141,9 @@ func newTimedPairResult(timestamp time.Time,
 	result pairResult) TimedPairResult {
 
 	return TimedPairResult{
-		Timestamp:      timestamp,
-		MinPenalizeAmt: result.minPenalizeAmt,
-		Success:        result.success,
+		Timestamp: timestamp,
+		Amt:       result.amt,
+		Success:   result.success,
 	}
 }
 
@@ -497,8 +496,8 @@ func (m *MissionControl) applyPaymentResult(
 				"Control: pair=%v", pair)
 		} else {
 			log.Debugf("Reporting pair failure to Mission "+
-				"Control: pair=%v, minPenalizeAmt=%v",
-				pair, pairResult.minPenalizeAmt)
+				"Control: pair=%v, amt=%v",
+				pair, pairResult.amt)
 		}
 
 		m.setLastPairResult(

routing/result_interpretation.go
@@ -17,9 +17,9 @@ var (
 // pairResult contains the result of the interpretation of a payment attempt for
 // a specific node pair.
 type pairResult struct {
-	// minPenalizeAmt is the minimum amount for which a penalty should be
-	// applied based on this result. Only applies to fail results.
-	minPenalizeAmt lnwire.MilliSatoshi
+	// amt is the amount that was forwarded for this pair. Can be set to
+	// zero for failures that are amount independent.
+	amt lnwire.MilliSatoshi
 
 	// success indicates whether the payment attempt was successful through
 	// this pair.
@@ -29,24 +29,28 @@ type pairResult struct {
 // failPairResult creates a new result struct for a failure.
 func failPairResult(minPenalizeAmt lnwire.MilliSatoshi) pairResult {
 	return pairResult{
-		minPenalizeAmt: minPenalizeAmt,
+		amt: minPenalizeAmt,
 	}
 }
 
-// successPairResult creates a new result struct for a success.
-func successPairResult() pairResult {
+// newSuccessPairResult creates a new result struct for a success.
+func successPairResult(successAmt lnwire.MilliSatoshi) pairResult {
 	return pairResult{
 		success: true,
+		amt:     successAmt,
 	}
 }
 
 // String returns the human-readable representation of a pair result.
 func (p pairResult) String() string {
+	var resultType string
 	if p.success {
-		return "success"
+		resultType = "success"
+	} else {
+		resultType = "failed"
 	}
 
-	return fmt.Sprintf("failed (minPenalizeAmt=%v)", p.minPenalizeAmt)
+	return fmt.Sprintf("%v (amt=%v)", resultType, p.amt)
 }
 
 // interpretedResult contains the result of the interpretation of a payment
@@ -458,9 +462,9 @@ func (i *interpretedResult) successPairRange(
 	rt *route.Route, fromIdx, toIdx int) {
 
 	for idx := fromIdx; idx <= toIdx; idx++ {
-		pair, _ := getPair(rt, idx)
+		pair, amt := getPair(rt, idx)
 
-		i.pairResults[pair] = successPairResult()
+		i.pairResults[pair] = successPairResult(amt)
 	}
 }
 

matheusd/dcrlnd

lnrpc/routerrpc/router_server.go
@@ -494,9 +494,8 @@ func (s *Server) QueryMissionControl(ctx context.Context,
 // toRPCPairData marshalls mission control pair data to the rpc struct.
 func toRPCPairData(data *routing.TimedPairResult) *PairData {
 	rpcData := PairData{
-		MinPenalizeAmtAtoms: int64(
-			data.MinPenalizeAmt.ToAtoms(),
-		),
+		AmtAtoms:              int64(data.Amt.ToAtoms()),
+		AmtMAtoms:             int64(data.Amt),
 		LastAttemptSuccessful: data.Success,
 	}
 

routing/missioncontrol.go
@@ -128,11 +128,10 @@ type TimedPairResult struct {
 	// timestamp is the time when this result was obtained.
 	Timestamp time.Time
 
-	// minPenalizeAmt is the minimum amount for which a penalty should be
-	// applied based on this result. Only applies to fail results.
-	MinPenalizeAmt lnwire.MilliAtom
+	// Amt is the amount that was forwarded in the last payment attempt.
+	Amt lnwire.MilliAtom
 
-	// success indicates whether the payment attempt was successful through
+	// Success indicates whether the payment attempt was successful through
 	// this pair.
 	Success bool
 }
@@ -142,9 +141,9 @@ func newTimedPairResult(timestamp time.Time,
 	result pairResult) TimedPairResult {
 
 	return TimedPairResult{
-		Timestamp:      timestamp,
-		MinPenalizeAmt: result.minPenalizeAmt,
-		Success:        result.success,
+		Timestamp: timestamp,
+		Amt:       result.amt,
+		Success:   result.success,
 	}
 }
 
@@ -497,8 +496,8 @@ func (m *MissionControl) applyPaymentResult(
 				"Control: pair=%v", pair)
 		} else {
 			log.Debugf("Reporting pair failure to Mission "+
-				"Control: pair=%v, minPenalizeAmt=%v",
-				pair, pairResult.minPenalizeAmt)
+				"Control: pair=%v, amt=%v",
+				pair, pairResult.amt)
 		}
 
 		m.setLastPairResult(

routing/result_interpretation.go
@@ -17,9 +17,9 @@ var (
 // pairResult contains the result of the interpretation of a payment attempt for
 // a specific node pair.
 type pairResult struct {
-	// minPenalizeAmt is the minimum amount for which a penalty should be
-	// applied based on this result. Only applies to fail results.
-	minPenalizeAmt lnwire.MilliAtom
+	// amt is the amount that was forwarded for this pair. Can be set to
+	// zero for failures that are amount independent.
+	amt lnwire.MilliAtom
 
 	// success indicates whether the payment attempt was successful through
 	// this pair.
@@ -29,24 +29,28 @@ type pairResult struct {
 // failPairResult creates a new result struct for a failure.
 func failPairResult(minPenalizeAmt lnwire.MilliAtom) pairResult {
 	return pairResult{
-		minPenalizeAmt: minPenalizeAmt,
+		amt: minPenalizeAmt,
 	}
 }
 
-// successPairResult creates a new result struct for a success.
-func successPairResult() pairResult {
+// newSuccessPairResult creates a new result struct for a success.
+func successPairResult(successAmt lnwire.MilliAtom) pairResult {
 	return pairResult{
 		success: true,
+		amt:     successAmt,
 	}
 }
 
 // String returns the human-readable representation of a pair result.
 func (p pairResult) String() string {
+	var resultType string
 	if p.success {
-		return "success"
+		resultType = "success"
+	} else {
+		resultType = "failed"
 	}
 
-	return fmt.Sprintf("failed (minPenalizeAmt=%v)", p.minPenalizeAmt)
+	return fmt.Sprintf("%v (amt=%v)", resultType, p.amt)
 }
 
 // interpretedResult contains the result of the interpretation of a payment
@@ -458,9 +462,9 @@ func (i *interpretedResult) successPairRange(
 	rt *route.Route, fromIdx, toIdx int) {
 
 	for idx := fromIdx; idx <= toIdx; idx++ {
-		pair, _ := getPair(rt, idx)
+		pair, amt := getPair(rt, idx)
 
-		i.pairResults[pair] = successPairResult()
+		i.pairResults[pair] = successPairResult(amt)
 	}
 }
 