14db0499f806224b50864703f201f3ac37eb074c
fguisso <fguisso@gmail.com>
2020-03-05 15:20:31 -0300

    dcrlncli: fix typo listunspent

--- cmd/dcrlncli/commands.go
+++ cmd/dcrlncli/commands.go
@@ -306,13 +306,13 @@ func sendCoins(ctx *cli.Context) error {
 var listUnspentCommand = cli.Command{
 	Name:      "listunspent",
 	Category:  "On-chain",
-	Usage:     "List utxos available for spending.",
+	Usage:     "List UTXOs available for spending.",
 	ArgsUsage: "[min-confs [max-confs]] [--unconfirmed_only]",
 	Description: `
-	For each spendable utxo currently in the wallet, with at least min_confs
-	confirmations, and at most max_confs confirmations, lists the txid,
+	For each spendable UTXO currently in the wallet, with at least 'min_confs'
+	confirmations, and at most 'max_confs' confirmations, lists the txid,
 	index, amount, address, address type, scriptPubkey and number of
-	confirmations.  Use --min_confs=0 to include unconfirmed coins. To list
+	confirmations.  Use '--min_confs=0' to include unconfirmed coins. To list
 	all coins with at least min_confs confirmations, omit the second
 	argument or flag '--max_confs'. To list all confirmed and unconfirmed
 	coins, no arguments are required. To see only unconfirmed coins, use
@@ -322,19 +322,19 @@ var listUnspentCommand = cli.Command{
 	Flags: []cli.Flag{
 		cli.Int64Flag{
 			Name:  "min_confs",
-			Usage: "the minimum number of confirmations for a utxo",
+			Usage: "The minimum number of confirmations for a UTXO",
 		},
 		cli.Int64Flag{
 			Name:  "max_confs",
-			Usage: "the maximum number of confirmations for a utxo",
+			Usage: "The maximum number of confirmations for a UTXO",
 		},
 		cli.BoolFlag{
 			Name: "unconfirmed_only",
-			Usage: "when min_confs and max_confs are zero, " +
-				"setting false implicitly overrides max_confs " +
-				"to be MaxInt32, otherwise max_confs remains " +
+			Usage: "When min_confs and max_confs are zero, " +
+				"setting false implicitly overrides 'max_confs' " +
+				"to be MaxInt32, otherwise 'max_confs' remains " +
 				"zero. An error is returned if the value is " +
-				"true and both min_confs and max_confs are " +
+				"true and both 'min_confs' and 'max_confs' are " +
 				"non-zero. (default: false)",
 		},
 	},
