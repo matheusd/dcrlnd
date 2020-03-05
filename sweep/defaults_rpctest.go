4d4acdfbe81aeac284627c4102bca70ca11cc477
Matheus Degiovani <opensource@matheusd.com>
2020-03-05 12:16:11 -0300

    sweep: Reduce sweep timer timeout for itests
    This allows itests that depend on sweeping inputs to run faster.

--- sweep/defaults_rpctest.go
+++ sweep/defaults_rpctest.go
@@ -13,5 +13,8 @@ var (
 	//
 	// To speed up integration tests waiting for a sweep to happen, the
 	// batch window is shortened.
-	DefaultBatchWindowDuration = 2 * time.Second
+	//
+	// No integration tests currently rely on sweeping multiple inputs
+	// simultaneously so this can be a really short time.
+	DefaultBatchWindowDuration = 20 * time.Millisecond
 )
