96381f4eb9f6b7e33fbc7e092ed733de890c20c0
fguisso <fguisso@gmail.com>
2020-03-10 15:41:47 -0300

    dcrlncli: fix typo fwdinghistory

--- cmd/dcrlncli/commands.go
+++ cmd/dcrlncli/commands.go
@@ -3612,37 +3612,37 @@ var forwardingHistoryCommand = cli.Command{
 	ArgsUsage: "start_time [end_time] [index_offset] [max_events]",
 	Description: `
 	Query the HTLC switch's internal forwarding log for all completed
-	payment circuits (HTLCs) over a particular time range (--start_time and
-	--end_time). The start and end times are meant to be expressed in
-	seconds since the Unix epoch. If --start_time isn't provided,
-	then 24 hours ago is used.  If --end_time isn't provided,
+	payment circuits (HTLCs) over a particular time range '--start_time' and
+	'--end_time'. The start and end times are meant to be expressed in
+	seconds since the Unix epoch. If '--start_time' isn't provided,
+	then 24 hours ago is used.  If '--end_time' isn't provided,
 	then the current time is used.
 
 	The max number of events returned is 50k. The default number is 100,
-	callers can use the --max_events param to modify this value.
+	callers can use the '--max_events' param to modify this value.
 
-	Finally, callers can skip a series of events using the --index_offset
+	Finally, callers can skip a series of events using the '--index_offset'
 	parameter. Each response will contain the offset index of the last
 	entry. Using this callers can manually paginate within a time slice.
 	`,
 	Flags: []cli.Flag{
 		cli.Int64Flag{
 			Name: "start_time",
-			Usage: "the starting time for the query, expressed in " +
+			Usage: "The starting time for the query, expressed in " +
 				"seconds since the unix epoch",
 		},
 		cli.Int64Flag{
 			Name: "end_time",
-			Usage: "the end time for the query, expressed in " +
+			Usage: "The end time for the query, expressed in " +
 				"seconds since the unix epoch",
 		},
 		cli.Int64Flag{
 			Name:  "index_offset",
-			Usage: "the number of events to skip",
+			Usage: "The number of events to skip",
 		},
 		cli.Int64Flag{
 			Name:  "max_events",
-			Usage: "the max number of events to return",
+			Usage: "The max number of events to return",
 		},
 	},
 	Action: actionDecorator(forwardingHistory),
