a3fe2c8e1e3e62bdf891cc5fd77c707661fc0ec2
carla <kirkcohenc@gmail.com>
2020-03-25 08:41:00 +0200

    lnrpc: add commit fees to WaitingPendingClose

--- rpcserver.go
+++ rpcserver.go
@@ -2801,6 +2801,10 @@ func (r *rpcServer) PendingChannels(ctx context.Context,
 			commitments.LocalTxid =
 				waitingClose.LocalCommitment.CommitTx.TxHash().
 					String()
+
+			commitments.LocalCommitFeeAtoms = uint64(
+				waitingClose.LocalCommitment.CommitFee,
+			)
 		}
 
 		// Report remote commit. May not be present when DLP is active.
@@ -2808,6 +2812,10 @@ func (r *rpcServer) PendingChannels(ctx context.Context,
 			commitments.RemoteTxid =
 				waitingClose.RemoteCommitment.CommitTx.TxHash().
 					String()
+
+			commitments.RemoteCommitFeeAtoms = uint64(
+				waitingClose.RemoteCommitment.CommitFee,
+			)
 		}
 
 		// Report the remote pending commit if any.
@@ -2827,7 +2835,9 @@ func (r *rpcServer) PendingChannels(ctx context.Context,
 		default:
 			hash := remoteCommitDiff.Commitment.CommitTx.TxHash()
 			commitments.RemotePendingTxid = hash.String()
-
+			commitments.RemoteCommitFeeAtoms = uint64(
+				remoteCommitDiff.Commitment.CommitFee,
+			)
 		}
 
 		channel := &lnrpc.PendingChannelsResponse_PendingChannel{
