b33dcacdc225315cdd1eee39167930b509e8adfe
carla <kirkcohenc@gmail.com>
2020-04-03 09:13:53 +0200

    lnrpc: use initiator enum for pending channel channel intiator
    Update the PendingChannel message from a bool to an enum to
    differentiate between the case where the remote party initiated the
    channel and we have no record of the channel initiator. The previous
    field has not been included in a release of lnd, so we can replace the
    field directly.

--- rpcserver.go
+++ rpcserver.go
@@ -2646,6 +2646,16 @@ func (r *rpcServer) PendingChannels(ctx context.Context,
 
 	resp := &lnrpc.PendingChannelsResponse{}
 
+	// rpcInitiator returns the correct lnrpc initiator for channels where
+	// we have a record of the opening channel.
+	rpcInitiator := func(isInitiator bool) lnrpc.Initiator {
+		if isInitiator {
+			return lnrpc.Initiator_INITIATOR_LOCAL
+		}
+
+		return lnrpc.Initiator_INITIATOR_REMOTE
+	}
+
 	// First, we'll populate the response with all the channels that are
 	// soon to be opened. We can easily fetch this data from the database
 	// and map the db struct to the proto response.
@@ -2680,7 +2690,7 @@ func (r *rpcServer) PendingChannels(ctx context.Context,
 				RemoteBalance:          int64(localCommitment.RemoteBalance.ToAtoms()),
 				LocalChanReserveAtoms:  int64(pendingChan.LocalChanCfg.ChanReserve),
 				RemoteChanReserveAtoms: int64(pendingChan.RemoteChanCfg.ChanReserve),
-				Initiator:              pendingChan.IsInitiator,
+				Initiator:              rpcInitiator(pendingChan.IsInitiator),
 				CommitmentType:         rpcCommitmentType(pendingChan.ChanType),
 			},
 			CommitSize: commitSize,
@@ -2707,12 +2717,18 @@ func (r *rpcServer) PendingChannels(ctx context.Context,
 		// needed regardless of how this channel was closed.
 		pub := pendingClose.RemotePub.SerializeCompressed()
 		chanPoint := pendingClose.ChanPoint
+
+		// Create the pending channel. If this channel was closed before
+		// we started storing historical channel data, we will not know
+		// who initiated the channel, so we set the initiator field to
+		// unknown.
 		channel := &lnrpc.PendingChannelsResponse_PendingChannel{
 			RemoteNodePub:  hex.EncodeToString(pub),
 			ChannelPoint:   chanPoint.String(),
 			Capacity:       int64(pendingClose.Capacity),
 			LocalBalance:   int64(pendingClose.SettledBalance),
 			CommitmentType: lnrpc.CommitmentType_UNKNOWN_COMMITMENT_TYPE,
+			Initiator:      lnrpc.Initiator_INITIATOR_UNKNOWN,
 		}
 
 		// Lookup the channel in the historical channel bucket to obtain
@@ -2730,7 +2746,7 @@ func (r *rpcServer) PendingChannels(ctx context.Context,
 		case channeldb.ErrChannelNotFound:
 
 		case nil:
-			channel.Initiator = historical.IsInitiator
+			channel.Initiator = rpcInitiator(historical.IsInitiator)
 			channel.CommitmentType = rpcCommitmentType(
 				historical.ChanType,
 			)
@@ -2860,7 +2876,7 @@ func (r *rpcServer) PendingChannels(ctx context.Context,
 			RemoteBalance:          int64(waitingClose.LocalCommitment.RemoteBalance.ToAtoms()),
 			LocalChanReserveAtoms:  int64(waitingClose.LocalChanCfg.ChanReserve),
 			RemoteChanReserveAtoms: int64(waitingClose.RemoteChanCfg.ChanReserve),
-			Initiator:              waitingClose.IsInitiator,
+			Initiator:              rpcInitiator(waitingClose.IsInitiator),
 			CommitmentType:         rpcCommitmentType(waitingClose.ChanType),
 		}
 
