bce6bba40a30f18216e249cb686aa9d5c4481ee0
Conner Fromknecht <conner@lightning.engineering>
2020-04-14 10:51:30 -0700

    htlcswitch/link: add pending commit ticker for stall detection
    This commit adds a PendingCommitTicker to the link config, which allows
    us to control how quickly we fail the link if the commitment dance
    stalls. Now that the mailbox has the ability to cancel packets, when the
    link fails it will reset the mailbox packets on exit, forcing a
    reevaluation of the HTLCs against their mailbox expiries.

--- htlcswitch/link.go
+++ htlcswitch/link.go
@@ -224,6 +224,11 @@ type ChannelLinkConfig struct {
 	// syncing.
 	FwdPkgGCTicker ticker.Ticker
 
+	// PendingCommitTicker is a ticker that allows the link to determine if
+	// a locally initiated commitment dance gets stuck waiting for the
+	// remote party to revoke.
+	PendingCommitTicker ticker.Ticker
+
 	// BatchSize is the max size of a batch of updates done to the link
 	// before we do a state update.
 	BatchSize uint32
@@ -1099,6 +1104,11 @@ func (l *channelLink) htlcManager() {
 				return
 			}
 
+		case <-l.cfg.PendingCommitTicker.Ticks():
+			l.fail(LinkFailureError{code: ErrRemoteUnresponsive},
+				"unable to complete dance")
+			return
+
 		// A message from the switch was just received. This indicates
 		// that the link is an intermediate hop in a multi-hop HTLC
 		// circuit.
@@ -1935,6 +1945,8 @@ func (l *channelLink) updateCommitTx() error {
 
 	theirCommitSig, htlcSigs, pendingHTLCs, err := l.channel.SignNextCommitment()
 	if err == lnwallet.ErrNoWindow {
+		l.cfg.PendingCommitTicker.Resume()
+
 		l.log.Tracef("revocation window exhausted, unable to send: "+
 			"%v, pend_updates=%v, dangling_closes%v",
 			l.channel.PendingLocalUpdateCount(),
@@ -1953,6 +1965,8 @@ func (l *channelLink) updateCommitTx() error {
 	if err := l.ackDownStreamPackets(); err != nil {
 		return err
 	}
+
+	l.cfg.PendingCommitTicker.Pause()
 
 	// The remote party now has a new pending commitment, so we'll update
 	// the contract court to be aware of this new set (the prior old remote
