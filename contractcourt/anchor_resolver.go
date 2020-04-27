lightningnetwork/lnd

cnct: resolve anchors post-confirmation
Sweeping anchors and being able to bump the fee was already added in a
previous commit. This commit extends anchor sweeping with an anchor
resolver object that becomes active after the commitment tx confirms.
At that point, the anchors do not serve the purpose of getting the
commitment tranaction confirmed anymore. It is however still possible to
reclaim some of their value if using a low fee rate.

contractcourt/anchor_resolver.go
@@ -1,0 +1,165 @@
+package contractcourt
+
+import (
+	"errors"
+	"io"
+
+	"github.com/btcsuite/btcd/wire"
+	"github.com/lightningnetwork/lnd/input"
+	"github.com/lightningnetwork/lnd/lnwallet"
+	"github.com/lightningnetwork/lnd/sweep"
+)
+
+// anchorResolver is a resolver that will attempt to sweep our anchor output.
+type anchorResolver struct {
+	// anchorSignDescriptor contains the information that is required to
+	// sweep the anchor.
+	anchorSignDescriptor input.SignDescriptor
+
+	// anchor is the outpoint on the commitment transaction.
+	anchor wire.OutPoint
+
+	// resolved reflects if the contract has been fully resolved or not.
+	resolved bool
+
+	// broadcastHeight is the height that the original contract was
+	// broadcast to the main-chain at. We'll use this value to bound any
+	// historical queries to the chain for spends/confirmations.
+	broadcastHeight uint32
+
+	// chanPoint is the channel point of the original contract.
+	chanPoint wire.OutPoint
+
+	contractResolverKit
+}
+
+// newAnchorResolver instantiates a new anchor resolver.
+func newAnchorResolver(anchorSignDescriptor input.SignDescriptor,
+	anchor wire.OutPoint, broadcastHeight uint32,
+	chanPoint wire.OutPoint, resCfg ResolverConfig) *anchorResolver {
+
+	r := &anchorResolver{
+		contractResolverKit:  *newContractResolverKit(resCfg),
+		anchorSignDescriptor: anchorSignDescriptor,
+		anchor:               anchor,
+		broadcastHeight:      broadcastHeight,
+		chanPoint:            chanPoint,
+	}
+
+	r.initLogger(r)
+
+	return r
+}
+
+// ResolverKey returns an identifier which should be globally unique for this
+// particular resolver within the chain the original contract resides within.
+func (c *anchorResolver) ResolverKey() []byte {
+	// The anchor resolver is stateless and doesn't need a database key.
+	return nil
+}
+
+// Resolve offers the anchor output to the sweeper and waits for it to be swept.
+func (c *anchorResolver) Resolve() (ContractResolver, error) {
+	// Attempt to update the sweep parameters to the post-confirmation
+	// situation. We don't want to force sweep anymore, because the anchor
+	// lost its special purpose to get the commitment confirmed. It is just
+	// an output that we want to sweep only if it is economical to do so.
+	relayFeeRate := c.Sweeper.RelayFeePerKW()
+
+	resultChan, err := c.Sweeper.UpdateParams(
+		c.anchor,
+		sweep.ParamsUpdate{
+			Fee: sweep.FeePreference{
+				FeeRate: relayFeeRate,
+			},
+			Force: false,
+		},
+	)
+
+	// After a restart or when the remote force closes, the sweeper is not
+	// yet aware of the anchor. In that case, offer it as a new input to the
+	// sweeper. An exclusive group is not necessary anymore, because we know
+	// that this is the only anchor that can be swept.
+	if err == lnwallet.ErrNotMine {
+		anchorInput := input.MakeBaseInput(
+			&c.anchor,
+			input.CommitmentAnchor,
+			&c.anchorSignDescriptor,
+			c.broadcastHeight,
+		)
+
+		resultChan, err = c.Sweeper.SweepInput(
+			&anchorInput,
+			sweep.Params{
+				Fee: sweep.FeePreference{
+					FeeRate: relayFeeRate,
+				},
+			},
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	select {
+	case sweepRes := <-resultChan:
+		switch sweepRes.Err {
+
+		// Anchor was swept successfully.
+		case nil:
+			c.log.Debugf("anchor swept by tx %v",
+				sweepRes.Tx.TxHash())
+
+		// Anchor was swept by someone else. This is possible after the
+		// 16 block csv lock.
+		case sweep.ErrRemoteSpend:
+			c.log.Warnf("our anchor spent by someone else")
+
+		// The sweeper gave up on sweeping the anchor. This happens
+		// after the maximum number of sweep attempts has been reached.
+		// See sweep.DefaultMaxSweepAttempts. Sweep attempts are
+		// interspaced with random delays picked from a range that
+		// increases exponentially.
+		//
+		// We consider the anchor as being lost.
+		case sweep.ErrTooManyAttempts:
+			c.log.Warnf("anchor sweep abandoned")
+
+		// An unexpected error occurred.
+		default:
+			c.log.Errorf("unable to sweep anchor: %v", sweepRes.Err)
+
+			return nil, sweepRes.Err
+		}
+
+	case <-c.quit:
+		return nil, errResolverShuttingDown
+	}
+
+	c.resolved = true
+	return nil, nil
+}
+
+// Stop signals the resolver to cancel any current resolution processes, and
+// suspend.
+//
+// NOTE: Part of the ContractResolver interface.
+func (c *anchorResolver) Stop() {
+	close(c.quit)
+}
+
+// IsResolved returns true if the stored state in the resolve is fully
+// resolved. In this case the target output can be forgotten.
+//
+// NOTE: Part of the ContractResolver interface.
+func (c *anchorResolver) IsResolved() bool {
+	return c.resolved
+}
+
+func (c *anchorResolver) Encode(w io.Writer) error {
+	return errors.New("serialization not supported")
+}
+
+// A compile time assertion to ensure anchorResolver meets the
+// ContractResolver interface.
+var _ ContractResolver = (*anchorResolver)(nil)

matheusd/dcrlnd

contractcourt/anchor_resolver.go
@@ -1,0 +1,165 @@
+package contractcourt
+
+import (
+	"errors"
+	"io"
+
+	"github.com/decred/dcrd/wire"
+	"github.com/decred/dcrlnd/input"
+	"github.com/decred/dcrlnd/lnwallet"
+	"github.com/decred/dcrlnd/sweep"
+)
+
+// anchorResolver is a resolver that will attempt to sweep our anchor output.
+type anchorResolver struct {
+	// anchorSignDescriptor contains the information that is required to
+	// sweep the anchor.
+	anchorSignDescriptor input.SignDescriptor
+
+	// anchor is the outpoint on the commitment transaction.
+	anchor wire.OutPoint
+
+	// resolved reflects if the contract has been fully resolved or not.
+	resolved bool
+
+	// broadcastHeight is the height that the original contract was
+	// broadcast to the main-chain at. We'll use this value to bound any
+	// historical queries to the chain for spends/confirmations.
+	broadcastHeight uint32
+
+	// chanPoint is the channel point of the original contract.
+	chanPoint wire.OutPoint
+
+	contractResolverKit
+}
+
+// newAnchorResolver instantiates a new anchor resolver.
+func newAnchorResolver(anchorSignDescriptor input.SignDescriptor,
+	anchor wire.OutPoint, broadcastHeight uint32,
+	chanPoint wire.OutPoint, resCfg ResolverConfig) *anchorResolver {
+
+	r := &anchorResolver{
+		contractResolverKit:  *newContractResolverKit(resCfg),
+		anchorSignDescriptor: anchorSignDescriptor,
+		anchor:               anchor,
+		broadcastHeight:      broadcastHeight,
+		chanPoint:            chanPoint,
+	}
+
+	r.initLogger(r)
+
+	return r
+}
+
+// ResolverKey returns an identifier which should be globally unique for this
+// particular resolver within the chain the original contract resides within.
+func (c *anchorResolver) ResolverKey() []byte {
+	// The anchor resolver is stateless and doesn't need a database key.
+	return nil
+}
+
+// Resolve offers the anchor output to the sweeper and waits for it to be swept.
+func (c *anchorResolver) Resolve() (ContractResolver, error) {
+	// Attempt to update the sweep parameters to the post-confirmation
+	// situation. We don't want to force sweep anymore, because the anchor
+	// lost its special purpose to get the commitment confirmed. It is just
+	// an output that we want to sweep only if it is economical to do so.
+	relayFeeRate := c.Sweeper.RelayFeePerKB()
+
+	resultChan, err := c.Sweeper.UpdateParams(
+		c.anchor,
+		sweep.ParamsUpdate{
+			Fee: sweep.FeePreference{
+				FeeRate: relayFeeRate,
+			},
+			Force: false,
+		},
+	)
+
+	// After a restart or when the remote force closes, the sweeper is not
+	// yet aware of the anchor. In that case, offer it as a new input to the
+	// sweeper. An exclusive group is not necessary anymore, because we know
+	// that this is the only anchor that can be swept.
+	if err == lnwallet.ErrNotMine {
+		anchorInput := input.MakeBaseInput(
+			&c.anchor,
+			input.CommitmentAnchor,
+			&c.anchorSignDescriptor,
+			c.broadcastHeight,
+		)
+
+		resultChan, err = c.Sweeper.SweepInput(
+			&anchorInput,
+			sweep.Params{
+				Fee: sweep.FeePreference{
+					FeeRate: relayFeeRate,
+				},
+			},
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	select {
+	case sweepRes := <-resultChan:
+		switch sweepRes.Err {
+
+		// Anchor was swept successfully.
+		case nil:
+			c.log.Debugf("anchor swept by tx %v",
+				sweepRes.Tx.TxHash())
+
+		// Anchor was swept by someone else. This is possible after the
+		// 16 block csv lock.
+		case sweep.ErrRemoteSpend:
+			c.log.Warnf("our anchor spent by someone else")
+
+		// The sweeper gave up on sweeping the anchor. This happens
+		// after the maximum number of sweep attempts has been reached.
+		// See sweep.DefaultMaxSweepAttempts. Sweep attempts are
+		// interspaced with random delays picked from a range that
+		// increases exponentially.
+		//
+		// We consider the anchor as being lost.
+		case sweep.ErrTooManyAttempts:
+			c.log.Warnf("anchor sweep abandoned")
+
+		// An unexpected error occurred.
+		default:
+			c.log.Errorf("unable to sweep anchor: %v", sweepRes.Err)
+
+			return nil, sweepRes.Err
+		}
+
+	case <-c.quit:
+		return nil, errResolverShuttingDown
+	}
+
+	c.resolved = true
+	return nil, nil
+}
+
+// Stop signals the resolver to cancel any current resolution processes, and
+// suspend.
+//
+// NOTE: Part of the ContractResolver interface.
+func (c *anchorResolver) Stop() {
+	close(c.quit)
+}
+
+// IsResolved returns true if the stored state in the resolve is fully
+// resolved. In this case the target output can be forgotten.
+//
+// NOTE: Part of the ContractResolver interface.
+func (c *anchorResolver) IsResolved() bool {
+	return c.resolved
+}
+
+func (c *anchorResolver) Encode(w io.Writer) error {
+	return errors.New("serialization not supported")
+}
+
+// A compile time assertion to ensure anchorResolver meets the
+// ContractResolver interface.
+var _ ContractResolver = (*anchorResolver)(nil)