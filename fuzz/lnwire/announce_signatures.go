lightningnetwork/lnd

fuzz/lnwire: adding fuzz harnesses for all lnwire messages + zlib

fuzz/lnwire/announce_signatures.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+// Fuzz_announce_signatures is used by go-fuzz.
+func Fuzz_announce_signatures(data []byte) int {
+	// Prefix with MsgAnnounceSignatures.
+	data = prefixWithMsgType(data, lnwire.MsgAnnounceSignatures)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.AnnounceSignatures{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/channel_reestablish.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+// Fuzz_channel_reestablish is used by go-fuzz.
+func Fuzz_channel_reestablish(data []byte) int {
+	// Prefix with MsgChannelReestablish.
+	data = prefixWithMsgType(data, lnwire.MsgChannelReestablish)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.ChannelReestablish{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/channel_update.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+// Fuzz_channel_update is used by go-fuzz.
+func Fuzz_channel_update(data []byte) int {
+	// Prefix with MsgChannelUpdate.
+	data = prefixWithMsgType(data, lnwire.MsgChannelUpdate)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.ChannelUpdate{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/gossip_timestamp_range.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+// Fuzz_gossip_timestamp_range is used by go-fuzz.
+func Fuzz_gossip_timestamp_range(data []byte) int {
+	// Prefix with MsgGossipTimestampRange.
+	data = prefixWithMsgType(data, lnwire.MsgGossipTimestampRange)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.GossipTimestampRange{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/init.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+// Fuzz_init is used by go-fuzz.
+func Fuzz_init(data []byte) int {
+	// Prefix with MsgInit.
+	data = prefixWithMsgType(data, lnwire.MsgInit)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.Init{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/ping.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+// Fuzz_ping is used by go-fuzz.
+func Fuzz_ping(data []byte) int {
+	// Prefix with MsgPing.
+	data = prefixWithMsgType(data, lnwire.MsgPing)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.Ping{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/query_channel_range.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+// Fuzz_query_channel_range is used by go-fuzz.
+func Fuzz_query_channel_range(data []byte) int {
+	// Prefix with MsgQueryChannelRange.
+	data = prefixWithMsgType(data, lnwire.MsgQueryChannelRange)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.QueryChannelRange{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/query_short_chan_ids_zlib.go
@@ -1,0 +1,51 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"bytes"
+	"compress/zlib"
+	"encoding/binary"
+
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+// Fuzz_query_short_chan_ids_zlib is used by go-fuzz.
+func Fuzz_query_short_chan_ids_zlib(data []byte) int {
+
+	var buf bytes.Buffer
+	zlibWriter := zlib.NewWriter(&buf)
+	_, err := zlibWriter.Write(data)
+	if err != nil {
+		// Zlib bug?
+		panic(err)
+	}
+
+	if err := zlibWriter.Close(); err != nil {
+		// Zlib bug?
+		panic(err)
+	}
+
+	compressedPayload := buf.Bytes()
+
+	chainhash := []byte("00000000000000000000000000000000")
+	numBytesInBody := len(compressedPayload) + 1
+	zlibByte := []byte("\x01")
+
+	bodyBytes := make([]byte, 2)
+	binary.BigEndian.PutUint16(bodyBytes, uint16(numBytesInBody))
+
+	payload := append(chainhash, bodyBytes...)
+	payload = append(payload, zlibByte...)
+	payload = append(payload, compressedPayload...)
+
+	// Prefix with MsgQueryShortChanIDs.
+	payload = prefixWithMsgType(payload, lnwire.MsgQueryShortChanIDs)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.QueryShortChanIDs{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(payload, &emptyMsg)
+}

matheusd/dcrlnd

fuzz/lnwire/announce_signatures.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/decred/dcrlnd/lnwire"
+)
+
+// Fuzz_announce_signatures is used by go-fuzz.
+func Fuzz_announce_signatures(data []byte) int {
+	// Prefix with MsgAnnounceSignatures.
+	data = prefixWithMsgType(data, lnwire.MsgAnnounceSignatures)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.AnnounceSignatures{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/channel_reestablish.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/decred/dcrlnd/lnwire"
+)
+
+// Fuzz_channel_reestablish is used by go-fuzz.
+func Fuzz_channel_reestablish(data []byte) int {
+	// Prefix with MsgChannelReestablish.
+	data = prefixWithMsgType(data, lnwire.MsgChannelReestablish)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.ChannelReestablish{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/channel_update.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/decred/dcrlnd/lnwire"
+)
+
+// Fuzz_channel_update is used by go-fuzz.
+func Fuzz_channel_update(data []byte) int {
+	// Prefix with MsgChannelUpdate.
+	data = prefixWithMsgType(data, lnwire.MsgChannelUpdate)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.ChannelUpdate{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/gossip_timestamp_range.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/decred/dcrlnd/lnwire"
+)
+
+// Fuzz_gossip_timestamp_range is used by go-fuzz.
+func Fuzz_gossip_timestamp_range(data []byte) int {
+	// Prefix with MsgGossipTimestampRange.
+	data = prefixWithMsgType(data, lnwire.MsgGossipTimestampRange)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.GossipTimestampRange{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/init.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/decred/dcrlnd/lnwire"
+)
+
+// Fuzz_init is used by go-fuzz.
+func Fuzz_init(data []byte) int {
+	// Prefix with MsgInit.
+	data = prefixWithMsgType(data, lnwire.MsgInit)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.Init{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/ping.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/decred/dcrlnd/lnwire"
+)
+
+// Fuzz_ping is used by go-fuzz.
+func Fuzz_ping(data []byte) int {
+	// Prefix with MsgPing.
+	data = prefixWithMsgType(data, lnwire.MsgPing)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.Ping{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/query_channel_range.go
@@ -1,0 +1,20 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"github.com/decred/dcrlnd/lnwire"
+)
+
+// Fuzz_query_channel_range is used by go-fuzz.
+func Fuzz_query_channel_range(data []byte) int {
+	// Prefix with MsgQueryChannelRange.
+	data = prefixWithMsgType(data, lnwire.MsgQueryChannelRange)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.QueryChannelRange{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

fuzz/lnwire/query_short_chan_ids_zlib.go
@@ -1,0 +1,51 @@
+// +build gofuzz
+
+package lnwirefuzz
+
+import (
+	"bytes"
+	"compress/zlib"
+	"encoding/binary"
+
+	"github.com/decred/dcrlnd/lnwire"
+)
+
+// Fuzz_query_short_chan_ids_zlib is used by go-fuzz.
+func Fuzz_query_short_chan_ids_zlib(data []byte) int {
+
+	var buf bytes.Buffer
+	zlibWriter := zlib.NewWriter(&buf)
+	_, err := zlibWriter.Write(data)
+	if err != nil {
+		// Zlib bug?
+		panic(err)
+	}
+
+	if err := zlibWriter.Close(); err != nil {
+		// Zlib bug?
+		panic(err)
+	}
+
+	compressedPayload := buf.Bytes()
+
+	chainhash := []byte("00000000000000000000000000000000")
+	numBytesInBody := len(compressedPayload) + 1
+	zlibByte := []byte("\x01")
+
+	bodyBytes := make([]byte, 2)
+	binary.BigEndian.PutUint16(bodyBytes, uint16(numBytesInBody))
+
+	payload := append(chainhash, bodyBytes...)
+	payload = append(payload, zlibByte...)
+	payload = append(payload, compressedPayload...)
+
+	// Prefix with MsgQueryShortChanIDs.
+	payload = prefixWithMsgType(payload, lnwire.MsgQueryShortChanIDs)
+
+	// Create an empty message so that the FuzzHarness func can check
+	// if the max payload constraint is violated.
+	emptyMsg := lnwire.QueryShortChanIDs{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(payload, &emptyMsg)
+}