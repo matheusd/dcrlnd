lightningnetwork/lnd

input+lnwallet: use individual commit weight calculations for channel type
Based on the channel type, the commitment weight will be calculated.

lnwallet/channel.go
@@ -3772,7 +3772,8 @@ func (lc *LightningChannel) computeView(view *htlcView, remoteChain bool,
 		totalHtlcWeight += input.HTLCWeight
 	}
 
-	totalCommitWeight := input.CommitWeight + totalHtlcWeight
+	totalCommitWeight := CommitWeight(lc.channelState.ChanType) +
+		totalHtlcWeight
 	return ourBalance, theirBalance, totalCommitWeight, filteredHTLCView, nil
 }
 
@@ -6322,7 +6323,7 @@ func CreateCooperativeCloseTx(fundingTxIn wire.TxIn,
 // CalcFee returns the commitment fee to use for the given
 // fee rate (fee-per-kw).
 func (lc *LightningChannel) CalcFee(feeRate chainfee.SatPerKWeight) btcutil.Amount {
-	return feeRate.FeeForWeight(input.CommitWeight)
+	return feeRate.FeeForWeight(CommitWeight(lc.channelState.ChanType))
 }
 
 // MaxFeeRate returns the maximum fee rate given an allocation of the channel

lnwallet/commitment.go
@@ -224,6 +224,16 @@ func CommitScriptToRemote(chanType channeldb.ChannelType,
 	}, 0, nil
 }
 
+// CommitWeight returns the base commitment weight before adding HTLCs.
+func CommitWeight(chanType channeldb.ChannelType) int64 {
+	// If this commitment has anchors, it will be slightly heavier.
+	if chanType.HasAnchors() {
+		return input.AnchorCommitWeight
+	}
+
+	return input.CommitWeight
+}
+
 // CommitScriptAnchors return the scripts to use for the local and remote
 // anchor.
 func CommitScriptAnchors(localChanCfg,
@@ -364,7 +374,8 @@ func (cb *CommitmentBuilder) createUnsignedCommitmentTx(ourBalance,
 	// on its total weight. Once we have the total weight, we'll multiply
 	// by the current fee-per-kw, then divide by 1000 to get the proper
 	// fee.
-	totalCommitWeight := input.CommitWeight + (input.HTLCWeight * numHTLCs)
+	totalCommitWeight := CommitWeight(cb.chanState.ChanType) +
+		input.HTLCWeight*numHTLCs
 
 	// With the weight known, we can now calculate the commitment fee,
 	// ensuring that we account for any dust outputs trimmed above.

matheusd/dcrlnd

lnwallet/channel.go
@@ -3785,7 +3785,8 @@ func (lc *LightningChannel) computeView(view *htlcView, remoteChain bool,
 		totalHtlcSize += input.HTLCOutputSize
 	}
 
-	totalCommitSize := input.CommitmentTxSize + totalHtlcSize
+	totalCommitSize := CommitSize(lc.channelState.ChanType) +
+		totalHtlcSize
 	return ourBalance, theirBalance, totalCommitSize, filteredHTLCView, nil
 }
 
@@ -6340,7 +6341,7 @@ func CreateCooperativeCloseTx(fundingTxIn wire.TxIn,
 // CalcFee returns the commitment fee to use for the given
 // fee rate (fee-per-kw).
 func (lc *LightningChannel) CalcFee(feeRate chainfee.AtomPerKByte) dcrutil.Amount {
-	return feeRate.FeeForSize(input.CommitmentTxSize)
+	return feeRate.FeeForSize(CommitSize(lc.channelState.ChanType))
 }
 
 // MaxFeeRate returns the maximum fee rate given an allocation of the channel

lnwallet/commitment.go
@@ -227,6 +227,16 @@ func CommitScriptToRemote(chanType channeldb.ChannelType,
 	}, 0, nil
 }
 
+// CommitSize returns the base commitment size before adding HTLCs.
+func CommitSize(chanType channeldb.ChannelType) int64 {
+	// If this commitment has anchors, it will be slightly heavier.
+	if chanType.HasAnchors() {
+		return input.CommitmentWithAnchorsTxSize
+	}
+
+	return input.CommitmentTxSize
+}
+
 // CommitScriptAnchors return the scripts to use for the local and remote
 // anchor.
 func CommitScriptAnchors(localChanCfg,
@@ -371,7 +381,8 @@ func (cb *CommitmentBuilder) createUnsignedCommitmentTx(ourBalance,
 	// on its total size. Once we have the total size, we'll multiply
 	// by the current fee-per-kb, then divide by 1000 to get the proper
 	// fee.
-	totalCommitSize := input.CommitmentTxSize + (input.HTLCOutputSize * numHTLCs)
+	totalCommitSize := CommitSize(cb.chanState.ChanType) +
+		input.HTLCOutputSize*numHTLCs
 
 	// With the size known, we can now calculate the commitment fee,
 	// ensuring that we account for any dust outputs trimmed above.