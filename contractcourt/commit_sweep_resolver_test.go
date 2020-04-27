lightningnetwork/lnd

cnct+sweeper: give contract court access to relay fee
Relay fee is needed later to sweep anchors at the minimum fee rate.

contractcourt/commit_sweep_resolver_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/lightningnetwork/lnd/chainntnfs"
 	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/lnwallet"
+	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 	"github.com/lightningnetwork/lnd/sweep"
 )
 
@@ -118,6 +119,10 @@ func (s *mockSweeper) CreateSweepTx(inputs []input.Input, feePref sweep.FeePrefe
 	currentBlockHeight uint32) (*wire.MsgTx, error) {
 
 	return nil, nil
+}
+
+func (s *mockSweeper) RelayFeePerKW() chainfee.SatPerKWeight {
+	return 253
 }
 
 var _ UtxoSweeper = &mockSweeper{}

contractcourt/interfaces.go
@@ -9,6 +9,7 @@ import (
 	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/invoices"
 	"github.com/lightningnetwork/lnd/lntypes"
+	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/sweep"
 )
@@ -51,4 +52,8 @@ type UtxoSweeper interface {
 	// estimate before generating the required witnesses.
 	CreateSweepTx(inputs []input.Input, feePref sweep.FeePreference,
 		currentBlockHeight uint32) (*wire.MsgTx, error)
+
+	// RelayFeePerKW returns the minimum fee rate required for transactions
+	// to be relayed.
+	RelayFeePerKW() chainfee.SatPerKWeight
 }

sweep/sweeper.go
@@ -384,6 +384,12 @@ func (s *UtxoSweeper) Start() error {
 	return nil
 }
 
+// RelayFeePerKW returns the minimum fee rate required for transactions to be
+// relayed.
+func (s *UtxoSweeper) RelayFeePerKW() chainfee.SatPerKWeight {
+	return s.relayFeeRate
+}
+
 // Stop stops sweeper from listening to block epochs and constructing sweep
 // txes.
 func (s *UtxoSweeper) Stop() error {

matheusd/dcrlnd

contractcourt/commit_sweep_resolver_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/decred/dcrlnd/chainntnfs"
 	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/lnwallet"
+	"github.com/decred/dcrlnd/lnwallet/chainfee"
 	"github.com/decred/dcrlnd/sweep"
 )
 
@@ -120,6 +121,10 @@ func (s *mockSweeper) CreateSweepTx(inputs []input.Input, feePref sweep.FeePrefe
 	currentBlockHeight uint32) (*wire.MsgTx, error) {
 
 	return nil, nil
+}
+
+func (s *mockSweeper) RelayFeePerKB() chainfee.AtomPerKByte {
+	return 1e4
 }
 
 var _ UtxoSweeper = &mockSweeper{}

contractcourt/interfaces.go
@@ -9,6 +9,7 @@ import (
 	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/invoices"
 	"github.com/decred/dcrlnd/lntypes"
+	"github.com/decred/dcrlnd/lnwallet/chainfee"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/sweep"
 )
@@ -51,4 +52,8 @@ type UtxoSweeper interface {
 	// estimate before generating the required witnesses.
 	CreateSweepTx(inputs []input.Input, feePref sweep.FeePreference,
 		currentBlockHeight uint32) (*wire.MsgTx, error)
+
+	// RelayFeePerKB returns the minimum fee rate required for transactions
+	// to be relayed.
+	RelayFeePerKB() chainfee.AtomPerKByte
 }

sweep/sweeper.go
@@ -398,6 +398,12 @@ func (s *UtxoSweeper) Start() error {
 	return nil
 }
 
+// RelayFeePerKB returns the minimum fee rate required for transactions to be
+// relayed.
+func (s *UtxoSweeper) RelayFeePerKB() chainfee.AtomPerKByte {
+	return s.relayFeeRate
+}
+
 // Stop stops sweeper from listening to block epochs and constructing sweep
 // txes.
 func (s *UtxoSweeper) Stop() error {