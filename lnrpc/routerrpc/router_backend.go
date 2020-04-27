lightningnetwork/lnd

rpcserver+routerrpc: add subscribe events to router backend
Add SubscribeHtlcEvents to RouterBackend in preparation for the
addition of a HtlcNotifier stream.

lnrpc/routerrpc/router_backend.go
@@ -20,6 +20,7 @@ import (
 	"github.com/lightningnetwork/lnd/record"
 	"github.com/lightningnetwork/lnd/routing"
 	"github.com/lightningnetwork/lnd/routing/route"
+	"github.com/lightningnetwork/lnd/subscribe"
 	"github.com/lightningnetwork/lnd/zpay32"
 )
 
@@ -68,6 +69,10 @@ type RouterBackend struct {
 	// DefaultFinalCltvDelta is the default value used as final cltv delta
 	// when an RPC caller doesn't specify a value.
 	DefaultFinalCltvDelta uint16
+
+	// SubscribeHtlcEvents returns a subscription client for the node's
+	// htlc events.
+	SubscribeHtlcEvents func() (*subscribe.Client, error)
 }
 
 // MissionControl defines the mission control dependencies of routerrpc.

matheusd/dcrlnd

lnrpc/routerrpc/router_backend.go
@@ -20,6 +20,7 @@ import (
 	"github.com/decred/dcrlnd/record"
 	"github.com/decred/dcrlnd/routing"
 	"github.com/decred/dcrlnd/routing/route"
+	"github.com/decred/dcrlnd/subscribe"
 	"github.com/decred/dcrlnd/zpay32"
 )
 
@@ -68,6 +69,10 @@ type RouterBackend struct {
 	// DefaultFinalCltvDelta is the default value used as final cltv delta
 	// when an RPC caller doesn't specify a value.
 	DefaultFinalCltvDelta uint16
+
+	// SubscribeHtlcEvents returns a subscription client for the node's
+	// htlc events.
+	SubscribeHtlcEvents func() (*subscribe.Client, error)
 }
 
 // MissionControl defines the mission control dependencies of routerrpc.