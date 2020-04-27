lightningnetwork/lnd

channeldb: convert to uniformly use new kvdb abstractions
In this commit, we migrate all the code in `channeldb` to only reference
the new `kvdb` package rather than `bbolt` directly.

In many instances, we need to add two version to fetch a bucket as both
read and write when needed. As an example, we add a new
`fetchChanBucketRw` function. This function is identical to
`fetchChanBucket`, but it will be used to fetch the main channel bucket
for all _write_ transactions. We need a new method as you can pass a
write transaction where a read is accepted, but not the other way around
due to the stronger typing of the new `kvdb` package.

channeldb/migration_01_to_11/graph.go
@@ -13,7 +13,7 @@ import (
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
-	"github.com/coreos/bbolt"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/lnwire"
 )
 
@@ -175,10 +175,10 @@ func newChannelGraph(db *DB, rejectCacheSize, chanCacheSize int) *ChannelGraph {
 // node based off the source node.
 func (c *ChannelGraph) SourceNode() (*LightningNode, error) {
 	var source *LightningNode
-	err := c.db.View(func(tx *bbolt.Tx) error {
+	err := kvdb.View(c.db, func(tx kvdb.ReadTx) error {
 		// First grab the nodes bucket which stores the mapping from
 		// pubKey to node information.
-		nodes := tx.Bucket(nodeBucket)
+		nodes := tx.ReadBucket(nodeBucket)
 		if nodes == nil {
 			return ErrGraphNotFound
 		}
@@ -202,7 +202,7 @@ func (c *ChannelGraph) SourceNode() (*LightningNode, error) {
 // of the graph. The source node is treated as the center node within a
 // star-graph. This method may be used to kick off a path finding algorithm in
 // order to explore the reachability of another node based off the source node.
-func (c *ChannelGraph) sourceNode(nodes *bbolt.Bucket) (*LightningNode, error) {
+func (c *ChannelGraph) sourceNode(nodes kvdb.ReadBucket) (*LightningNode, error) {
 	selfPub := nodes.Get(sourceKey)
 	if selfPub == nil {
 		return nil, ErrSourceNodeNotSet
@@ -225,10 +225,10 @@ func (c *ChannelGraph) sourceNode(nodes *bbolt.Bucket) (*LightningNode, error) {
 func (c *ChannelGraph) SetSourceNode(node *LightningNode) error {
 	nodePubBytes := node.PubKeyBytes[:]
 
-	return c.db.Update(func(tx *bbolt.Tx) error {
+	return kvdb.Update(c.db, func(tx kvdb.RwTx) error {
 		// First grab the nodes bucket which stores the mapping from
 		// pubKey to node information.
-		nodes, err := tx.CreateBucketIfNotExists(nodeBucket)
+		nodes, err := tx.CreateTopLevelBucket(nodeBucket)
 		if err != nil {
 			return err
 		}
@@ -245,8 +245,8 @@ func (c *ChannelGraph) SetSourceNode(node *LightningNode) error {
 	})
 }
 
-func addLightningNode(tx *bbolt.Tx, node *LightningNode) error {
-	nodes, err := tx.CreateBucketIfNotExists(nodeBucket)
+func addLightningNode(tx kvdb.RwTx, node *LightningNode) error {
+	nodes, err := tx.CreateTopLevelBucket(nodeBucket)
 	if err != nil {
 		return err
 	}
@@ -270,17 +270,17 @@ func addLightningNode(tx *bbolt.Tx, node *LightningNode) error {
 // buckets using an existing database transaction. The returned boolean will be
 // true if the updated policy belongs to node1, and false if the policy belonged
 // to node2.
-func updateEdgePolicy(tx *bbolt.Tx, edge *ChannelEdgePolicy) (bool, error) {
-	edges := tx.Bucket(edgeBucket)
-	if edges == nil {
+func updateEdgePolicy(tx kvdb.RwTx, edge *ChannelEdgePolicy) (bool, error) {
+	edges, err := tx.CreateTopLevelBucket(edgeBucket)
+	if err != nil {
 		return false, ErrEdgeNotFound
 
 	}
-	edgeIndex := edges.Bucket(edgeIndexBucket)
+	edgeIndex := edges.NestedReadWriteBucket(edgeIndexBucket)
 	if edgeIndex == nil {
 		return false, ErrEdgeNotFound
 	}
-	nodes, err := tx.CreateBucketIfNotExists(nodeBucket)
+	nodes, err := tx.CreateTopLevelBucket(nodeBucket)
 	if err != nil {
 		return false, err
 	}
@@ -551,8 +551,8 @@ func (c *ChannelEdgePolicy) IsDisabled() bool {
 		lnwire.ChanUpdateDisabled
 }
 
-func putLightningNode(nodeBucket *bbolt.Bucket, aliasBucket *bbolt.Bucket,
-	updateIndex *bbolt.Bucket, node *LightningNode) error {
+func putLightningNode(nodeBucket kvdb.RwBucket, aliasBucket kvdb.RwBucket,
+	updateIndex kvdb.RwBucket, node *LightningNode) error {
 
 	var (
 		scratch [16]byte
@@ -680,7 +680,7 @@ func putLightningNode(nodeBucket *bbolt.Bucket, aliasBucket *bbolt.Bucket,
 	return nodeBucket.Put(nodePub, b.Bytes())
 }
 
-func fetchLightningNode(nodeBucket *bbolt.Bucket,
+func fetchLightningNode(nodeBucket kvdb.ReadBucket,
 	nodePub []byte) (LightningNode, error) {
 
 	nodeBytes := nodeBucket.Get(nodePub)
@@ -863,7 +863,7 @@ func deserializeChanEdgeInfo(r io.Reader) (ChannelEdgeInfo, error) {
 	return edgeInfo, nil
 }
 
-func putChanEdgePolicy(edges, nodes *bbolt.Bucket, edge *ChannelEdgePolicy,
+func putChanEdgePolicy(edges, nodes kvdb.RwBucket, edge *ChannelEdgePolicy,
 	from, to []byte) error {
 
 	var edgeKey [33 + 8]byte
@@ -943,7 +943,7 @@ func putChanEdgePolicy(edges, nodes *bbolt.Bucket, edge *ChannelEdgePolicy,
 // in this bucket.
 // Maintaining the bucket this way allows a fast retrieval of disabled
 // channels, for example when prune is needed.
-func updateEdgePolicyDisabledIndex(edges *bbolt.Bucket, chanID uint64,
+func updateEdgePolicyDisabledIndex(edges kvdb.RwBucket, chanID uint64,
 	direction bool, disabled bool) error {
 
 	var disabledEdgeKey [8 + 1]byte
@@ -968,7 +968,7 @@ func updateEdgePolicyDisabledIndex(edges *bbolt.Bucket, chanID uint64,
 
 // putChanEdgePolicyUnknown marks the edge policy as unknown
 // in the edges bucket.
-func putChanEdgePolicyUnknown(edges *bbolt.Bucket, channelID uint64,
+func putChanEdgePolicyUnknown(edges kvdb.RwBucket, channelID uint64,
 	from []byte) error {
 
 	var edgeKey [33 + 8]byte
@@ -983,8 +983,8 @@ func putChanEdgePolicyUnknown(edges *bbolt.Bucket, channelID uint64,
 	return edges.Put(edgeKey[:], unknownPolicy)
 }
 
-func fetchChanEdgePolicy(edges *bbolt.Bucket, chanID []byte,
-	nodePub []byte, nodes *bbolt.Bucket) (*ChannelEdgePolicy, error) {
+func fetchChanEdgePolicy(edges kvdb.ReadBucket, chanID []byte,
+	nodePub []byte, nodes kvdb.ReadBucket) (*ChannelEdgePolicy, error) {
 
 	var edgeKey [33 + 8]byte
 	copy(edgeKey[:], nodePub)
@@ -1084,7 +1084,7 @@ func serializeChanEdgePolicy(w io.Writer, edge *ChannelEdgePolicy,
 }
 
 func deserializeChanEdgePolicy(r io.Reader,
-	nodes *bbolt.Bucket) (*ChannelEdgePolicy, error) {
+	nodes kvdb.ReadBucket) (*ChannelEdgePolicy, error) {
 
 	edge := &ChannelEdgePolicy{}
 

channeldb/migration_01_to_11/migration_09_legacy_serialization.go
@@ -7,7 +7,7 @@ import (
 	"io"
 	"sort"
 
-	"github.com/coreos/bbolt"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/lntypes"
 	"github.com/lightningnetwork/lnd/lnwire"
 )
@@ -76,8 +76,8 @@ func (db *DB) addPayment(payment *outgoingPayment) error {
 	}
 	paymentBytes := b.Bytes()
 
-	return db.Batch(func(tx *bbolt.Tx) error {
-		payments, err := tx.CreateBucketIfNotExists(paymentBucket)
+	return kvdb.Update(db, func(tx kvdb.RwTx) error {
+		payments, err := tx.CreateTopLevelBucket(paymentBucket)
 		if err != nil {
 			return err
 		}
@@ -104,8 +104,8 @@ func (db *DB) addPayment(payment *outgoingPayment) error {
 func (db *DB) fetchAllPayments() ([]*outgoingPayment, error) {
 	var payments []*outgoingPayment
 
-	err := db.View(func(tx *bbolt.Tx) error {
-		bucket := tx.Bucket(paymentBucket)
+	err := kvdb.View(db, func(tx kvdb.ReadTx) error {
+		bucket := tx.ReadBucket(paymentBucket)
 		if bucket == nil {
 			return ErrNoPaymentsCreated
 		}
@@ -140,7 +140,7 @@ func (db *DB) fetchAllPayments() ([]*outgoingPayment, error) {
 // NOTE: Deprecated. Kept around for migration purposes.
 func (db *DB) fetchPaymentStatus(paymentHash [32]byte) (PaymentStatus, error) {
 	var paymentStatus = StatusUnknown
-	err := db.View(func(tx *bbolt.Tx) error {
+	err := kvdb.View(db, func(tx kvdb.ReadTx) error {
 		var err error
 		paymentStatus, err = fetchPaymentStatusTx(tx, paymentHash)
 		return err
@@ -158,11 +158,11 @@ func (db *DB) fetchPaymentStatus(paymentHash [32]byte) (PaymentStatus, error) {
 // can be composed into other atomic operations.
 //
 // NOTE: Deprecated. Kept around for migration purposes.
-func fetchPaymentStatusTx(tx *bbolt.Tx, paymentHash [32]byte) (PaymentStatus, error) {
+func fetchPaymentStatusTx(tx kvdb.ReadTx, paymentHash [32]byte) (PaymentStatus, error) {
 	// The default status for all payments that aren't recorded in database.
 	var paymentStatus = StatusUnknown
 
-	bucket := tx.Bucket(paymentStatusBucket)
+	bucket := tx.ReadBucket(paymentStatusBucket)
 	if bucket == nil {
 		return paymentStatus, nil
 	}
@@ -375,14 +375,14 @@ func deserializeHopMigration9(r io.Reader) (*Hop, error) {
 func (db *DB) fetchPaymentsMigration9() ([]*Payment, error) {
 	var payments []*Payment
 
-	err := db.View(func(tx *bbolt.Tx) error {
-		paymentsBucket := tx.Bucket(paymentsRootBucket)
+	err := kvdb.View(db, func(tx kvdb.ReadTx) error {
+		paymentsBucket := tx.ReadBucket(paymentsRootBucket)
 		if paymentsBucket == nil {
 			return nil
 		}
 
 		return paymentsBucket.ForEach(func(k, v []byte) error {
-			bucket := paymentsBucket.Bucket(k)
+			bucket := paymentsBucket.NestedReadBucket(k)
 			if bucket == nil {
 				// We only expect sub-buckets to be found in
 				// this top-level bucket.
@@ -401,13 +401,13 @@ func (db *DB) fetchPaymentsMigration9() ([]*Payment, error) {
 			// payment has was possible. These will be found in a
 			// sub-bucket indexed by their sequence number if
 			// available.
-			dup := bucket.Bucket(paymentDuplicateBucket)
+			dup := bucket.NestedReadBucket(paymentDuplicateBucket)
 			if dup == nil {
 				return nil
 			}
 
 			return dup.ForEach(func(k, v []byte) error {
-				subBucket := dup.Bucket(k)
+				subBucket := dup.NestedReadBucket(k)
 				if subBucket == nil {
 					// We one bucket for each duplicate to
 					// be found.
@@ -437,7 +437,7 @@ func (db *DB) fetchPaymentsMigration9() ([]*Payment, error) {
 	return payments, nil
 }
 
-func fetchPaymentMigration9(bucket *bbolt.Bucket) (*Payment, error) {
+func fetchPaymentMigration9(bucket kvdb.ReadBucket) (*Payment, error) {
 	var (
 		err error
 		p   = &Payment{}

channeldb/migration_01_to_11/migration_11_invoices_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/btcsuite/btcd/btcec"
 	bitcoinCfg "github.com/btcsuite/btcd/chaincfg"
-	"github.com/coreos/bbolt"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/zpay32"
 	litecoinCfg "github.com/ltcsuite/ltcd/chaincfg"
 )
@@ -26,8 +26,8 @@ var (
 
 // beforeMigrationFuncV11 insert the test invoices in the database.
 func beforeMigrationFuncV11(t *testing.T, d *DB, invoices []Invoice) {
-	err := d.Update(func(tx *bbolt.Tx) error {
-		invoicesBucket, err := tx.CreateBucketIfNotExists(
+	err := kvdb.Update(d, func(tx kvdb.RwTx) error {
+		invoicesBucket, err := tx.CreateTopLevelBucket(
 			invoiceBucket,
 		)
 		if err != nil {

channeldb/migtest/raw_db.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/coreos/bbolt"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 )
 
 // DumpDB dumps go code describing the contents of the database to stdout. This
@@ -21,8 +21,8 @@ import (
 // 		hex("1111"): hex("5783492373"),
 // 	},
 // }
-func DumpDB(tx *bbolt.Tx, rootKey []byte) error {
-	bucket := tx.Bucket(rootKey)
+func DumpDB(tx kvdb.ReadTx, rootKey []byte) error {
+	bucket := tx.ReadBucket(rootKey)
 	if bucket == nil {
 		return fmt.Errorf("bucket %v not found", string(rootKey))
 	}
@@ -30,13 +30,13 @@ func DumpDB(tx *bbolt.Tx, rootKey []byte) error {
 	return dumpBucket(bucket)
 }
 
-func dumpBucket(bucket *bbolt.Bucket) error {
+func dumpBucket(bucket kvdb.ReadBucket) error {
 	fmt.Printf("map[string]interface{} {\n")
 	err := bucket.ForEach(func(k, v []byte) error {
 		key := toString(k)
 		fmt.Printf("%v: ", key)
 
-		subBucket := bucket.Bucket(k)
+		subBucket := bucket.NestedReadBucket(k)
 		if subBucket != nil {
 			err := dumpBucket(subBucket)
 			if err != nil {
@@ -58,8 +58,8 @@ func dumpBucket(bucket *bbolt.Bucket) error {
 }
 
 // RestoreDB primes the database with the given data set.
-func RestoreDB(tx *bbolt.Tx, rootKey []byte, data map[string]interface{}) error {
-	bucket, err := tx.CreateBucket(rootKey)
+func RestoreDB(tx kvdb.RwTx, rootKey []byte, data map[string]interface{}) error {
+	bucket, err := tx.CreateTopLevelBucket(rootKey)
 	if err != nil {
 		return err
 	}
@@ -67,7 +67,7 @@ func RestoreDB(tx *bbolt.Tx, rootKey []byte, data map[string]interface{}) error 
 	return restoreDB(bucket, data)
 }
 
-func restoreDB(bucket *bbolt.Bucket, data map[string]interface{}) error {
+func restoreDB(bucket kvdb.RwBucket, data map[string]interface{}) error {
 	for k, v := range data {
 		key := []byte(k)
 
@@ -100,8 +100,8 @@ func restoreDB(bucket *bbolt.Bucket, data map[string]interface{}) error {
 }
 
 // VerifyDB verifies the database against the given data set.
-func VerifyDB(tx *bbolt.Tx, rootKey []byte, data map[string]interface{}) error {
-	bucket := tx.Bucket(rootKey)
+func VerifyDB(tx kvdb.ReadTx, rootKey []byte, data map[string]interface{}) error {
+	bucket := tx.ReadBucket(rootKey)
 	if bucket == nil {
 		return fmt.Errorf("bucket %v not found", string(rootKey))
 	}
@@ -109,7 +109,7 @@ func VerifyDB(tx *bbolt.Tx, rootKey []byte, data map[string]interface{}) error {
 	return verifyDB(bucket, data)
 }
 
-func verifyDB(bucket *bbolt.Bucket, data map[string]interface{}) error {
+func verifyDB(bucket kvdb.ReadBucket, data map[string]interface{}) error {
 	for k, v := range data {
 		key := []byte(k)
 
@@ -126,7 +126,7 @@ func verifyDB(bucket *bbolt.Bucket, data map[string]interface{}) error {
 
 		// Key contains a sub-bucket.
 		case map[string]interface{}:
-			subBucket := bucket.Bucket(key)
+			subBucket := bucket.NestedReadBucket(key)
 			if subBucket == nil {
 				return fmt.Errorf("bucket %v not found", k)
 			}

matheusd/dcrlnd

channeldb/migration_01_to_11/graph.go
@@ -13,8 +13,8 @@ import (
 	"github.com/decred/dcrd/dcrec/secp256k1/v2"
 	"github.com/decred/dcrd/dcrutil/v2"
 	"github.com/decred/dcrd/wire"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/lnwire"
-	bbolt "go.etcd.io/bbbolt"
 )
 
 var (
@@ -175,10 +175,10 @@ func newChannelGraph(db *DB, rejectCacheSize, chanCacheSize int) *ChannelGraph {
 // node based off the source node.
 func (c *ChannelGraph) SourceNode() (*LightningNode, error) {
 	var source *LightningNode
-	err := c.db.View(func(tx *bbolt.Tx) error {
+	err := kvdb.View(c.db, func(tx kvdb.ReadTx) error {
 		// First grab the nodes bucket which stores the mapping from
 		// pubKey to node information.
-		nodes := tx.Bucket(nodeBucket)
+		nodes := tx.ReadBucket(nodeBucket)
 		if nodes == nil {
 			return ErrGraphNotFound
 		}
@@ -202,7 +202,7 @@ func (c *ChannelGraph) SourceNode() (*LightningNode, error) {
 // of the graph. The source node is treated as the center node within a
 // star-graph. This method may be used to kick off a path finding algorithm in
 // order to explore the reachability of another node based off the source node.
-func (c *ChannelGraph) sourceNode(nodes *bbolt.Bucket) (*LightningNode, error) {
+func (c *ChannelGraph) sourceNode(nodes kvdb.ReadBucket) (*LightningNode, error) {
 	selfPub := nodes.Get(sourceKey)
 	if selfPub == nil {
 		return nil, ErrSourceNodeNotSet
@@ -225,10 +225,10 @@ func (c *ChannelGraph) sourceNode(nodes *bbolt.Bucket) (*LightningNode, error) {
 func (c *ChannelGraph) SetSourceNode(node *LightningNode) error {
 	nodePubBytes := node.PubKeyBytes[:]
 
-	return c.db.Update(func(tx *bbolt.Tx) error {
+	return kvdb.Update(c.db, func(tx kvdb.RwTx) error {
 		// First grab the nodes bucket which stores the mapping from
 		// pubKey to node information.
-		nodes, err := tx.CreateBucketIfNotExists(nodeBucket)
+		nodes, err := tx.CreateTopLevelBucket(nodeBucket)
 		if err != nil {
 			return err
 		}
@@ -245,8 +245,8 @@ func (c *ChannelGraph) SetSourceNode(node *LightningNode) error {
 	})
 }
 
-func addLightningNode(tx *bbolt.Tx, node *LightningNode) error {
-	nodes, err := tx.CreateBucketIfNotExists(nodeBucket)
+func addLightningNode(tx kvdb.RwTx, node *LightningNode) error {
+	nodes, err := tx.CreateTopLevelBucket(nodeBucket)
 	if err != nil {
 		return err
 	}
@@ -270,17 +270,17 @@ func addLightningNode(tx *bbolt.Tx, node *LightningNode) error {
 // buckets using an existing database transaction. The returned boolean will be
 // true if the updated policy belongs to node1, and false if the policy
 // belonged to node2.
-func updateEdgePolicy(tx *bbolt.Tx, edge *ChannelEdgePolicy) (bool, error) {
-	edges := tx.Bucket(edgeBucket)
-	if edges == nil {
+func updateEdgePolicy(tx kvdb.RwTx, edge *ChannelEdgePolicy) (bool, error) {
+	edges, err := tx.CreateTopLevelBucket(edgeBucket)
+	if err != nil {
 		return false, ErrEdgeNotFound
 
 	}
-	edgeIndex := edges.Bucket(edgeIndexBucket)
+	edgeIndex := edges.NestedReadWriteBucket(edgeIndexBucket)
 	if edgeIndex == nil {
 		return false, ErrEdgeNotFound
 	}
-	nodes, err := tx.CreateBucketIfNotExists(nodeBucket)
+	nodes, err := tx.CreateTopLevelBucket(nodeBucket)
 	if err != nil {
 		return false, err
 	}
@@ -551,8 +551,8 @@ func (c *ChannelEdgePolicy) IsDisabled() bool {
 		lnwire.ChanUpdateDisabled
 }
 
-func putLightningNode(nodeBucket *bbolt.Bucket, aliasBucket *bbolt.Bucket,
-	updateIndex *bbolt.Bucket, node *LightningNode) error {
+func putLightningNode(nodeBucket kvdb.RwBucket, aliasBucket kvdb.RwBucket,
+	updateIndex kvdb.RwBucket, node *LightningNode) error {
 
 	var (
 		scratch [16]byte
@@ -680,7 +680,7 @@ func putLightningNode(nodeBucket *bbolt.Bucket, aliasBucket *bbolt.Bucket,
 	return nodeBucket.Put(nodePub, b.Bytes())
 }
 
-func fetchLightningNode(nodeBucket *bbolt.Bucket,
+func fetchLightningNode(nodeBucket kvdb.ReadBucket,
 	nodePub []byte) (LightningNode, error) {
 
 	nodeBytes := nodeBucket.Get(nodePub)
@@ -863,7 +863,7 @@ func deserializeChanEdgeInfo(r io.Reader) (ChannelEdgeInfo, error) {
 	return edgeInfo, nil
 }
 
-func putChanEdgePolicy(edges, nodes *bbolt.Bucket, edge *ChannelEdgePolicy,
+func putChanEdgePolicy(edges, nodes kvdb.RwBucket, edge *ChannelEdgePolicy,
 	from, to []byte) error {
 
 	var edgeKey [33 + 8]byte
@@ -943,7 +943,7 @@ func putChanEdgePolicy(edges, nodes *bbolt.Bucket, edge *ChannelEdgePolicy,
 // in this bucket.
 // Maintaining the bucket this way allows a fast retrieval of disabled
 // channels, for example when prune is needed.
-func updateEdgePolicyDisabledIndex(edges *bbolt.Bucket, chanID uint64,
+func updateEdgePolicyDisabledIndex(edges kvdb.RwBucket, chanID uint64,
 	direction bool, disabled bool) error {
 
 	var disabledEdgeKey [8 + 1]byte
@@ -968,7 +968,7 @@ func updateEdgePolicyDisabledIndex(edges *bbolt.Bucket, chanID uint64,
 
 // putChanEdgePolicyUnknown marks the edge policy as unknown
 // in the edges bucket.
-func putChanEdgePolicyUnknown(edges *bbolt.Bucket, channelID uint64,
+func putChanEdgePolicyUnknown(edges kvdb.RwBucket, channelID uint64,
 	from []byte) error {
 
 	var edgeKey [33 + 8]byte
@@ -983,8 +983,8 @@ func putChanEdgePolicyUnknown(edges *bbolt.Bucket, channelID uint64,
 	return edges.Put(edgeKey[:], unknownPolicy)
 }
 
-func fetchChanEdgePolicy(edges *bbolt.Bucket, chanID []byte,
-	nodePub []byte, nodes *bbolt.Bucket) (*ChannelEdgePolicy, error) {
+func fetchChanEdgePolicy(edges kvdb.ReadBucket, chanID []byte,
+	nodePub []byte, nodes kvdb.ReadBucket) (*ChannelEdgePolicy, error) {
 
 	var edgeKey [33 + 8]byte
 	copy(edgeKey[:], nodePub)
@@ -1084,7 +1084,7 @@ func serializeChanEdgePolicy(w io.Writer, edge *ChannelEdgePolicy,
 }
 
 func deserializeChanEdgePolicy(r io.Reader,
-	nodes *bbolt.Bucket) (*ChannelEdgePolicy, error) {
+	nodes kvdb.ReadBucket) (*ChannelEdgePolicy, error) {
 
 	edge := &ChannelEdgePolicy{}
 

channeldb/migration_01_to_11/migration_09_legacy_serialization.go
@@ -7,9 +7,9 @@ import (
 	"io"
 	"sort"
 
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/lntypes"
 	"github.com/decred/dcrlnd/lnwire"
-	bbolt "go.etcd.io/bbbolt"
 )
 
 var (
@@ -76,8 +76,8 @@ func (db *DB) addPayment(payment *outgoingPayment) error {
 	}
 	paymentBytes := b.Bytes()
 
-	return db.Batch(func(tx *bbolt.Tx) error {
-		payments, err := tx.CreateBucketIfNotExists(paymentBucket)
+	return kvdb.Update(db, func(tx kvdb.RwTx) error {
+		payments, err := tx.CreateTopLevelBucket(paymentBucket)
 		if err != nil {
 			return err
 		}
@@ -104,8 +104,8 @@ func (db *DB) addPayment(payment *outgoingPayment) error {
 func (db *DB) fetchAllPayments() ([]*outgoingPayment, error) {
 	var payments []*outgoingPayment
 
-	err := db.View(func(tx *bbolt.Tx) error {
-		bucket := tx.Bucket(paymentBucket)
+	err := kvdb.View(db, func(tx kvdb.ReadTx) error {
+		bucket := tx.ReadBucket(paymentBucket)
 		if bucket == nil {
 			return ErrNoPaymentsCreated
 		}
@@ -140,7 +140,7 @@ func (db *DB) fetchAllPayments() ([]*outgoingPayment, error) {
 // NOTE: Deprecated. Kept around for migration purposes.
 func (db *DB) fetchPaymentStatus(paymentHash [32]byte) (PaymentStatus, error) {
 	var paymentStatus = StatusUnknown
-	err := db.View(func(tx *bbolt.Tx) error {
+	err := kvdb.View(db, func(tx kvdb.ReadTx) error {
 		var err error
 		paymentStatus, err = fetchPaymentStatusTx(tx, paymentHash)
 		return err
@@ -158,11 +158,11 @@ func (db *DB) fetchPaymentStatus(paymentHash [32]byte) (PaymentStatus, error) {
 // can be composed into other atomic operations.
 //
 // NOTE: Deprecated. Kept around for migration purposes.
-func fetchPaymentStatusTx(tx *bbolt.Tx, paymentHash [32]byte) (PaymentStatus, error) {
+func fetchPaymentStatusTx(tx kvdb.ReadTx, paymentHash [32]byte) (PaymentStatus, error) {
 	// The default status for all payments that aren't recorded in database.
 	var paymentStatus = StatusUnknown
 
-	bucket := tx.Bucket(paymentStatusBucket)
+	bucket := tx.ReadBucket(paymentStatusBucket)
 	if bucket == nil {
 		return paymentStatus, nil
 	}
@@ -375,14 +375,14 @@ func deserializeHopMigration9(r io.Reader) (*Hop, error) {
 func (db *DB) fetchPaymentsMigration9() ([]*Payment, error) {
 	var payments []*Payment
 
-	err := db.View(func(tx *bbolt.Tx) error {
-		paymentsBucket := tx.Bucket(paymentsRootBucket)
+	err := kvdb.View(db, func(tx kvdb.ReadTx) error {
+		paymentsBucket := tx.ReadBucket(paymentsRootBucket)
 		if paymentsBucket == nil {
 			return nil
 		}
 
 		return paymentsBucket.ForEach(func(k, v []byte) error {
-			bucket := paymentsBucket.Bucket(k)
+			bucket := paymentsBucket.NestedReadBucket(k)
 			if bucket == nil {
 				// We only expect sub-buckets to be found in
 				// this top-level bucket.
@@ -401,13 +401,13 @@ func (db *DB) fetchPaymentsMigration9() ([]*Payment, error) {
 			// payment has was possible. These will be found in a
 			// sub-bucket indexed by their sequence number if
 			// available.
-			dup := bucket.Bucket(paymentDuplicateBucket)
+			dup := bucket.NestedReadBucket(paymentDuplicateBucket)
 			if dup == nil {
 				return nil
 			}
 
 			return dup.ForEach(func(k, v []byte) error {
-				subBucket := dup.Bucket(k)
+				subBucket := dup.NestedReadBucket(k)
 				if subBucket == nil {
 					// We one bucket for each duplicate to
 					// be found.
@@ -437,7 +437,7 @@ func (db *DB) fetchPaymentsMigration9() ([]*Payment, error) {
 	return payments, nil
 }
 
-func fetchPaymentMigration9(bucket *bbolt.Bucket) (*Payment, error) {
+func fetchPaymentMigration9(bucket kvdb.ReadBucket) (*Payment, error) {
 	var (
 		err error
 		p   = &Payment{}

channeldb/migration_01_to_11/migration_11_invoices_test.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/decred/dcrd/chaincfg/v2"
 	"github.com/decred/dcrd/dcrec/secp256k1/v2"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/zpay32"
-	bbolt "go.etcd.io/bbbolt"
 )
 
 var (
@@ -25,8 +25,8 @@ var (
 
 // beforeMigrationFuncV11 insert the test invoices in the database.
 func beforeMigrationFuncV11(t *testing.T, d *DB, invoices []Invoice) {
-	err := d.Update(func(tx *bbolt.Tx) error {
-		invoicesBucket, err := tx.CreateBucketIfNotExists(
+	err := kvdb.Update(d, func(tx kvdb.RwTx) error {
+		invoicesBucket, err := tx.CreateTopLevelBucket(
 			invoiceBucket,
 		)
 		if err != nil {

channeldb/migtest/raw_db.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"strings"
 
-	bbolt "go.etcd.io/bbbolt"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 )
 
 // DumpDB dumps go code describing the contents of the database to stdout. This
@@ -21,8 +21,8 @@ import (
 // 		hex("1111"): hex("5783492373"),
 // 	},
 // }
-func DumpDB(tx *bbolt.Tx, rootKey []byte) error {
-	bucket := tx.Bucket(rootKey)
+func DumpDB(tx kvdb.ReadTx, rootKey []byte) error {
+	bucket := tx.ReadBucket(rootKey)
 	if bucket == nil {
 		return fmt.Errorf("bucket %v not found", string(rootKey))
 	}
@@ -30,13 +30,13 @@ func DumpDB(tx *bbolt.Tx, rootKey []byte) error {
 	return dumpBucket(bucket)
 }
 
-func dumpBucket(bucket *bbolt.Bucket) error {
+func dumpBucket(bucket kvdb.ReadBucket) error {
 	fmt.Printf("map[string]interface{} {\n")
 	err := bucket.ForEach(func(k, v []byte) error {
 		key := toString(k)
 		fmt.Printf("%v: ", key)
 
-		subBucket := bucket.Bucket(k)
+		subBucket := bucket.NestedReadBucket(k)
 		if subBucket != nil {
 			err := dumpBucket(subBucket)
 			if err != nil {
@@ -58,8 +58,8 @@ func dumpBucket(bucket *bbolt.Bucket) error {
 }
 
 // RestoreDB primes the database with the given data set.
-func RestoreDB(tx *bbolt.Tx, rootKey []byte, data map[string]interface{}) error {
-	bucket, err := tx.CreateBucket(rootKey)
+func RestoreDB(tx kvdb.RwTx, rootKey []byte, data map[string]interface{}) error {
+	bucket, err := tx.CreateTopLevelBucket(rootKey)
 	if err != nil {
 		return err
 	}
@@ -67,7 +67,7 @@ func RestoreDB(tx *bbolt.Tx, rootKey []byte, data map[string]interface{}) error 
 	return restoreDB(bucket, data)
 }
 
-func restoreDB(bucket *bbolt.Bucket, data map[string]interface{}) error {
+func restoreDB(bucket kvdb.RwBucket, data map[string]interface{}) error {
 	for k, v := range data {
 		key := []byte(k)
 
@@ -100,8 +100,8 @@ func restoreDB(bucket *bbolt.Bucket, data map[string]interface{}) error {
 }
 
 // VerifyDB verifies the database against the given data set.
-func VerifyDB(tx *bbolt.Tx, rootKey []byte, data map[string]interface{}) error {
-	bucket := tx.Bucket(rootKey)
+func VerifyDB(tx kvdb.ReadTx, rootKey []byte, data map[string]interface{}) error {
+	bucket := tx.ReadBucket(rootKey)
 	if bucket == nil {
 		return fmt.Errorf("bucket %v not found", string(rootKey))
 	}
@@ -109,7 +109,7 @@ func VerifyDB(tx *bbolt.Tx, rootKey []byte, data map[string]interface{}) error {
 	return verifyDB(bucket, data)
 }
 
-func verifyDB(bucket *bbolt.Bucket, data map[string]interface{}) error {
+func verifyDB(bucket kvdb.ReadBucket, data map[string]interface{}) error {
 	for k, v := range data {
 		key := []byte(k)
 
@@ -126,7 +126,7 @@ func verifyDB(bucket *bbolt.Bucket, data map[string]interface{}) error {
 
 		// Key contains a sub-bucket.
 		case map[string]interface{}:
-			subBucket := bucket.Bucket(key)
+			subBucket := bucket.NestedReadBucket(key)
 			if subBucket == nil {
 				return fmt.Errorf("bucket %v not found", k)
 			}