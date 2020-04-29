lightningnetwork/lnd

lnrpc+rpcserver: expose git commit hash in getinfo

rpcserver.go
@@ -2408,6 +2408,7 @@ func (r *rpcServer) GetInfo(ctx context.Context,
 		Color:               routing.EncodeHexColor(nodeAnn.RGBColor),
 		BestHeaderTimestamp: int64(bestHeaderTimestamp),
 		Version:             build.Version() + " commit=" + build.Commit,
+		CommitHash:          build.CommitHash,
 		SyncedToGraph:       isGraphSynced,
 		Features:            features,
 	}, nil

matheusd/dcrlnd

rpcserver.go
@@ -2383,6 +2383,7 @@ func (r *rpcServer) GetInfo(ctx context.Context,
 		Color:               routing.EncodeHexColor(nodeAnn.RGBColor),
 		BestHeaderTimestamp: bestHeaderTimestamp,
 		Version:             build.Version(),
+		CommitHash:          build.Commit,
 		SyncedToGraph:       isGraphSynced,
 		Features:            features,
 		ServerActive:        r.server.Started() && !r.server.Stopped(),