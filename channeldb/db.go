halseth/lnd

channeldb/migration13: migrate to mpp structure
This commit migrates the payments in the database to a new structure
that allows for multiple htlcs per payments. The migration introduces a
new sub-bucket that contains a list of htlcs and moves the old single
htlc into that.

channeldb/db.go
@@ -14,6 +14,7 @@ import (
 	"github.com/coreos/bbolt"
 	"github.com/go-errors/errors"
 	"github.com/lightningnetwork/lnd/channeldb/migration12"
+	"github.com/lightningnetwork/lnd/channeldb/migration13"
 	"github.com/lightningnetwork/lnd/channeldb/migration_01_to_11"
 	"github.com/lightningnetwork/lnd/clock"
 	"github.com/lightningnetwork/lnd/lnwire"
@@ -123,6 +124,11 @@ var (
 			// and features, remove receipt.
 			number:    12,
 			migration: migration12.MigrateInvoiceTLV,
+		},
+		{
+			// Migrate to multi-path payments.
+			number:    13,
+			migration: migration13.MigrateMPP,
 		},
 	}
 

channeldb/log.go
@@ -4,6 +4,7 @@ import (
 	"github.com/btcsuite/btclog"
 	"github.com/lightningnetwork/lnd/build"
 	"github.com/lightningnetwork/lnd/channeldb/migration12"
+	"github.com/lightningnetwork/lnd/channeldb/migration13"
 	"github.com/lightningnetwork/lnd/channeldb/migration_01_to_11"
 )
 
@@ -29,4 +30,5 @@ func UseLogger(logger btclog.Logger) {
 	log = logger
 	migration_01_to_11.UseLogger(logger)
 	migration12.UseLogger(logger)
+	migration13.UseLogger(logger)
 }

channeldb/migration13/log.go
@@ -1,0 +1,14 @@
+package migration13
+
+import (
+	"github.com/btcsuite/btclog"
+)
+
+// log is a logger that is initialized as disabled.  This means the package will
+// not perform any logging by default until a logger is set.
+var log = btclog.Disabled
+
+// UseLogger uses a specified Logger to output package logging info.
+func UseLogger(logger btclog.Logger) {
+	log = logger
+}

matheusd/dcrlnd

channeldb/db.go
@@ -12,6 +12,7 @@ import (
 	"github.com/decred/dcrd/dcrec/secp256k1/v3"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/channeldb/migration12"
+	"github.com/decred/dcrlnd/channeldb/migration13"
 	"github.com/decred/dcrlnd/channeldb/migration_01_to_11"
 	"github.com/decred/dcrlnd/clock"
 	"github.com/decred/dcrlnd/lnwire"
@@ -123,6 +124,11 @@ var (
 			// and features, remove receipt.
 			number:    12,
 			migration: migration12.MigrateInvoiceTLV,
+		},
+		{
+			// Migrate to multi-path payments.
+			number:    13,
+			migration: migration13.MigrateMPP,
 		},
 	}
 

channeldb/log.go
@@ -3,6 +3,7 @@ package channeldb
 import (
 	"github.com/decred/dcrlnd/build"
 	"github.com/decred/dcrlnd/channeldb/migration12"
+	"github.com/decred/dcrlnd/channeldb/migration13"
 	"github.com/decred/dcrlnd/channeldb/migration_01_to_11"
 	"github.com/decred/slog"
 )
@@ -29,4 +30,5 @@ func UseLogger(logger slog.Logger) {
 	log = logger
 	migration_01_to_11.UseLogger(logger)
 	migration12.UseLogger(logger)
+	migration13.UseLogger(logger)
 }

channeldb/migration13/log.go
@@ -1,0 +1,14 @@
+package migration13
+
+import (
+	"github.com/decred/slog"
+)
+
+// log is a logger that is initialized as disabled.  This means the package will
+// not perform any logging by default until a logger is set.
+var log = slog.Disabled
+
+// UseLogger uses a specified Logger to output package logging info.
+func UseLogger(logger slog.Logger) {
+	log = logger
+}
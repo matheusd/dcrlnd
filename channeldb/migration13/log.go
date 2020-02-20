392a2acc98896302421d2075aed84da48793e7d3
Joost Jager <joost.jager@gmail.com>
2020-02-20 18:08:01 +0100

    channeldb/migration13: migrate to mpp structure
    This commit migrates the payments in the database to a new structure
    that allows for multiple htlcs per payments. The migration introduces a
    new sub-bucket that contains a list of htlcs and moves the old single
    htlc into that.

---
+++ channeldb/migration13/log.go
@@ -0,0 +1,14 @@
+package migration13
+
+import (
+	"github.com/decred/slog"
+)
+
+// log is a logger that is initialized as disabled.  This means the package will
+// not perform any logging by default until a logger is set.
+var log = slog.Disabled
+
+// UseLogger uses a specified Logger to output package logging info.
+func UseLogger(logger slog.Logger) {
+	log = logger
+}
