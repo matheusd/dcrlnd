lightningnetwork/lnd

input+lnwallet: use individual commit weight calculations for channel type
Based on the channel type, the commitment weight will be calculated.

lnwallet/channel.go
@@ -3772,7 +3772,8 @@ func (lc *LightningChannel) computeView(view *htlcView, remoteChain bool,
 		totalHtlcWeight += input.HTLCWeight
 	}
 
-	totalCommitWeight := input.CommitWeight + totalHtlcWeight
+	totalCommitWeight := CommitWeight(lc.channelState.ChanType) +
+		totalHtlcWeight
 	return ourBalance, theirBalance, totalCommitWeight, filteredHTLCView, nil
 }
 
@@ -6322,7 +6323,7 @@ func CreateCooperativeCloseTx(fundingTxIn wire.TxIn,
 // CalcFee returns the commitment fee to use for the given
 // fee rate (fee-per-kw).
 func (lc *LightningChannel) CalcFee(feeRate chainfee.SatPerKWeight) btcutil.Amount {
-	return feeRate.FeeForWeight(input.CommitWeight)
+	return feeRate.FeeForWeight(CommitWeight(lc.channelState.ChanType))
 }
 
 // MaxFeeRate returns the maximum fee rate given an allocation of the channel

matheusd/dcrlnd

lnwallet/channel.go
@@ -3787,7 +3787,8 @@ func (lc *LightningChannel) computeView(view *htlcView, remoteChain bool,
 		totalHtlcSize += input.HTLCOutputSize
 	}
 
-	totalCommitSize := input.CommitmentTxSize + totalHtlcSize
+	totalCommitSize := CommitSize(lc.channelState.ChanType) +
+		totalHtlcSize
 	return ourBalance, theirBalance, totalCommitSize, filteredHTLCView, nil
 }
 
@@ -6338,7 +6339,7 @@ func CreateCooperativeCloseTx(fundingTxIn wire.TxIn,
 // CalcFee returns the commitment fee to use for the given
 // fee rate (fee-per-kw).
 func (lc *LightningChannel) CalcFee(feeRate chainfee.AtomPerKByte) dcrutil.Amount {
-	return feeRate.FeeForSize(input.CommitmentTxSize)
+	return feeRate.FeeForSize(CommitSize(lc.channelState.ChanType))
 }
 
 // MaxFeeRate returns the maximum fee rate given an allocation of the channel