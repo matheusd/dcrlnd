lightningnetwork/lnd

multi: return input.Signature from SignOutputRaw

discovery/gossiper_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/go-errors/errors"
 	"github.com/lightningnetwork/lnd/chainntnfs"
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/lnpeer"
 	"github.com/lightningnetwork/lnd/lntest/wait"
 	"github.com/lightningnetwork/lnd/lnwire"
@@ -96,7 +97,7 @@ type mockSigner struct {
 }
 
 func (n *mockSigner) SignMessage(pubKey *btcec.PublicKey,
-	msg []byte) (*btcec.Signature, error) {
+	msg []byte) (input.Signature, error) {
 
 	if !pubKey.IsEqual(n.privKey.PubKey()) {
 		return nil, fmt.Errorf("unknown public key")

input/script_utils_test.go
@@ -303,12 +303,14 @@ func TestHTLCSenderSpendValidation(t *testing.T) {
 			SigHashes:     sweepTxSigHashes,
 			InputIndex:    0,
 		}
-		bobSigBytes, err := bobSigner.SignOutputRaw(sweepTx, &bobSignDesc)
+		bobSig, err := bobSigner.SignOutputRaw(sweepTx, &bobSignDesc)
 		if err != nil {
 			t.Fatalf("unable to generate alice signature: %v", err)
 		}
 
-		bobRecvrSig, err = btcec.ParseDERSignature(bobSigBytes, btcec.S256())
+		bobRecvrSig, err = btcec.ParseDERSignature(
+			bobSig.Serialize(), btcec.S256(),
+		)
 		if err != nil {
 			t.Fatalf("unable to parse signature: %v", err)
 		}
@@ -700,12 +702,14 @@ func TestHTLCReceiverSpendValidation(t *testing.T) {
 			SigHashes:     sweepTxSigHashes,
 			InputIndex:    0,
 		}
-		aliceSigBytes, err := aliceSigner.SignOutputRaw(sweepTx, &aliceSignDesc)
+		aliceSig, err := aliceSigner.SignOutputRaw(sweepTx, &aliceSignDesc)
 		if err != nil {
 			t.Fatalf("unable to generate alice signature: %v", err)
 		}
 
-		aliceSenderSig, err = btcec.ParseDERSignature(aliceSigBytes, btcec.S256())
+		aliceSenderSig, err = btcec.ParseDERSignature(
+			aliceSig.Serialize(), btcec.S256(),
+		)
 		if err != nil {
 			t.Fatalf("unable to parse signature: %v", err)
 		}

netann/channel_update_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/btcsuite/btcd/btcec"
+	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/lnwallet"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/netann"
@@ -17,7 +18,7 @@ type mockSigner struct {
 }
 
 func (m *mockSigner) SignMessage(pk *btcec.PublicKey,
-	msg []byte) (*btcec.Signature, error) {
+	msg []byte) (input.Signature, error) {
 
 	if m.err != nil {
 		return nil, m.err

matheusd/dcrlnd

discovery/gossiper_test.go
@@ -22,6 +22,7 @@ import (
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/chainntnfs"
 	"github.com/decred/dcrlnd/channeldb"
+	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/lnpeer"
 	"github.com/decred/dcrlnd/lntest/wait"
 	"github.com/decred/dcrlnd/lnwire"
@@ -90,7 +91,7 @@ type mockSigner struct {
 }
 
 func (n *mockSigner) SignMessage(pubKey *secp256k1.PublicKey,
-	msg []byte) (*ecdsa.Signature, error) {
+	msg []byte) (input.Signature, error) {
 
 	if !pubKey.IsEqual(n.privKey.PubKey()) {
 		return nil, fmt.Errorf("unknown public key")

input/script_utils_test.go
@@ -305,12 +305,14 @@ func TestHTLCSenderSpendValidation(t *testing.T) {
 			HashType:      bobSigHash,
 			InputIndex:    0,
 		}
-		bobSigBytes, err := bobSigner.SignOutputRaw(sweepTx, &bobSignDesc)
+		bobSig, err := bobSigner.SignOutputRaw(sweepTx, &bobSignDesc)
 		if err != nil {
 			t.Fatalf("unable to generate alice signature: %v", err)
 		}
 
-		bobRecvrSig, err = ecdsa.ParseDERSignature(bobSigBytes)
+		bobRecvrSig, err = ecdsa.ParseDERSignature(
+			bobSig.Serialize(),
+		)
 		if err != nil {
 			t.Fatalf("unable to parse signature: %v", err)
 		}
@@ -699,7 +701,9 @@ func TestHTLCReceiverSpendValidation(t *testing.T) {
 			t.Fatalf("unable to generate alice signature: %v", err)
 		}
 
-		aliceSenderSig, err = ecdsa.ParseDERSignature(aliceSigBytes)
+		aliceSenderSig, err = ecdsa.ParseDERSignature(
+			aliceSigBytes.Serialize(),
+		)
 		if err != nil {
 			t.Fatalf("unable to parse signature: %v", err)
 		}

netann/channel_update_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/decred/dcrd/dcrec/secp256k1/v3"
-	"github.com/decred/dcrd/dcrec/secp256k1/v3/ecdsa"
+	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/lnwallet"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/netann"
@@ -18,7 +18,7 @@ type mockSigner struct {
 }
 
 func (m *mockSigner) SignMessage(pk *secp256k1.PublicKey,
-	msg []byte) (*ecdsa.Signature, error) {
+	msg []byte) (input.Signature, error) {
 
 	if m.err != nil {
 		return nil, m.err