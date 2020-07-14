lightningnetwork/lnd

channeldb: convert to uniformly use new kvdb abstractions
In this commit, we migrate all the code in `channeldb` to only reference
the new `kvdb` package rather than `bbolt` directly.

In many instances, we need to add two version to fetch a bucket as both
read and write when needed. As an example, we add a new
`fetchChanBucketRw` function. This function is identical to
`fetchChanBucket`, but it will be used to fetch the main channel bucket
for all _write_ transactions. We need a new method as you can pass a
write transaction where a read is accepted, but not the other way around
due to the stronger typing of the new `kvdb` package.

channeldb/duplicate_payments.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	"github.com/btcsuite/btcd/btcec"
-	"github.com/coreos/bbolt"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/lntypes"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/routing/route"
@@ -61,7 +61,7 @@ type duplicateHTLCAttemptInfo struct {
 
 // fetchDuplicatePaymentStatus fetches the payment status of the payment. If the
 // payment isn't found, it will default to "StatusUnknown".
-func fetchDuplicatePaymentStatus(bucket *bbolt.Bucket) PaymentStatus {
+func fetchDuplicatePaymentStatus(bucket kvdb.ReadBucket) PaymentStatus {
 	if bucket.Get(duplicatePaymentSettleInfoKey) != nil {
 		return StatusSucceeded
 	}
@@ -129,7 +129,7 @@ func deserializeDuplicatePaymentCreationInfo(r io.Reader) (
 	return c, nil
 }
 
-func fetchDuplicatePayment(bucket *bbolt.Bucket) (*MPPayment, error) {
+func fetchDuplicatePayment(bucket kvdb.ReadBucket) (*MPPayment, error) {
 	seqBytes := bucket.Get(duplicatePaymentSequenceKey)
 	if seqBytes == nil {
 		return nil, fmt.Errorf("sequence number not found")
@@ -209,7 +209,7 @@ func fetchDuplicatePayment(bucket *bbolt.Bucket) (*MPPayment, error) {
 	return payment, nil
 }
 
-func fetchDuplicatePayments(paymentHashBucket *bbolt.Bucket) ([]*MPPayment,
+func fetchDuplicatePayments(paymentHashBucket kvdb.ReadBucket) ([]*MPPayment,
 	error) {
 
 	var payments []*MPPayment
@@ -217,13 +217,13 @@ func fetchDuplicatePayments(paymentHashBucket *bbolt.Bucket) ([]*MPPayment,
 	// For older versions of lnd, duplicate payments to a payment has was
 	// possible. These will be found in a sub-bucket indexed by their
 	// sequence number if available.
-	dup := paymentHashBucket.Bucket(duplicatePaymentsBucket)
+	dup := paymentHashBucket.NestedReadBucket(duplicatePaymentsBucket)
 	if dup == nil {
 		return nil, nil
 	}
 
 	err := dup.ForEach(func(k, v []byte) error {
-		subBucket := dup.Bucket(k)
+		subBucket := dup.NestedReadBucket(k)
 		if subBucket == nil {
 			// We one bucket for each duplicate to be found.
 			return fmt.Errorf("non bucket element" +

channeldb/migration_01_to_11/migration_11_invoices_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/btcsuite/btcd/btcec"
 	bitcoinCfg "github.com/btcsuite/btcd/chaincfg"
-	"github.com/coreos/bbolt"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/zpay32"
 	litecoinCfg "github.com/ltcsuite/ltcd/chaincfg"
 )
@@ -26,8 +26,8 @@ var (
 
 // beforeMigrationFuncV11 insert the test invoices in the database.
 func beforeMigrationFuncV11(t *testing.T, d *DB, invoices []Invoice) {
-	err := d.Update(func(tx *bbolt.Tx) error {
-		invoicesBucket, err := tx.CreateBucketIfNotExists(
+	err := kvdb.Update(d, func(tx kvdb.RwTx) error {
+		invoicesBucket, err := tx.CreateTopLevelBucket(
 			invoiceBucket,
 		)
 		if err != nil {

channeldb/migration_01_to_11/migrations_test.go
@@ -11,9 +11,9 @@ import (
 	"time"
 
 	"github.com/btcsuite/btcutil"
-	"github.com/coreos/bbolt"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/go-errors/errors"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/lntypes"
 	"github.com/lightningnetwork/lnd/lnwire"
 )
@@ -59,8 +59,8 @@ func TestPaymentStatusesMigration(t *testing.T) {
 		// locally-sourced payment should end up with an InFlight
 		// status, while the other should remain unchanged, which
 		// defaults to Grounded.
-		err = d.Update(func(tx *bbolt.Tx) error {
-			circuits, err := tx.CreateBucketIfNotExists(
+		err = kvdb.Update(d, func(tx kvdb.RwTx) error {
+			circuits, err := tx.CreateTopLevelBucket(
 				[]byte("circuit-adds"),
 			)
 			if err != nil {
@@ -377,8 +377,8 @@ func TestMigrateOptionalChannelCloseSummaryFields(t *testing.T) {
 			// Get the old serialization format for this test's
 			// close summary, and it to the closed channel bucket.
 			old := test.oldSerialization(test.closeSummary)
-			err = d.Update(func(tx *bbolt.Tx) error {
-				closedChanBucket, err := tx.CreateBucketIfNotExists(
+			err = kvdb.Update(d, func(tx kvdb.RwTx) error {
+				closedChanBucket, err := tx.CreateTopLevelBucket(
 					closedChannelBucket,
 				)
 				if err != nil {
@@ -404,8 +404,8 @@ func TestMigrateOptionalChannelCloseSummaryFields(t *testing.T) {
 			newSerialization := b.Bytes()
 
 			var dbSummary []byte
-			err = d.View(func(tx *bbolt.Tx) error {
-				closedChanBucket := tx.Bucket(closedChannelBucket)
+			err = kvdb.View(d, func(tx kvdb.ReadTx) error {
+				closedChanBucket := tx.ReadBucket(closedChannelBucket)
 				if closedChanBucket == nil {
 					return errors.New("unable to find bucket")
 				}
@@ -482,8 +482,8 @@ func TestMigrateGossipMessageStoreKeys(t *testing.T) {
 			t.Fatalf("unable to serialize message: %v", err)
 		}
 
-		err := db.Update(func(tx *bbolt.Tx) error {
-			messageStore, err := tx.CreateBucketIfNotExists(
+		err := kvdb.Update(db, func(tx kvdb.RwTx) error {
+			messageStore, err := tx.CreateTopLevelBucket(
 				messageStoreBucket,
 			)
 			if err != nil {
@@ -503,8 +503,8 @@ func TestMigrateGossipMessageStoreKeys(t *testing.T) {
 	//   3. The message matches the original.
 	afterMigration := func(db *DB) {
 		var rawMsg []byte
-		err := db.View(func(tx *bbolt.Tx) error {
-			messageStore := tx.Bucket(messageStoreBucket)
+		err := kvdb.View(db, func(tx kvdb.ReadTx) error {
+			messageStore := tx.ReadBucket(messageStoreBucket)
 			if messageStore == nil {
 				return errors.New("message store bucket not " +
 					"found")
@@ -666,8 +666,8 @@ func TestOutgoingPaymentsMigration(t *testing.T) {
 
 		// Finally, check that the payment sequence number is updated
 		// to reflect the migrated payments.
-		err = d.View(func(tx *bbolt.Tx) error {
-			payments := tx.Bucket(paymentsRootBucket)
+		err = kvdb.Update(d, func(tx kvdb.RwTx) error {
+			payments := tx.ReadWriteBucket(paymentsRootBucket)
 			if payments == nil {
 				return fmt.Errorf("payments bucket not found")
 			}
@@ -746,8 +746,8 @@ func TestPaymentRouteSerialization(t *testing.T) {
 	// We'll first add a series of fake payments, using the existing legacy
 	// serialization format.
 	beforeMigrationFunc := func(d *DB) {
-		err := d.Update(func(tx *bbolt.Tx) error {
-			paymentsBucket, err := tx.CreateBucket(
+		err := kvdb.Update(d, func(tx kvdb.RwTx) error {
+			paymentsBucket, err := tx.CreateTopLevelBucket(
 				paymentsRootBucket,
 			)
 			if err != nil {
@@ -798,7 +798,7 @@ func TestPaymentRouteSerialization(t *testing.T) {
 				// the proper bucket. If this is the duplicate
 				// payment, then we'll grab the dup bucket,
 				// otherwise, we'll use the top level bucket.
-				var payHashBucket *bbolt.Bucket
+				var payHashBucket kvdb.RwBucket
 				if i < numPayments-1 {
 					payHashBucket, err = paymentsBucket.CreateBucket(
 						payInfo.PaymentHash[:],
@@ -807,7 +807,7 @@ func TestPaymentRouteSerialization(t *testing.T) {
 						t.Fatalf("unable to create payments bucket: %v", err)
 					}
 				} else {
-					payHashBucket = paymentsBucket.Bucket(
+					payHashBucket = paymentsBucket.NestedReadWriteBucket(
 						payInfo.PaymentHash[:],
 					)
 					dupPayBucket, err := payHashBucket.CreateBucket(

channeldb/nodes.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/wire"
-	"github.com/coreos/bbolt"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 )
 
 var (
@@ -101,8 +101,8 @@ func (l *LinkNode) Sync() error {
 
 	// Finally update the database by storing the link node and updating
 	// any relevant indexes.
-	return l.db.Update(func(tx *bbolt.Tx) error {
-		nodeMetaBucket := tx.Bucket(nodeInfoBucket)
+	return kvdb.Update(l.db, func(tx kvdb.RwTx) error {
+		nodeMetaBucket := tx.ReadWriteBucket(nodeInfoBucket)
 		if nodeMetaBucket == nil {
 			return ErrLinkNodesNotFound
 		}
@@ -114,7 +114,7 @@ func (l *LinkNode) Sync() error {
 // putLinkNode serializes then writes the encoded version of the passed link
 // node into the nodeMetaBucket. This function is provided in order to allow
 // the ability to re-use a database transaction across many operations.
-func putLinkNode(nodeMetaBucket *bbolt.Bucket, l *LinkNode) error {
+func putLinkNode(nodeMetaBucket kvdb.RwBucket, l *LinkNode) error {
 	// First serialize the LinkNode into its raw-bytes encoding.
 	var b bytes.Buffer
 	if err := serializeLinkNode(&b, l); err != nil {
@@ -130,13 +130,13 @@ func putLinkNode(nodeMetaBucket *bbolt.Bucket, l *LinkNode) error {
 // DeleteLinkNode removes the link node with the given identity from the
 // database.
 func (db *DB) DeleteLinkNode(identity *btcec.PublicKey) error {
-	return db.Update(func(tx *bbolt.Tx) error {
+	return kvdb.Update(db, func(tx kvdb.RwTx) error {
 		return db.deleteLinkNode(tx, identity)
 	})
 }
 
-func (db *DB) deleteLinkNode(tx *bbolt.Tx, identity *btcec.PublicKey) error {
-	nodeMetaBucket := tx.Bucket(nodeInfoBucket)
+func (db *DB) deleteLinkNode(tx kvdb.RwTx, identity *btcec.PublicKey) error {
+	nodeMetaBucket := tx.ReadWriteBucket(nodeInfoBucket)
 	if nodeMetaBucket == nil {
 		return ErrLinkNodesNotFound
 	}
@@ -150,7 +150,7 @@ func (db *DB) deleteLinkNode(tx *bbolt.Tx, identity *btcec.PublicKey) error {
 // key cannot be found, then ErrNodeNotFound if returned.
 func (db *DB) FetchLinkNode(identity *btcec.PublicKey) (*LinkNode, error) {
 	var linkNode *LinkNode
-	err := db.View(func(tx *bbolt.Tx) error {
+	err := kvdb.View(db, func(tx kvdb.ReadTx) error {
 		node, err := fetchLinkNode(tx, identity)
 		if err != nil {
 			return err
@@ -163,10 +163,10 @@ func (db *DB) FetchLinkNode(identity *btcec.PublicKey) (*LinkNode, error) {
 	return linkNode, err
 }
 
-func fetchLinkNode(tx *bbolt.Tx, targetPub *btcec.PublicKey) (*LinkNode, error) {
+func fetchLinkNode(tx kvdb.ReadTx, targetPub *btcec.PublicKey) (*LinkNode, error) {
 	// First fetch the bucket for storing node metadata, bailing out early
 	// if it hasn't been created yet.
-	nodeMetaBucket := tx.Bucket(nodeInfoBucket)
+	nodeMetaBucket := tx.ReadBucket(nodeInfoBucket)
 	if nodeMetaBucket == nil {
 		return nil, ErrLinkNodesNotFound
 	}
@@ -191,7 +191,7 @@ func fetchLinkNode(tx *bbolt.Tx, targetPub *btcec.PublicKey) (*LinkNode, error) 
 // whom we have active channels with.
 func (db *DB) FetchAllLinkNodes() ([]*LinkNode, error) {
 	var linkNodes []*LinkNode
-	err := db.View(func(tx *bbolt.Tx) error {
+	err := kvdb.View(db, func(tx kvdb.ReadTx) error {
 		nodes, err := db.fetchAllLinkNodes(tx)
 		if err != nil {
 			return err
@@ -209,8 +209,8 @@ func (db *DB) FetchAllLinkNodes() ([]*LinkNode, error) {
 
 // fetchAllLinkNodes uses an existing database transaction to fetch all nodes
 // with whom we have active channels with.
-func (db *DB) fetchAllLinkNodes(tx *bbolt.Tx) ([]*LinkNode, error) {
-	nodeMetaBucket := tx.Bucket(nodeInfoBucket)
+func (db *DB) fetchAllLinkNodes(tx kvdb.ReadTx) ([]*LinkNode, error) {
+	nodeMetaBucket := tx.ReadBucket(nodeInfoBucket)
 	if nodeMetaBucket == nil {
 		return nil, ErrLinkNodesNotFound
 	}

matheusd/dcrlnd

channeldb/duplicate_payments.go
@@ -8,10 +8,10 @@ import (
 	"time"
 
 	"github.com/decred/dcrd/dcrec/secp256k1/v3"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/lntypes"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/routing/route"
-	bbolt "go.etcd.io/bbbolt"
 )
 
 var (
@@ -61,7 +61,7 @@ type duplicateHTLCAttemptInfo struct {
 
 // fetchDuplicatePaymentStatus fetches the payment status of the payment. If the
 // payment isn't found, it will default to "StatusUnknown".
-func fetchDuplicatePaymentStatus(bucket *bbolt.Bucket) PaymentStatus {
+func fetchDuplicatePaymentStatus(bucket kvdb.ReadBucket) PaymentStatus {
 	if bucket.Get(duplicatePaymentSettleInfoKey) != nil {
 		return StatusSucceeded
 	}
@@ -129,7 +129,7 @@ func deserializeDuplicatePaymentCreationInfo(r io.Reader) (
 	return c, nil
 }
 
-func fetchDuplicatePayment(bucket *bbolt.Bucket) (*MPPayment, error) {
+func fetchDuplicatePayment(bucket kvdb.ReadBucket) (*MPPayment, error) {
 	seqBytes := bucket.Get(duplicatePaymentSequenceKey)
 	if seqBytes == nil {
 		return nil, fmt.Errorf("sequence number not found")
@@ -209,7 +209,7 @@ func fetchDuplicatePayment(bucket *bbolt.Bucket) (*MPPayment, error) {
 	return payment, nil
 }
 
-func fetchDuplicatePayments(paymentHashBucket *bbolt.Bucket) ([]*MPPayment,
+func fetchDuplicatePayments(paymentHashBucket kvdb.ReadBucket) ([]*MPPayment,
 	error) {
 
 	var payments []*MPPayment
@@ -217,13 +217,13 @@ func fetchDuplicatePayments(paymentHashBucket *bbolt.Bucket) ([]*MPPayment,
 	// For older versions of lnd, duplicate payments to a payment has was
 	// possible. These will be found in a sub-bucket indexed by their
 	// sequence number if available.
-	dup := paymentHashBucket.Bucket(duplicatePaymentsBucket)
+	dup := paymentHashBucket.NestedReadBucket(duplicatePaymentsBucket)
 	if dup == nil {
 		return nil, nil
 	}
 
 	err := dup.ForEach(func(k, v []byte) error {
-		subBucket := dup.Bucket(k)
+		subBucket := dup.NestedReadBucket(k)
 		if subBucket == nil {
 			// We one bucket for each duplicate to be found.
 			return fmt.Errorf("non bucket element" +

channeldb/migration_01_to_11/migration_11_invoices_test.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/decred/dcrd/chaincfg/v2"
 	"github.com/decred/dcrd/dcrec/secp256k1/v3/ecdsa"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/zpay32"
-	bbolt "go.etcd.io/bbbolt"
 )
 
 var (
@@ -24,8 +24,8 @@ var (
 
 // beforeMigrationFuncV11 insert the test invoices in the database.
 func beforeMigrationFuncV11(t *testing.T, d *DB, invoices []Invoice) {
-	err := d.Update(func(tx *bbolt.Tx) error {
-		invoicesBucket, err := tx.CreateBucketIfNotExists(
+	err := kvdb.Update(d, func(tx kvdb.RwTx) error {
+		invoicesBucket, err := tx.CreateTopLevelBucket(
 			invoiceBucket,
 		)
 		if err != nil {

channeldb/migration_01_to_11/migrations_test.go
@@ -12,10 +12,10 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/decred/dcrd/dcrutil/v2"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/lntypes"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/go-errors/errors"
-	bbolt "go.etcd.io/bbbolt"
 )
 
 // TestPaymentStatusesMigration checks that already completed payments will have
@@ -59,8 +59,8 @@ func TestPaymentStatusesMigration(t *testing.T) {
 		// locally-sourced payment should end up with an InFlight
 		// status, while the other should remain unchanged, which
 		// defaults to Grounded.
-		err = d.Update(func(tx *bbolt.Tx) error {
-			circuits, err := tx.CreateBucketIfNotExists(
+		err = kvdb.Update(d, func(tx kvdb.RwTx) error {
+			circuits, err := tx.CreateTopLevelBucket(
 				[]byte("circuit-adds"),
 			)
 			if err != nil {
@@ -377,8 +377,8 @@ func TestMigrateOptionalChannelCloseSummaryFields(t *testing.T) {
 			// Get the old serialization format for this test's
 			// close summary, and it to the closed channel bucket.
 			old := test.oldSerialization(test.closeSummary)
-			err = d.Update(func(tx *bbolt.Tx) error {
-				closedChanBucket, err := tx.CreateBucketIfNotExists(
+			err = kvdb.Update(d, func(tx kvdb.RwTx) error {
+				closedChanBucket, err := tx.CreateTopLevelBucket(
 					closedChannelBucket,
 				)
 				if err != nil {
@@ -404,8 +404,8 @@ func TestMigrateOptionalChannelCloseSummaryFields(t *testing.T) {
 			newSerialization := b.Bytes()
 
 			var dbSummary []byte
-			err = d.View(func(tx *bbolt.Tx) error {
-				closedChanBucket := tx.Bucket(closedChannelBucket)
+			err = kvdb.View(d, func(tx kvdb.ReadTx) error {
+				closedChanBucket := tx.ReadBucket(closedChannelBucket)
 				if closedChanBucket == nil {
 					return errors.New("unable to find bucket")
 				}
@@ -480,8 +480,8 @@ func TestMigrateGossipMessageStoreKeys(t *testing.T) {
 			t.Fatalf("unable to serialize message: %v", err)
 		}
 
-		err := db.Update(func(tx *bbolt.Tx) error {
-			messageStore, err := tx.CreateBucketIfNotExists(
+		err := kvdb.Update(db, func(tx kvdb.RwTx) error {
+			messageStore, err := tx.CreateTopLevelBucket(
 				messageStoreBucket,
 			)
 			if err != nil {
@@ -501,8 +501,8 @@ func TestMigrateGossipMessageStoreKeys(t *testing.T) {
 	//   3. The message matches the original.
 	afterMigration := func(db *DB) {
 		var rawMsg []byte
-		err := db.View(func(tx *bbolt.Tx) error {
-			messageStore := tx.Bucket(messageStoreBucket)
+		err := kvdb.View(db, func(tx kvdb.ReadTx) error {
+			messageStore := tx.ReadBucket(messageStoreBucket)
 			if messageStore == nil {
 				return errors.New("message store bucket not " +
 					"found")
@@ -664,8 +664,8 @@ func TestOutgoingPaymentsMigration(t *testing.T) {
 
 		// Finally, check that the payment sequence number is updated
 		// to reflect the migrated payments.
-		err = d.View(func(tx *bbolt.Tx) error {
-			payments := tx.Bucket(paymentsRootBucket)
+		err = kvdb.Update(d, func(tx kvdb.RwTx) error {
+			payments := tx.ReadWriteBucket(paymentsRootBucket)
 			if payments == nil {
 				return fmt.Errorf("payments bucket not found")
 			}
@@ -744,8 +744,8 @@ func TestPaymentRouteSerialization(t *testing.T) {
 	// We'll first add a series of fake payments, using the existing legacy
 	// serialization format.
 	beforeMigrationFunc := func(d *DB) {
-		err := d.Update(func(tx *bbolt.Tx) error {
-			paymentsBucket, err := tx.CreateBucket(
+		err := kvdb.Update(d, func(tx kvdb.RwTx) error {
+			paymentsBucket, err := tx.CreateTopLevelBucket(
 				paymentsRootBucket,
 			)
 			if err != nil {
@@ -796,7 +796,7 @@ func TestPaymentRouteSerialization(t *testing.T) {
 				// the proper bucket. If this is the duplicate
 				// payment, then we'll grab the dup bucket,
 				// otherwise, we'll use the top level bucket.
-				var payHashBucket *bbolt.Bucket
+				var payHashBucket kvdb.RwBucket
 				if i < numPayments-1 {
 					payHashBucket, err = paymentsBucket.CreateBucket(
 						payInfo.PaymentHash[:],
@@ -805,7 +805,7 @@ func TestPaymentRouteSerialization(t *testing.T) {
 						t.Fatalf("unable to create payments bucket: %v", err)
 					}
 				} else {
-					payHashBucket = paymentsBucket.Bucket(
+					payHashBucket = paymentsBucket.NestedReadWriteBucket(
 						payInfo.PaymentHash[:],
 					)
 					dupPayBucket, err := payHashBucket.CreateBucket(

channeldb/nodes.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/decred/dcrd/dcrec/secp256k1/v3"
 	"github.com/decred/dcrd/wire"
-	bbolt "go.etcd.io/bbbolt"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 )
 
 var (
@@ -109,8 +109,8 @@ func (l *LinkNode) Sync() error {
 
 	// Finally update the database by storing the link node and updating
 	// any relevant indexes.
-	return l.db.Update(func(tx *bbolt.Tx) error {
-		nodeMetaBucket := tx.Bucket(nodeInfoBucket)
+	return kvdb.Update(l.db, func(tx kvdb.RwTx) error {
+		nodeMetaBucket := tx.ReadWriteBucket(nodeInfoBucket)
 		if nodeMetaBucket == nil {
 			return ErrLinkNodesNotFound
 		}
@@ -122,7 +122,7 @@ func (l *LinkNode) Sync() error {
 // putLinkNode serializes then writes the encoded version of the passed link
 // node into the nodeMetaBucket. This function is provided in order to allow
 // the ability to re-use a database transaction across many operations.
-func putLinkNode(nodeMetaBucket *bbolt.Bucket, l *LinkNode) error {
+func putLinkNode(nodeMetaBucket kvdb.RwBucket, l *LinkNode) error {
 	// First serialize the LinkNode into its raw-bytes encoding.
 	var b bytes.Buffer
 	if err := serializeLinkNode(&b, l); err != nil {
@@ -138,13 +138,13 @@ func putLinkNode(nodeMetaBucket *bbolt.Bucket, l *LinkNode) error {
 // DeleteLinkNode removes the link node with the given identity from the
 // database.
 func (db *DB) DeleteLinkNode(identity *secp256k1.PublicKey) error {
-	return db.Update(func(tx *bbolt.Tx) error {
+	return kvdb.Update(db, func(tx kvdb.RwTx) error {
 		return db.deleteLinkNode(tx, identity)
 	})
 }
 
-func (db *DB) deleteLinkNode(tx *bbolt.Tx, identity *secp256k1.PublicKey) error {
-	nodeMetaBucket := tx.Bucket(nodeInfoBucket)
+func (db *DB) deleteLinkNode(tx kvdb.RwTx, identity *secp256k1.PublicKey) error {
+	nodeMetaBucket := tx.ReadWriteBucket(nodeInfoBucket)
 	if nodeMetaBucket == nil {
 		return ErrLinkNodesNotFound
 	}
@@ -158,7 +158,7 @@ func (db *DB) deleteLinkNode(tx *bbolt.Tx, identity *secp256k1.PublicKey) error 
 // key cannot be found, then ErrNodeNotFound if returned.
 func (db *DB) FetchLinkNode(identity *secp256k1.PublicKey) (*LinkNode, error) {
 	var linkNode *LinkNode
-	err := db.View(func(tx *bbolt.Tx) error {
+	err := kvdb.View(db, func(tx kvdb.ReadTx) error {
 		node, err := fetchLinkNode(tx, identity)
 		if err != nil {
 			return err
@@ -171,10 +171,10 @@ func (db *DB) FetchLinkNode(identity *secp256k1.PublicKey) (*LinkNode, error) {
 	return linkNode, err
 }
 
-func fetchLinkNode(tx *bbolt.Tx, targetPub *secp256k1.PublicKey) (*LinkNode, error) {
+func fetchLinkNode(tx kvdb.ReadTx, targetPub *secp256k1.PublicKey) (*LinkNode, error) {
 	// First fetch the bucket for storing node metadata, bailing out early
 	// if it hasn't been created yet.
-	nodeMetaBucket := tx.Bucket(nodeInfoBucket)
+	nodeMetaBucket := tx.ReadBucket(nodeInfoBucket)
 	if nodeMetaBucket == nil {
 		return nil, ErrLinkNodesNotFound
 	}
@@ -199,7 +199,7 @@ func fetchLinkNode(tx *bbolt.Tx, targetPub *secp256k1.PublicKey) (*LinkNode, err
 // whom we have active channels with.
 func (db *DB) FetchAllLinkNodes() ([]*LinkNode, error) {
 	var linkNodes []*LinkNode
-	err := db.View(func(tx *bbolt.Tx) error {
+	err := kvdb.View(db, func(tx kvdb.ReadTx) error {
 		nodes, err := db.fetchAllLinkNodes(tx)
 		if err != nil {
 			return err
@@ -217,8 +217,8 @@ func (db *DB) FetchAllLinkNodes() ([]*LinkNode, error) {
 
 // fetchAllLinkNodes uses an existing database transaction to fetch all nodes
 // with whom we have active channels with.
-func (db *DB) fetchAllLinkNodes(tx *bbolt.Tx) ([]*LinkNode, error) {
-	nodeMetaBucket := tx.Bucket(nodeInfoBucket)
+func (db *DB) fetchAllLinkNodes(tx kvdb.ReadTx) ([]*LinkNode, error) {
+	nodeMetaBucket := tx.ReadBucket(nodeInfoBucket)
 	if nodeMetaBucket == nil {
 		return nil, ErrLinkNodesNotFound
 	}