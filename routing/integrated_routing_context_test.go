lightningnetwork/lnd

routing: move payment session constructor

routing/integrated_routing_context_test.go
@@ -140,17 +140,19 @@ func (c *integratedRoutingContext) testPayment(maxShards uint32) ([]htlcAttempt,
 		MaxShards:      maxShards,
 	}
 
-	session := &paymentSession{
-		getBandwidthHints: getBandwidthHints,
-		payment:           &payment,
-		pathFinder:        findPath,
-		getRoutingGraph: func() (routingGraph, func(), error) {
+	session, err := newPaymentSession(
+		&payment, getBandwidthHints,
+		func() (routingGraph, func(), error) {
 			return c.graph, func() {}, nil
 		},
-		pathFindingConfig: c.pathFindingCfg,
-		missionControl:    mc,
-		minShardAmt:       lnwire.NewMSatFromSatoshis(5000),
+		mc, c.pathFindingCfg,
+	)
+	if err != nil {
+		c.t.Fatal(err)
 	}
+
+	// Override default minimum shard amount.
+	session.minShardAmt = lnwire.NewMSatFromSatoshis(5000)
 
 	// Now the payment control loop starts. It will keep trying routes until
 	// the payment succeeds.

routing/payment_session.go
@@ -126,6 +126,30 @@ type paymentSession struct {
 	minShardAmt lnwire.MilliSatoshi
 }
 
+// newPaymentSession instantiates a new payment session.
+func newPaymentSession(p *LightningPayment,
+	getBandwidthHints func() (map[uint64]lnwire.MilliSatoshi, error),
+	getRoutingGraph func() (routingGraph, func(), error),
+	missionControl MissionController, pathFindingConfig PathFindingConfig) (
+	*paymentSession, error) {
+
+	edges, err := RouteHintsToEdges(p.RouteHints, p.Target)
+	if err != nil {
+		return nil, err
+	}
+
+	return &paymentSession{
+		additionalEdges:   edges,
+		getBandwidthHints: getBandwidthHints,
+		payment:           p,
+		pathFinder:        findPath,
+		getRoutingGraph:   getRoutingGraph,
+		pathFindingConfig: pathFindingConfig,
+		missionControl:    missionControl,
+		minShardAmt:       DefaultShardMinAmt,
+	}, nil
+}
+
 // RequestRoute returns a route which is likely to be capable for successfully
 // routing the specified HTLC payment to the target node. Initially the first
 // set of paths returned from this method may encounter routing failure along

routing/payment_session_test.go
@@ -13,9 +13,36 @@ func TestRequestRoute(t *testing.T) {
 		height = 10
 	)
 
-	findPath := func(
-		g *graphParams,
-		r *RestrictParams, cfg *PathFindingConfig,
+	cltvLimit := uint32(30)
+	finalCltvDelta := uint16(8)
+
+	payment := &LightningPayment{
+		CltvLimit:      cltvLimit,
+		FinalCLTVDelta: finalCltvDelta,
+		Amount:         1000,
+		FeeLimit:       1000,
+	}
+
+	session, err := newPaymentSession(
+		payment,
+		func() (map[uint64]lnwire.MilliSatoshi,
+			error) {
+
+			return nil, nil
+		},
+		func() (routingGraph, func(), error) {
+			return &sessionGraph{}, func() {}, nil
+		},
+		&MissionControl{cfg: &MissionControlConfig{}},
+		PathFindingConfig{},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Override pathfinder with a mock.
+	session.pathFinder = func(
+		g *graphParams, r *RestrictParams, cfg *PathFindingConfig,
 		source, target route.Vertex, amt lnwire.MilliSatoshi,
 		finalHtlcExpiry int32) ([]*channeldb.ChannelEdgePolicy, error) {
 
@@ -36,32 +63,6 @@ func TestRequestRoute(t *testing.T) {
 		}
 
 		return path, nil
-	}
-
-	cltvLimit := uint32(30)
-	finalCltvDelta := uint16(8)
-
-	payment := &LightningPayment{
-		CltvLimit:      cltvLimit,
-		FinalCLTVDelta: finalCltvDelta,
-		Amount:         1000,
-		FeeLimit:       1000,
-	}
-
-	session := &paymentSession{
-		getBandwidthHints: func() (map[uint64]lnwire.MilliSatoshi,
-			error) {
-
-			return nil, nil
-		},
-		payment:    payment,
-		pathFinder: findPath,
-		missionControl: &MissionControl{
-			cfg: &MissionControlConfig{},
-		},
-		getRoutingGraph: func() (routingGraph, func(), error) {
-			return &sessionGraph{}, func() {}, nil
-		},
 	}
 
 	route, err := session.RequestRoute(

matheusd/dcrlnd

routing/integrated_routing_context_test.go
@@ -140,17 +140,19 @@ func (c *integratedRoutingContext) testPayment(maxShards uint32) ([]htlcAttempt,
 		MaxShards:      maxShards,
 	}
 
-	session := &paymentSession{
-		getBandwidthHints: getBandwidthHints,
-		payment:           &payment,
-		pathFinder:        findPath,
-		getRoutingGraph: func() (routingGraph, func(), error) {
+	session, err := newPaymentSession(
+		&payment, getBandwidthHints,
+		func() (routingGraph, func(), error) {
 			return c.graph, func() {}, nil
 		},
-		pathFindingConfig: c.pathFindingCfg,
-		missionControl:    mc,
-		minShardAmt:       lnwire.NewMAtomsFromAtoms(5000),
+		mc, c.pathFindingCfg,
+	)
+	if err != nil {
+		c.t.Fatal(err)
 	}
+
+	// Override default minimum shard amount.
+	session.minShardAmt = lnwire.NewMAtomsFromAtoms(5000)
 
 	// Now the payment control loop starts. It will keep trying routes until
 	// the payment succeeds.

routing/payment_session.go
@@ -126,6 +126,30 @@ type paymentSession struct {
 	minShardAmt lnwire.MilliAtom
 }
 
+// newPaymentSession instantiates a new payment session.
+func newPaymentSession(p *LightningPayment,
+	getBandwidthHints func() (map[uint64]lnwire.MilliAtom, error),
+	getRoutingGraph func() (routingGraph, func(), error),
+	missionControl MissionController, pathFindingConfig PathFindingConfig) (
+	*paymentSession, error) {
+
+	edges, err := RouteHintsToEdges(p.RouteHints, p.Target)
+	if err != nil {
+		return nil, err
+	}
+
+	return &paymentSession{
+		additionalEdges:   edges,
+		getBandwidthHints: getBandwidthHints,
+		payment:           p,
+		pathFinder:        findPath,
+		getRoutingGraph:   getRoutingGraph,
+		pathFindingConfig: pathFindingConfig,
+		missionControl:    missionControl,
+		minShardAmt:       DefaultShardMinAmt,
+	}, nil
+}
+
 // RequestRoute returns a route which is likely to be capable for successfully
 // routing the specified HTLC payment to the target node. Initially the first
 // set of paths returned from this method may encounter routing failure along

routing/payment_session_test.go
@@ -13,9 +13,36 @@ func TestRequestRoute(t *testing.T) {
 		height = 10
 	)
 
-	findPath := func(
-		g *graphParams,
-		r *RestrictParams, cfg *PathFindingConfig,
+	cltvLimit := uint32(30)
+	finalCltvDelta := uint16(8)
+
+	payment := &LightningPayment{
+		CltvLimit:      cltvLimit,
+		FinalCLTVDelta: finalCltvDelta,
+		Amount:         1000,
+		FeeLimit:       1000,
+	}
+
+	session, err := newPaymentSession(
+		payment,
+		func() (map[uint64]lnwire.MilliAtom,
+			error) {
+
+			return nil, nil
+		},
+		func() (routingGraph, func(), error) {
+			return &sessionGraph{}, func() {}, nil
+		},
+		&MissionControl{cfg: &MissionControlConfig{}},
+		PathFindingConfig{},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Override pathfinder with a mock.
+	session.pathFinder = func(
+		g *graphParams, r *RestrictParams, cfg *PathFindingConfig,
 		source, target route.Vertex, amt lnwire.MilliAtom,
 		finalHtlcExpiry int32) ([]*channeldb.ChannelEdgePolicy, error) {
 
@@ -36,32 +63,6 @@ func TestRequestRoute(t *testing.T) {
 		}
 
 		return path, nil
-	}
-
-	cltvLimit := uint32(30)
-	finalCltvDelta := uint16(8)
-
-	payment := &LightningPayment{
-		CltvLimit:      cltvLimit,
-		FinalCLTVDelta: finalCltvDelta,
-		Amount:         1000,
-		FeeLimit:       1000,
-	}
-
-	session := &paymentSession{
-		getBandwidthHints: func() (map[uint64]lnwire.MilliAtom,
-			error) {
-
-			return nil, nil
-		},
-		payment:    payment,
-		pathFinder: findPath,
-		missionControl: &MissionControl{
-			cfg: &MissionControlConfig{},
-		},
-		getRoutingGraph: func() (routingGraph, func(), error) {
-			return &sessionGraph{}, func() {}, nil
-		},
 	}
 
 	route, err := session.RequestRoute(