lightningnetwork/lnd

lnwallet/chanfunding: add new ThawHeight() method to ShimIntent+CannedAssembler
As frozen channels can only be created via the non-default channel
assembler, we extend both the ShimIntent and CannedAssembler to also
accept and expose this new channel status along with the thaw height.

rpcserver.go
@@ -1545,7 +1545,7 @@ func newFundingShimAssembler(chanPointShim *lnrpc.ChanPointShim,
 	// With all the parts assembled, we can now make the canned assembler
 	// to pass into the wallet.
 	return chanfunding.NewCannedAssembler(
-		*chanPoint, btcutil.Amount(chanPointShim.Amt),
+		0, *chanPoint, btcutil.Amount(chanPointShim.Amt),
 		&localKeyDesc, remoteKey, initiator,
 	), nil
 }

matheusd/dcrlnd

rpcserver.go
@@ -1524,7 +1524,7 @@ func newFundingShimAssembler(chanPointShim *lnrpc.ChanPointShim,
 	// With all the parts assembled, we can now make the canned assembler
 	// to pass into the wallet.
 	return chanfunding.NewCannedAssembler(
-		*chanPoint, dcrutil.Amount(chanPointShim.Amt),
+		0, *chanPoint, dcrutil.Amount(chanPointShim.Amt),
 		&localKeyDesc, remoteKey, initiator,
 	), nil
 }