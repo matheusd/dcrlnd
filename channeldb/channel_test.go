lightningnetwork/lnd

channeldb: inject clock into database
Use our standard clock mock for database time queries.

channeldb/channel_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/btcsuite/btcutil"
 	_ "github.com/btcsuite/btcwallet/walletdb/bdb"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/lightningnetwork/lnd/clock"
 	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/shachain"
@@ -68,6 +69,8 @@ var (
 	privKey, pubKey = btcec.PrivKeyFromBytes(btcec.S256(), key[:])
 
 	wireSig, _ = lnwire.NewSigFromSignature(testSig)
+
+	testClock = clock.NewTestClock(testNow)
 )
 
 // makeTestDB creates a new instance of the ChannelDB for testing purposes. A
@@ -82,7 +85,7 @@ func makeTestDB() (*DB, func(), error) {
 	}
 
 	// Next, create channeldb for the first time.
-	cdb, err := Open(tempDirName)
+	cdb, err := Open(tempDirName, OptionClock(testClock))
 	if err != nil {
 		return nil, nil, err
 	}

channeldb/db.go
@@ -15,6 +15,7 @@ import (
 	"github.com/go-errors/errors"
 	"github.com/lightningnetwork/lnd/channeldb/migration12"
 	"github.com/lightningnetwork/lnd/channeldb/migration_01_to_11"
+	"github.com/lightningnetwork/lnd/clock"
 	"github.com/lightningnetwork/lnd/lnwire"
 )
 
@@ -137,8 +138,7 @@ type DB struct {
 	*bbolt.DB
 	dbPath string
 	graph  *ChannelGraph
-
-	Now func() time.Time
+	clock  clock.Clock
 }
 
 // Open opens an existing channeldb. Any necessary schemas migrations due to
@@ -172,7 +172,7 @@ func Open(dbPath string, modifiers ...OptionModifier) (*DB, error) {
 	chanDB := &DB{
 		DB:     bdb,
 		dbPath: dbPath,
-		Now:    time.Now,
+		clock:  opts.clock,
 	}
 	chanDB.graph = newChannelGraph(
 		chanDB, opts.RejectCacheSize, opts.ChannelCacheSize,

channeldb/options.go
@@ -1,5 +1,7 @@
 package channeldb
 
+import "github.com/lightningnetwork/lnd/clock"
+
 const (
 	// DefaultRejectCacheSize is the default number of rejectCacheEntries to
 	// cache for use in the rejection cache of incoming gossip traffic. This
@@ -26,6 +28,9 @@ type Options struct {
 	// freelist to disk, resulting in improved performance at the expense of
 	// increased startup time.
 	NoFreelistSync bool
+
+	// clock is the time source used by the database.
+	clock clock.Clock
 }
 
 // DefaultOptions returns an Options populated with default values.
@@ -34,6 +39,7 @@ func DefaultOptions() Options {
 		RejectCacheSize:  DefaultRejectCacheSize,
 		ChannelCacheSize: DefaultChannelCacheSize,
 		NoFreelistSync:   true,
+		clock:            clock.NewDefaultClock(),
 	}
 }
 
@@ -58,5 +64,12 @@ func OptionSetChannelCacheSize(n int) OptionModifier {
 func OptionSetSyncFreelist(b bool) OptionModifier {
 	return func(o *Options) {
 		o.NoFreelistSync = !b
+	}
+}
+
+// OptionClock sets a non-default clock dependency.
+func OptionClock(clock clock.Clock) OptionModifier {
+	return func(o *Options) {
+		o.clock = clock
 	}
 }

matheusd/dcrlnd

channeldb/channel_test.go
@@ -17,6 +17,7 @@ import (
 	"github.com/decred/dcrd/dcrec/secp256k1/v3"
 	"github.com/decred/dcrd/dcrutil/v2"
 	"github.com/decred/dcrd/wire"
+	"github.com/decred/dcrlnd/clock"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/shachain"
 )
@@ -74,6 +75,8 @@ var (
 	privKey, pubKey = privKeyFromBytes(key[:])
 
 	wireSig, _ = lnwire.NewSigFromSignature(testSig)
+
+	testClock = clock.NewTestClock(testNow)
 )
 
 // makeTestDB creates a new instance of the ChannelDB for testing purposes. A
@@ -88,7 +91,7 @@ func makeTestDB() (*DB, func(), error) {
 	}
 
 	// Next, create channeldb for the first time.
-	cdb, err := Open(tempDirName)
+	cdb, err := Open(tempDirName, OptionClock(testClock))
 	if err != nil {
 		return nil, nil, err
 	}

channeldb/db.go
@@ -13,6 +13,7 @@ import (
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/channeldb/migration12"
 	"github.com/decred/dcrlnd/channeldb/migration_01_to_11"
+	"github.com/decred/dcrlnd/clock"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/go-errors/errors"
 	bolt "go.etcd.io/bbolt"
@@ -137,8 +138,7 @@ type DB struct {
 	*bolt.DB
 	dbPath string
 	graph  *ChannelGraph
-
-	Now func() time.Time
+	clock  clock.Clock
 }
 
 // Open opens an existing channeldb. Any necessary schemas migrations due to
@@ -172,7 +172,7 @@ func Open(dbPath string, modifiers ...OptionModifier) (*DB, error) {
 	chanDB := &DB{
 		DB:     bdb,
 		dbPath: dbPath,
-		Now:    time.Now,
+		clock:  opts.clock,
 	}
 	chanDB.graph = newChannelGraph(
 		chanDB, opts.RejectCacheSize, opts.ChannelCacheSize,

channeldb/options.go
@@ -1,5 +1,7 @@
 package channeldb
 
+import "github.com/decred/dcrlnd/clock"
+
 const (
 	// DefaultRejectCacheSize is the default number of rejectCacheEntries to
 	// cache for use in the rejection cache of incoming gossip traffic. This
@@ -26,6 +28,9 @@ type Options struct {
 	// freelist to disk, resulting in improved performance at the expense of
 	// increased startup time.
 	NoFreelistSync bool
+
+	// clock is the time source used by the database.
+	clock clock.Clock
 }
 
 // DefaultOptions returns an Options populated with default values.
@@ -34,6 +39,7 @@ func DefaultOptions() Options {
 		RejectCacheSize:  DefaultRejectCacheSize,
 		ChannelCacheSize: DefaultChannelCacheSize,
 		NoFreelistSync:   true,
+		clock:            clock.NewDefaultClock(),
 	}
 }
 
@@ -58,5 +64,12 @@ func OptionSetChannelCacheSize(n int) OptionModifier {
 func OptionSetSyncFreelist(b bool) OptionModifier {
 	return func(o *Options) {
 		o.NoFreelistSync = !b
+	}
+}
+
+// OptionClock sets a non-default clock dependency.
+func OptionClock(clock clock.Clock) OptionModifier {
+	return func(o *Options) {
+		o.clock = clock
 	}
 }