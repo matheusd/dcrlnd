lightningnetwork/lnd

htlcswitch: convert to use new kvdb abstraction

htlcswitch/circuit_map.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/coreos/bbolt"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/go-errors/errors"
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/htlcswitch/hop"
 	"github.com/lightningnetwork/lnd/lnwire"
 )
@@ -213,13 +213,13 @@ func NewCircuitMap(cfg *CircuitMapConfig) (CircuitMap, error) {
 // initBuckets ensures that the primary buckets used by the circuit are
 // initialized so that we can assume their existence after startup.
 func (cm *circuitMap) initBuckets() error {
-	return cm.cfg.DB.Update(func(tx *bbolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(circuitKeystoneKey)
+	return kvdb.Update(cm.cfg.DB, func(tx kvdb.RwTx) error {
+		_, err := tx.CreateTopLevelBucket(circuitKeystoneKey)
 		if err != nil {
 			return err
 		}
 
-		_, err = tx.CreateBucketIfNotExists(circuitAddKey)
+		_, err = tx.CreateTopLevelBucket(circuitAddKey)
 		return err
 	})
 }
@@ -238,10 +238,10 @@ func (cm *circuitMap) restoreMemState() error {
 		pending = make(map[CircuitKey]*PaymentCircuit)
 	)
 
-	if err := cm.cfg.DB.Update(func(tx *bbolt.Tx) error {
+	if err := kvdb.Update(cm.cfg.DB, func(tx kvdb.RwTx) error {
 		// Restore any of the circuits persisted in the circuit bucket
 		// back into memory.
-		circuitBkt := tx.Bucket(circuitAddKey)
+		circuitBkt := tx.ReadWriteBucket(circuitAddKey)
 		if circuitBkt == nil {
 			return ErrCorruptedCircuitMap
 		}
@@ -262,7 +262,7 @@ func (cm *circuitMap) restoreMemState() error {
 
 		// Furthermore, load the keystone bucket and resurrect the
 		// keystones used in any open circuits.
-		keystoneBkt := tx.Bucket(circuitKeystoneKey)
+		keystoneBkt := tx.ReadWriteBucket(circuitKeystoneKey)
 		if keystoneBkt == nil {
 			return ErrCorruptedCircuitMap
 		}
@@ -463,8 +463,8 @@ func (cm *circuitMap) TrimOpenCircuits(chanID lnwire.ShortChannelID,
 		return nil
 	}
 
-	return cm.cfg.DB.Update(func(tx *bbolt.Tx) error {
-		keystoneBkt := tx.Bucket(circuitKeystoneKey)
+	return kvdb.Update(cm.cfg.DB, func(tx kvdb.RwTx) error {
+		keystoneBkt := tx.ReadWriteBucket(circuitKeystoneKey)
 		if keystoneBkt == nil {
 			return ErrCorruptedCircuitMap
 		}
@@ -616,8 +616,8 @@ func (cm *circuitMap) CommitCircuits(circuits ...*PaymentCircuit) (
 	// Write the entire batch of circuits to the persistent circuit bucket
 	// using bolt's Batch write. This method must be called from multiple,
 	// distinct goroutines to have any impact on performance.
-	err := cm.cfg.DB.Batch(func(tx *bbolt.Tx) error {
-		circuitBkt := tx.Bucket(circuitAddKey)
+	err := kvdb.Batch(cm.cfg.DB.Backend, func(tx kvdb.RwTx) error {
+		circuitBkt := tx.ReadWriteBucket(circuitAddKey)
 		if circuitBkt == nil {
 			return ErrCorruptedCircuitMap
 		}
@@ -706,10 +706,10 @@ func (cm *circuitMap) OpenCircuits(keystones ...Keystone) error {
 	}
 	cm.mtx.RUnlock()
 
-	err := cm.cfg.DB.Update(func(tx *bbolt.Tx) error {
+	err := kvdb.Update(cm.cfg.DB, func(tx kvdb.RwTx) error {
 		// Now, load the circuit bucket to which we will write the
 		// already serialized circuit.
-		keystoneBkt := tx.Bucket(circuitKeystoneKey)
+		keystoneBkt := tx.ReadWriteBucket(circuitKeystoneKey)
 		if keystoneBkt == nil {
 			return ErrCorruptedCircuitMap
 		}
@@ -847,13 +847,13 @@ func (cm *circuitMap) DeleteCircuits(inKeys ...CircuitKey) error {
 	}
 	cm.mtx.Unlock()
 
-	err := cm.cfg.DB.Batch(func(tx *bbolt.Tx) error {
+	err := kvdb.Batch(cm.cfg.DB.Backend, func(tx kvdb.RwTx) error {
 		for _, circuit := range removedCircuits {
 			// If this htlc made it to an outgoing link, load the
 			// keystone bucket from which we will remove the
 			// outgoing circuit key.
 			if circuit.HasKeystone() {
-				keystoneBkt := tx.Bucket(circuitKeystoneKey)
+				keystoneBkt := tx.ReadWriteBucket(circuitKeystoneKey)
 				if keystoneBkt == nil {
 					return ErrCorruptedCircuitMap
 				}
@@ -868,7 +868,7 @@ func (cm *circuitMap) DeleteCircuits(inKeys ...CircuitKey) error {
 
 			// Remove the circuit itself based on the incoming
 			// circuit key.
-			circuitBkt := tx.Bucket(circuitAddKey)
+			circuitBkt := tx.ReadWriteBucket(circuitAddKey)
 			if circuitBkt == nil {
 				return ErrCorruptedCircuitMap
 			}

htlcswitch/link_test.go
@@ -19,12 +19,12 @@ import (
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
-	"github.com/coreos/bbolt"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/go-errors/errors"
 	sphinx "github.com/lightningnetwork/lightning-onion"
 	"github.com/lightningnetwork/lnd/build"
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/contractcourt"
 	"github.com/lightningnetwork/lnd/htlcswitch/hodl"
 	"github.com/lightningnetwork/lnd/htlcswitch/hop"
@@ -5170,32 +5170,32 @@ type mockPackager struct {
 	failLoadFwdPkgs bool
 }
 
-func (*mockPackager) AddFwdPkg(tx *bbolt.Tx, fwdPkg *channeldb.FwdPkg) error {
+func (*mockPackager) AddFwdPkg(tx kvdb.RwTx, fwdPkg *channeldb.FwdPkg) error {
 	return nil
 }
 
-func (*mockPackager) SetFwdFilter(tx *bbolt.Tx, height uint64,
+func (*mockPackager) SetFwdFilter(tx kvdb.RwTx, height uint64,
 	fwdFilter *channeldb.PkgFilter) error {
 	return nil
 }
 
-func (*mockPackager) AckAddHtlcs(tx *bbolt.Tx,
+func (*mockPackager) AckAddHtlcs(tx kvdb.RwTx,
 	addRefs ...channeldb.AddRef) error {
 	return nil
 }
 
-func (m *mockPackager) LoadFwdPkgs(tx *bbolt.Tx) ([]*channeldb.FwdPkg, error) {
+func (m *mockPackager) LoadFwdPkgs(tx kvdb.ReadTx) ([]*channeldb.FwdPkg, error) {
 	if m.failLoadFwdPkgs {
 		return nil, fmt.Errorf("failing LoadFwdPkgs")
 	}
 	return nil, nil
 }
 
-func (*mockPackager) RemovePkg(tx *bbolt.Tx, height uint64) error {
+func (*mockPackager) RemovePkg(tx kvdb.RwTx, height uint64) error {
 	return nil
 }
 
-func (*mockPackager) AckSettleFails(tx *bbolt.Tx,
+func (*mockPackager) AckSettleFails(tx kvdb.RwTx,
 	settleFailRefs ...channeldb.SettleFailRef) error {
 	return nil
 }

htlcswitch/payment_result.go
@@ -7,8 +7,8 @@ import (
 	"io"
 	"sync"
 
-	"github.com/coreos/bbolt"
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/multimutex"
 )
@@ -137,8 +137,8 @@ func (store *networkResultStore) storeResult(paymentID uint64,
 	var paymentIDBytes [8]byte
 	binary.BigEndian.PutUint64(paymentIDBytes[:], paymentID)
 
-	err := store.db.Batch(func(tx *bbolt.Tx) error {
-		networkResults, err := tx.CreateBucketIfNotExists(
+	err := kvdb.Batch(store.db.Backend, func(tx kvdb.RwTx) error {
+		networkResults, err := tx.CreateTopLevelBucket(
 			networkResultStoreBucketKey,
 		)
 		if err != nil {
@@ -180,7 +180,7 @@ func (store *networkResultStore) subscribeResult(paymentID uint64) (
 		resultChan = make(chan *networkResult, 1)
 	)
 
-	err := store.db.View(func(tx *bbolt.Tx) error {
+	err := kvdb.View(store.db, func(tx kvdb.ReadTx) error {
 		var err error
 		result, err = fetchResult(tx, paymentID)
 		switch {
@@ -226,7 +226,7 @@ func (store *networkResultStore) getResult(pid uint64) (
 	*networkResult, error) {
 
 	var result *networkResult
-	err := store.db.View(func(tx *bbolt.Tx) error {
+	err := kvdb.View(store.db, func(tx kvdb.ReadTx) error {
 		var err error
 		result, err = fetchResult(tx, pid)
 		return err
@@ -238,11 +238,11 @@ func (store *networkResultStore) getResult(pid uint64) (
 	return result, nil
 }
 
-func fetchResult(tx *bbolt.Tx, pid uint64) (*networkResult, error) {
+func fetchResult(tx kvdb.ReadTx, pid uint64) (*networkResult, error) {
 	var paymentIDBytes [8]byte
 	binary.BigEndian.PutUint64(paymentIDBytes[:], pid)
 
-	networkResults := tx.Bucket(networkResultStoreBucketKey)
+	networkResults := tx.ReadBucket(networkResultStoreBucketKey)
 	if networkResults == nil {
 		return nil, ErrPaymentIDNotFound
 	}

htlcswitch/sequencer.go
@@ -3,9 +3,9 @@ package htlcswitch
 import (
 	"sync"
 
-	"github.com/coreos/bbolt"
 	"github.com/go-errors/errors"
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 )
 
 // defaultSequenceBatchSize specifies the window of sequence numbers that are
@@ -87,8 +87,8 @@ func (s *persistentSequencer) NextID() (uint64, error) {
 	// allocated will start from the last known tip on disk, which is fine
 	// as we only require uniqueness of the allocated numbers.
 	var nextHorizonID uint64
-	if err := s.db.Update(func(tx *bbolt.Tx) error {
-		nextIDBkt := tx.Bucket(nextPaymentIDKey)
+	if err := kvdb.Update(s.db, func(tx kvdb.RwTx) error {
+		nextIDBkt := tx.ReadWriteBucket(nextPaymentIDKey)
 		if nextIDBkt == nil {
 			return ErrSequencerCorrupted
 		}
@@ -121,8 +121,8 @@ func (s *persistentSequencer) NextID() (uint64, error) {
 
 // initDB populates the bucket used to generate payment sequence numbers.
 func (s *persistentSequencer) initDB() error {
-	return s.db.Update(func(tx *bbolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(nextPaymentIDKey)
+	return kvdb.Update(s.db, func(tx kvdb.RwTx) error {
+		_, err := tx.CreateTopLevelBucket(nextPaymentIDKey)
 		return err
 	})
 }

htlcswitch/switch.go
@@ -10,10 +10,10 @@ import (
 
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
-	"github.com/coreos/bbolt"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/lightningnetwork/lnd/chainntnfs"
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/contractcourt"
 	"github.com/lightningnetwork/lnd/htlcswitch/hop"
 	"github.com/lightningnetwork/lnd/lntypes"
@@ -1419,7 +1419,7 @@ func (s *Switch) closeCircuit(pkt *htlcPacket) (*PaymentCircuit, error) {
 // we're the originator of the payment, so the link stops attempting to
 // re-broadcast.
 func (s *Switch) ackSettleFail(settleFailRefs ...channeldb.SettleFailRef) error {
-	return s.cfg.DB.Batch(func(tx *bbolt.Tx) error {
+	return kvdb.Batch(s.cfg.DB.Backend, func(tx kvdb.RwTx) error {
 		return s.cfg.SwitchPackager.AckSettleFails(tx, settleFailRefs...)
 	})
 }
@@ -1865,7 +1865,7 @@ func (s *Switch) reforwardResponses() error {
 func (s *Switch) loadChannelFwdPkgs(source lnwire.ShortChannelID) ([]*channeldb.FwdPkg, error) {
 
 	var fwdPkgs []*channeldb.FwdPkg
-	if err := s.cfg.DB.Update(func(tx *bbolt.Tx) error {
+	if err := kvdb.Update(s.cfg.DB, func(tx kvdb.RwTx) error {
 		var err error
 		fwdPkgs, err = s.cfg.SwitchPackager.LoadChannelFwdPkgs(
 			tx, source,

htlcswitch/test_utils.go
@@ -21,10 +21,10 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 	"github.com/btcsuite/fastsha256"
-	"github.com/coreos/bbolt"
 	"github.com/go-errors/errors"
 	sphinx "github.com/lightningnetwork/lightning-onion"
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/contractcourt"
 	"github.com/lightningnetwork/lnd/htlcswitch/hop"
 	"github.com/lightningnetwork/lnd/input"
@@ -420,7 +420,7 @@ func createTestChannel(alicePrivKey, bobPrivKey []byte,
 		aliceStoredChannels, err := dbAlice.FetchOpenChannels(aliceKeyPub)
 		switch err {
 		case nil:
-		case bbolt.ErrDatabaseNotOpen:
+		case kvdb.ErrDatabaseNotOpen:
 			dbAlice, err = channeldb.Open(dbAlice.Path())
 			if err != nil {
 				return nil, errors.Errorf("unable to reopen alice "+
@@ -464,7 +464,7 @@ func createTestChannel(alicePrivKey, bobPrivKey []byte,
 		bobStoredChannels, err := dbBob.FetchOpenChannels(bobKeyPub)
 		switch err {
 		case nil:
-		case bbolt.ErrDatabaseNotOpen:
+		case kvdb.ErrDatabaseNotOpen:
 			dbBob, err = channeldb.Open(dbBob.Path())
 			if err != nil {
 				return nil, errors.Errorf("unable to reopen bob "+

matheusd/dcrlnd

htlcswitch/circuit_map.go
@@ -7,10 +7,10 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/decred/dcrlnd/channeldb"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/htlcswitch/hop"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/go-errors/errors"
-	bbolt "go.etcd.io/bbolt"
 )
 
 var (
@@ -213,13 +213,13 @@ func NewCircuitMap(cfg *CircuitMapConfig) (CircuitMap, error) {
 // initBuckets ensures that the primary buckets used by the circuit are
 // initialized so that we can assume their existence after startup.
 func (cm *circuitMap) initBuckets() error {
-	return cm.cfg.DB.Update(func(tx *bbolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(circuitKeystoneKey)
+	return kvdb.Update(cm.cfg.DB, func(tx kvdb.RwTx) error {
+		_, err := tx.CreateTopLevelBucket(circuitKeystoneKey)
 		if err != nil {
 			return err
 		}
 
-		_, err = tx.CreateBucketIfNotExists(circuitAddKey)
+		_, err = tx.CreateTopLevelBucket(circuitAddKey)
 		return err
 	})
 }
@@ -238,10 +238,10 @@ func (cm *circuitMap) restoreMemState() error {
 		pending = make(map[CircuitKey]*PaymentCircuit)
 	)
 
-	if err := cm.cfg.DB.Update(func(tx *bbolt.Tx) error {
+	if err := kvdb.Update(cm.cfg.DB, func(tx kvdb.RwTx) error {
 		// Restore any of the circuits persisted in the circuit bucket
 		// back into memory.
-		circuitBkt := tx.Bucket(circuitAddKey)
+		circuitBkt := tx.ReadWriteBucket(circuitAddKey)
 		if circuitBkt == nil {
 			return ErrCorruptedCircuitMap
 		}
@@ -262,7 +262,7 @@ func (cm *circuitMap) restoreMemState() error {
 
 		// Furthermore, load the keystone bucket and resurrect the
 		// keystones used in any open circuits.
-		keystoneBkt := tx.Bucket(circuitKeystoneKey)
+		keystoneBkt := tx.ReadWriteBucket(circuitKeystoneKey)
 		if keystoneBkt == nil {
 			return ErrCorruptedCircuitMap
 		}
@@ -463,8 +463,8 @@ func (cm *circuitMap) TrimOpenCircuits(chanID lnwire.ShortChannelID,
 		return nil
 	}
 
-	return cm.cfg.DB.Update(func(tx *bbolt.Tx) error {
-		keystoneBkt := tx.Bucket(circuitKeystoneKey)
+	return kvdb.Update(cm.cfg.DB, func(tx kvdb.RwTx) error {
+		keystoneBkt := tx.ReadWriteBucket(circuitKeystoneKey)
 		if keystoneBkt == nil {
 			return ErrCorruptedCircuitMap
 		}
@@ -616,8 +616,8 @@ func (cm *circuitMap) CommitCircuits(circuits ...*PaymentCircuit) (
 	// Write the entire batch of circuits to the persistent circuit bucket
 	// using bbolt's Batch write. This method must be called from multiple,
 	// distinct goroutines to have any impact on performance.
-	err := cm.cfg.DB.Batch(func(tx *bbolt.Tx) error {
-		circuitBkt := tx.Bucket(circuitAddKey)
+	err := kvdb.Batch(cm.cfg.DB.Backend, func(tx kvdb.RwTx) error {
+		circuitBkt := tx.ReadWriteBucket(circuitAddKey)
 		if circuitBkt == nil {
 			return ErrCorruptedCircuitMap
 		}
@@ -706,10 +706,10 @@ func (cm *circuitMap) OpenCircuits(keystones ...Keystone) error {
 	}
 	cm.mtx.RUnlock()
 
-	err := cm.cfg.DB.Update(func(tx *bbolt.Tx) error {
+	err := kvdb.Update(cm.cfg.DB, func(tx kvdb.RwTx) error {
 		// Now, load the circuit bucket to which we will write the
 		// already serialized circuit.
-		keystoneBkt := tx.Bucket(circuitKeystoneKey)
+		keystoneBkt := tx.ReadWriteBucket(circuitKeystoneKey)
 		if keystoneBkt == nil {
 			return ErrCorruptedCircuitMap
 		}
@@ -847,13 +847,13 @@ func (cm *circuitMap) DeleteCircuits(inKeys ...CircuitKey) error {
 	}
 	cm.mtx.Unlock()
 
-	err := cm.cfg.DB.Batch(func(tx *bbolt.Tx) error {
+	err := kvdb.Batch(cm.cfg.DB.Backend, func(tx kvdb.RwTx) error {
 		for _, circuit := range removedCircuits {
 			// If this htlc made it to an outgoing link, load the
 			// keystone bucket from which we will remove the
 			// outgoing circuit key.
 			if circuit.HasKeystone() {
-				keystoneBkt := tx.Bucket(circuitKeystoneKey)
+				keystoneBkt := tx.ReadWriteBucket(circuitKeystoneKey)
 				if keystoneBkt == nil {
 					return ErrCorruptedCircuitMap
 				}
@@ -868,7 +868,7 @@ func (cm *circuitMap) DeleteCircuits(inKeys ...CircuitKey) error {
 
 			// Remove the circuit itself based on the incoming
 			// circuit key.
-			circuitBkt := tx.Bucket(circuitAddKey)
+			circuitBkt := tx.ReadWriteBucket(circuitAddKey)
 			if circuitBkt == nil {
 				return ErrCorruptedCircuitMap
 			}

htlcswitch/link_test.go
@@ -16,7 +16,6 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/go-errors/errors"
-	bbolt "go.etcd.io/bbolt"
 
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrec/secp256k1/v3"
@@ -26,6 +25,7 @@ import (
 
 	"github.com/decred/dcrlnd/build"
 	"github.com/decred/dcrlnd/channeldb"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/contractcourt"
 	"github.com/decred/dcrlnd/htlcswitch/hodl"
 	"github.com/decred/dcrlnd/htlcswitch/hop"
@@ -5155,32 +5155,32 @@ type mockPackager struct {
 	failLoadFwdPkgs bool
 }
 
-func (*mockPackager) AddFwdPkg(tx *bbolt.Tx, fwdPkg *channeldb.FwdPkg) error {
+func (*mockPackager) AddFwdPkg(tx kvdb.RwTx, fwdPkg *channeldb.FwdPkg) error {
 	return nil
 }
 
-func (*mockPackager) SetFwdFilter(tx *bbolt.Tx, height uint64,
+func (*mockPackager) SetFwdFilter(tx kvdb.RwTx, height uint64,
 	fwdFilter *channeldb.PkgFilter) error {
 	return nil
 }
 
-func (*mockPackager) AckAddHtlcs(tx *bbolt.Tx,
+func (*mockPackager) AckAddHtlcs(tx kvdb.RwTx,
 	addRefs ...channeldb.AddRef) error {
 	return nil
 }
 
-func (m *mockPackager) LoadFwdPkgs(tx *bbolt.Tx) ([]*channeldb.FwdPkg, error) {
+func (m *mockPackager) LoadFwdPkgs(tx kvdb.ReadTx) ([]*channeldb.FwdPkg, error) {
 	if m.failLoadFwdPkgs {
 		return nil, fmt.Errorf("failing LoadFwdPkgs")
 	}
 	return nil, nil
 }
 
-func (*mockPackager) RemovePkg(tx *bbolt.Tx, height uint64) error {
+func (*mockPackager) RemovePkg(tx kvdb.RwTx, height uint64) error {
 	return nil
 }
 
-func (*mockPackager) AckSettleFails(tx *bbolt.Tx,
+func (*mockPackager) AckSettleFails(tx kvdb.RwTx,
 	settleFailRefs ...channeldb.SettleFailRef) error {
 	return nil
 }

htlcswitch/payment_result.go
@@ -8,9 +8,9 @@ import (
 	"sync"
 
 	"github.com/decred/dcrlnd/channeldb"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/multimutex"
-	bbolt "go.etcd.io/bbolt"
 )
 
 var (
@@ -137,8 +137,8 @@ func (store *networkResultStore) storeResult(paymentID uint64,
 	var paymentIDBytes [8]byte
 	binary.BigEndian.PutUint64(paymentIDBytes[:], paymentID)
 
-	err := store.db.Batch(func(tx *bbolt.Tx) error {
-		networkResults, err := tx.CreateBucketIfNotExists(
+	err := kvdb.Batch(store.db.Backend, func(tx kvdb.RwTx) error {
+		networkResults, err := tx.CreateTopLevelBucket(
 			networkResultStoreBucketKey,
 		)
 		if err != nil {
@@ -180,7 +180,7 @@ func (store *networkResultStore) subscribeResult(paymentID uint64) (
 		resultChan = make(chan *networkResult, 1)
 	)
 
-	err := store.db.View(func(tx *bbolt.Tx) error {
+	err := kvdb.View(store.db, func(tx kvdb.ReadTx) error {
 		var err error
 		result, err = fetchResult(tx, paymentID)
 		switch {
@@ -226,7 +226,7 @@ func (store *networkResultStore) getResult(pid uint64) (
 	*networkResult, error) {
 
 	var result *networkResult
-	err := store.db.View(func(tx *bbolt.Tx) error {
+	err := kvdb.View(store.db, func(tx kvdb.ReadTx) error {
 		var err error
 		result, err = fetchResult(tx, pid)
 		return err
@@ -238,11 +238,11 @@ func (store *networkResultStore) getResult(pid uint64) (
 	return result, nil
 }
 
-func fetchResult(tx *bbolt.Tx, pid uint64) (*networkResult, error) {
+func fetchResult(tx kvdb.ReadTx, pid uint64) (*networkResult, error) {
 	var paymentIDBytes [8]byte
 	binary.BigEndian.PutUint64(paymentIDBytes[:], pid)
 
-	networkResults := tx.Bucket(networkResultStoreBucketKey)
+	networkResults := tx.ReadBucket(networkResultStoreBucketKey)
 	if networkResults == nil {
 		return nil, ErrPaymentIDNotFound
 	}

htlcswitch/sequencer.go
@@ -4,8 +4,8 @@ import (
 	"sync"
 
 	"github.com/decred/dcrlnd/channeldb"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/go-errors/errors"
-	bbolt "go.etcd.io/bbolt"
 )
 
 // defaultSequenceBatchSize specifies the window of sequence numbers that are
@@ -87,8 +87,8 @@ func (s *persistentSequencer) NextID() (uint64, error) {
 	// allocated will start from the last known tip on disk, which is fine
 	// as we only require uniqueness of the allocated numbers.
 	var nextHorizonID uint64
-	if err := s.db.Update(func(tx *bbolt.Tx) error {
-		nextIDBkt := tx.Bucket(nextPaymentIDKey)
+	if err := kvdb.Update(s.db, func(tx kvdb.RwTx) error {
+		nextIDBkt := tx.ReadWriteBucket(nextPaymentIDKey)
 		if nextIDBkt == nil {
 			return ErrSequencerCorrupted
 		}
@@ -121,8 +121,8 @@ func (s *persistentSequencer) NextID() (uint64, error) {
 
 // initDB populates the bucket used to generate payment sequence numbers.
 func (s *persistentSequencer) initDB() error {
-	return s.db.Update(func(tx *bbolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(nextPaymentIDKey)
+	return kvdb.Update(s.db, func(tx kvdb.RwTx) error {
+		_, err := tx.CreateTopLevelBucket(nextPaymentIDKey)
 		return err
 	})
 }

htlcswitch/switch.go
@@ -13,6 +13,7 @@ import (
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/chainntnfs"
 	"github.com/decred/dcrlnd/channeldb"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/contractcourt"
 	"github.com/decred/dcrlnd/htlcswitch/hop"
 	"github.com/decred/dcrlnd/lntypes"
@@ -20,7 +21,6 @@ import (
 	"github.com/decred/dcrlnd/lnwallet/chainfee"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/ticker"
-	bbolt "go.etcd.io/bbolt"
 )
 
 const (
@@ -1419,7 +1419,7 @@ func (s *Switch) closeCircuit(pkt *htlcPacket) (*PaymentCircuit, error) {
 // we're the originator of the payment, so the link stops attempting to
 // re-broadcast.
 func (s *Switch) ackSettleFail(settleFailRefs ...channeldb.SettleFailRef) error {
-	return s.cfg.DB.Batch(func(tx *bbolt.Tx) error {
+	return kvdb.Batch(s.cfg.DB.Backend, func(tx kvdb.RwTx) error {
 		return s.cfg.SwitchPackager.AckSettleFails(tx, settleFailRefs...)
 	})
 }
@@ -1481,7 +1481,7 @@ func (s *Switch) teardownCircuit(pkt *htlcPacket) error {
 
 // CloseLink creates and sends the close channel command to the target link
 // directing the specified closure type. If the closure type is CloseRegular,
-// targetFeePerKw parameter should be the ideal fee-per-kb that will be used as
+// targetFeePerKB parameter should be the ideal fee-per-kb that will be used as
 // a starting point for close negotiation. The deliveryScript parameter is an
 // optional parameter which sets a user specified script to close out to.
 func (s *Switch) CloseLink(chanPoint *wire.OutPoint,
@@ -1865,7 +1865,7 @@ func (s *Switch) reforwardResponses() error {
 func (s *Switch) loadChannelFwdPkgs(source lnwire.ShortChannelID) ([]*channeldb.FwdPkg, error) {
 
 	var fwdPkgs []*channeldb.FwdPkg
-	if err := s.cfg.DB.Update(func(tx *bbolt.Tx) error {
+	if err := kvdb.Update(s.cfg.DB, func(tx kvdb.RwTx) error {
 		var err error
 		fwdPkgs, err = s.cfg.SwitchPackager.LoadChannelFwdPkgs(
 			tx, source,

htlcswitch/test_utils.go
@@ -22,6 +22,7 @@ import (
 	"github.com/decred/dcrd/dcrutil/v2"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/channeldb"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/contractcourt"
 	"github.com/decred/dcrlnd/htlcswitch/hop"
 	"github.com/decred/dcrlnd/input"
@@ -36,7 +37,6 @@ import (
 	"github.com/decred/dcrlnd/ticker"
 	sphinx "github.com/decred/lightning-onion/v3"
 	"github.com/go-errors/errors"
-	bbolt "go.etcd.io/bbolt"
 )
 
 func modNScalar(b []byte) *secp256k1.ModNScalar {
@@ -425,7 +425,7 @@ func createTestChannel(alicePrivKey, bobPrivKey []byte,
 		aliceStoredChannels, err := dbAlice.FetchOpenChannels(aliceKeyPub)
 		switch err {
 		case nil:
-		case bbolt.ErrDatabaseNotOpen:
+		case kvdb.ErrDatabaseNotOpen:
 			dbAlice, err = channeldb.Open(dbAlice.Path())
 			if err != nil {
 				return nil, errors.Errorf("unable to reopen alice "+
@@ -469,7 +469,7 @@ func createTestChannel(alicePrivKey, bobPrivKey []byte,
 		bobStoredChannels, err := dbBob.FetchOpenChannels(bobKeyPub)
 		switch err {
 		case nil:
-		case bbolt.ErrDatabaseNotOpen:
+		case kvdb.ErrDatabaseNotOpen:
 			dbBob, err = channeldb.Open(dbBob.Path())
 			if err != nil {
 				return nil, errors.Errorf("unable to reopen bob "+