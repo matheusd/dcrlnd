lightningnetwork/lnd

lnwallet/channel: take remote commitment view into availableBalance calculation
Since our HTLC must also be added to the remote commitment, we do the
balance caluclation also from the remote chain perspective and report
our minimum balance from the two commit views as our available balance.

lnwallet/channel.go
@@ -6030,8 +6030,18 @@ func (lc *LightningChannel) availableBalance() (lnwire.MilliSatoshi, int64) {
 	// add updates concurrently, causing our balance to go down if we're
 	// the initiator, but this is a problem on the protocol level.
 	ourLocalCommitBalance, commitWeight := lc.availableCommitmentBalance(
-		htlcView,
+		htlcView, false,
 	)
+
+	// Do the same calculation from the remote commitment point of view.
+	ourRemoteCommitBalance, _ := lc.availableCommitmentBalance(
+		htlcView, true,
+	)
+
+	// Return which ever balance is lowest.
+	if ourRemoteCommitBalance < ourLocalCommitBalance {
+		return ourRemoteCommitBalance, commitWeight
+	}
 
 	return ourLocalCommitBalance, commitWeight
 }
@@ -6043,14 +6053,14 @@ func (lc *LightningChannel) availableBalance() (lnwire.MilliSatoshi, int64) {
 // commitment, increasing the commitment fee we must pay as an initiator,
 // eating into our balance. It will make sure we won't violate the channel
 // reserve constraints for this amount.
-func (lc *LightningChannel) availableCommitmentBalance(view *htlcView) (
-	lnwire.MilliSatoshi, int64) {
+func (lc *LightningChannel) availableCommitmentBalance(view *htlcView,
+	remoteChain bool) (lnwire.MilliSatoshi, int64) {
 
 	// Compute the current balances for this commitment. This will take
 	// into account HTLCs to determine the commit weight, which the
 	// initiator must pay the fee for.
 	ourBalance, theirBalance, commitWeight, filteredView, err := lc.computeView(
-		view, false, false,
+		view, remoteChain, false,
 	)
 	if err != nil {
 		lc.log.Errorf("Unable to fetch available balance: %v", err)
@@ -6117,6 +6127,17 @@ func (lc *LightningChannel) availableCommitmentBalance(view *htlcView) (
 	htlcFee := lnwire.NewMSatFromSatoshis(
 		htlcTimeoutFee(feePerKw),
 	)
+
+	// If we are looking at the remote commitment, we must use the remote
+	// dust limit and the fee for adding an HTLC success transaction.
+	if remoteChain {
+		dustlimit = lnwire.NewMSatFromSatoshis(
+			lc.channelState.RemoteChanCfg.DustLimit,
+		)
+		htlcFee = lnwire.NewMSatFromSatoshis(
+			htlcSuccessFee(feePerKw),
+		)
+	}
 
 	// The HTLC output will be manifested on the commitment if it
 	// is non-dust after paying the HTLC fee.

lnwallet/channel_test.go
@@ -4749,9 +4749,6 @@ func TestChanAvailableBalanceNearHtlcFee(t *testing.T) {
 	aliceDustlimit := lnwire.NewMSatFromSatoshis(
 		aliceChannel.channelState.LocalChanCfg.DustLimit,
 	)
-	bobDustlimit := lnwire.NewMSatFromSatoshis(
-		bobChannel.channelState.LocalChanCfg.DustLimit,
-	)
 	feeRate := chainfee.SatPerKWeight(
 		aliceChannel.channelState.LocalCommitment.FeePerKw,
 	)
@@ -4763,6 +4760,9 @@ func TestChanAvailableBalanceNearHtlcFee(t *testing.T) {
 	)
 	htlcTimeoutFee := lnwire.NewMSatFromSatoshis(
 		htlcTimeoutFee(feeRate),
+	)
+	htlcSuccessFee := lnwire.NewMSatFromSatoshis(
+		htlcSuccessFee(feeRate),
 	)
 
 	// Helper method to check the current reported balance.
@@ -4889,7 +4889,12 @@ func TestChanAvailableBalanceNearHtlcFee(t *testing.T) {
 	// HTLC from Bob to the commitment transaction. Bob's balance should
 	// reflect this, by only reporting dust amount being available. Alice
 	// should still report a zero balance.
-	bobNonDustHtlc := bobDustlimit + htlcTimeoutFee
+
+	// Since the dustlimit is different for the two commitments, the
+	// largest HTLC Bob can send that Alice can afford on both commitments
+	// (remember she cannot afford to pay the HTLC fee) is the largest dust
+	// HTLC on Alice's commitemnt, since her dust limit is lower.
+	bobNonDustHtlc := aliceDustlimit + htlcSuccessFee
 	expBobBalance = bobNonDustHtlc - 1
 	expAliceBalance = 0
 	checkBalance(t, expAliceBalance, expBobBalance)

matheusd/dcrlnd

lnwallet/channel.go
@@ -6050,8 +6050,18 @@ func (lc *LightningChannel) availableBalance() (lnwire.MilliAtom, int64) {
 	// add updates concurrently, causing our balance to go down if we're
 	// the initiator, but this is a problem on the protocol level.
 	ourLocalCommitBalance, commitSize := lc.availableCommitmentBalance(
-		htlcView,
+		htlcView, false,
 	)
+
+	// Do the same calculation from the remote commitment point of view.
+	ourRemoteCommitBalance, _ := lc.availableCommitmentBalance(
+		htlcView, true,
+	)
+
+	// Return which ever balance is lowest.
+	if ourRemoteCommitBalance < ourLocalCommitBalance {
+		return ourRemoteCommitBalance, commitSize
+	}
 
 	return ourLocalCommitBalance, commitSize
 }
@@ -6063,14 +6073,14 @@ func (lc *LightningChannel) availableBalance() (lnwire.MilliAtom, int64) {
 // commitment, increasing the commitment fee we must pay as an initiator,
 // eating into our balance. It will make sure we won't violate the channel
 // reserve constraints for this amount.
-func (lc *LightningChannel) availableCommitmentBalance(view *htlcView) (
-	lnwire.MilliAtom, int64) {
+func (lc *LightningChannel) availableCommitmentBalance(view *htlcView,
+	remoteChain bool) (lnwire.MilliAtom, int64) {
 
 	// Compute the current balances for this commitment. This will take
 	// into account HTLCs to determine the commit weight, which the
 	// initiator must pay the fee for.
 	ourBalance, theirBalance, commitSize, filteredView, err := lc.computeView(
-		view, false, false,
+		view, remoteChain, false,
 	)
 	if err != nil {
 		lc.log.Errorf("Unable to fetch available balance: %v", err)
@@ -6137,6 +6147,17 @@ func (lc *LightningChannel) availableCommitmentBalance(view *htlcView) (
 	htlcFee := lnwire.NewMAtomsFromAtoms(
 		htlcTimeoutFee(feePerKB),
 	)
+
+	// If we are looking at the remote commitment, we must use the remote
+	// dust limit and the fee for adding an HTLC success transaction.
+	if remoteChain {
+		dustlimit = lnwire.NewMAtomsFromAtoms(
+			lc.channelState.RemoteChanCfg.DustLimit,
+		)
+		htlcFee = lnwire.NewMAtomsFromAtoms(
+			htlcSuccessFee(feePerKB),
+		)
+	}
 
 	// The HTLC output will be manifested on the commitment if it
 	// is non-dust after paying the HTLC fee.

lnwallet/channel_test.go
@@ -4818,9 +4818,6 @@ func TestChanAvailableBalanceNearHtlcFee(t *testing.T) {
 	aliceDustlimit := lnwire.NewMAtomsFromAtoms(
 		aliceChannel.channelState.LocalChanCfg.DustLimit,
 	)
-	bobDustlimit := lnwire.NewMAtomsFromAtoms(
-		bobChannel.channelState.LocalChanCfg.DustLimit,
-	)
 	feeRate := chainfee.AtomPerKByte(
 		aliceChannel.channelState.LocalCommitment.FeePerKB,
 	)
@@ -4832,6 +4829,9 @@ func TestChanAvailableBalanceNearHtlcFee(t *testing.T) {
 	)
 	htlcTimeoutFee := lnwire.NewMAtomsFromAtoms(
 		htlcTimeoutFee(feeRate),
+	)
+	htlcSuccessFee := lnwire.NewMAtomsFromAtoms(
+		htlcSuccessFee(feeRate),
 	)
 
 	// Helper method to check the current reported balance.
@@ -4958,7 +4958,12 @@ func TestChanAvailableBalanceNearHtlcFee(t *testing.T) {
 	// HTLC from Bob to the commitment transaction. Bob's balance should
 	// reflect this, by only reporting dust amount being available. Alice
 	// should still report a zero balance.
-	bobNonDustHtlc := bobDustlimit + htlcTimeoutFee
+
+	// Since the dustlimit is different for the two commitments, the
+	// largest HTLC Bob can send that Alice can afford on both commitments
+	// (remember she cannot afford to pay the HTLC fee) is the largest dust
+	// HTLC on Alice's commitemnt, since her dust limit is lower.
+	bobNonDustHtlc := aliceDustlimit + htlcSuccessFee
 	expBobBalance = bobNonDustHtlc - 1
 	expAliceBalance = 0
 	checkBalance(t, expAliceBalance, expBobBalance)