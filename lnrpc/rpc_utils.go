lightningnetwork/lnd

walletrpc: add list sweeps to rpc and cli

lnrpc/rpc_utils.go
@@ -1,0 +1,53 @@
+package lnrpc
+
+import (
+	"encoding/hex"
+	"sort"
+
+	"github.com/lightningnetwork/lnd/lnwallet"
+)
+
+// RPCTransactionDetails returns a set of rpc transaction details.
+func RPCTransactionDetails(txns []*lnwallet.TransactionDetail) *TransactionDetails {
+	txDetails := &TransactionDetails{
+		Transactions: make([]*Transaction, len(txns)),
+	}
+
+	for i, tx := range txns {
+		var destAddresses []string
+		for _, destAddress := range tx.DestAddresses {
+			destAddresses = append(destAddresses, destAddress.EncodeAddress())
+		}
+
+		// We also get unconfirmed transactions, so BlockHash can be
+		// nil.
+		blockHash := ""
+		if tx.BlockHash != nil {
+			blockHash = tx.BlockHash.String()
+		}
+
+		txDetails.Transactions[i] = &Transaction{
+			TxHash:           tx.Hash.String(),
+			Amount:           int64(tx.Value),
+			NumConfirmations: tx.NumConfirmations,
+			BlockHash:        blockHash,
+			BlockHeight:      tx.BlockHeight,
+			TimeStamp:        tx.Timestamp,
+			TotalFees:        tx.TotalFees,
+			DestAddresses:    destAddresses,
+			RawTxHex:         hex.EncodeToString(tx.RawTx),
+		}
+	}
+
+	// Sort transactions by number of confirmations rather than height so
+	// that unconfirmed transactions (height =0; confirmations =-1) will
+	// follow the most recently set of confirmed transactions. If we sort
+	// by height, unconfirmed transactions will follow our oldest
+	// transactions, because they have lower block heights.
+	sort.Slice(txDetails.Transactions, func(i, j int) bool {
+		return txDetails.Transactions[i].NumConfirmations <
+			txDetails.Transactions[j].NumConfirmations
+	})
+
+	return txDetails
+}

lnrpc/walletrpc/walletkit_server.go
@@ -19,6 +19,7 @@ import (
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lnrpc/signrpc"
 	"github.com/lightningnetwork/lnd/lnwallet"
+	"github.com/lightningnetwork/lnd/lnwallet/btcwallet"
 	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 	"github.com/lightningnetwork/lnd/sweep"
 	"google.golang.org/grpc"
@@ -87,6 +88,10 @@ var (
 		"/walletrpc.WalletKit/BumpFee": {{
 			Entity: "onchain",
 			Action: "write",
+		}},
+		"/walletrpc.WalletKit/ListSweeps": {{
+			Entity: "onchain",
+			Action: "read",
 		}},
 	}
 
@@ -554,4 +559,68 @@ func (w *WalletKit) BumpFee(ctx context.Context,
 	}
 
 	return &BumpFeeResponse{}, nil
+}
+
+// ListSweeps returns a list of the sweeps that our node has published.
+func (w *WalletKit) ListSweeps(ctx context.Context,
+	in *ListSweepsRequest) (*ListSweepsResponse, error) {
+
+	sweeps, err := w.cfg.Sweeper.ListSweeps()
+	if err != nil {
+		return nil, err
+	}
+
+	sweepTxns := make(map[string]bool)
+
+	txids := make([]string, len(sweeps))
+	for i, sweep := range sweeps {
+		sweepTxns[sweep.String()] = true
+		txids[i] = sweep.String()
+	}
+
+	// If the caller does not want verbose output, just return the set of
+	// sweep txids.
+	if !in.Verbose {
+		txidResp := &ListSweepsResponse_TransactionIDs{
+			TransactionIds: txids,
+		}
+
+		return &ListSweepsResponse{
+			Sweeps: &ListSweepsResponse_TransactionIds{
+				TransactionIds: txidResp,
+			},
+		}, nil
+	}
+
+	// If the caller does want full transaction lookups, query our wallet
+	// for all transactions, including unconfirmed transactions.
+	transactions, err := w.cfg.Wallet.ListTransactionDetails(
+		0, btcwallet.UnconfirmedHeight,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	var sweepTxDetails []*lnwallet.TransactionDetail
+	for _, tx := range transactions {
+		_, ok := sweepTxns[tx.Hash.String()]
+		if !ok {
+			continue
+		}
+
+		sweepTxDetails = append(sweepTxDetails, tx)
+	}
+
+	// Fail if we have not retrieved all of our sweep transactions from the
+	// wallet.
+	if len(sweepTxDetails) != len(txids) {
+		return nil, fmt.Errorf("not all sweeps found by list "+
+			"transactions: %v, %v", len(sweepTxDetails), len(txids))
+	}
+
+	return &ListSweepsResponse{
+		Sweeps: &ListSweepsResponse_TransactionDetails{
+			TransactionDetails: lnrpc.RPCTransactionDetails(transactions),
+		},
+	}, nil
 }

matheusd/dcrlnd

lnrpc/rpc_utils.go
@@ -1,0 +1,53 @@
+package lnrpc
+
+import (
+	"encoding/hex"
+	"sort"
+
+	"github.com/decred/dcrlnd/lnwallet"
+)
+
+// RPCTransactionDetails returns a set of rpc transaction details.
+func RPCTransactionDetails(txns []*lnwallet.TransactionDetail) *TransactionDetails {
+	txDetails := &TransactionDetails{
+		Transactions: make([]*Transaction, len(txns)),
+	}
+
+	for i, tx := range txns {
+		var destAddresses []string
+		for _, destAddress := range tx.DestAddresses {
+			destAddresses = append(destAddresses, destAddress.Address())
+		}
+
+		// We also get unconfirmed transactions, so BlockHash can be
+		// nil.
+		blockHash := ""
+		if tx.BlockHash != nil {
+			blockHash = tx.BlockHash.String()
+		}
+
+		txDetails.Transactions[i] = &Transaction{
+			TxHash:           tx.Hash.String(),
+			Amount:           int64(tx.Value),
+			NumConfirmations: tx.NumConfirmations,
+			BlockHash:        blockHash,
+			BlockHeight:      tx.BlockHeight,
+			TimeStamp:        tx.Timestamp,
+			TotalFees:        tx.TotalFees,
+			DestAddresses:    destAddresses,
+			RawTxHex:         hex.EncodeToString(tx.RawTx),
+		}
+	}
+
+	// Sort transactions by number of confirmations rather than height so
+	// that unconfirmed transactions (height =0; confirmations =-1) will
+	// follow the most recently set of confirmed transactions. If we sort
+	// by height, unconfirmed transactions will follow our oldest
+	// transactions, because they have lower block heights.
+	sort.Slice(txDetails.Transactions, func(i, j int) bool {
+		return txDetails.Transactions[i].NumConfirmations <
+			txDetails.Transactions[j].NumConfirmations
+	})
+
+	return txDetails
+}

lnrpc/walletrpc/walletkit_server.go
@@ -20,6 +20,7 @@ import (
 	"github.com/decred/dcrlnd/lnrpc/signrpc"
 	"github.com/decred/dcrlnd/lnwallet"
 	"github.com/decred/dcrlnd/lnwallet/chainfee"
+	"github.com/decred/dcrlnd/lnwallet/dcrwallet"
 	"github.com/decred/dcrlnd/sweep"
 	"google.golang.org/grpc"
 	"gopkg.in/macaroon-bakery.v2/bakery"
@@ -87,6 +88,10 @@ var (
 		"/walletrpc.WalletKit/BumpFee": {{
 			Entity: "onchain",
 			Action: "write",
+		}},
+		"/walletrpc.WalletKit/ListSweeps": {{
+			Entity: "onchain",
+			Action: "read",
 		}},
 	}
 
@@ -558,4 +563,68 @@ func (w *WalletKit) BumpFee(ctx context.Context,
 	}
 
 	return &BumpFeeResponse{}, nil
+}
+
+// ListSweeps returns a list of the sweeps that our node has published.
+func (w *WalletKit) ListSweeps(ctx context.Context,
+	in *ListSweepsRequest) (*ListSweepsResponse, error) {
+
+	sweeps, err := w.cfg.Sweeper.ListSweeps()
+	if err != nil {
+		return nil, err
+	}
+
+	sweepTxns := make(map[string]bool)
+
+	txids := make([]string, len(sweeps))
+	for i, sweep := range sweeps {
+		sweepTxns[sweep.String()] = true
+		txids[i] = sweep.String()
+	}
+
+	// If the caller does not want verbose output, just return the set of
+	// sweep txids.
+	if !in.Verbose {
+		txidResp := &ListSweepsResponse_TransactionIDs{
+			TransactionIds: txids,
+		}
+
+		return &ListSweepsResponse{
+			Sweeps: &ListSweepsResponse_TransactionIds{
+				TransactionIds: txidResp,
+			},
+		}, nil
+	}
+
+	// If the caller does want full transaction lookups, query our wallet
+	// for all transactions, including unconfirmed transactions.
+	transactions, err := w.cfg.Wallet.ListTransactionDetails(
+		0, dcrwallet.UnconfirmedHeight,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	var sweepTxDetails []*lnwallet.TransactionDetail
+	for _, tx := range transactions {
+		_, ok := sweepTxns[tx.Hash.String()]
+		if !ok {
+			continue
+		}
+
+		sweepTxDetails = append(sweepTxDetails, tx)
+	}
+
+	// Fail if we have not retrieved all of our sweep transactions from the
+	// wallet.
+	if len(sweepTxDetails) != len(txids) {
+		return nil, fmt.Errorf("not all sweeps found by list "+
+			"transactions: %v, %v", len(sweepTxDetails), len(txids))
+	}
+
+	return &ListSweepsResponse{
+		Sweeps: &ListSweepsResponse_TransactionDetails{
+			TransactionDetails: lnrpc.RPCTransactionDetails(transactions),
+		},
+	}, nil
 }