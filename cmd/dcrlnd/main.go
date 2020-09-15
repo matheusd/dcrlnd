lightningnetwork/lnd

lnd+cmd: move interrupt into cmd
If the main package is used as a library, we don't want it to
register interrupt signals itself. Rather we want to pass in the
shutdown channel manually. We do this in the cmd now.

cmd/lnd/main.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"os"
 
-	flags "github.com/jessevdk/go-flags"
+	"github.com/jessevdk/go-flags"
 	"github.com/lightningnetwork/lnd"
+	"github.com/lightningnetwork/lnd/signal"
 )
 
 func main() {
@@ -17,9 +18,15 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Hook interceptor for os signals.
+	signal.Intercept()
+
 	// Call the "real" main in a nested manner so the defers will properly
 	// be executed in the case of a graceful shutdown.
-	if err := lnd.Main(loadedConfig, lnd.ListenerCfg{}); err != nil {
+	err = lnd.Main(
+		loadedConfig, lnd.ListenerCfg{}, signal.ShutdownChannel(),
+	)
+	if err != nil {
 		if e, ok := err.(*flags.Error); ok && e.Type == flags.ErrHelp {
 		} else {
 			_, _ = fmt.Fprintln(os.Stderr, err)

matheusd/dcrlnd

cmd/dcrlnd/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/decred/dcrlnd"
+	"github.com/decred/dcrlnd/signal"
 	flags "github.com/jessevdk/go-flags"
 )
 
@@ -17,9 +18,15 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Hook interceptor for os signals.
+	signal.Intercept()
+
 	// Call the "real" main in a nested manner so the defers will properly
 	// be executed in the case of a graceful shutdown.
-	if err := dcrlnd.Main(loadedConfig, dcrlnd.ListenerCfg{}); err != nil {
+	err = dcrlnd.Main(
+		loadedConfig, dcrlnd.ListenerCfg{}, signal.ShutdownChannel(),
+	)
+	if err != nil {
 		if e, ok := err.(*flags.Error); ok && e.Type == flags.ErrHelp {
 		} else {
 			_, _ = fmt.Fprintln(os.Stderr, err)