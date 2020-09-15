lightningnetwork/lnd

lnd+watchtower: rename NodePrivKey to NodeKeyECDH

watchtower/config.go
@@ -63,9 +63,9 @@ type Config struct {
 	// successfully sent funds can be received.
 	NewAddress func() (btcutil.Address, error)
 
-	// NodePrivKey is private key to be used in accepting new brontide
-	// connections.
-	NodePrivKey *btcec.PrivateKey
+	// NodeKeyECDH is the ECDH capable wrapper of the key to be used in
+	// accepting new brontide connections.
+	NodeKeyECDH *btcec.PrivateKey
 
 	// PublishTx provides the ability to send a signed transaction to the
 	// network.

watchtower/wtserver/server.go
@@ -33,9 +33,9 @@ type Config struct {
 	// storing state updates.
 	DB DB
 
-	// NodePrivKey is private key to be used in accepting new brontide
-	// connections.
-	NodePrivKey *btcec.PrivateKey
+	// NodeKeyECDH is the the ECDH capable wrapper of the key to be used in
+	// accepting new brontide connections.
+	NodeKeyECDH *btcec.PrivateKey
 
 	// Listeners specifies which address to which clients may connect.
 	Listeners []net.Listener

matheusd/dcrlnd

watchtower/config.go
@@ -68,9 +68,9 @@ type Config struct {
 	// successfully sent funds can be received.
 	NewAddress func() (dcrutil.Address, error)
 
-	// NodePrivKey is private key to be used in accepting new brontide
-	// connections.
-	NodePrivKey *secp256k1.PrivateKey
+	// NodeKeyECDH is the ECDH capable wrapper of the key to be used in
+	// accepting new brontide connections.
+	NodeKeyECDH *secp256k1.PrivateKey
 
 	// PublishTx provides the ability to send a signed transaction to the
 	// network.

watchtower/wtserver/server.go
@@ -33,9 +33,9 @@ type Config struct {
 	// storing state updates.
 	DB DB
 
-	// NodePrivKey is private key to be used in accepting new brontide
-	// connections.
-	NodePrivKey *secp256k1.PrivateKey
+	// NodeKeyECDH is the the ECDH capable wrapper of the key to be used in
+	// accepting new brontide connections.
+	NodeKeyECDH *secp256k1.PrivateKey
 
 	// Listeners specifies which address to which clients may connect.
 	Listeners []net.Listener