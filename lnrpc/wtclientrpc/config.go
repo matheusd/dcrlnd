lightningnetwork/lnd

rpc: use existing rpc logger for wtclientrpc
The logger string used to identify the wtclient and wtclientrpc loggers
was the same, leading to being unable to modify the log level of the
wtclient logger as it would be overwritten with the wtclientrpc's one.
To simplify things, we decide to use the existing RPC logger for
wtclientrpc.

lnrpc/wtclientrpc/config.go
@@ -1,6 +1,7 @@
 package wtclientrpc
 
 import (
+	"github.com/btcsuite/btclog"
 	"github.com/lightningnetwork/lnd/lncfg"
 	"github.com/lightningnetwork/lnd/watchtower/wtclient"
 )
@@ -22,4 +23,7 @@ type Config struct {
 	// addresses to ensure we don't leak any information when running over
 	// non-clear networks, e.g. Tor, etc.
 	Resolver lncfg.TCPResolver
+
+	// Log is the logger instance we should log output to.
+	Log btclog.Logger
 }

subrpcserver_config.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btclog"
 	"github.com/lightningnetwork/lnd/autopilot"
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/htlcswitch"
@@ -94,7 +95,8 @@ func (s *subRPCServerConfigs) PopulateDependencies(cc *chainControl,
 	tower *watchtower.Standalone,
 	towerClient wtclient.Client,
 	tcpResolver lncfg.TCPResolver,
-	genInvoiceFeatures func() *lnwire.FeatureVector) error {
+	genInvoiceFeatures func() *lnwire.FeatureVector,
+	rpcLogger btclog.Logger) error {
 
 	// First, we'll use reflect to obtain a version of the config struct
 	// that allows us to programmatically inspect its fields.
@@ -243,6 +245,9 @@ func (s *subRPCServerConfigs) PopulateDependencies(cc *chainControl,
 			}
 			subCfgValue.FieldByName("Resolver").Set(
 				reflect.ValueOf(tcpResolver),
+			)
+			subCfgValue.FieldByName("Log").Set(
+				reflect.ValueOf(rpcLogger),
 			)
 
 		default:

matheusd/dcrlnd

lnrpc/wtclientrpc/config.go
@@ -3,6 +3,7 @@ package wtclientrpc
 import (
 	"github.com/decred/dcrlnd/lncfg"
 	"github.com/decred/dcrlnd/watchtower/wtclient"
+	"github.com/decred/slog"
 )
 
 // Config is the primary configuration struct for the watchtower RPC server. It
@@ -22,4 +23,7 @@ type Config struct {
 	// addresses to ensure we don't leak any information when running over
 	// non-clear networks, e.g. Tor, etc.
 	Resolver lncfg.TCPResolver
+
+	// Log is the logger instance we should log output to.
+	Log slog.Logger
 }

subrpcserver_config.go
@@ -25,6 +25,7 @@ import (
 	"github.com/decred/dcrlnd/sweep"
 	"github.com/decred/dcrlnd/watchtower"
 	"github.com/decred/dcrlnd/watchtower/wtclient"
+	"github.com/decred/slog"
 )
 
 // subRPCServerConfigs is special sub-config in the main configuration that
@@ -94,7 +95,8 @@ func (s *subRPCServerConfigs) PopulateDependencies(cc *chainControl,
 	tower *watchtower.Standalone,
 	towerClient wtclient.Client,
 	tcpResolver lncfg.TCPResolver,
-	genInvoiceFeatures func() *lnwire.FeatureVector) error {
+	genInvoiceFeatures func() *lnwire.FeatureVector,
+	rpcLogger slog.Logger) error {
 
 	// First, we'll use reflect to obtain a version of the config struct
 	// that allows us to programmatically inspect its fields.
@@ -240,6 +242,9 @@ func (s *subRPCServerConfigs) PopulateDependencies(cc *chainControl,
 			}
 			subCfgValue.FieldByName("Resolver").Set(
 				reflect.ValueOf(tcpResolver),
+			)
+			subCfgValue.FieldByName("Log").Set(
+				reflect.ValueOf(rpcLogger),
 			)
 
 		default: