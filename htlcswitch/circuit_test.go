lightningnetwork/lnd

server+mod: use ECDH interface for sphinx router

htlcswitch/circuit_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/htlcswitch"
 	"github.com/lightningnetwork/lnd/htlcswitch/hop"
+	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lnwire"
 )
 
@@ -84,7 +85,8 @@ func initTestExtracter() {
 // db and no garbage collection.
 func newOnionProcessor(t *testing.T) *hop.OnionProcessor {
 	sphinxRouter := sphinx.NewRouter(
-		sphinxPrivKey, &bitcoinCfg.SimNetParams, sphinx.NewMemoryReplayLog(),
+		&keychain.PrivKeyECDH{PrivKey: sphinxPrivKey},
+		&bitcoinCfg.SimNetParams, sphinx.NewMemoryReplayLog(),
 	)
 
 	if err := sphinxRouter.Start(); err != nil {

matheusd/dcrlnd

htlcswitch/circuit_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/htlcswitch"
 	"github.com/decred/dcrlnd/htlcswitch/hop"
+	"github.com/decred/dcrlnd/keychain"
 	"github.com/decred/dcrlnd/lnwire"
 	sphinx "github.com/decred/lightning-onion/v3"
 )
@@ -84,7 +85,8 @@ func initTestExtracter() {
 // db and no garbage collection.
 func newOnionProcessor(t *testing.T) *hop.OnionProcessor {
 	sphinxRouter := sphinx.NewRouter(
-		sphinxPrivKey, chaincfg.SimNetParams(),
+		&keychain.PrivKeyECDH{PrivKey: sphinxPrivKey},
+		chaincfg.SimNetParams(),
 		sphinx.NewMemoryReplayLog(),
 	)
 