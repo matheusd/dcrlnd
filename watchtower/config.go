lightningnetwork/lnd

watchtower: use ECDH interface for watchtower session

watchtower/config.go
@@ -5,10 +5,10 @@ import (
 	"net"
 	"time"
 
-	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
+	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/tor"
 	"github.com/lightningnetwork/lnd/watchtower/lookout"
 )
@@ -65,7 +65,7 @@ type Config struct {
 
 	// NodeKeyECDH is the ECDH capable wrapper of the key to be used in
 	// accepting new brontide connections.
-	NodeKeyECDH *btcec.PrivateKey
+	NodeKeyECDH keychain.SingleKeyECDH
 
 	// PublishTx provides the ability to send a signed transaction to the
 	// network.

watchtower/wtclient/session_negotiator.go
@@ -5,8 +5,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/watchtower/blob"
 	"github.com/lightningnetwork/lnd/watchtower/wtdb"
@@ -58,7 +58,8 @@ type NegotiatorConfig struct {
 	// Dial initiates an outbound brontide connection to the given address
 	// using a specified private key. The peer is returned in the event of a
 	// successful connection.
-	Dial func(*btcec.PrivateKey, *lnwire.NetAddress) (wtserver.Peer, error)
+	Dial func(keychain.SingleKeyECDH, *lnwire.NetAddress) (wtserver.Peer,
+		error)
 
 	// SendMessage writes a wtwire message to remote peer.
 	SendMessage func(wtserver.Peer, wtwire.Message) error
@@ -315,13 +316,21 @@ func (n *sessionNegotiator) createSession(tower *wtdb.Tower,
 		return ErrNoTowerAddrs
 	}
 
-	sessionPriv, err := DeriveSessionKey(n.cfg.SecretKeyRing, keyIndex)
+	sessionKeyDesc, err := n.cfg.SecretKeyRing.DeriveKey(
+		keychain.KeyLocator{
+			Family: keychain.KeyFamilyTowerSession,
+			Index:  keyIndex,
+		},
+	)
 	if err != nil {
 		return err
 	}
+	sessionKey := keychain.NewPubKeyECDH(
+		sessionKeyDesc, n.cfg.SecretKeyRing,
+	)
 
 	for _, lnAddr := range tower.LNAddrs() {
-		err = n.tryAddress(sessionPriv, keyIndex, tower, lnAddr)
+		err := n.tryAddress(sessionKey, keyIndex, tower, lnAddr)
 		switch {
 		case err == ErrPermanentTowerFailure:
 			// TODO(conner): report to iterator? can then be reset
@@ -346,11 +355,11 @@ func (n *sessionNegotiator) createSession(tower *wtdb.Tower,
 // The address should belong to the tower's set of addresses. This method only
 // returns true if all steps succeed and the new session has been persisted, and
 // fails otherwise.
-func (n *sessionNegotiator) tryAddress(privKey *btcec.PrivateKey,
+func (n *sessionNegotiator) tryAddress(sessionKey keychain.SingleKeyECDH,
 	keyIndex uint32, tower *wtdb.Tower, lnAddr *lnwire.NetAddress) error {
 
 	// Connect to the tower address using our generated session key.
-	conn, err := n.cfg.Dial(privKey, lnAddr)
+	conn, err := n.cfg.Dial(sessionKey, lnAddr)
 	if err != nil {
 		return err
 	}
@@ -417,9 +426,7 @@ func (n *sessionNegotiator) tryAddress(privKey *btcec.PrivateKey,
 		// TODO(conner): validate reward address
 		rewardPkScript := createSessionReply.Data
 
-		sessionID := wtdb.NewSessionIDFromPubKey(
-			privKey.PubKey(),
-		)
+		sessionID := wtdb.NewSessionIDFromPubKey(sessionKey.PubKey())
 		clientSession := &wtdb.ClientSession{
 			ClientSessionBody: wtdb.ClientSessionBody{
 				TowerID:        tower.ID,
@@ -428,7 +435,7 @@ func (n *sessionNegotiator) tryAddress(privKey *btcec.PrivateKey,
 				RewardPkScript: rewardPkScript,
 			},
 			Tower:          tower,
-			SessionKeyECDH: privKey,
+			SessionKeyECDH: sessionKey,
 			ID:             sessionID,
 		}
 

watchtower/wtclient/session_queue.go
@@ -6,9 +6,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/lightningnetwork/lnd/input"
+	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/watchtower/wtdb"
 	"github.com/lightningnetwork/lnd/watchtower/wtserver"
@@ -41,8 +41,8 @@ type sessionQueueConfig struct {
 
 	// Dial allows the client to dial the tower using it's public key and
 	// net address.
-	Dial func(*btcec.PrivateKey,
-		*lnwire.NetAddress) (wtserver.Peer, error)
+	Dial func(keychain.SingleKeyECDH, *lnwire.NetAddress) (wtserver.Peer,
+		error)
 
 	// SendMessage encodes, encrypts, and writes a message to the given peer.
 	SendMessage func(wtserver.Peer, wtwire.Message) error

watchtower/wtdb/client_session.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/btcsuite/btcd/btcec"
+	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/watchtower/blob"
 	"github.com/lightningnetwork/lnd/watchtower/wtpolicy"
@@ -65,7 +65,7 @@ type ClientSession struct {
 	//
 	// NOTE: This value is not serialized. It is derived using the KeyIndex
 	// on startup to avoid storing private keys on disk.
-	SessionKeyECDH *btcec.PrivateKey
+	SessionKeyECDH keychain.SingleKeyECDH
 }
 
 // ClientSessionBody represents the primary components of a ClientSession that

watchtower/wtserver/server.go
@@ -8,10 +8,10 @@ import (
 	"sync"
 	"time"
 
-	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/connmgr"
 	"github.com/btcsuite/btcutil"
+	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/watchtower/wtdb"
 	"github.com/lightningnetwork/lnd/watchtower/wtwire"
@@ -35,7 +35,7 @@ type Config struct {
 
 	// NodeKeyECDH is the the ECDH capable wrapper of the key to be used in
 	// accepting new brontide connections.
-	NodeKeyECDH *btcec.PrivateKey
+	NodeKeyECDH keychain.SingleKeyECDH
 
 	// Listeners specifies which address to which clients may connect.
 	Listeners []net.Listener

matheusd/dcrlnd

watchtower/config.go
@@ -7,9 +7,9 @@ import (
 
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/chaincfg/v3"
-	"github.com/decred/dcrd/dcrec/secp256k1/v3"
 	"github.com/decred/dcrd/dcrutil/v3"
 	"github.com/decred/dcrd/wire"
+	"github.com/decred/dcrlnd/keychain"
 	"github.com/decred/dcrlnd/tor"
 	"github.com/decred/dcrlnd/watchtower/lookout"
 )
@@ -70,7 +70,7 @@ type Config struct {
 
 	// NodeKeyECDH is the ECDH capable wrapper of the key to be used in
 	// accepting new brontide connections.
-	NodeKeyECDH *secp256k1.PrivateKey
+	NodeKeyECDH keychain.SingleKeyECDH
 
 	// PublishTx provides the ability to send a signed transaction to the
 	// network.

watchtower/wtclient/session_negotiator.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/decred/dcrd/chaincfg/chainhash"
-	"github.com/decred/dcrd/dcrec/secp256k1/v3"
+	"github.com/decred/dcrlnd/keychain"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/watchtower/blob"
 	"github.com/decred/dcrlnd/watchtower/wtdb"
@@ -58,7 +58,8 @@ type NegotiatorConfig struct {
 	// Dial initiates an outbound brontide connection to the given address
 	// using a specified private key. The peer is returned in the event of a
 	// successful connection.
-	Dial func(*secp256k1.PrivateKey, *lnwire.NetAddress) (wtserver.Peer, error)
+	Dial func(keychain.SingleKeyECDH, *lnwire.NetAddress) (wtserver.Peer,
+		error)
 
 	// SendMessage writes a wtwire message to remote peer.
 	SendMessage func(wtserver.Peer, wtwire.Message) error
@@ -315,13 +316,21 @@ func (n *sessionNegotiator) createSession(tower *wtdb.Tower,
 		return ErrNoTowerAddrs
 	}
 
-	sessionPriv, err := DeriveSessionKey(n.cfg.SecretKeyRing, keyIndex)
+	sessionKeyDesc, err := n.cfg.SecretKeyRing.DeriveKey(
+		keychain.KeyLocator{
+			Family: keychain.KeyFamilyTowerSession,
+			Index:  keyIndex,
+		},
+	)
 	if err != nil {
 		return err
 	}
+	sessionKey := keychain.NewPubKeyECDH(
+		sessionKeyDesc, n.cfg.SecretKeyRing,
+	)
 
 	for _, lnAddr := range tower.LNAddrs() {
-		err = n.tryAddress(sessionPriv, keyIndex, tower, lnAddr)
+		err := n.tryAddress(sessionKey, keyIndex, tower, lnAddr)
 		switch {
 		case err == ErrPermanentTowerFailure:
 			// TODO(conner): report to iterator? can then be reset
@@ -346,11 +355,11 @@ func (n *sessionNegotiator) createSession(tower *wtdb.Tower,
 // The address should belong to the tower's set of addresses. This method only
 // returns true if all steps succeed and the new session has been persisted, and
 // fails otherwise.
-func (n *sessionNegotiator) tryAddress(privKey *secp256k1.PrivateKey,
+func (n *sessionNegotiator) tryAddress(sessionKey keychain.SingleKeyECDH,
 	keyIndex uint32, tower *wtdb.Tower, lnAddr *lnwire.NetAddress) error {
 
 	// Connect to the tower address using our generated session key.
-	conn, err := n.cfg.Dial(privKey, lnAddr)
+	conn, err := n.cfg.Dial(sessionKey, lnAddr)
 	if err != nil {
 		return err
 	}
@@ -417,9 +426,7 @@ func (n *sessionNegotiator) tryAddress(privKey *secp256k1.PrivateKey,
 		// TODO(conner): validate reward address
 		rewardPkScript := createSessionReply.Data
 
-		sessionID := wtdb.NewSessionIDFromPubKey(
-			privKey.PubKey(),
-		)
+		sessionID := wtdb.NewSessionIDFromPubKey(sessionKey.PubKey())
 		clientSession := &wtdb.ClientSession{
 			ClientSessionBody: wtdb.ClientSessionBody{
 				TowerID:        tower.ID,
@@ -428,7 +435,7 @@ func (n *sessionNegotiator) tryAddress(privKey *secp256k1.PrivateKey,
 				RewardPkScript: rewardPkScript,
 			},
 			Tower:          tower,
-			SessionKeyECDH: privKey,
+			SessionKeyECDH: sessionKey,
 			ID:             sessionID,
 		}
 

watchtower/wtclient/session_queue.go
@@ -7,8 +7,8 @@ import (
 	"time"
 
 	"github.com/decred/dcrd/chaincfg/chainhash"
-	"github.com/decred/dcrd/dcrec/secp256k1/v3"
 	"github.com/decred/dcrlnd/input"
+	"github.com/decred/dcrlnd/keychain"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/watchtower/wtdb"
 	"github.com/decred/dcrlnd/watchtower/wtserver"
@@ -41,8 +41,8 @@ type sessionQueueConfig struct {
 
 	// Dial allows the client to dial the tower using it's public key and
 	// net address.
-	Dial func(*secp256k1.PrivateKey,
-		*lnwire.NetAddress) (wtserver.Peer, error)
+	Dial func(keychain.SingleKeyECDH, *lnwire.NetAddress) (wtserver.Peer,
+		error)
 
 	// SendMessage encodes, encrypts, and writes a message to the given peer.
 	SendMessage func(wtserver.Peer, wtwire.Message) error

watchtower/wtdb/client_session.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/decred/dcrd/dcrec/secp256k1/v3"
+	"github.com/decred/dcrlnd/keychain"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/watchtower/blob"
 	"github.com/decred/dcrlnd/watchtower/wtpolicy"
@@ -65,7 +65,7 @@ type ClientSession struct {
 	//
 	// NOTE: This value is not serialized. It is derived using the KeyIndex
 	// on startup to avoid storing private keys on disk.
-	SessionKeyECDH *secp256k1.PrivateKey
+	SessionKeyECDH keychain.SingleKeyECDH
 }
 
 // ClientSessionBody represents the primary components of a ClientSession that

watchtower/wtserver/server.go
@@ -10,8 +10,8 @@ import (
 
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/connmgr"
-	"github.com/decred/dcrd/dcrec/secp256k1/v3"
 	"github.com/decred/dcrd/dcrutil/v3"
+	"github.com/decred/dcrlnd/keychain"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/watchtower/wtdb"
 	"github.com/decred/dcrlnd/watchtower/wtwire"
@@ -35,7 +35,7 @@ type Config struct {
 
 	// NodeKeyECDH is the the ECDH capable wrapper of the key to be used in
 	// accepting new brontide connections.
-	NodeKeyECDH *secp256k1.PrivateKey
+	NodeKeyECDH keychain.SingleKeyECDH
 
 	// Listeners specifies which address to which clients may connect.
 	Listeners []net.Listener