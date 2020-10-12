lightningnetwork/lnd

routerrpc: clarify proto fields to be outgoing

lnrpc/routerrpc/forward_interceptor.go
@@ -157,9 +157,9 @@ func (r *forwardInterceptor) holdAndForwardToClient(
 			ChanId: inKey.ChanID.ToUint64(),
 			HtlcId: inKey.HtlcID,
 		},
-		HtlcPaymentHash: htlc.PaymentHash[:],
-		AmountMsat:      uint64(htlc.Amount),
-		Expiry:          htlc.Expiry,
+		HtlcPaymentHash:    htlc.PaymentHash[:],
+		OutgoingAmountMsat: uint64(htlc.Amount),
+		OutgoingExpiry:     htlc.Expiry,
 	}
 
 	return r.stream.Send(interceptionRequest)

matheusd/dcrlnd

lnrpc/routerrpc/forward_interceptor.go
@@ -157,9 +157,9 @@ func (r *forwardInterceptor) holdAndForwardToClient(
 			ChanId: inKey.ChanID.ToUint64(),
 			HtlcId: inKey.HtlcID,
 		},
-		HtlcPaymentHash: htlc.PaymentHash[:],
-		AmountMAtoms:    uint64(htlc.Amount),
-		Expiry:          htlc.Expiry,
+		HtlcPaymentHash:      htlc.PaymentHash[:],
+		OutgoingAmountMAtoms: uint64(htlc.Amount),
+		OutgoingExpiry:       htlc.Expiry,
 	}
 
 	return r.stream.Send(interceptionRequest)