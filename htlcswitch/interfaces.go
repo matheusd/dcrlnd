lightningnetwork/lnd

htlcswitch: expose additional fields for intercepted packets

htlcswitch/interfaces.go
@@ -207,6 +207,9 @@ type InterceptedPacket struct {
 	// packet.
 	IncomingCircuit channeldb.CircuitKey
 
+	// OutgoingChanID is the destination channel for this packet.
+	OutgoingChanID lnwire.ShortChannelID
+
 	// Hash is the payment hash of the htlc.
 	Hash lntypes.Hash
 
@@ -216,6 +219,13 @@ type InterceptedPacket struct {
 
 	// OutgoingAmount is the amount to forward.
 	OutgoingAmount lnwire.MilliSatoshi
+
+	// IncomingExpiry is the absolute block height at which the incoming
+	// htlc expires.
+	IncomingExpiry uint32
+
+	// IncomingAmount is the amount of the accepted htlc.
+	IncomingAmount lnwire.MilliSatoshi
 }
 
 // InterceptedForward is passed to the ForwardInterceptor for every forwarded

lnrpc/routerrpc/forward_interceptor.go
@@ -157,9 +157,12 @@ func (r *forwardInterceptor) holdAndForwardToClient(
 			ChanId: inKey.ChanID.ToUint64(),
 			HtlcId: inKey.HtlcID,
 		},
-		PaymentHash:        htlc.Hash[:],
-		OutgoingAmountMsat: uint64(htlc.OutgoingAmount),
-		OutgoingExpiry:     htlc.OutgoingExpiry,
+		OutgoingRequestedChanId: htlc.OutgoingChanID.ToUint64(),
+		PaymentHash:             htlc.Hash[:],
+		OutgoingAmountMsat:      uint64(htlc.OutgoingAmount),
+		OutgoingExpiry:          htlc.OutgoingExpiry,
+		IncomingAmountMsat:      uint64(htlc.IncomingAmount),
+		IncomingExpiry:          htlc.IncomingExpiry,
 	}
 
 	return r.stream.Send(interceptionRequest)

lntest/itest/lnd_forward_interceptor_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/lightningnetwork/lnd/lnrpc/routerrpc"
 	"github.com/lightningnetwork/lnd/lntest"
 	"github.com/lightningnetwork/lnd/routing/route"
+	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -133,6 +134,21 @@ func testForwardInterceptor(net *lntest.NetworkHarness, t *harnessTest) {
 	for i := 0; i < len(testCases); i++ {
 		select {
 		case request := <-interceptedChan:
+			// Assert sanity of informational packet data.
+			require.NotZero(t.t, request.OutgoingRequestedChanId)
+			require.NotZero(t.t, request.IncomingExpiry)
+			require.NotZero(t.t, request.IncomingAmountMsat)
+
+			require.Less(
+				t.t,
+				request.OutgoingExpiry, request.IncomingExpiry,
+			)
+			require.Less(
+				t.t,
+				request.OutgoingAmountMsat,
+				request.IncomingAmountMsat,
+			)
+
 			testCase := testCases[i]
 
 			// For held packets we ignore, keeping them in hold status.

matheusd/dcrlnd

htlcswitch/interfaces.go
@@ -208,6 +208,9 @@ type InterceptedPacket struct {
 	// packet.
 	IncomingCircuit channeldb.CircuitKey
 
+	// OutgoingChanID is the destination channel for this packet.
+	OutgoingChanID lnwire.ShortChannelID
+
 	// Hash is the payment hash of the htlc.
 	Hash lntypes.Hash
 
@@ -217,6 +220,13 @@ type InterceptedPacket struct {
 
 	// OutgoingAmount is the amount to forward.
 	OutgoingAmount lnwire.MilliAtom
+
+	// IncomingExpiry is the absolute block height at which the incoming
+	// htlc expires.
+	IncomingExpiry uint32
+
+	// IncomingAmount is the amount of the accepted htlc.
+	IncomingAmount lnwire.MilliAtom
 }
 
 // InterceptedForward is passed to the ForwardInterceptor for every forwarded

lnrpc/routerrpc/forward_interceptor.go
@@ -157,9 +157,12 @@ func (r *forwardInterceptor) holdAndForwardToClient(
 			ChanId: inKey.ChanID.ToUint64(),
 			HtlcId: inKey.HtlcID,
 		},
-		PaymentHash:          htlc.Hash[:],
-		OutgoingAmountMAtoms: uint64(htlc.OutgoingAmount),
-		OutgoingExpiry:       htlc.OutgoingExpiry,
+		OutgoingRequestedChanId: htlc.OutgoingChanID.ToUint64(),
+		PaymentHash:             htlc.Hash[:],
+		OutgoingAmountMAtoms:    uint64(htlc.OutgoingAmount),
+		OutgoingExpiry:          htlc.OutgoingExpiry,
+		IncomingAmountMAtoms:    uint64(htlc.IncomingAmount),
+		IncomingExpiry:          htlc.IncomingExpiry,
 	}
 
 	return r.stream.Send(interceptionRequest)

lntest/itest/lnd_forward_interceptor_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/decred/dcrlnd/lnrpc/routerrpc"
 	"github.com/decred/dcrlnd/lntest"
 	"github.com/decred/dcrlnd/routing/route"
+	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -133,6 +134,21 @@ func testForwardInterceptor(net *lntest.NetworkHarness, t *harnessTest) {
 	for i := 0; i < len(testCases); i++ {
 		select {
 		case request := <-interceptedChan:
+			// Assert sanity of informational packet data.
+			require.NotZero(t.t, request.OutgoingRequestedChanId)
+			require.NotZero(t.t, request.IncomingExpiry)
+			require.NotZero(t.t, request.IncomingAmountMAtoms)
+
+			require.Less(
+				t.t,
+				request.OutgoingExpiry, request.IncomingExpiry,
+			)
+			require.Less(
+				t.t,
+				request.OutgoingAmountMAtoms,
+				request.IncomingAmountMAtoms,
+			)
+
 			testCase := testCases[i]
 
 			// For held packets we ignore, keeping them in hold status.