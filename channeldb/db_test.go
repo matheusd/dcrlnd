Roasbeef/lnd

kvdb+etcd: enable channeldb testing with both bdb/etcd

channeldb/db_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/shachain"
@@ -33,7 +34,13 @@ func TestOpenWithCreate(t *testing.T) {
 
 	// Next, open thereby creating channeldb for the first time.
 	dbPath := filepath.Join(tempDirName, "cdb")
-	cdb, err := Open(dbPath)
+	backend, cleanup, err := kvdb.GetTestBackend(dbPath, "cdb")
+	if err != nil {
+		t.Fatalf("unable to get test db backend: %v", err)
+	}
+	defer cleanup()
+	cdb, err := CreateWithBackend(backend)
+
 	if err != nil {
 		t.Fatalf("unable to create channeldb: %v", err)
 	}
@@ -63,7 +70,13 @@ func TestWipe(t *testing.T) {
 
 	// Next, open thereby creating channeldb for the first time.
 	dbPath := filepath.Join(tempDirName, "cdb")
-	cdb, err := Open(dbPath)
+	backend, cleanup, err := kvdb.GetTestBackend(dbPath, "cdb")
+	if err != nil {
+		t.Fatalf("unable to get test db backend: %v", err)
+	}
+	defer cleanup()
+	cdb, err := CreateWithBackend(backend)
+
 	if err != nil {
 		t.Fatalf("unable to create channeldb: %v", err)
 	}

channeldb/kvdb/backend.go
@@ -1,0 +1,79 @@
+package kvdb
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/lightningnetwork/lnd/channeldb/kvdb/etcd"
+)
+
+// fileExists returns true if the file exists, and false otherwise.
+func fileExists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// GetBoltBackend opens (or creates if doesn't exits) a bbolt
+// backed database and returns a kvdb.Backend wrapping it.
+func GetBoltBackend(path, name string, noFreeListSync bool) (Backend, error) {
+	dbFilePath := filepath.Join(path, name)
+	var (
+		db  Backend
+		err error
+	)
+
+	if !fileExists(dbFilePath) {
+		if !fileExists(path) {
+			if err := os.MkdirAll(path, 0700); err != nil {
+				return nil, err
+			}
+		}
+
+		db, err = Create(BoltBackendName, dbFilePath, noFreeListSync)
+	} else {
+		db, err = Open(BoltBackendName, dbFilePath, noFreeListSync)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// GetTestBackend opens (or creates if doesn't exist) a bbolt or etcd
+// backed database (for testing), and returns a kvdb.Backend and a cleanup
+// func. Whether to create/open bbolt or embedded etcd database is based
+// on the TestBackend constant which is conditionally compiled with build tag.
+// The passed path is used to hold all db files, while the name is only used
+// for bbolt.
+func GetTestBackend(path, name string) (Backend, func(), error) {
+	empty := func() {}
+
+	if TestBackend == BoltBackendName {
+		db, err := GetBoltBackend(path, name, true)
+		if err != nil {
+			return nil, nil, err
+		}
+		return db, empty, nil
+	} else if TestBackend == EtcdBackendName {
+		config, cleanup, err := etcd.NewEmbeddedEtcdInstance(path)
+		if err != nil {
+			return nil, nil, err
+		}
+		backend, err := Open(EtcdBackendName, *config)
+		if err != nil {
+			cleanup()
+			return nil, nil, err
+		}
+		return backend, cleanup, nil
+	}
+
+	return nil, nil, fmt.Errorf("unknown backend")
+}

matheusd/dcrlnd

channeldb/db_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrutil/v3"
 	"github.com/decred/dcrd/wire"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/keychain"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/shachain"
@@ -32,7 +33,13 @@ func TestOpenWithCreate(t *testing.T) {
 
 	// Next, open thereby creating channeldb for the first time.
 	dbPath := filepath.Join(tempDirName, "cdb")
-	cdb, err := Open(dbPath)
+	backend, cleanup, err := kvdb.GetTestBackend(dbPath, "cdb")
+	if err != nil {
+		t.Fatalf("unable to get test db backend: %v", err)
+	}
+	defer cleanup()
+
+	cdb, err := CreateWithBackend(backend)
 	if err != nil {
 		t.Fatalf("unable to create channeldb: %v", err)
 	}
@@ -72,7 +79,13 @@ func TestWipe(t *testing.T) {
 
 	// Next, open thereby creating channeldb for the first time.
 	dbPath := filepath.Join(tempDirName, "cdb")
-	cdb, err := Open(dbPath)
+	backend, cleanup, err := kvdb.GetTestBackend(dbPath, "cdb")
+	if err != nil {
+		t.Fatalf("unable to get test db backend: %v", err)
+	}
+	defer cleanup()
+
+	cdb, err := CreateWithBackend(backend)
 	if err != nil {
 		t.Fatalf("unable to create channeldb: %v", err)
 	}

channeldb/kvdb/backend.go
@@ -1,0 +1,79 @@
+package kvdb
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/decred/dcrlnd/channeldb/kvdb/etcd"
+)
+
+// fileExists returns true if the file exists, and false otherwise.
+func fileExists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// GetBoltBackend opens (or creates if doesn't exits) a bbolt
+// backed database and returns a kvdb.Backend wrapping it.
+func GetBoltBackend(path, name string, noFreeListSync bool) (Backend, error) {
+	dbFilePath := filepath.Join(path, name)
+	var (
+		db  Backend
+		err error
+	)
+
+	if !fileExists(dbFilePath) {
+		if !fileExists(path) {
+			if err := os.MkdirAll(path, 0700); err != nil {
+				return nil, err
+			}
+		}
+
+		db, err = Create(BoltBackendName, dbFilePath, noFreeListSync)
+	} else {
+		db, err = Open(BoltBackendName, dbFilePath, noFreeListSync)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// GetTestBackend opens (or creates if doesn't exist) a bbolt or etcd
+// backed database (for testing), and returns a kvdb.Backend and a cleanup
+// func. Whether to create/open bbolt or embedded etcd database is based
+// on the TestBackend constant which is conditionally compiled with build tag.
+// The passed path is used to hold all db files, while the name is only used
+// for bolt.
+func GetTestBackend(path, name string) (Backend, func(), error) {
+	empty := func() {}
+
+	if TestBackend == BoltBackendName {
+		db, err := GetBoltBackend(path, name, true)
+		if err != nil {
+			return nil, nil, err
+		}
+		return db, empty, nil
+	} else if TestBackend == EtcdBackendName {
+		config, cleanup, err := etcd.NewEmbeddedEtcdInstance(path)
+		if err != nil {
+			return nil, nil, err
+		}
+		backend, err := Open(EtcdBackendName, *config)
+		if err != nil {
+			cleanup()
+			return nil, nil, err
+		}
+		return backend, cleanup, nil
+	}
+
+	return nil, nil, fmt.Errorf("unknown backend")
+}