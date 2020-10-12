Roasbeef/lnd

lncfg: add configuration for user specified db backend
This commit extends lncfg to support user specified database backend.
This supports configuration for both bolt and etcd (while only allowing
one or the other).

config.go
@@ -356,6 +356,8 @@ type config struct {
 	ProtocolOptions *lncfg.ProtocolOptions `group:"protocol" namespace:"protocol"`
 
 	AllowCircularRoute bool `long:"allow-circular-route" description:"If true, our node will allow htlc forwards that arrive and depart on the same channel."`
+
+	DB *lncfg.DB `group:"db" namespace:"db"`
 }
 
 // loadConfig initializes and parses the config using a config file and command
@@ -464,6 +466,7 @@ func loadConfig() (*config, error) {
 		},
 		MaxOutgoingCltvExpiry:   htlcswitch.DefaultMaxOutgoingCltvExpiry,
 		MaxChannelFeeAllocation: htlcswitch.DefaultMaxLinkFeeAllocation,
+		DB:                      lncfg.DefaultDB(),
 	}
 
 	// Pre-parse the command line options to pick up an alternative config
@@ -1139,6 +1142,7 @@ func loadConfig() (*config, error) {
 		cfg.Workers,
 		cfg.Caches,
 		cfg.WtClient,
+		cfg.DB,
 	)
 	if err != nil {
 		return nil, err
@@ -1160,6 +1164,14 @@ func loadConfig() (*config, error) {
 	}
 
 	return &cfg, nil
+}
+
+// localDatabaseDir returns the default directory where the
+// local bolt db files are stored.
+func (c *config) localDatabaseDir() string {
+	return filepath.Join(cfg.DataDir,
+		defaultGraphSubDirname,
+		normalizeNetwork(activeNetParams.Name))
 }
 
 // cleanAndExpandPath expands environment variables and leading ~ in the

lncfg/db.go
@@ -1,0 +1,85 @@
+package lncfg
+
+import (
+	"fmt"
+
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb/etcd"
+)
+
+const (
+	dbName      = "channel.db"
+	boltBackend = "bolt"
+	etcdBackend = "etcd"
+)
+
+// BoltDB holds bolt configuration.
+type BoltDB struct {
+	NoFreeListSync bool `long:"nofreelistsync" description:"If true, prevents the database from syncing its freelist to disk"`
+}
+
+// EtcdDB hold etcd configuration.
+type EtcdDB struct {
+	Host string `long:"host" description:"Etcd database host."`
+
+	User string `long:"user" description:"Etcd database user."`
+
+	Pass string `long:"pass" description:"Password for the database user."`
+
+	CollectStats bool `long:"collect_stats" description:"Wheter to collect etcd commit stats."`
+}
+
+// DB holds database configuration for LND.
+type DB struct {
+	Backend string `long:"backend" description:"The selected database backend."`
+
+	Etcd *EtcdDB `group:"etcd" namespace:"etcd" description:"Etcd settings."`
+
+	Bolt *BoltDB `group:"bolt" namespace:"bolt" description:"Bolt settings."`
+}
+
+// NewDB creates and returns a new default DB config.
+func DefaultDB() *DB {
+	return &DB{
+		Backend: boltBackend,
+		Bolt: &BoltDB{
+			NoFreeListSync: true,
+		},
+	}
+}
+
+// Validate validates the DB config.
+func (db *DB) Validate() error {
+	switch db.Backend {
+	case boltBackend:
+
+	case etcdBackend:
+		if db.Etcd.Host == "" {
+			return fmt.Errorf("host must be set")
+		}
+
+	default:
+		return fmt.Errorf("unknwon backend, must be either \"%v\" or \"%v\"",
+			boltBackend, etcdBackend)
+	}
+
+	return nil
+}
+
+// GetBackend returns a kvdb.Backend as set in the DB config.
+func (db *DB) GetBackend(path string) (kvdb.Backend, error) {
+	if db.Backend == etcdBackend {
+		backendConfig := etcd.BackendConfig{
+			Host:               db.Etcd.Host,
+			User:               db.Etcd.User,
+			Pass:               db.Etcd.Pass,
+			CollectCommitStats: db.Etcd.CollectStats,
+		}
+		return kvdb.Open(kvdb.EtcdBackendName, backendConfig)
+	}
+
+	return kvdb.GetBoltBackend(path, dbName, db.Bolt.NoFreeListSync)
+}
+
+// Compile-time constraint to ensure Workers implements the Validator interface.
+var _ Validator = (*DB)(nil)

matheusd/dcrlnd

config.go
@@ -244,6 +244,8 @@ type Config struct {
 
 	AllowCircularRoute bool `long:"allow-circular-route" description:"If true, our node will allow htlc forwards that arrive and depart on the same channel."`
 
+	DB *lncfg.DB `group:"db" namespace:"db"`
+
 	// registeredChains keeps track of all chains that have been registered
 	// with the daemon.
 	registeredChains *chainRegistry
@@ -330,6 +332,7 @@ func DefaultConfig() Config {
 		},
 		MaxOutgoingCltvExpiry:   htlcswitch.DefaultMaxOutgoingCltvExpiry,
 		MaxChannelFeeAllocation: htlcswitch.DefaultMaxLinkFeeAllocation,
+		DB:                      lncfg.DefaultDB(),
 		registeredChains:        newChainRegistry(),
 	}
 }
@@ -933,6 +936,7 @@ func ValidateConfig(cfg Config, usageMessage string) (*Config, error) {
 		cfg.Workers,
 		cfg.Caches,
 		cfg.WtClient,
+		cfg.DB,
 	)
 	if err != nil {
 		return nil, err
@@ -948,6 +952,14 @@ func ValidateConfig(cfg Config, usageMessage string) (*Config, error) {
 
 	// All good, return the sanitized result.
 	return &cfg, err
+}
+
+// localDatabaseDir returns the default directory where the
+// local bolt db files are stored.
+func (c *Config) localDatabaseDir() string {
+	return filepath.Join(c.DataDir,
+		defaultGraphSubDirname,
+		normalizeNetwork(activeNetParams.Name))
 }
 
 // CleanAndExpandPath expands environment variables and leading ~ in the

lncfg/db.go
@@ -1,0 +1,85 @@
+package lncfg
+
+import (
+	"fmt"
+
+	"github.com/decred/dcrlnd/channeldb/kvdb"
+	"github.com/decred/dcrlnd/channeldb/kvdb/etcd"
+)
+
+const (
+	dbName      = "channel.db"
+	boltBackend = "bolt"
+	etcdBackend = "etcd"
+)
+
+// BoltDB holds bolt configuration.
+type BoltDB struct {
+	NoFreeListSync bool `long:"nofreelistsync" description:"If true, prevents the database from syncing its freelist to disk"`
+}
+
+// EtcdDB hold etcd configuration.
+type EtcdDB struct {
+	Host string `long:"host" description:"Etcd database host."`
+
+	User string `long:"user" description:"Etcd database user."`
+
+	Pass string `long:"pass" description:"Password for the database user."`
+
+	CollectStats bool `long:"collect_stats" description:"Wheter to collect etcd commit stats."`
+}
+
+// DB holds database configuration for LND.
+type DB struct {
+	Backend string `long:"backend" description:"The selected database backend."`
+
+	Etcd *EtcdDB `group:"etcd" namespace:"etcd" description:"Etcd settings."`
+
+	Bolt *BoltDB `group:"bolt" namespace:"bolt" description:"Bolt settings."`
+}
+
+// NewDB creates and returns a new default DB config.
+func DefaultDB() *DB {
+	return &DB{
+		Backend: boltBackend,
+		Bolt: &BoltDB{
+			NoFreeListSync: true,
+		},
+	}
+}
+
+// Validate validates the DB config.
+func (db *DB) Validate() error {
+	switch db.Backend {
+	case boltBackend:
+
+	case etcdBackend:
+		if db.Etcd.Host == "" {
+			return fmt.Errorf("etcd host must be set")
+		}
+
+	default:
+		return fmt.Errorf("unknown backend, must be either \"%v\" or \"%v\"",
+			boltBackend, etcdBackend)
+	}
+
+	return nil
+}
+
+// GetBackend returns a kvdb.Backend as set in the DB config.
+func (db *DB) GetBackend(path string) (kvdb.Backend, error) {
+	if db.Backend == etcdBackend {
+		backendConfig := etcd.BackendConfig{
+			Host:               db.Etcd.Host,
+			User:               db.Etcd.User,
+			Pass:               db.Etcd.Pass,
+			CollectCommitStats: db.Etcd.CollectStats,
+		}
+		return kvdb.Open(kvdb.EtcdBackendName, backendConfig)
+	}
+
+	return kvdb.GetBoltBackend(path, dbName, db.Bolt.NoFreeListSync)
+}
+
+// Compile-time constraint to ensure Workers implements the Validator interface.
+var _ Validator = (*DB)(nil)