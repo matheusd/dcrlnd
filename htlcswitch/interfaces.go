lightningnetwork/lnd

htlcswitch: expose custom records on intercepted packet

htlcswitch/interfaces.go
@@ -8,6 +8,7 @@ import (
 	"github.com/lightningnetwork/lnd/lntypes"
 	"github.com/lightningnetwork/lnd/lnwallet"
 	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/lightningnetwork/lnd/record"
 )
 
 // InvoiceDatabase is an interface which represents the persistent subsystem
@@ -226,6 +227,10 @@ type InterceptedPacket struct {
 
 	// IncomingAmount is the amount of the accepted htlc.
 	IncomingAmount lnwire.MilliSatoshi
+
+	// CustomRecords are user-defined records in the custom type range that
+	// were included in the payload.
+	CustomRecords record.CustomSet
 }
 
 // InterceptedForward is passed to the ForwardInterceptor for every forwarded

htlcswitch/packet.go
@@ -4,6 +4,7 @@ import (
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/htlcswitch/hop"
 	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/lightningnetwork/lnd/record"
 )
 
 // htlcPacket is a wrapper around htlc lnwire update, which adds additional
@@ -91,6 +92,10 @@ type htlcPacket struct {
 	// will be extraced from the hop payload recevived by the incoming
 	// link.
 	outgoingTimeout uint32
+
+	// customRecords are user-defined records in the custom type range that
+	// were included in the payload.
+	customRecords record.CustomSet
 }
 
 // inKey returns the circuit key used to identify the incoming htlc.

matheusd/dcrlnd

htlcswitch/interfaces.go
@@ -8,6 +8,7 @@ import (
 	"github.com/decred/dcrlnd/lntypes"
 	"github.com/decred/dcrlnd/lnwallet"
 	"github.com/decred/dcrlnd/lnwire"
+	"github.com/decred/dcrlnd/record"
 )
 
 // InvoiceDatabase is an interface which represents the persistent subsystem
@@ -227,6 +228,10 @@ type InterceptedPacket struct {
 
 	// IncomingAmount is the amount of the accepted htlc.
 	IncomingAmount lnwire.MilliAtom
+
+	// CustomRecords are user-defined records in the custom type range that
+	// were included in the payload.
+	CustomRecords record.CustomSet
 }
 
 // InterceptedForward is passed to the ForwardInterceptor for every forwarded

htlcswitch/packet.go
@@ -4,6 +4,7 @@ import (
 	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/htlcswitch/hop"
 	"github.com/decred/dcrlnd/lnwire"
+	"github.com/decred/dcrlnd/record"
 )
 
 // htlcPacket is a wrapper around htlc lnwire update, which adds additional
@@ -91,6 +92,10 @@ type htlcPacket struct {
 	// will be extraced from the hop payload recevived by the incoming
 	// link.
 	outgoingTimeout uint32
+
+	// customRecords are user-defined records in the custom type range that
+	// were included in the payload.
+	customRecords record.CustomSet
 }
 
 // inKey returns the circuit key used to identify the incoming htlc.