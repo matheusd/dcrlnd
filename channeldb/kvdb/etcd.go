Roasbeef/lnd

channeldb+kvdb: walletdb/kvdb interface etcd implementation
This commit adds a full interface implementation of the walletdb/kvdb
interface with detailed tests.

channeldb/kvdb/etcd.go
@@ -1,0 +1,10 @@
+package kvdb
+
+import (
+	_ "github.com/lightningnetwork/lnd/channeldb/kvdb/etcd" // Import to register backend.
+)
+
+// EtcdBackendName is the name of the backend that should be passed into
+// kvdb.Create to initialize a new instance of kvdb.Backend backed by a live
+// instance of etcd.
+const EtcdBackendName = "etcd"

matheusd/dcrlnd

channeldb/kvdb/etcd.go
@@ -1,0 +1,10 @@
+package kvdb
+
+import (
+	_ "github.com/decred/dcrlnd/channeldb/kvdb/etcd" // Import to register backend.
+)
+
+// EtcdBackendName is the name of the backend that should be passed into
+// kvdb.Create to initialize a new instance of kvdb.Backend backed by a live
+// instance of etcd.
+const EtcdBackendName = "etcd"