lightningnetwork/lnd

chainntfns: rename to CacheConfig and QueryDisabled

lnwallet/interface_test.go
@@ -3014,8 +3014,8 @@ func TestLightningWallet(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unable to create db: %v", err)
 	}
-	testCfg := chainntnfs.Config{
-		HeightHintCacheQueryDisable: false,
+	testCfg := chainntnfs.CacheConfig{
+		QueryDisable: false,
 	}
 	hintCache, err := chainntnfs.NewHeightHintCache(testCfg, db)
 	if err != nil {

matheusd/dcrlnd

lnwallet/interface_test.go
@@ -3066,8 +3066,8 @@ func TestLightningWallet(t *testing.T) {
 			if err != nil {
 				t.Fatalf("unable to create db: %v", err)
 			}
-			hintCacheCfg := chainntnfs.Config{
-				HeightHintCacheQueryDisable: false,
+			hintCacheCfg := chainntnfs.CacheConfig{
+				QueryDisable: false,
 			}
 			hintCache, err := chainntnfs.NewHeightHintCache(hintCacheCfg, db)
 			if err != nil {