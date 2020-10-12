lightningnetwork/lnd

make+itest: make itest Windows compatible

Makefile
@@ -132,6 +132,11 @@ build-itest:
 	$(GOBUILD) -tags="$(ITEST_TAGS)" -o lnd-itest $(ITEST_LDFLAGS) $(PKG)/cmd/lnd
 	$(GOBUILD) -tags="$(ITEST_TAGS)" -o lncli-itest $(ITEST_LDFLAGS) $(PKG)/cmd/lncli
 
+build-itest-windows:
+	@$(call print, "Building itest lnd and lncli.")
+	$(GOBUILD) -tags="$(ITEST_TAGS)" -o lnd-itest.exe $(ITEST_LDFLAGS) $(PKG)/cmd/lnd
+	$(GOBUILD) -tags="$(ITEST_TAGS)" -o lncli-itest.exe $(ITEST_LDFLAGS) $(PKG)/cmd/lncli
+
 install:
 	@$(call print, "Installing lnd and lncli.")
 	$(GOINSTALL) -tags="${tags}" $(LDFLAGS) $(PKG)/cmd/lnd
@@ -157,6 +162,8 @@ itest-only:
 	lntest/itest/log_check_errors.sh
 
 itest: btcd build-itest itest-only
+
+itest-windows: btcd build-itest-windows itest-only
 
 unit: btcd
 	@$(call print, "Running unit tests.")

matheusd/dcrlnd

Makefile
@@ -148,10 +148,15 @@ build-itest:
 	$(GOBUILD) -tags="$(ITEST_TAGS)" -o dcrlnd-itest $(LDFLAGS) $(PKG)/cmd/dcrlnd
 	$(GOBUILD) -tags="$(ITEST_TAGS)" -o dcrlncli-itest $(LDFLAGS) $(PKG)/cmd/dcrlncli
 
-build-all: 
+build-all:
 	@$(call print, "Building debug dcrlnd and dcrlncli with all submodules.")
 	$(GOBUILD) -o dcrlnd-debug $(LDFLAGS) $(PKG)/cmd/dcrlnd
 	$(GOBUILD) -o dcrlncli-debug $(LDFLAGS) $(PKG)/cmd/dcrlncli
+
+build-itest-windows:
+	@$(call print, "Building itest lnd and lncli.")
+	$(GOBUILD) -tags="$(ITEST_TAGS)" -o dcrlnd-itest.exe $(ITEST_LDFLAGS) $(PKG)/cmd/dcrlnd
+	$(GOBUILD) -tags="$(ITEST_TAGS)" -o dcrlncli-itest.exe $(ITEST_LDFLAGS) $(PKG)/cmd/dcrlncli
 
 install:
 	@$(call print, "Installing dcrlnd and dcrlncli.")
@@ -179,6 +184,8 @@ itest-only:
 #	lntest/itest/log_check_errors.sh
 
 itest: dcrd dcrwallet build-itest itest-only
+
+itest-windows: dcrd dcrwallet build-itest-windows itest-only
 
 unit-only:
 	@$(call print, "Running unit tests.")