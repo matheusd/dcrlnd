lightningnetwork/lnd

channeldb: index payments by sequence number
Add an entry to a payments index bucket which maps sequence number
to payment hash when we initiate payments. This allows for more
efficient paginated queries. We create the top level bucket in its
own migration so that we do not need to create it on the fly.

When we retry payments and provide them with a new sequence number, we
delete the index for their existing payment so that we do not have an
index that points to a non-existent payment.

If we delete a payment, we also delete its index entry. This prevents
us from looking up entries from indexes to payments that do not exist.

channeldb/db.go
@@ -16,6 +16,7 @@ import (
 	mig "github.com/lightningnetwork/lnd/channeldb/migration"
 	"github.com/lightningnetwork/lnd/channeldb/migration12"
 	"github.com/lightningnetwork/lnd/channeldb/migration13"
+	"github.com/lightningnetwork/lnd/channeldb/migration16"
 	"github.com/lightningnetwork/lnd/channeldb/migration_01_to_11"
 	"github.com/lightningnetwork/lnd/clock"
 	"github.com/lightningnetwork/lnd/lnwire"
@@ -144,6 +145,19 @@ var (
 			number:    14,
 			migration: mig.CreateTLB(payAddrIndexBucket),
 		},
+		{
+			// Initialize payment index bucket which will be used
+			// to index payments by sequence number. This index will
+			// be used to allow more efficient ListPayments queries.
+			number:    15,
+			migration: mig.CreateTLB(paymentsIndexBucket),
+		},
+		{
+			// Add our existing payments to the index bucket created
+			// in migration 15.
+			number:    16,
+			migration: migration16.MigrateSequenceIndex,
+		},
 	}
 
 	// Big endian is the preferred byte order, due to cursor scans over
@@ -257,6 +271,7 @@ var topLevelBuckets = [][]byte{
 	fwdPackagesKey,
 	invoiceBucket,
 	payAddrIndexBucket,
+	paymentsIndexBucket,
 	nodeInfoBucket,
 	nodeBucket,
 	edgeBucket,

channeldb/log.go
@@ -6,6 +6,7 @@ import (
 	mig "github.com/lightningnetwork/lnd/channeldb/migration"
 	"github.com/lightningnetwork/lnd/channeldb/migration12"
 	"github.com/lightningnetwork/lnd/channeldb/migration13"
+	"github.com/lightningnetwork/lnd/channeldb/migration16"
 	"github.com/lightningnetwork/lnd/channeldb/migration_01_to_11"
 )
 
@@ -33,4 +34,5 @@ func UseLogger(logger btclog.Logger) {
 	migration_01_to_11.UseLogger(logger)
 	migration12.UseLogger(logger)
 	migration13.UseLogger(logger)
+	migration16.UseLogger(logger)
 }

channeldb/migration16/log.go
@@ -1,0 +1,14 @@
+package migration16
+
+import (
+	"github.com/btcsuite/btclog"
+)
+
+// log is a logger that is initialized as disabled.  This means the package will
+// not perform any logging by default until a logger is set.
+var log = btclog.Disabled
+
+// UseLogger uses a specified Logger to output package logging info.
+func UseLogger(logger btclog.Logger) {
+	log = logger
+}

channeldb/migration16/migration_test.go
@@ -1,0 +1,144 @@
+package migration16
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
+	"github.com/lightningnetwork/lnd/channeldb/migtest"
+)
+
+var (
+	hexStr = migtest.Hex
+
+	hash1Str   = "02acee76ebd53d00824410cf6adecad4f50334dac702bd5a2d3ba01b91709f0e"
+	hash1      = hexStr(hash1Str)
+	paymentID1 = hexStr("0000000000000001")
+
+	hash2Str   = "62eb3f0a48f954e495d0c14ac63df04a67cefa59dafdbcd3d5046d1f5647840c"
+	hash2      = hexStr(hash2Str)
+	paymentID2 = hexStr("0000000000000002")
+
+	paymentID3 = hexStr("0000000000000003")
+
+	// pre is the data in the payments root bucket in database version 13 format.
+	pre = map[string]interface{}{
+		// A payment without duplicates.
+		hash1: map[string]interface{}{
+			"payment-sequence-key": paymentID1,
+		},
+
+		// A payment with a duplicate.
+		hash2: map[string]interface{}{
+			"payment-sequence-key": paymentID2,
+			"payment-duplicate-bucket": map[string]interface{}{
+				paymentID3: map[string]interface{}{
+					"payment-sequence-key": paymentID3,
+				},
+			},
+		},
+	}
+
+	preFails = map[string]interface{}{
+		// A payment without duplicates.
+		hash1: map[string]interface{}{
+			"payment-sequence-key": paymentID1,
+			"payment-duplicate-bucket": map[string]interface{}{
+				paymentID1: map[string]interface{}{
+					"payment-sequence-key": paymentID1,
+				},
+			},
+		},
+	}
+
+	// post is the expected data after migration.
+	post = map[string]interface{}{
+		paymentID1: paymentHashIndex(hash1Str),
+		paymentID2: paymentHashIndex(hash2Str),
+		paymentID3: paymentHashIndex(hash2Str),
+	}
+)
+
+// paymentHashIndex produces a string that represents the value we expect for
+// our payment indexes from a hex encoded payment hash string.
+func paymentHashIndex(hashStr string) string {
+	hash, err := hex.DecodeString(hashStr)
+	if err != nil {
+		panic(err)
+	}
+
+	bytes, err := serializePaymentIndexEntry(hash)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(bytes)
+}
+
+// MigrateSequenceIndex asserts that the database is properly migrated to
+// contain a payments index.
+func TestMigrateSequenceIndex(t *testing.T) {
+	tests := []struct {
+		name       string
+		shouldFail bool
+		pre        map[string]interface{}
+		post       map[string]interface{}
+	}{
+		{
+			name:       "migration ok",
+			shouldFail: false,
+			pre:        pre,
+			post:       post,
+		},
+		{
+			name:       "duplicate sequence number",
+			shouldFail: true,
+			pre:        preFails,
+			post:       post,
+		},
+		{
+			name:       "no payments",
+			shouldFail: false,
+			pre:        nil,
+			post:       nil,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+
+		t.Run(test.name, func(t *testing.T) {
+			// Before the migration we have a payments bucket.
+			before := func(tx kvdb.RwTx) error {
+				return migtest.RestoreDB(
+					tx, paymentsRootBucket, test.pre,
+				)
+			}
+
+			// After the migration, we should have an untouched
+			// payments bucket and a new index bucket.
+			after := func(tx kvdb.RwTx) error {
+				if err := migtest.VerifyDB(
+					tx, paymentsRootBucket, test.pre,
+				); err != nil {
+					return err
+				}
+
+				// If we expect our migration to fail, we don't
+				// expect an index bucket.
+				if test.shouldFail {
+					return nil
+				}
+
+				return migtest.VerifyDB(
+					tx, paymentsIndexBucket, test.post,
+				)
+			}
+
+			migtest.ApplyMigration(
+				t, before, after, MigrateSequenceIndex,
+				test.shouldFail,
+			)
+		})
+	}
+}

channeldb/payment_control_test.go
@@ -1,6 +1,7 @@
 package channeldb
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
@@ -9,9 +10,13 @@ import (
 	"testing"
 	"time"
 
+	"github.com/btcsuite/btcwallet/walletdb"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
 	"github.com/lightningnetwork/lnd/lntypes"
 	"github.com/lightningnetwork/lnd/record"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func genPreimage() ([32]byte, error) {
@@ -70,6 +75,7 @@ func TestPaymentControlSwitchFail(t *testing.T) {
 		t.Fatalf("unable to send htlc message: %v", err)
 	}
 
+	assertPaymentIndex(t, pControl, info.PaymentHash)
 	assertPaymentStatus(t, pControl, info.PaymentHash, StatusInFlight)
 	assertPaymentInfo(
 		t, pControl, info.PaymentHash, info, nil, nil,
@@ -88,12 +94,22 @@ func TestPaymentControlSwitchFail(t *testing.T) {
 		t, pControl, info.PaymentHash, info, &failReason, nil,
 	)
 
+	// Lookup the payment so we can get its old sequence number before it is
+	// overwritten.
+	payment, err := pControl.FetchPayment(info.PaymentHash)
+	assert.NoError(t, err)
+
 	// Sends the htlc again, which should succeed since the prior payment
 	// failed.
 	err = pControl.InitPayment(info.PaymentHash, info)
 	if err != nil {
 		t.Fatalf("unable to send htlc message: %v", err)
 	}
+
+	// Check that our index has been updated, and the old index has been
+	// removed.
+	assertPaymentIndex(t, pControl, info.PaymentHash)
+	assertNoIndex(t, pControl, payment.SequenceNum)
 
 	assertPaymentStatus(t, pControl, info.PaymentHash, StatusInFlight)
 	assertPaymentInfo(
@@ -145,7 +161,6 @@ func TestPaymentControlSwitchFail(t *testing.T) {
 
 	// Settle the attempt and verify that status was changed to
 	// StatusSucceeded.
-	var payment *MPPayment
 	payment, err = pControl.SettleAttempt(
 		info.PaymentHash, attempt.AttemptID,
 		&HTLCSettleInfo{
@@ -209,6 +224,7 @@ func TestPaymentControlSwitchDoubleSend(t *testing.T) {
 		t.Fatalf("unable to send htlc message: %v", err)
 	}
 
+	assertPaymentIndex(t, pControl, info.PaymentHash)
 	assertPaymentStatus(t, pControl, info.PaymentHash, StatusInFlight)
 	assertPaymentInfo(
 		t, pControl, info.PaymentHash, info, nil, nil,
@@ -326,7 +342,7 @@ func TestPaymentControlFailsWithoutInFlight(t *testing.T) {
 	assertPaymentStatus(t, pControl, info.PaymentHash, StatusUnknown)
 }
 
-// TestPaymentControlDeleteNonInFlight checks that calling DeletaPayments only
+// TestPaymentControlDeleteNonInFlight checks that calling DeletePayments only
 // deletes payments from the database that are not in-flight.
 func TestPaymentControlDeleteNonInFligt(t *testing.T) {
 	t.Parallel()
@@ -338,23 +354,37 @@ func TestPaymentControlDeleteNonInFligt(t *testing.T) {
 		t.Fatalf("unable to init db: %v", err)
 	}
 
+	// Create a sequence number for duplicate payments that will not collide
+	// with the sequence numbers for the payments we create. These values
+	// start at 1, so 9999 is a safe bet for this test.
+	var duplicateSeqNr = 9999
+
 	pControl := NewPaymentControl(db)
 
 	payments := []struct {
-		failed  bool
-		success bool
+		failed       bool
+		success      bool
+		hasDuplicate bool
 	}{
 		{
-			failed:  true,
-			success: false,
+			failed:       true,
+			success:      false,
+			hasDuplicate: false,
 		},
 		{
-			failed:  false,
-			success: true,
+			failed:       false,
+			success:      true,
+			hasDuplicate: false,
 		},
 		{
-			failed:  false,
-			success: false,
+			failed:       false,
+			success:      false,
+			hasDuplicate: false,
+		},
+		{
+			failed:       false,
+			success:      true,
+			hasDuplicate: true,
 		},
 	}
 
@@ -430,6 +460,16 @@ func TestPaymentControlDeleteNonInFligt(t *testing.T) {
 				t, pControl, info.PaymentHash, info, nil, htlc,
 			)
 		}
+
+		// If the payment is intended to have a duplicate payment, we
+		// add one.
+		if p.hasDuplicate {
+			appendDuplicatePayment(
+				t, pControl.db, info.PaymentHash,
+				uint64(duplicateSeqNr),
+			)
+			duplicateSeqNr++
+		}
 	}
 
 	// Delete payments.
@@ -451,6 +491,21 @@ func TestPaymentControlDeleteNonInFligt(t *testing.T) {
 	if status != StatusInFlight {
 		t.Fatalf("expected in-fligth status, got %v", status)
 	}
+
+	// Finally, check that we only have a single index left in the payment
+	// index bucket.
+	var indexCount int
+	err = kvdb.View(db, func(tx walletdb.ReadTx) error {
+		index := tx.ReadBucket(paymentsIndexBucket)
+
+		return index.ForEach(func(k, v []byte) error {
+			indexCount++
+			return nil
+		})
+	})
+	require.NoError(t, err)
+
+	require.Equal(t, 1, indexCount)
 }
 
 // TestPaymentControlMultiShard checks the ability of payment control to
@@ -495,6 +550,7 @@ func TestPaymentControlMultiShard(t *testing.T) {
 			t.Fatalf("unable to send htlc message: %v", err)
 		}
 
+		assertPaymentIndex(t, pControl, info.PaymentHash)
 		assertPaymentStatus(t, pControl, info.PaymentHash, StatusInFlight)
 		assertPaymentInfo(
 			t, pControl, info.PaymentHash, info, nil, nil,
@@ -909,4 +965,56 @@ func assertPaymentInfo(t *testing.T, p *PaymentControl, hash lntypes.Hash,
 	} else if htlc.Settle != nil {
 		t.Fatal("expected no settle info")
 	}
+}
+
+// fetchPaymentIndexEntry gets the payment hash for the sequence number provided
+// from our payment indexes bucket.
+func fetchPaymentIndexEntry(_ *testing.T, p *PaymentControl,
+	sequenceNumber uint64) (*lntypes.Hash, error) {
+
+	var hash lntypes.Hash
+
+	if err := kvdb.View(p.db, func(tx walletdb.ReadTx) error {
+		indexBucket := tx.ReadBucket(paymentsIndexBucket)
+		key := make([]byte, 8)
+		byteOrder.PutUint64(key, sequenceNumber)
+
+		indexValue := indexBucket.Get(key)
+		if indexValue == nil {
+			return errNoSequenceNrIndex
+		}
+
+		r := bytes.NewReader(indexValue)
+
+		var err error
+		hash, err = deserializePaymentIndex(r)
+		return err
+
+	}); err != nil {
+		return nil, err
+	}
+
+	return &hash, nil
+}
+
+// assertPaymentIndex looks up the index for a payment in the db and checks
+// that its payment hash matches the expected hash passed in.
+func assertPaymentIndex(t *testing.T, p *PaymentControl,
+	expectedHash lntypes.Hash) {
+
+	// Lookup the payment so that we have its sequence number and check
+	// that is has correctly been indexed in the payment indexes bucket.
+	pmt, err := p.FetchPayment(expectedHash)
+	require.NoError(t, err)
+
+	hash, err := fetchPaymentIndexEntry(t, p, pmt.SequenceNum)
+	require.NoError(t, err)
+	assert.Equal(t, expectedHash, *hash)
+}
+
+// assertNoIndex checks that an index for the sequence number provided does not
+// exist.
+func assertNoIndex(t *testing.T, p *PaymentControl, seqNr uint64) {
+	_, err := fetchPaymentIndexEntry(t, p, seqNr)
+	require.Equal(t, errNoSequenceNrIndex, err)
 }

matheusd/dcrlnd

channeldb/db.go
@@ -15,6 +15,7 @@ import (
 	mig "github.com/decred/dcrlnd/channeldb/migration"
 	"github.com/decred/dcrlnd/channeldb/migration12"
 	"github.com/decred/dcrlnd/channeldb/migration13"
+	"github.com/decred/dcrlnd/channeldb/migration16"
 	"github.com/decred/dcrlnd/channeldb/migration_01_to_11"
 	"github.com/decred/dcrlnd/clock"
 	"github.com/decred/dcrlnd/lnwire"
@@ -144,6 +145,19 @@ var (
 			number:    14,
 			migration: mig.CreateTLB(payAddrIndexBucket),
 		},
+		{
+			// Initialize payment index bucket which will be used
+			// to index payments by sequence number. This index will
+			// be used to allow more efficient ListPayments queries.
+			number:    15,
+			migration: mig.CreateTLB(paymentsIndexBucket),
+		},
+		{
+			// Add our existing payments to the index bucket created
+			// in migration 15.
+			number:    16,
+			migration: migration16.MigrateSequenceIndex,
+		},
 	}
 
 	// Big endian is the preferred byte order, due to cursor scans over
@@ -257,6 +271,7 @@ var topLevelBuckets = [][]byte{
 	fwdPackagesKey,
 	invoiceBucket,
 	payAddrIndexBucket,
+	paymentsIndexBucket,
 	nodeInfoBucket,
 	nodeBucket,
 	edgeBucket,

channeldb/log.go
@@ -5,6 +5,7 @@ import (
 	mig "github.com/decred/dcrlnd/channeldb/migration"
 	"github.com/decred/dcrlnd/channeldb/migration12"
 	"github.com/decred/dcrlnd/channeldb/migration13"
+	"github.com/decred/dcrlnd/channeldb/migration16"
 	"github.com/decred/dcrlnd/channeldb/migration_01_to_11"
 	"github.com/decred/slog"
 )
@@ -33,4 +34,5 @@ func UseLogger(logger slog.Logger) {
 	migration_01_to_11.UseLogger(logger)
 	migration12.UseLogger(logger)
 	migration13.UseLogger(logger)
+	migration16.UseLogger(logger)
 }

channeldb/migration16/log.go
@@ -1,0 +1,14 @@
+package migration16
+
+import (
+	"github.com/decred/slog"
+)
+
+// log is a logger that is initialized as disabled.  This means the package will
+// not perform any logging by default until a logger is set.
+var log = slog.Disabled
+
+// UseLogger uses a specified Logger to output package logging info.
+func UseLogger(logger slog.Logger) {
+	log = logger
+}

channeldb/migration16/migration_test.go
@@ -1,0 +1,144 @@
+package migration16
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/decred/dcrlnd/channeldb/kvdb"
+	"github.com/decred/dcrlnd/channeldb/migtest"
+)
+
+var (
+	hexStr = migtest.Hex
+
+	hash1Str   = "02acee76ebd53d00824410cf6adecad4f50334dac702bd5a2d3ba01b91709f0e"
+	hash1      = hexStr(hash1Str)
+	paymentID1 = hexStr("0000000000000001")
+
+	hash2Str   = "62eb3f0a48f954e495d0c14ac63df04a67cefa59dafdbcd3d5046d1f5647840c"
+	hash2      = hexStr(hash2Str)
+	paymentID2 = hexStr("0000000000000002")
+
+	paymentID3 = hexStr("0000000000000003")
+
+	// pre is the data in the payments root bucket in database version 13 format.
+	pre = map[string]interface{}{
+		// A payment without duplicates.
+		hash1: map[string]interface{}{
+			"payment-sequence-key": paymentID1,
+		},
+
+		// A payment with a duplicate.
+		hash2: map[string]interface{}{
+			"payment-sequence-key": paymentID2,
+			"payment-duplicate-bucket": map[string]interface{}{
+				paymentID3: map[string]interface{}{
+					"payment-sequence-key": paymentID3,
+				},
+			},
+		},
+	}
+
+	preFails = map[string]interface{}{
+		// A payment without duplicates.
+		hash1: map[string]interface{}{
+			"payment-sequence-key": paymentID1,
+			"payment-duplicate-bucket": map[string]interface{}{
+				paymentID1: map[string]interface{}{
+					"payment-sequence-key": paymentID1,
+				},
+			},
+		},
+	}
+
+	// post is the expected data after migration.
+	post = map[string]interface{}{
+		paymentID1: paymentHashIndex(hash1Str),
+		paymentID2: paymentHashIndex(hash2Str),
+		paymentID3: paymentHashIndex(hash2Str),
+	}
+)
+
+// paymentHashIndex produces a string that represents the value we expect for
+// our payment indexes from a hex encoded payment hash string.
+func paymentHashIndex(hashStr string) string {
+	hash, err := hex.DecodeString(hashStr)
+	if err != nil {
+		panic(err)
+	}
+
+	bytes, err := serializePaymentIndexEntry(hash)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(bytes)
+}
+
+// MigrateSequenceIndex asserts that the database is properly migrated to
+// contain a payments index.
+func TestMigrateSequenceIndex(t *testing.T) {
+	tests := []struct {
+		name       string
+		shouldFail bool
+		pre        map[string]interface{}
+		post       map[string]interface{}
+	}{
+		{
+			name:       "migration ok",
+			shouldFail: false,
+			pre:        pre,
+			post:       post,
+		},
+		{
+			name:       "duplicate sequence number",
+			shouldFail: true,
+			pre:        preFails,
+			post:       post,
+		},
+		{
+			name:       "no payments",
+			shouldFail: false,
+			pre:        nil,
+			post:       nil,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+
+		t.Run(test.name, func(t *testing.T) {
+			// Before the migration we have a payments bucket.
+			before := func(tx kvdb.RwTx) error {
+				return migtest.RestoreDB(
+					tx, paymentsRootBucket, test.pre,
+				)
+			}
+
+			// After the migration, we should have an untouched
+			// payments bucket and a new index bucket.
+			after := func(tx kvdb.RwTx) error {
+				if err := migtest.VerifyDB(
+					tx, paymentsRootBucket, test.pre,
+				); err != nil {
+					return err
+				}
+
+				// If we expect our migration to fail, we don't
+				// expect an index bucket.
+				if test.shouldFail {
+					return nil
+				}
+
+				return migtest.VerifyDB(
+					tx, paymentsIndexBucket, test.post,
+				)
+			}
+
+			migtest.ApplyMigration(
+				t, before, after, MigrateSequenceIndex,
+				test.shouldFail,
+			)
+		})
+	}
+}

channeldb/payment_control_test.go
@@ -1,6 +1,7 @@
 package channeldb
 
 import (
+	"bytes"
 	"crypto/rand"
 	"fmt"
 	"io"
@@ -8,9 +9,13 @@ import (
 	"testing"
 	"time"
 
+	"github.com/btcsuite/btcwallet/walletdb"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
 	"github.com/decred/dcrlnd/lntypes"
 	"github.com/decred/dcrlnd/record"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func genPreimage() (lntypes.Preimage, error) {
@@ -69,6 +74,7 @@ func TestPaymentControlSwitchFail(t *testing.T) {
 		t.Fatalf("unable to send htlc message: %v", err)
 	}
 
+	assertPaymentIndex(t, pControl, info.PaymentHash)
 	assertPaymentStatus(t, pControl, info.PaymentHash, StatusInFlight)
 	assertPaymentInfo(
 		t, pControl, info.PaymentHash, info, nil, nil,
@@ -87,12 +93,22 @@ func TestPaymentControlSwitchFail(t *testing.T) {
 		t, pControl, info.PaymentHash, info, &failReason, nil,
 	)
 
+	// Lookup the payment so we can get its old sequence number before it is
+	// overwritten.
+	payment, err := pControl.FetchPayment(info.PaymentHash)
+	assert.NoError(t, err)
+
 	// Sends the htlc again, which should succeed since the prior payment
 	// failed.
 	err = pControl.InitPayment(info.PaymentHash, info)
 	if err != nil {
 		t.Fatalf("unable to send htlc message: %v", err)
 	}
+
+	// Check that our index has been updated, and the old index has been
+	// removed.
+	assertPaymentIndex(t, pControl, info.PaymentHash)
+	assertNoIndex(t, pControl, payment.SequenceNum)
 
 	assertPaymentStatus(t, pControl, info.PaymentHash, StatusInFlight)
 	assertPaymentInfo(
@@ -144,7 +160,6 @@ func TestPaymentControlSwitchFail(t *testing.T) {
 
 	// Settle the attempt and verify that status was changed to
 	// StatusSucceeded.
-	var payment *MPPayment
 	payment, err = pControl.SettleAttempt(
 		info.PaymentHash, attempt.AttemptID,
 		&HTLCSettleInfo{
@@ -208,6 +223,7 @@ func TestPaymentControlSwitchDoubleSend(t *testing.T) {
 		t.Fatalf("unable to send htlc message: %v", err)
 	}
 
+	assertPaymentIndex(t, pControl, info.PaymentHash)
 	assertPaymentStatus(t, pControl, info.PaymentHash, StatusInFlight)
 	assertPaymentInfo(
 		t, pControl, info.PaymentHash, info, nil, nil,
@@ -325,7 +341,7 @@ func TestPaymentControlFailsWithoutInFlight(t *testing.T) {
 	assertPaymentStatus(t, pControl, info.PaymentHash, StatusUnknown)
 }
 
-// TestPaymentControlDeleteNonInFlight checks that calling DeletaPayments only
+// TestPaymentControlDeleteNonInFlight checks that calling DeletePayments only
 // deletes payments from the database that are not in-flight.
 func TestPaymentControlDeleteNonInFligt(t *testing.T) {
 	t.Parallel()
@@ -337,23 +353,37 @@ func TestPaymentControlDeleteNonInFligt(t *testing.T) {
 		t.Fatalf("unable to init db: %v", err)
 	}
 
+	// Create a sequence number for duplicate payments that will not collide
+	// with the sequence numbers for the payments we create. These values
+	// start at 1, so 9999 is a safe bet for this test.
+	var duplicateSeqNr = 9999
+
 	pControl := NewPaymentControl(db)
 
 	payments := []struct {
-		failed  bool
-		success bool
+		failed       bool
+		success      bool
+		hasDuplicate bool
 	}{
 		{
-			failed:  true,
-			success: false,
+			failed:       true,
+			success:      false,
+			hasDuplicate: false,
 		},
 		{
-			failed:  false,
-			success: true,
+			failed:       false,
+			success:      true,
+			hasDuplicate: false,
 		},
 		{
-			failed:  false,
-			success: false,
+			failed:       false,
+			success:      false,
+			hasDuplicate: false,
+		},
+		{
+			failed:       false,
+			success:      true,
+			hasDuplicate: true,
 		},
 	}
 
@@ -429,6 +459,16 @@ func TestPaymentControlDeleteNonInFligt(t *testing.T) {
 				t, pControl, info.PaymentHash, info, nil, htlc,
 			)
 		}
+
+		// If the payment is intended to have a duplicate payment, we
+		// add one.
+		if p.hasDuplicate {
+			appendDuplicatePayment(
+				t, pControl.db, info.PaymentHash,
+				uint64(duplicateSeqNr),
+			)
+			duplicateSeqNr++
+		}
 	}
 
 	// Delete payments.
@@ -450,6 +490,21 @@ func TestPaymentControlDeleteNonInFligt(t *testing.T) {
 	if status != StatusInFlight {
 		t.Fatalf("expected in-fligth status, got %v", status)
 	}
+
+	// Finally, check that we only have a single index left in the payment
+	// index bucket.
+	var indexCount int
+	err = kvdb.View(db, func(tx walletdb.ReadTx) error {
+		index := tx.ReadBucket(paymentsIndexBucket)
+
+		return index.ForEach(func(k, v []byte) error {
+			indexCount++
+			return nil
+		})
+	})
+	require.NoError(t, err)
+
+	require.Equal(t, 1, indexCount)
 }
 
 // TestPaymentControlMultiShard checks the ability of payment control to
@@ -494,6 +549,7 @@ func TestPaymentControlMultiShard(t *testing.T) {
 			t.Fatalf("unable to send htlc message: %v", err)
 		}
 
+		assertPaymentIndex(t, pControl, info.PaymentHash)
 		assertPaymentStatus(t, pControl, info.PaymentHash, StatusInFlight)
 		assertPaymentInfo(
 			t, pControl, info.PaymentHash, info, nil, nil,
@@ -908,4 +964,56 @@ func assertPaymentInfo(t *testing.T, p *PaymentControl, hash lntypes.Hash,
 	} else if htlc.Settle != nil {
 		t.Fatal("expected no settle info")
 	}
+}
+
+// fetchPaymentIndexEntry gets the payment hash for the sequence number provided
+// from our payment indexes bucket.
+func fetchPaymentIndexEntry(_ *testing.T, p *PaymentControl,
+	sequenceNumber uint64) (*lntypes.Hash, error) {
+
+	var hash lntypes.Hash
+
+	if err := kvdb.View(p.db, func(tx walletdb.ReadTx) error {
+		indexBucket := tx.ReadBucket(paymentsIndexBucket)
+		key := make([]byte, 8)
+		byteOrder.PutUint64(key, sequenceNumber)
+
+		indexValue := indexBucket.Get(key)
+		if indexValue == nil {
+			return errNoSequenceNrIndex
+		}
+
+		r := bytes.NewReader(indexValue)
+
+		var err error
+		hash, err = deserializePaymentIndex(r)
+		return err
+
+	}); err != nil {
+		return nil, err
+	}
+
+	return &hash, nil
+}
+
+// assertPaymentIndex looks up the index for a payment in the db and checks
+// that its payment hash matches the expected hash passed in.
+func assertPaymentIndex(t *testing.T, p *PaymentControl,
+	expectedHash lntypes.Hash) {
+
+	// Lookup the payment so that we have its sequence number and check
+	// that is has correctly been indexed in the payment indexes bucket.
+	pmt, err := p.FetchPayment(expectedHash)
+	require.NoError(t, err)
+
+	hash, err := fetchPaymentIndexEntry(t, p, pmt.SequenceNum)
+	require.NoError(t, err)
+	assert.Equal(t, expectedHash, *hash)
+}
+
+// assertNoIndex checks that an index for the sequence number provided does not
+// exist.
+func assertNoIndex(t *testing.T, p *PaymentControl, seqNr uint64) {
+	_, err := fetchPaymentIndexEntry(t, p, seqNr)
+	require.Equal(t, errNoSequenceNrIndex, err)
 }