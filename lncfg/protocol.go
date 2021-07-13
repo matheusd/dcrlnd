fcb9846cd9947b465340086fb25896e6bf0fae1d
Wilmer Paulino <wilmer.paulino@gmail.com>
2021-07-13 15:11:14 -0700

    lncfg: add protocol config option for script enforced lease support

--- lncfg/protocol.go
+++ lncfg/protocol.go
@@ -20,6 +20,11 @@ type ProtocolOptions struct {
 
 	// Anchors enables anchor commitments.
 	Anchors bool `long:"anchors" description:"EXPERIMENTAL: some features of anchor channels don't work on decred's mainnet"`
+
+	// ScriptEnforcedLease should be set if we don't want to support
+	// opening or accepting channels having the script enforced commitment
+	// type for leased channel.
+	ScriptEnforcedLease bool `long:"script-enforced-lease" description:"disable support for script enforced lease commitments"`
 }
 
 // Wumbo returns true if lnd should permit the creation and acceptance of wumbo
@@ -32,4 +37,10 @@ func (l *ProtocolOptions) Wumbo() bool {
 // commitment type.
 func (l *ProtocolOptions) NoAnchorCommitments() bool {
 	return !l.Anchors
+}
+
+// ScriptEnforcementLease returns true if we have disabled support for the
+// script enforcement commitment type for leased channels.
+func (l *ProtocolOptions) ScriptEnforcementLease() bool {
+	return l.ScriptEnforcedLease
 }
