52f44d61aaae1f063e4e54251ea10d5e62b8acf1
Andras Banki-Horvath <bhandras@gmail.com>
2021-08-17 16:27:08 +0200

    channeldb: migration to flatten the htlc attempts bucket

--- channeldb/db.go
+++ channeldb/db.go
@@ -16,6 +16,7 @@ import (
 	"github.com/decred/dcrlnd/channeldb/migration16"
 	"github.com/decred/dcrlnd/channeldb/migration20"
 	"github.com/decred/dcrlnd/channeldb/migration21"
+	"github.com/decred/dcrlnd/channeldb/migration23"
 	"github.com/decred/dcrlnd/channeldb/migration_01_to_11"
 	"github.com/decred/dcrlnd/clock"
 	"github.com/decred/dcrlnd/kvdb"
@@ -192,6 +193,10 @@ var (
 			// queried by individual htlc sets.
 			number:    22,
 			migration: mig.CreateTLB(setIDIndexBucket),
+		},
+		{
+			number:    23,
+			migration: migration23.MigrateHtlcAttempts,
 		},
 		// Note: There are decred-only changes in the codec which may
 		// require porting when bringing upstream migrations from lnd.
