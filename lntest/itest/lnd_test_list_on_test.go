bcc9f479601c3e0024de10a875ac946ffec9ec38
Elle Mouton <elle.mouton@gmail.com>
2021-08-31 14:48:33 +0200

    lntest: show that multi addresses are not used
    This commit adds an itest to demonstrate that if a peer advertises
    multiplie external IP addresses, then they will not all be used to
    reconnect to the peer during reconnection. This will be fixed in a
    follow-up commit.

--- lntest/itest/lnd_test_list_on_test.go
+++ lntest/itest/lnd_test_list_on_test.go
@@ -56,6 +56,10 @@ var allTestCases = []*testCase{
 		test: testDisconnectingTargetPeer,
 	},
 	{
+		name: "reconnect after ip change",
+		test: testReconnectAfterIPChange,
+	},
+	{
 		name: "graph topology notifications",
 		test: testGraphTopologyNotifications,
 	},
