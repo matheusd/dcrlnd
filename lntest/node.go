be697260d0b3220c5d83ce33171d4daa25b47726
Olaoluwa Osuntokun <laolu32@gmail.com>
2021-10-19 16:19:53 -0700

    rpc: optimize DescribeGraph by caching the response
    Unfortunately, we can't use the graph cache directly here as it doesn't
    include all the information we need, since it only includes the minimal
    amount of information needed for path finding.
    
    Instead, we use a simple mutex guarded cache that evicts itself after a
    certain interval. The default is set small enough that most users
    shouldn't really notice. We also provide a way to disable the cache, and
    also disable the cache in the itests.
    
    Fixes https://github.com/lightningnetwork/lnd/issues/1232

--- lntest/node.go
+++ lntest/node.go
@@ -292,6 +292,7 @@ func (cfg NodeConfig) genArgs() []string {
 	args = append(args, fmt.Sprintf("--invoicemacaroonpath=%v", cfg.InvoiceMacPath))
 	args = append(args, fmt.Sprintf("--trickledelay=%v", trickleDelay))
 	args = append(args, fmt.Sprintf("--profile=%d", cfg.ProfilePort))
+	args = append(args, fmt.Sprintf("--caches.rpc-graph-cache-duration=0"))
 
 	if cfg.RemoteWallet {
 		args = append(args, fmt.Sprintf("--dcrwallet.grpchost=localhost:%d", cfg.WalletPort))
