e4c37eafd254f4e58a3f27b0ad4465800a199c6d
Elle Mouton <elle.mouton@gmail.com>
2021-12-10 13:06:48 +0200

    lnwallet+docs: minrelayfee always above fee floor
    The minimum relay fee is always ensured to be above our fee floor except
    in the very first min relay fee query to bitcoind. This commit ensures
    that the fee floor is respected in this first query.

--- lnwallet/chainfee/minfeemanager_test.go
+++ lnwallet/chainfee/minfeemanager_test.go
@@ -23,8 +23,10 @@ func (m *mockChainBackend) fetchFee() (AtomPerKByte, error) {
 func TestMinFeeManager(t *testing.T) {
 	t.Parallel()
 
+	// Initialize the mock backend and let it have a minimum fee rate
+	// below our fee floor.
 	chainBackend := &mockChainBackend{
-		minFee: AtomPerKByte(10000),
+		minFee: FeePerKBFloor - 1,
 	}
 
 	// Initialise the min fee manager. This should call the chain backend
@@ -36,11 +38,14 @@ func TestMinFeeManager(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, 1, chainBackend.callCount)
 
+	// Check that the minimum fee rate is clamped by our fee floor.
+	require.Equal(t, feeManager.minFeePerKW, FeePerKBFloor)
+
 	// If the fee is requested again, the stored fee should be returned
 	// and the chain backend should not be queried.
 	chainBackend.minFee = AtomPerKByte(20000)
 	minFee := feeManager.fetchMinFee()
-	require.Equal(t, minFee, AtomPerKByte(10000))
+	require.Equal(t, minFee, FeePerKBFloor)
 	require.Equal(t, 1, chainBackend.callCount)
 
 	// Fake the passing of time.
