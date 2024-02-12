lightningnetwork/lnd

channeldb/test: test with postgres

channeldb/setup_test.go
@@ -1,0 +1,11 @@
+package channeldb
+
+import (
+	"testing"
+
+	"github.com/lightningnetwork/lnd/kvdb"
+)
+
+func TestMain(m *testing.M) {
+	kvdb.RunTests(m)
+}

contractcourt/setup_test.go
@@ -1,0 +1,11 @@
+package contractcourt
+
+import (
+	"testing"
+
+	"github.com/lightningnetwork/lnd/kvdb"
+)
+
+func TestMain(m *testing.M) {
+	kvdb.RunTests(m)
+}

kvdb/kvdb_no_postgres.go
@@ -1,0 +1,19 @@
+// +build !kvdb_postgres
+
+package kvdb
+
+import (
+	"errors"
+
+	"github.com/lightningnetwork/lnd/kvdb/postgres"
+)
+
+const PostgresBackend = false
+
+func NewPostgresFixture(dbName string) (postgres.Fixture, error) {
+	return nil, errors.New("postgres backend not available")
+}
+
+func StartEmbeddedPostgres() (func() error, error) {
+	return nil, errors.New("postgres backend not available")
+}

kvdb/kvdb_postgres.go
@@ -1,0 +1,15 @@
+// +build kvdb_postgres
+
+package kvdb
+
+import "github.com/lightningnetwork/lnd/kvdb/postgres"
+
+const PostgresBackend = true
+
+func NewPostgresFixture(dbName string) (postgres.Fixture, error) {
+	return postgres.NewFixture(dbName)
+}
+
+func StartEmbeddedPostgres() (func() error, error) {
+	return postgres.StartEmbeddedPostgres()
+}

routing/setup_test.go
@@ -1,0 +1,11 @@
+package routing
+
+import (
+	"testing"
+
+	"github.com/lightningnetwork/lnd/kvdb"
+)
+
+func TestMain(m *testing.M) {
+	kvdb.RunTests(m)
+}

sweep/setup_test.go
@@ -1,0 +1,11 @@
+package sweep
+
+import (
+	"testing"
+
+	"github.com/lightningnetwork/lnd/kvdb"
+)
+
+func TestMain(m *testing.M) {
+	kvdb.RunTests(m)
+}

matheusd/dcrlnd

channeldb/setup_test.go
@@ -1,0 +1,11 @@
+package channeldb
+
+import (
+	"testing"
+
+	"github.com/decred/dcrlnd/kvdb"
+)
+
+func TestMain(m *testing.M) {
+	kvdb.RunTests(m)
+}

contractcourt/setup_test.go
@@ -1,0 +1,11 @@
+package contractcourt
+
+import (
+	"testing"
+
+	"github.com/decred/dcrlnd/kvdb"
+)
+
+func TestMain(m *testing.M) {
+	kvdb.RunTests(m)
+}

kvdb/kvdb_no_postgres.go
@@ -1,0 +1,20 @@
+//go:build !kvdb_postgres
+// +build !kvdb_postgres
+
+package kvdb
+
+import (
+	"errors"
+
+	"github.com/decred/dcrlnd/kvdb/postgres"
+)
+
+const PostgresBackend = false
+
+func NewPostgresFixture(dbName string) (postgres.Fixture, error) {
+	return nil, errors.New("postgres backend not available")
+}
+
+func StartEmbeddedPostgres() (func() error, error) {
+	return nil, errors.New("postgres backend not available")
+}

kvdb/kvdb_postgres.go
@@ -1,0 +1,16 @@
+//go:build kvdb_postgres
+// +build kvdb_postgres
+
+package kvdb
+
+import "github.com/decred/dcrlnd/kvdb/postgres"
+
+const PostgresBackend = true
+
+func NewPostgresFixture(dbName string) (postgres.Fixture, error) {
+	return postgres.NewFixture(dbName)
+}
+
+func StartEmbeddedPostgres() (func() error, error) {
+	return postgres.StartEmbeddedPostgres()
+}

routing/setup_test.go
@@ -1,0 +1,11 @@
+package routing
+
+import (
+	"testing"
+
+	"github.com/decred/dcrlnd/kvdb"
+)
+
+func TestMain(m *testing.M) {
+	kvdb.RunTests(m)
+}

sweep/setup_test.go
@@ -1,0 +1,11 @@
+package sweep
+
+import (
+	"testing"
+
+	"github.com/decred/dcrlnd/kvdb"
+)
+
+func TestMain(m *testing.M) {
+	kvdb.RunTests(m)
+}