lightningnetwork/lnd

itest: refactor tests to be re-used

lntest/itest/lnd_onchain_test.go
@@ -22,6 +22,14 @@ import (
 //
 // TODO(wilmer): Add RBF case once btcd supports it.
 func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
+	runCPFP(net, t, net.Alice, net.Bob)
+}
+
+// runCPFP ensures that the daemon can bump an unconfirmed  transaction's fee
+// rate by broadcasting a Child-Pays-For-Parent (CPFP) transaction.
+func runCPFP(net *lntest.NetworkHarness, t *harnessTest,
+	alice, bob *lntest.HarnessNode) {
+
 	// Skip this test for neutrino, as it's not aware of mempool
 	// transactions.
 	if net.BackendCfg.Name() == lntest.NeutrinoBackendName {
@@ -31,14 +39,14 @@ func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
 	// We'll start the test by sending Alice some coins, which she'll use to
 	// send to Bob.
 	ctxb := context.Background()
-	net.SendCoins(t.t, btcutil.SatoshiPerBitcoin, net.Alice)
+	net.SendCoins(t.t, btcutil.SatoshiPerBitcoin, alice)
 
 	// Create an address for Bob to send the coins to.
 	addrReq := &lnrpc.NewAddressRequest{
 		Type: lnrpc.AddressType_WITNESS_PUBKEY_HASH,
 	}
 	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
-	resp, err := net.Bob.NewAddress(ctxt, addrReq)
+	resp, err := bob.NewAddress(ctxt, addrReq)
 	if err != nil {
 		t.Fatalf("unable to get new address for bob: %v", err)
 	}
@@ -50,7 +58,7 @@ func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
 		Amount: btcutil.SatoshiPerBitcoin,
 	}
 	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
-	if _, err = net.Alice.SendCoins(ctxt, sendReq); err != nil {
+	if _, err = alice.SendCoins(ctxt, sendReq); err != nil {
 		t.Fatalf("unable to send coins to bob: %v", err)
 	}
 
@@ -88,7 +96,7 @@ func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
 		TxidBytes:   txid[:],
 		OutputIndex: uint32(bobOutputIdx),
 	}
-	assertWalletUnspent(t, net.Bob, op)
+	assertWalletUnspent(t, bob, op)
 
 	// We'll attempt to bump the fee of this transaction by performing a
 	// CPFP from Alice's point of view.
@@ -99,7 +107,7 @@ func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
 		),
 	}
 	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
-	_, err = net.Bob.WalletKitClient.BumpFee(ctxt, bumpFeeReq)
+	_, err = bob.WalletKitClient.BumpFee(ctxt, bumpFeeReq)
 	if err != nil {
 		t.Fatalf("unable to bump fee: %v", err)
 	}
@@ -115,7 +123,7 @@ func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
 	// UtxoSweeper. We'll ensure it's using the fee rate specified.
 	pendingSweepsReq := &walletrpc.PendingSweepsRequest{}
 	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
-	pendingSweepsResp, err := net.Bob.WalletKitClient.PendingSweeps(
+	pendingSweepsResp, err := bob.WalletKitClient.PendingSweeps(
 		ctxt, pendingSweepsReq,
 	)
 	if err != nil {
@@ -146,7 +154,7 @@ func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
 	err = wait.NoError(func() error {
 		req := &walletrpc.PendingSweepsRequest{}
 		ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
-		resp, err := net.Bob.WalletKitClient.PendingSweeps(ctxt, req)
+		resp, err := bob.WalletKitClient.PendingSweeps(ctxt, req)
 		if err != nil {
 			return fmt.Errorf("unable to retrieve bob's pending "+
 				"sweeps: %v", err)

lntest/itest/lnd_psbt_test.go
@@ -19,9 +19,6 @@ import (
 // by using a Partially Signed Bitcoin Transaction that funds the channel
 // multisig funding output.
 func testPsbtChanFunding(net *lntest.NetworkHarness, t *harnessTest) {
-	ctxb := context.Background()
-	const chanSize = funding.MaxBtcFundingAmount
-
 	// First, we'll create two new nodes that we'll use to open channels
 	// between for this test. Dave gets some coins that will be used to
 	// fund the PSBT, just to make sure that Carol has an empty wallet.
@@ -31,6 +28,17 @@ func testPsbtChanFunding(net *lntest.NetworkHarness, t *harnessTest) {
 	dave := net.NewNode(t.t, "dave", nil)
 	defer shutdownAndAssert(net, t, dave)
 
+	runPsbtChanFunding(net, t, carol, dave)
+}
+
+// runPsbtChanFunding makes sure a channel can be opened between carol and dave
+// by using a Partially Signed Bitcoin Transaction that funds the channel
+// multisig funding output.
+func runPsbtChanFunding(net *lntest.NetworkHarness, t *harnessTest, carol,
+	dave *lntest.HarnessNode) {
+
+	ctxb := context.Background()
+	const chanSize = funding.MaxBtcFundingAmount
 	net.SendCoins(t.t, btcutil.SatoshiPerBitcoin, dave)
 
 	// Before we start the test, we'll ensure both sides are connected so

lntest/itest/lnd_wallet_import_test.go
@@ -594,9 +594,6 @@ func testWalletImportAccount(net *lntest.NetworkHarness, t *harnessTest) {
 func testWalletImportAccountScenario(net *lntest.NetworkHarness, t *harnessTest,
 	addrType walletrpc.AddressType) {
 
-	ctxb := context.Background()
-	const utxoAmt int64 = btcutil.SatoshiPerBitcoin
-
 	// We'll start our test by having two nodes, Carol and Dave. Carol's
 	// default wallet account will be imported into Dave's node.
 	carol := net.NewNode(t.t, "carol", nil)
@@ -604,6 +601,15 @@ func testWalletImportAccountScenario(net *lntest.NetworkHarness, t *harnessTest,
 
 	dave := net.NewNode(t.t, "dave", nil)
 	defer shutdownAndAssert(net, t, dave)
+
+	runWalletImportAccountScenario(net, t, addrType, carol, dave)
+}
+
+func runWalletImportAccountScenario(net *lntest.NetworkHarness, t *harnessTest,
+	addrType walletrpc.AddressType, carol, dave *lntest.HarnessNode) {
+
+	ctxb := context.Background()
+	const utxoAmt int64 = btcutil.SatoshiPerBitcoin
 
 	ctxt, cancel := context.WithTimeout(ctxb, defaultTimeout)
 	defer cancel()

matheusd/dcrlnd

lntest/itest/lnd_onchain_test.go
@@ -24,6 +24,14 @@ import (
 //
 // TODO(wilmer): Add RBF case once btcd supports it.
 func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
+	runCPFP(net, t, net.Alice, net.Bob)
+}
+
+// runCPFP ensures that the daemon can bump an unconfirmed  transaction's fee
+// rate by broadcasting a Child-Pays-For-Parent (CPFP) transaction.
+func runCPFP(net *lntest.NetworkHarness, t *harnessTest,
+	alice, bob *lntest.HarnessNode) {
+
 	// Skip this test for neutrino, as it's not aware of mempool
 	// transactions.
 	if net.BackendCfg.Name() == "spv" {
@@ -33,14 +41,14 @@ func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
 	// We'll start the test by sending Alice some coins, which she'll use to
 	// send to Bob.
 	ctxb := context.Background()
-	net.SendCoins(t.t, dcrutil.AtomsPerCoin, net.Alice)
+	net.SendCoins(t.t, dcrutil.AtomsPerCoin, alice)
 
 	// Create an address for Bob to send the coins to.
 	addrReq := &lnrpc.NewAddressRequest{
 		Type: lnrpc.AddressType_PUBKEY_HASH,
 	}
 	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
-	resp, err := net.Bob.NewAddress(ctxt, addrReq)
+	resp, err := bob.NewAddress(ctxt, addrReq)
 	if err != nil {
 		t.Fatalf("unable to get new address for bob: %v", err)
 	}
@@ -52,7 +60,7 @@ func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
 		Amount: dcrutil.AtomsPerCoin,
 	}
 	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
-	if _, err = net.Alice.SendCoins(ctxt, sendReq); err != nil {
+	if _, err = alice.SendCoins(ctxt, sendReq); err != nil {
 		t.Fatalf("unable to send coins to bob: %v", err)
 	}
 
@@ -90,7 +98,7 @@ func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
 		TxidBytes:   txid[:],
 		OutputIndex: uint32(bobOutputIdx),
 	}
-	assertWalletUnspent(t, net.Bob, op)
+	assertWalletUnspent(t, bob, op)
 
 	// We'll attempt to bump the fee of this transaction by performing a
 	// CPFP from Alice's point of view.
@@ -99,7 +107,7 @@ func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
 		AtomsPerByte: uint32(sweep.DefaultMaxFeeRate / 1000),
 	}
 	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
-	_, err = net.Bob.WalletKitClient.BumpFee(ctxt, bumpFeeReq)
+	_, err = bob.WalletKitClient.BumpFee(ctxt, bumpFeeReq)
 	if err != nil {
 		t.Fatalf("unable to bump fee: %v", err)
 	}
@@ -115,7 +123,7 @@ func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
 	// UtxoSweeper. We'll ensure it's using the fee rate specified.
 	pendingSweepsReq := &walletrpc.PendingSweepsRequest{}
 	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
-	pendingSweepsResp, err := net.Bob.WalletKitClient.PendingSweeps(
+	pendingSweepsResp, err := bob.WalletKitClient.PendingSweeps(
 		ctxt, pendingSweepsReq,
 	)
 	if err != nil {
@@ -146,7 +154,7 @@ func testCPFP(net *lntest.NetworkHarness, t *harnessTest) {
 	err = wait.NoError(func() error {
 		req := &walletrpc.PendingSweepsRequest{}
 		ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
-		resp, err := net.Bob.WalletKitClient.PendingSweeps(ctxt, req)
+		resp, err := bob.WalletKitClient.PendingSweeps(ctxt, req)
 		if err != nil {
 			return fmt.Errorf("unable to retrieve bob's pending "+
 				"sweeps: %v", err)

lntest/itest/lnd_psbt_test.go
@@ -23,7 +23,6 @@ import (
 func testPsbtChanFunding(net *lntest.NetworkHarness, t *harnessTest) {
 	t.Skipf("psbt is not currently implemented in dcrlnd")
 
-	ctxb := context.Background()
 	const chanSize = defaultChanAmt
 
 	// First, we'll create two new nodes that we'll use to open channels
@@ -35,6 +34,18 @@ func testPsbtChanFunding(net *lntest.NetworkHarness, t *harnessTest) {
 	dave := net.NewNode(t.t, "dave", nil)
 	defer shutdownAndAssert(net, t, dave)
 
+	net.SendCoins(t.t, dcrutil.AtomsPerCoin, dave)
+	runPsbtChanFunding(net, t, carol, dave)
+}
+
+// runPsbtChanFunding makes sure a channel can be opened between carol and dave
+// by using a Partially Signed Bitcoin Transaction that funds the channel
+// multisig funding output.
+func runPsbtChanFunding(net *lntest.NetworkHarness, t *harnessTest, carol,
+	dave *lntest.HarnessNode) {
+
+	ctxb := context.Background()
+	const chanSize = funding.MaxDecredFundingAmount
 	net.SendCoins(t.t, dcrutil.AtomsPerCoin, dave)
 
 	// Before we start the test, we'll ensure both sides are connected so

lntest/itest/lnd_wallet_import_test.go
@@ -228,8 +228,8 @@ func psbtSendFromImportedAccount(t *harnessTest, srcNode, destNode,
 				},
 			},
 		},
-		Fees: &walletrpc.FundPsbtRequest_SatPerVbyte{
-			SatPerVbyte: 1,
+		Fees: &walletrpc.FundPsbtRequest_AtomsPerByte{
+			AtomsPerByte: 1,
 		},
 		Account: account,
 	}
@@ -358,8 +358,8 @@ func fundChanAndCloseFromImportedAccount(t *harnessTest, srcNode, destNode,
 		Template: &walletrpc.FundPsbtRequest_Psbt{
 			Psbt: rawPsbt,
 		},
-		Fees: &walletrpc.FundPsbtRequest_SatPerVbyte{
-			SatPerVbyte: 1,
+		Fees: &walletrpc.FundPsbtRequest_AtomsPerByte{
+			AtomsPerByte: 1,
 		},
 		Account: account,
 	}
@@ -596,9 +596,6 @@ func testWalletImportAccount(net *lntest.NetworkHarness, t *harnessTest) {
 func testWalletImportAccountScenario(net *lntest.NetworkHarness, t *harnessTest,
 	addrType walletrpc.AddressType) {
 
-	ctxb := context.Background()
-	const utxoAmt int64 = dcrutil.AtomsPerCoin
-
 	// We'll start our test by having two nodes, Carol and Dave. Carol's
 	// default wallet account will be imported into Dave's node.
 	carol := net.NewNode(t.t, "carol", nil)
@@ -606,6 +603,15 @@ func testWalletImportAccountScenario(net *lntest.NetworkHarness, t *harnessTest,
 
 	dave := net.NewNode(t.t, "dave", nil)
 	defer shutdownAndAssert(net, t, dave)
+
+	runWalletImportAccountScenario(net, t, addrType, carol, dave)
+}
+
+func runWalletImportAccountScenario(net *lntest.NetworkHarness, t *harnessTest,
+	addrType walletrpc.AddressType, carol, dave *lntest.HarnessNode) {
+
+	ctxb := context.Background()
+	const utxoAmt int64 = dcrutil.AtomsPerCoin
 
 	ctxt, cancel := context.WithTimeout(ctxb, defaultTimeout)
 	defer cancel()