wilmerpaulino/lnd

lnwire: add new ChannelType field as TLV record to Open/AcceptChannel
In this commit, we add a new ChannelType field as a new TLV record to
the OpenChannel message. During this change, we make a few tweaks to the
generic TLV encode/decode methods for the ExtraOpaqueData struct to have
it work on the level of tlv.RecordProducer instead of tlv.Record, as
this reduces line noise a bit.

We also partially undo existing logic that would attempt to "prepend"
any new TLV records to the end of the ExtraOpaqueData if one was already
present within the struct. This is based on the assumption that if we've
read a message from disk to order to re-send/transmit it, then the
ExtraOpaqueData is fully populated so we'll write that as is. Otherwise,
a message is being encoded for the first time, and we expect all fields
that are known TLV fields to be specified within the struct itself.

This change required the unit tests to be modified slightly, as we'll
always encode a fresh set of TLV records if none was already specified
within the struct.

lnwire/accept_channel.go
@@ -1,11 +1,11 @@
 package lnwire
 
 import (
-	"fmt"
 	"io"
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcutil"
+	"github.com/lightningnetwork/lnd/tlv"
 )
 
 // AcceptChannel is the message Bob sends to Alice after she initiates the
@@ -94,6 +94,10 @@ type AcceptChannel struct {
 	// and its length followed by the script will be written if it is set.
 	UpfrontShutdownScript DeliveryAddress
 
+	// ChannelType is the explicit channel type the initiator wishes to
+	// open.
+	ChannelType *ChannelType
+
 	// ExtraData is the set of data that was appended to this message to
 	// fill out the full maximum transport message size. These fields can
 	// be used to specify optional data such as custom TLV fields.
@@ -116,11 +120,11 @@ var _ Message = (*AcceptChannel)(nil)
 //
 // This is part of the lnwire.Message interface.
 func (a *AcceptChannel) Encode(w io.Writer, pver uint32) error {
-	// Since the upfront script is encoded as a TLV record, concatenate it
-	// with the ExtraData, and write them as one.
-	tlvRecords, err := packShutdownScript(
-		a.UpfrontShutdownScript, a.ExtraData,
-	)
+	recordProducers := []tlv.RecordProducer{&a.UpfrontShutdownScript}
+	if a.ChannelType != nil {
+		recordProducers = append(recordProducers, a.ChannelType)
+	}
+	err := EncodeMessageExtraData(&a.ExtraData, recordProducers...)
 	if err != nil {
 		return err
 	}
@@ -140,7 +144,7 @@ func (a *AcceptChannel) Encode(w io.Writer, pver uint32) error {
 		a.DelayedPaymentPoint,
 		a.HtlcPoint,
 		a.FirstCommitmentPoint,
-		tlvRecords,
+		a.ExtraData,
 	)
 }
 
@@ -179,72 +183,24 @@ func (a *AcceptChannel) Decode(r io.Reader, pver uint32) error {
 		return err
 	}
 
-	a.UpfrontShutdownScript, a.ExtraData, err = parseShutdownScript(
-		tlvRecords,
+	// Next we'll parse out the set of known records, keeping the raw tlv
+	// bytes untouched to ensure we don't drop any bytes erroneously.
+	var chanType ChannelType
+	typeMap, err := tlvRecords.ExtractRecords(
+		&a.UpfrontShutdownScript, &chanType,
 	)
 	if err != nil {
 		return err
 	}
 
+	// Set the corresponding TLV types if they were included in the stream.
+	if _, ok := typeMap[ChannelTypeRecordType]; ok {
+		a.ChannelType = &chanType
+	}
+
+	a.ExtraData = tlvRecords
+
 	return nil
-}
-
-// packShutdownScript takes an upfront shutdown script and an opaque data blob
-// and concatenates them.
-func packShutdownScript(addr DeliveryAddress, extraData ExtraOpaqueData) (
-	ExtraOpaqueData, error) {
-
-	// We'll always write the upfront shutdown script record, regardless of
-	// the script being empty.
-	var tlvRecords ExtraOpaqueData
-
-	// Pack it into a data blob as a TLV record.
-	err := tlvRecords.PackRecords(addr.NewRecord())
-	if err != nil {
-		return nil, fmt.Errorf("unable to pack upfront shutdown "+
-			"script as TLV record: %v", err)
-	}
-
-	// Concatenate the remaining blob with the shutdown script record.
-	tlvRecords = append(tlvRecords, extraData...)
-	return tlvRecords, nil
-}
-
-// parseShutdownScript reads and extract the upfront shutdown script from the
-// passe data blob. It returns the script, if any, and the remainder of the
-// data blob.
-//
-// This can be used to parse extra data for the OpenChannel and AcceptChannel
-// messages, where the shutdown script is mandatory if extra TLV data is
-// present.
-func parseShutdownScript(tlvRecords ExtraOpaqueData) (DeliveryAddress,
-	ExtraOpaqueData, error) {
-
-	// If no TLV data is present there can't be any script available.
-	if len(tlvRecords) == 0 {
-		return nil, tlvRecords, nil
-	}
-
-	// Otherwise the shutdown script MUST be present.
-	var addr DeliveryAddress
-	tlvs, err := tlvRecords.ExtractRecords(addr.NewRecord())
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Not among TLV records, this means the data was invalid.
-	if _, ok := tlvs[DeliveryAddrType]; !ok {
-		return nil, nil, fmt.Errorf("no shutdown script in non-empty " +
-			"data blob")
-	}
-
-	// Now that we have retrieved the address (which can be zero-length),
-	// we'll remove the bytes encoding it from the TLV data before
-	// returning it.
-	addrLen := len(addr)
-	tlvRecords = tlvRecords[addrLen+2:]
-
-	return addr, tlvRecords, nil
 }
 
 // MsgType returns the MessageType code which uniquely identifies this message

lnwire/open_channel.go
@@ -6,6 +6,7 @@ import (
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcutil"
+	"github.com/lightningnetwork/lnd/tlv"
 )
 
 // FundingFlag represents the possible bit mask values for the ChannelFlags
@@ -129,6 +130,10 @@ type OpenChannel struct {
 	// and its length followed by the script will be written if it is set.
 	UpfrontShutdownScript DeliveryAddress
 
+	// ChannelType is the explicit channel type the initiator wishes to
+	// open.
+	ChannelType *ChannelType
+
 	// ExtraData is the set of data that was appended to this message to
 	// fill out the full maximum transport message size. These fields can
 	// be used to specify optional data such as custom TLV fields.
@@ -151,11 +156,11 @@ var _ Message = (*OpenChannel)(nil)
 //
 // This is part of the lnwire.Message interface.
 func (o *OpenChannel) Encode(w io.Writer, pver uint32) error {
-	// Since the upfront script is encoded as a TLV record, concatenate it
-	// with the ExtraData, and write them as one.
-	tlvRecords, err := packShutdownScript(
-		o.UpfrontShutdownScript, o.ExtraData,
-	)
+	recordProducers := []tlv.RecordProducer{&o.UpfrontShutdownScript}
+	if o.ChannelType != nil {
+		recordProducers = append(recordProducers, o.ChannelType)
+	}
+	err := EncodeMessageExtraData(&o.ExtraData, recordProducers...)
 	if err != nil {
 		return err
 	}
@@ -179,7 +184,7 @@ func (o *OpenChannel) Encode(w io.Writer, pver uint32) error {
 		o.HtlcPoint,
 		o.FirstCommitmentPoint,
 		o.ChannelFlags,
-		tlvRecords,
+		o.ExtraData,
 	)
 }
 
@@ -222,12 +227,22 @@ func (o *OpenChannel) Decode(r io.Reader, pver uint32) error {
 		return err
 	}
 
-	o.UpfrontShutdownScript, o.ExtraData, err = parseShutdownScript(
-		tlvRecords,
+	// Next we'll parse out the set of known records, keeping the raw tlv
+	// bytes untouched to ensure we don't drop any bytes erroneously.
+	var chanType ChannelType
+	typeMap, err := tlvRecords.ExtractRecords(
+		&o.UpfrontShutdownScript, &chanType,
 	)
 	if err != nil {
 		return err
 	}
+
+	// Set the corresponding TLV types if they were included in the stream.
+	if _, ok := typeMap[ChannelTypeRecordType]; ok {
+		o.ChannelType = &chanType
+	}
+
+	o.ExtraData = tlvRecords
 
 	return nil
 }

matheusd/dcrlnd

lnwire/accept_channel.go
@@ -2,11 +2,11 @@ package lnwire
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 	"github.com/decred/dcrd/dcrutil/v4"
+	"github.com/decred/dcrlnd/tlv"
 )
 
 // AcceptChannel is the message Bob sends to Alice after she initiates the
@@ -95,6 +95,10 @@ type AcceptChannel struct {
 	// and its length followed by the script will be written if it is set.
 	UpfrontShutdownScript DeliveryAddress
 
+	// ChannelType is the explicit channel type the initiator wishes to
+	// open.
+	ChannelType *ChannelType
+
 	// ExtraData is the set of data that was appended to this message to
 	// fill out the full maximum transport message size. These fields can
 	// be used to specify optional data such as custom TLV fields.
@@ -117,11 +121,11 @@ var _ Message = (*AcceptChannel)(nil)
 //
 // This is part of the lnwire.Message interface.
 func (a *AcceptChannel) Encode(w *bytes.Buffer, pver uint32) error {
-	// Since the upfront script is encoded as a TLV record, concatenate it
-	// with the ExtraData, and write them as one.
-	tlvRecords, err := packShutdownScript(
-		a.UpfrontShutdownScript, a.ExtraData,
-	)
+	recordProducers := []tlv.RecordProducer{&a.UpfrontShutdownScript}
+	if a.ChannelType != nil {
+		recordProducers = append(recordProducers, a.ChannelType)
+	}
+	err := EncodeMessageExtraData(&a.ExtraData, recordProducers...)
 	if err != nil {
 		return err
 	}
@@ -182,7 +186,7 @@ func (a *AcceptChannel) Encode(w *bytes.Buffer, pver uint32) error {
 		return err
 	}
 
-	return WriteBytes(w, tlvRecords)
+	return WriteBytes(w, a.ExtraData)
 }
 
 // Decode deserializes the serialized AcceptChannel stored in the passed
@@ -220,72 +224,24 @@ func (a *AcceptChannel) Decode(r io.Reader, pver uint32) error {
 		return err
 	}
 
-	a.UpfrontShutdownScript, a.ExtraData, err = parseShutdownScript(
-		tlvRecords,
+	// Next we'll parse out the set of known records, keeping the raw tlv
+	// bytes untouched to ensure we don't drop any bytes erroneously.
+	var chanType ChannelType
+	typeMap, err := tlvRecords.ExtractRecords(
+		&a.UpfrontShutdownScript, &chanType,
 	)
 	if err != nil {
 		return err
 	}
 
+	// Set the corresponding TLV types if they were included in the stream.
+	if val, ok := typeMap[ChannelTypeRecordType]; ok && val == nil {
+		a.ChannelType = &chanType
+	}
+
+	a.ExtraData = tlvRecords
+
 	return nil
-}
-
-// packShutdownScript takes an upfront shutdown script and an opaque data blob
-// and concatenates them.
-func packShutdownScript(addr DeliveryAddress, extraData ExtraOpaqueData) (
-	ExtraOpaqueData, error) {
-
-	// We'll always write the upfront shutdown script record, regardless of
-	// the script being empty.
-	var tlvRecords ExtraOpaqueData
-
-	// Pack it into a data blob as a TLV record.
-	err := tlvRecords.PackRecords(addr.NewRecord())
-	if err != nil {
-		return nil, fmt.Errorf("unable to pack upfront shutdown "+
-			"script as TLV record: %v", err)
-	}
-
-	// Concatenate the remaining blob with the shutdown script record.
-	tlvRecords = append(tlvRecords, extraData...)
-	return tlvRecords, nil
-}
-
-// parseShutdownScript reads and extract the upfront shutdown script from the
-// passe data blob. It returns the script, if any, and the remainder of the
-// data blob.
-//
-// This can be used to parse extra data for the OpenChannel and AcceptChannel
-// messages, where the shutdown script is mandatory if extra TLV data is
-// present.
-func parseShutdownScript(tlvRecords ExtraOpaqueData) (DeliveryAddress,
-	ExtraOpaqueData, error) {
-
-	// If no TLV data is present there can't be any script available.
-	if len(tlvRecords) == 0 {
-		return nil, tlvRecords, nil
-	}
-
-	// Otherwise the shutdown script MUST be present.
-	var addr DeliveryAddress
-	tlvs, err := tlvRecords.ExtractRecords(addr.NewRecord())
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Not among TLV records, this means the data was invalid.
-	if _, ok := tlvs[DeliveryAddrType]; !ok {
-		return nil, nil, fmt.Errorf("no shutdown script in non-empty " +
-			"data blob")
-	}
-
-	// Now that we have retrieved the address (which can be zero-length),
-	// we'll remove the bytes encoding it from the TLV data before
-	// returning it.
-	addrLen := len(addr)
-	tlvRecords = tlvRecords[addrLen+2:]
-
-	return addr, tlvRecords, nil
 }
 
 // MsgType returns the MessageType code which uniquely identifies this message

lnwire/open_channel.go
@@ -7,6 +7,7 @@ import (
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 	"github.com/decred/dcrd/dcrutil/v4"
+	"github.com/decred/dcrlnd/tlv"
 )
 
 // FundingFlag represents the possible bit mask values for the ChannelFlags
@@ -130,6 +131,10 @@ type OpenChannel struct {
 	// and its length followed by the script will be written if it is set.
 	UpfrontShutdownScript DeliveryAddress
 
+	// ChannelType is the explicit channel type the initiator wishes to
+	// open.
+	ChannelType *ChannelType
+
 	// ExtraData is the set of data that was appended to this message to
 	// fill out the full maximum transport message size. These fields can
 	// be used to specify optional data such as custom TLV fields.
@@ -149,14 +154,12 @@ var _ Message = (*OpenChannel)(nil)
 // Encode serializes the target OpenChannel into the passed io.Writer
 // implementation. Serialization will observe the rules defined by the passed
 // protocol version.
-//
-// This is part of the lnwire.Message interface.
 func (o *OpenChannel) Encode(w *bytes.Buffer, pver uint32) error {
-	// Since the upfront script is encoded as a TLV record, concatenate it
-	// with the ExtraData, and write them as one.
-	tlvRecords, err := packShutdownScript(
-		o.UpfrontShutdownScript, o.ExtraData,
-	)
+	recordProducers := []tlv.RecordProducer{&o.UpfrontShutdownScript}
+	if o.ChannelType != nil {
+		recordProducers = append(recordProducers, o.ChannelType)
+	}
+	err := EncodeMessageExtraData(&o.ExtraData, recordProducers...)
 	if err != nil {
 		return err
 	}
@@ -234,7 +237,7 @@ func (o *OpenChannel) Encode(w *bytes.Buffer, pver uint32) error {
 		return err
 	}
 
-	return WriteBytes(w, tlvRecords)
+	return WriteBytes(w, o.ExtraData)
 }
 
 // Decode deserializes the serialized OpenChannel stored in the passed
@@ -276,12 +279,22 @@ func (o *OpenChannel) Decode(r io.Reader, pver uint32) error {
 		return err
 	}
 
-	o.UpfrontShutdownScript, o.ExtraData, err = parseShutdownScript(
-		tlvRecords,
+	// Next we'll parse out the set of known records, keeping the raw tlv
+	// bytes untouched to ensure we don't drop any bytes erroneously.
+	var chanType ChannelType
+	typeMap, err := tlvRecords.ExtractRecords(
+		&o.UpfrontShutdownScript, &chanType,
 	)
 	if err != nil {
 		return err
 	}
+
+	// Set the corresponding TLV types if they were included in the stream.
+	if val, ok := typeMap[ChannelTypeRecordType]; ok && val == nil {
+		o.ChannelType = &chanType
+	}
+
+	o.ExtraData = tlvRecords
 
 	return nil
 }