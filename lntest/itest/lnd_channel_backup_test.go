LightningNetwork/lnd

itest: fix anchor SCB sweep tests
Fixes another flake related to Neutrino and how it handles UTXOs.

lntest/itest/lnd_channel_backup_test.go
@@ -847,6 +847,12 @@ func testChanRestoreScenario(t *harnessTest, net *lntest.NetworkHarness,
 	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
 	net.SendCoins(ctxt, t.t, btcutil.SatoshiPerBitcoin, carol)
 
+	// For the anchor output case we need two UTXOs for Carol so she can
+	// sweep both the local and remote anchor.
+	if testCase.anchorCommit {
+		net.SendCoins(ctxt, t.t, btcutil.SatoshiPerBitcoin, carol)
+	}
+
 	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
 	net.SendCoins(ctxt, t.t, btcutil.SatoshiPerBitcoin, dave)
 
@@ -1076,6 +1082,12 @@ func testChanRestoreScenario(t *harnessTest, net *lntest.NetworkHarness,
 	// let's start up Carol again.
 	err = restartCarol()
 	require.NoError(t.t, err)
+
+	numUTXOs := 1
+	if testCase.anchorCommit {
+		numUTXOs = 2
+	}
+	assertNumUTXOs(t.t, carol, numUTXOs)
 
 	// Now that we have our new node up, we expect that it'll re-connect to
 	// Carol automatically based on the restored backup.

matheusd/dcrlnd

lntest/itest/lnd_channel_backup_test.go
@@ -848,6 +848,12 @@ func testChanRestoreScenario(t *harnessTest, net *lntest.NetworkHarness,
 	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
 	net.SendCoins(ctxt, t.t, dcrutil.AtomsPerCoin, carol)
 
+	// For the anchor output case we need two UTXOs for Carol so she can
+	// sweep both the local and remote anchor.
+	if testCase.anchorCommit {
+		net.SendCoins(ctxt, t.t, dcrutil.AtomsPerCoin, carol)
+	}
+
 	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
 	net.SendCoins(ctxt, t.t, dcrutil.AtomsPerCoin, dave)
 
@@ -1087,6 +1093,12 @@ func testChanRestoreScenario(t *harnessTest, net *lntest.NetworkHarness,
 	// let's start up Carol again.
 	err = restartCarol()
 	require.NoError(t.t, err)
+
+	numUTXOs := 1
+	if testCase.anchorCommit {
+		numUTXOs = 2
+	}
+	assertNumUTXOs(t.t, carol, numUTXOs)
 
 	// Now that we have our new node up, we expect that it'll re-connect to
 	// Carol automatically based on the restored backup.