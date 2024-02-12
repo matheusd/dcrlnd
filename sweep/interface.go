lightningnetwork/lnd

sweep: remove all unconfirmed descendant transactions when a sweep conflicts
Before this commit, we we were trying to sweep an anchor output, and
that output was spent by someone else (not the sweeper), then we would
report this back to the original resolver (allowing it to be cleaned
up), and also remove the set of inputs spent by that transaction from
the set we need to sweep.

However, it's possible that if a user is spending unconfirmed outputs,
then the wallet is holding onto an invalid transaction, as the outputs
that were used as inputs have been double spent elsewhere.

In this commit, we fix this issue by recursively removing all descendant
transactions of our past sweeps that have an intersecting input set as
the spending transaction. In cases where a user spent an unconfirmed
output to funding a channel, and that output was a descendant of the now
swept anchor output, the funds will now properly be marked as available.

Fixes #6241

sweep/interface.go
@@ -1,6 +1,7 @@
 package sweep
 
 import (
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/lightningnetwork/lnd/lnwallet"
 )
@@ -25,4 +26,13 @@ type Wallet interface {
 	// ability to execute a function closure under an exclusive coin
 	// selection lock.
 	WithCoinSelectLock(f func() error) error
+
+	// RemoveDescendants removes any wallet transactions that spends
+	// outputs created by the specified transaction.
+	RemoveDescendants(*wire.MsgTx) error
+
+	// FetchTx returns the transaction that corresponds to the transaction
+	// hash passed in. If the transaction can't be found then a nil
+	// transaction pointer is returned.
+	FetchTx(chainhash.Hash) (*wire.MsgTx, error)
 }

matheusd/dcrlnd

sweep/interface.go
@@ -1,6 +1,7 @@
 package sweep
 
 import (
+	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/lnwallet"
 )
@@ -32,4 +33,13 @@ type Wallet interface {
 	// ability to execute a function closure under an exclusive coin
 	// selection lock.
 	WithCoinSelectLock(f func() error) error
+
+	// RemoveDescendants removes any wallet transactions that spends
+	// outputs created by the specified transaction.
+	RemoveDescendants(*wire.MsgTx) error
+
+	// FetchTx returns the transaction that corresponds to the transaction
+	// hash passed in. If the transaction can't be found then a nil
+	// transaction pointer is returned.
+	FetchTx(chainhash.Hash) (*wire.MsgTx, error)
 }