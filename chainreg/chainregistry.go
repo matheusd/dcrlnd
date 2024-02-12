lightningnetwork/lnd

multi: use kvdb.Backend for height hint DB
In order to separate our databases more clearly, we refactor the height
hint cache DB to use a kvdb backend instead of the channel DB instance
directly.

chainreg/chainregistry.go
@@ -28,6 +28,7 @@ import (
 	"github.com/lightningnetwork/lnd/htlcswitch"
 	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/keychain"
+	"github.com/lightningnetwork/lnd/kvdb"
 	"github.com/lightningnetwork/lnd/lncfg"
 	"github.com/lightningnetwork/lnd/lnwallet"
 	"github.com/lightningnetwork/lnd/lnwallet/btcwallet"
@@ -70,7 +71,7 @@ type Config struct {
 
 	// HeightHintDB is a pointer to the database that stores the height
 	// hints.
-	HeightHintDB *channeldb.DB
+	HeightHintDB kvdb.Backend
 
 	// ChanStateDB is a pointer to the database that stores the channel
 	// state.

lnd.go
@@ -43,6 +43,7 @@ import (
 	"github.com/lightningnetwork/lnd/chanacceptor"
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/keychain"
+	"github.com/lightningnetwork/lnd/kvdb"
 	"github.com/lightningnetwork/lnd/lncfg"
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lnwallet"
@@ -706,7 +707,7 @@ func Main(cfg *Config, lisCfg ListenerCfg, interceptor signal.Interceptor) error
 		LitecoindMode:               cfg.LitecoindMode,
 		BtcdMode:                    cfg.BtcdMode,
 		LtcdMode:                    cfg.LtcdMode,
-		HeightHintDB:                dbs.graphDB,
+		HeightHintDB:                dbs.heightHintDB,
 		ChanStateDB:                 dbs.chanStateDB,
 		PrivateWalletPw:             privateWalletPw,
 		PublicWalletPw:              publicWalletPw,
@@ -1624,8 +1625,9 @@ func waitForWalletPassword(cfg *Config,
 // databaseInstances is a struct that holds all instances to the actual
 // databases that are used in lnd.
 type databaseInstances struct {
-	graphDB     *channeldb.DB
-	chanStateDB *channeldb.DB
+	graphDB      *channeldb.DB
+	chanStateDB  *channeldb.DB
+	heightHintDB kvdb.Backend
 }
 
 // initializeDatabases extracts the current databases that we'll use for normal
@@ -1655,7 +1657,9 @@ func initializeDatabases(ctx context.Context,
 	// If the remoteDB is nil, then we'll just open a local DB as normal,
 	// having the remote and local pointer be the exact same instance.
 	var (
-		dbs        = &databaseInstances{}
+		dbs = &databaseInstances{
+			heightHintDB: databaseBackends.HeightHintDB,
+		}
 		closeFuncs []func()
 	)
 	if databaseBackends.ChanStateDB == nil {

matheusd/dcrlnd

chainreg/chainregistry.go
@@ -25,6 +25,7 @@ import (
 	"github.com/decred/dcrlnd/htlcswitch"
 	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/keychain"
+	"github.com/decred/dcrlnd/kvdb"
 	"github.com/decred/dcrlnd/lncfg"
 	"github.com/decred/dcrlnd/lnwallet"
 	"github.com/decred/dcrlnd/lnwallet/chainfee"
@@ -58,7 +59,7 @@ type Config struct {
 
 	// HeightHintDB is a pointer to the database that stores the height
 	// hints.
-	HeightHintDB *channeldb.DB
+	HeightHintDB kvdb.Backend
 
 	// ChanStateDB is a pointer to the database that stores the channel
 	// state.

lnd.go
@@ -39,6 +39,7 @@ import (
 	"github.com/decred/dcrlnd/chanacceptor"
 	"github.com/decred/dcrlnd/channeldb"
 	"github.com/decred/dcrlnd/keychain"
+	"github.com/decred/dcrlnd/kvdb"
 	"github.com/decred/dcrlnd/lncfg"
 	"github.com/decred/dcrlnd/lnrpc"
 	"github.com/decred/dcrlnd/lnrpc/initchainsyncrpc"
@@ -701,7 +702,7 @@ func Main(cfg *Config, lisCfg ListenerCfg, interceptor signal.Interceptor) error
 		HeightHintCacheQueryDisable: cfg.HeightHintCacheQueryDisable,
 		DcrdMode:                    cfg.DcrdMode,
 		DcrwMode:                    cfg.Dcrwallet,
-		HeightHintDB:                dbs.graphDB,
+		HeightHintDB:                dbs.heightHintDB,
 		ChanStateDB:                 dbs.chanStateDB,
 		PrivateWalletPw:             privateWalletPw,
 		PublicWalletPw:              publicWalletPw,
@@ -1611,8 +1612,9 @@ func waitForWalletPassword(cfg *Config,
 // databaseInstances is a struct that holds all instances to the actual
 // databases that are used in lnd.
 type databaseInstances struct {
-	graphDB     *channeldb.DB
-	chanStateDB *channeldb.DB
+	graphDB      *channeldb.DB
+	chanStateDB  *channeldb.DB
+	heightHintDB kvdb.Backend
 }
 
 // initializeDatabases extracts the current databases that we'll use for normal
@@ -1642,7 +1644,9 @@ func initializeDatabases(ctx context.Context,
 	// If the remoteDB is nil, then we'll just open a local DB as normal,
 	// having the remote and local pointer be the exact same instance.
 	var (
-		dbs        = &databaseInstances{}
+		dbs = &databaseInstances{
+			heightHintDB: databaseBackends.HeightHintDB,
+		}
 		closeFuncs []func()
 	)
 	if databaseBackends.ChanStateDB == nil {