lightningnetwork/lnd

lnrpc: refactor populating lnrpc.Transaction inside SubscribeTransaction

lnrpc/rpc_utils.go
@@ -16,6 +16,33 @@ const (
 	RegisterRPCMiddlewareURI = "/lnrpc.Lightning/RegisterRPCMiddleware"
 )
 
+// RPCTransaction returns a rpc transaction.
+func RPCTransaction(tx *lnwallet.TransactionDetail) *Transaction {
+	var destAddresses []string
+	for _, destAddress := range tx.DestAddresses {
+		destAddresses = append(destAddresses, destAddress.EncodeAddress())
+	}
+
+	// We also get unconfirmed transactions, so BlockHash can be nil.
+	blockHash := ""
+	if tx.BlockHash != nil {
+		blockHash = tx.BlockHash.String()
+	}
+
+	return &Transaction{
+		TxHash:           tx.Hash.String(),
+		Amount:           int64(tx.Value),
+		NumConfirmations: tx.NumConfirmations,
+		BlockHash:        blockHash,
+		BlockHeight:      tx.BlockHeight,
+		TimeStamp:        tx.Timestamp,
+		TotalFees:        tx.TotalFees,
+		DestAddresses:    destAddresses,
+		RawTxHex:         hex.EncodeToString(tx.RawTx),
+		Label:            tx.Label,
+	}
+}
+
 // RPCTransactionDetails returns a set of rpc transaction details.
 func RPCTransactionDetails(txns []*lnwallet.TransactionDetail) *TransactionDetails {
 	txDetails := &TransactionDetails{
@@ -23,30 +50,7 @@ func RPCTransactionDetails(txns []*lnwallet.TransactionDetail) *TransactionDetai
 	}
 
 	for i, tx := range txns {
-		var destAddresses []string
-		for _, destAddress := range tx.DestAddresses {
-			destAddresses = append(destAddresses, destAddress.EncodeAddress())
-		}
-
-		// We also get unconfirmed transactions, so BlockHash can be
-		// nil.
-		blockHash := ""
-		if tx.BlockHash != nil {
-			blockHash = tx.BlockHash.String()
-		}
-
-		txDetails.Transactions[i] = &Transaction{
-			TxHash:           tx.Hash.String(),
-			Amount:           int64(tx.Value),
-			NumConfirmations: tx.NumConfirmations,
-			BlockHash:        blockHash,
-			BlockHeight:      tx.BlockHeight,
-			TimeStamp:        tx.Timestamp,
-			TotalFees:        tx.TotalFees,
-			DestAddresses:    destAddresses,
-			RawTxHex:         hex.EncodeToString(tx.RawTx),
-			Label:            tx.Label,
-		}
+		txDetails.Transactions[i] = RPCTransaction(tx)
 	}
 
 	// Sort transactions by number of confirmations rather than height so

matheusd/dcrlnd

lnrpc/rpc_utils.go
@@ -17,6 +17,33 @@ const (
 	RegisterRPCMiddlewareURI = "/lnrpc.Lightning/RegisterRPCMiddleware"
 )
 
+// RPCTransaction returns a rpc transaction.
+func RPCTransaction(tx *lnwallet.TransactionDetail) *Transaction {
+	var destAddresses []string
+	for _, destAddress := range tx.DestAddresses {
+		destAddresses = append(destAddresses, destAddress.String())
+	}
+
+	// We also get unconfirmed transactions, so BlockHash can be nil.
+	blockHash := ""
+	if tx.BlockHash != nil {
+		blockHash = tx.BlockHash.String()
+	}
+
+	return &Transaction{
+		TxHash:           tx.Hash.String(),
+		Amount:           int64(tx.Value),
+		NumConfirmations: tx.NumConfirmations,
+		BlockHash:        blockHash,
+		BlockHeight:      tx.BlockHeight,
+		TimeStamp:        tx.Timestamp,
+		TotalFees:        tx.TotalFees,
+		DestAddresses:    destAddresses,
+		RawTxHex:         hex.EncodeToString(tx.RawTx),
+		Label:            tx.Label,
+	}
+}
+
 // RPCTransactionDetails returns a set of rpc transaction details.
 func RPCTransactionDetails(txns []*lnwallet.TransactionDetail) *TransactionDetails {
 	txDetails := &TransactionDetails{
@@ -24,30 +51,7 @@ func RPCTransactionDetails(txns []*lnwallet.TransactionDetail) *TransactionDetai
 	}
 
 	for i, tx := range txns {
-		var destAddresses []string
-		for _, destAddress := range tx.DestAddresses {
-			destAddresses = append(destAddresses, destAddress.String())
-		}
-
-		// We also get unconfirmed transactions, so BlockHash can be
-		// nil.
-		blockHash := ""
-		if tx.BlockHash != nil {
-			blockHash = tx.BlockHash.String()
-		}
-
-		txDetails.Transactions[i] = &Transaction{
-			TxHash:           tx.Hash.String(),
-			Amount:           int64(tx.Value),
-			NumConfirmations: tx.NumConfirmations,
-			BlockHash:        blockHash,
-			BlockHeight:      tx.BlockHeight,
-			TimeStamp:        tx.Timestamp,
-			TotalFees:        tx.TotalFees,
-			DestAddresses:    destAddresses,
-			RawTxHex:         hex.EncodeToString(tx.RawTx),
-			Label:            tx.Label,
-		}
+		txDetails.Transactions[i] = RPCTransaction(tx)
 	}
 
 	// Sort transactions by number of confirmations rather than height so