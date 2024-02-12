lightningnetwork/lnd

multi: use key locator only for signing messages
To simplify the API surface of a remote signer even more, we refactor
the SignMessage and SignMessageCompact calls to only accept a key
locator as we always know what key we're using for signing anyway.

keychain/signer.go
@@ -5,34 +5,36 @@ import (
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 )
 
-func NewPubKeyMessageSigner(keyDesc KeyDescriptor,
+func NewPubKeyMessageSigner(pubKey *btcec.PublicKey, keyLoc KeyLocator,
 	signer MessageSignerRing) *PubKeyMessageSigner {
 
 	return &PubKeyMessageSigner{
-		keyDesc:      keyDesc,
+		pubKey:       pubKey,
+		keyLoc:       keyLoc,
 		digestSigner: signer,
 	}
 }
 
 type PubKeyMessageSigner struct {
-	keyDesc      KeyDescriptor
+	pubKey       *btcec.PublicKey
+	keyLoc       KeyLocator
 	digestSigner MessageSignerRing
 }
 
 func (p *PubKeyMessageSigner) PubKey() *btcec.PublicKey {
-	return p.keyDesc.PubKey
+	return p.pubKey
 }
 
 func (p *PubKeyMessageSigner) SignMessage(message []byte,
 	doubleHash bool) (*btcec.Signature, error) {
 
-	return p.digestSigner.SignMessage(p.keyDesc, message, doubleHash)
+	return p.digestSigner.SignMessage(p.keyLoc, message, doubleHash)
 }
 
 func (p *PubKeyMessageSigner) SignMessageCompact(msg []byte,
 	doubleHash bool) ([]byte, error) {
 
-	return p.digestSigner.SignMessageCompact(p.keyDesc, msg, doubleHash)
+	return p.digestSigner.SignMessageCompact(p.keyLoc, msg, doubleHash)
 }
 
 type PrivKeyMessageSigner struct {

lntest/mock/secretkeyring.go
@@ -12,8 +12,8 @@ type SecretKeyRing struct {
 }
 
 // DeriveNextKey currently returns dummy values.
-func (s *SecretKeyRing) DeriveNextKey(keyFam keychain.KeyFamily) (
-	keychain.KeyDescriptor, error) {
+func (s *SecretKeyRing) DeriveNextKey(
+	_ keychain.KeyFamily) (keychain.KeyDescriptor, error) {
 
 	return keychain.KeyDescriptor{
 		PubKey: s.RootKey.PubKey(),
@@ -21,28 +21,30 @@ func (s *SecretKeyRing) DeriveNextKey(keyFam keychain.KeyFamily) (
 }
 
 // DeriveKey currently returns dummy values.
-func (s *SecretKeyRing) DeriveKey(keyLoc keychain.KeyLocator) (keychain.KeyDescriptor,
-	error) {
+func (s *SecretKeyRing) DeriveKey(
+	_ keychain.KeyLocator) (keychain.KeyDescriptor, error) {
+
 	return keychain.KeyDescriptor{
 		PubKey: s.RootKey.PubKey(),
 	}, nil
 }
 
 // DerivePrivKey currently returns dummy values.
-func (s *SecretKeyRing) DerivePrivKey(keyDesc keychain.KeyDescriptor) (*btcec.PrivateKey,
-	error) {
+func (s *SecretKeyRing) DerivePrivKey(
+	_ keychain.KeyDescriptor) (*btcec.PrivateKey, error) {
+
 	return s.RootKey, nil
 }
 
 // ECDH currently returns dummy values.
-func (s *SecretKeyRing) ECDH(_ keychain.KeyDescriptor, pubKey *btcec.PublicKey) ([32]byte,
-	error) {
+func (s *SecretKeyRing) ECDH(_ keychain.KeyDescriptor,
+	_ *btcec.PublicKey) ([32]byte, error) {
 
 	return [32]byte{}, nil
 }
 
 // SignMessage signs the passed message and ignores the KeyDescriptor.
-func (s *SecretKeyRing) SignMessage(_ keychain.KeyDescriptor,
+func (s *SecretKeyRing) SignMessage(_ keychain.KeyLocator,
 	msg []byte, doubleHash bool) (*btcec.Signature, error) {
 
 	var digest []byte
@@ -55,7 +57,7 @@ func (s *SecretKeyRing) SignMessage(_ keychain.KeyDescriptor,
 }
 
 // SignMessageCompact signs the passed message.
-func (s *SecretKeyRing) SignMessageCompact(_ keychain.KeyDescriptor,
+func (s *SecretKeyRing) SignMessageCompact(_ keychain.KeyLocator,
 	msg []byte, doubleHash bool) ([]byte, error) {
 
 	var digest []byte

matheusd/dcrlnd

keychain/signer.go
@@ -6,34 +6,36 @@ import (
 	"github.com/decred/dcrd/dcrec/secp256k1/v4/ecdsa"
 )
 
-func NewPubKeyMessageSigner(keyDesc KeyDescriptor,
+func NewPubKeyMessageSigner(pubKey *secp256k1.PublicKey, keyLoc KeyLocator,
 	signer MessageSignerRing) *PubKeyMessageSigner {
 
 	return &PubKeyMessageSigner{
-		keyDesc:      keyDesc,
+		pubKey:       pubKey,
+		keyLoc:       keyLoc,
 		digestSigner: signer,
 	}
 }
 
 type PubKeyMessageSigner struct {
-	keyDesc      KeyDescriptor
+	pubKey       *secp256k1.PublicKey
+	keyLoc       KeyLocator
 	digestSigner MessageSignerRing
 }
 
 func (p *PubKeyMessageSigner) PubKey() *secp256k1.PublicKey {
-	return p.keyDesc.PubKey
+	return p.pubKey
 }
 
 func (p *PubKeyMessageSigner) SignMessage(message []byte,
 	doubleHash bool) (*ecdsa.Signature, error) {
 
-	return p.digestSigner.SignMessage(p.keyDesc, message, doubleHash)
+	return p.digestSigner.SignMessage(p.keyLoc, message, doubleHash)
 }
 
 func (p *PubKeyMessageSigner) SignMessageCompact(msg []byte,
 	doubleHash bool) ([]byte, error) {
 
-	return p.digestSigner.SignMessageCompact(p.keyDesc, msg, doubleHash)
+	return p.digestSigner.SignMessageCompact(p.keyLoc, msg, doubleHash)
 }
 
 type PrivKeyMessageSigner struct {

lntest/mock/secretkeyring.go
@@ -14,8 +14,8 @@ type SecretKeyRing struct {
 }
 
 // DeriveNextKey currently returns dummy values.
-func (s *SecretKeyRing) DeriveNextKey(keyFam keychain.KeyFamily) (
-	keychain.KeyDescriptor, error) {
+func (s *SecretKeyRing) DeriveNextKey(
+	_ keychain.KeyFamily) (keychain.KeyDescriptor, error) {
 
 	return keychain.KeyDescriptor{
 		PubKey: s.RootKey.PubKey(),
@@ -23,28 +23,30 @@ func (s *SecretKeyRing) DeriveNextKey(keyFam keychain.KeyFamily) (
 }
 
 // DeriveKey currently returns dummy values.
-func (s *SecretKeyRing) DeriveKey(keyLoc keychain.KeyLocator) (keychain.KeyDescriptor,
-	error) {
+func (s *SecretKeyRing) DeriveKey(
+	_ keychain.KeyLocator) (keychain.KeyDescriptor, error) {
+
 	return keychain.KeyDescriptor{
 		PubKey: s.RootKey.PubKey(),
 	}, nil
 }
 
 // DerivePrivKey currently returns dummy values.
-func (s *SecretKeyRing) DerivePrivKey(keyDesc keychain.KeyDescriptor) (*secp256k1.PrivateKey,
-	error) {
+func (s *SecretKeyRing) DerivePrivKey(
+	_ keychain.KeyDescriptor) (*secp256k1.PrivateKey, error) {
+
 	return s.RootKey, nil
 }
 
 // ECDH currently returns dummy values.
-func (s *SecretKeyRing) ECDH(_ keychain.KeyDescriptor, pubKey *secp256k1.PublicKey) ([32]byte,
-	error) {
+func (s *SecretKeyRing) ECDH(_ keychain.KeyDescriptor,
+	_ *secp256k1.PublicKey) ([32]byte, error) {
 
 	return [32]byte{}, nil
 }
 
 // SignMessage signs the passed message and ignores the KeyDescriptor.
-func (s *SecretKeyRing) SignMessage(_ keychain.KeyDescriptor,
+func (s *SecretKeyRing) SignMessage(_ keychain.KeyLocator,
 	msg []byte, doubleHash bool) (*ecdsa.Signature, error) {
 
 	var digest []byte
@@ -58,7 +60,7 @@ func (s *SecretKeyRing) SignMessage(_ keychain.KeyDescriptor,
 }
 
 // SignMessageCompact signs the passed message.
-func (s *SecretKeyRing) SignMessageCompact(_ keychain.KeyDescriptor,
+func (s *SecretKeyRing) SignMessageCompact(_ keychain.KeyLocator,
 	msg []byte, doubleHash bool) ([]byte, error) {
 
 	var digest []byte